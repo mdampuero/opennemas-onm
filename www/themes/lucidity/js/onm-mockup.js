1b957a07b86de34cbb3e1cfcf4567c325452e3c0
Sandra Pereira <sandra@openhost.es>
2010-03-04 15:36:26 +0000

    new template lucidity
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@19 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/themes/lucidity/js/onm-mockup.js
+++ www/themes/lucidity/js/onm-mockup.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+/* jQuery(document).ready(function(){
 
     jQuery.fn.fadeToggle = function(speed, easing, callback) {
        return this.animate({opacity: 'toggle'}, speed, easing, callback);
@@ -9,4 +9,13 @@ jQuery(document).ready(function(){
         });
     },4000);
 
+});
+*/
+//need jquery.cycle.all.2.72.js
+jQuery(document).ready(function(){
+     $('.slide_cicle').cycle({
+        fx:     'fade',
+        speed:    300,
+        delay:  -4000
+    });
 });
