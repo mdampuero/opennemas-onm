9a69044f61ca72d58c21b98c475f3707c5dfb4a6
David Martinez <macada@openhost.es>
2010-03-08 12:00:15 +0000

    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@48 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/themes/lucidity/compile/%%7E^7EC^7ECDB98D%%widget_ad_button.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2010-03-02 16:42:45
-         compiled from widget_ad_button.tpl */ ?>
-
-<div class="publi_310 clearfix">
-    <a href="http://www.puntogal.org"><img src="<?php echo @MEDIA_PATH_URL; ?>
-/publi/eu.gal.png" alt="" /></a>
-</div>
