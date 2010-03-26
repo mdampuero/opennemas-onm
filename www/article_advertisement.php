0ea428ad2b1a4cfd2efd24811a7cf1161aa344ca
Sandra Pereira <sandra@openhost.es>
2010-03-26 22:25:19 +0000

    publicidade
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@239 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/article_advertisement.php
+++ www/article_advertisement.php
@@ -1,4 +1,6 @@
 <?php
+//Provisional misma publicidad top y buttom en todo
+
 defined('SITE_URL') or die('Direct access is forbidden');
 
 $ccm = ContentCategoryManager::get_instance();
@@ -7,7 +9,7 @@ $category = $ccm->get_id($category_name);
 $category = (!isset($category) || ($category=='home'))? 0: $category;
 $advertisement = Advertisement::getInstance();
 
-$banners = $advertisement->getAdvertisements(array(101, 102, 103, 104, 105, 106, 107), $category);
+$banners = $advertisement->getAdvertisements(array(1,2,101, 102, 103, 104, 105, 106, 107, 9,10), $category);
 
 //$advertisement->render($banners, &$tpl);
 $advertisement->render($banners, &$advertisement);
