bb5e67ecce6a526a5df2d3df965e3e61b1fa1ce2
Sandra Pereira <sandra@openhost.es>
2010-03-26 09:39:22 +0000

    mejoras
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@185 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/module_other_headlines.php
+++ www/module_other_headlines.php
@@ -6,7 +6,7 @@
 
 $titulares = $cm->find_headlines();
 
-$i=1;
+$i=0;
 foreach($categories as $this_category) {
     $category_titulares = array();
     foreach ($titulares as $titul) {
@@ -19,7 +19,7 @@ foreach($categories as $this_category) {
     $titulares_cat[$i] =array_slice($category_titulares, 0, 5, true);
     $i++;
 }
-
+ var_dump($titulares_cat);
 $tpl->assign('categories_data',$categories);
 $tpl->assign('titulares_cat',$titulares_cat);
 
