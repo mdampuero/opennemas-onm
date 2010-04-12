7936c2f38947fca3dfabc20404ca056f658b8057
David Martinez <macada@openhost.es>
2010-04-12 00:15:28 +0000

    new template - arquitects
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@309 6b240e21-b319-415b-acc2-1cc10fe194ed

---
+++ www/themes/arquitects/static/meta.php
@@ -0,0 +1,4 @@
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>También arquitectura - su portal de noticias sobre arquitectura, interiorismo y decración.</title>
+	<meta name="description" content="También arquitectura - su portal de noticias sobre arquitectura, interiorismo y decración." />
+   
