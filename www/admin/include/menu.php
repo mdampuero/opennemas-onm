502e26d9a0487415332550ec49f46c081f445786
Sandra Pereira <sandra@openhost.es>
2010-04-23 10:42:09 +0000

    BACKEND: Cambiar menú
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@359 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/admin/include/menu.php
+++ www/admin/include/menu.php
@@ -3,55 +3,56 @@
 $menuXml = <<<MENUSTRING
 <?xml version="1.0"?>
 <menu>
-    <submenu title="Contenidos" link="index.php" target="centro">
+    <submenu title="Frontpage" link="index.php" target="centro">
         <node title="Inicio" link="index.php" target="centro" />
-        <node title="Gestor de Portada" link="article.php" target="centro" privilege="ARTICLE_FRONTPAGE" />
-        <node title="Gestor de Pendientes" link="article.php?action=list_pendientes" target="centro" privilege="ARTICLE_ADMINPEND" />
-        <node title="Gestor de Comentarios" link="comment.php" target="centro" privilege="COMMENT_ADMIN" />
-       <!--  <node title="Gestor de Opinión" link="opinion.php" target="centro" privilege="OPINION_ADMIN" /> -->
-        <node title="Gestor de Publicidad" link="advertisement.php" target="centro" privilege="ADVERTISEMENT_ADMIN" />
-        <node title="Hemeroteca" link="article.php?action=list_hemeroteca" target="centro" privilege="ARCHIVE_ADMIN" />
-        <node title="Páginas Estáticas" link="static_pages.php" target="centro" privilege="STATIC_ADMIN" />
-        <node title="Gestor Widgets" link="widget.php" target="centro" privilege="WIDGET_ADMIN" />
-        <node title="Portadas papel" link="kiosko.php" target="centro" privilege="FILE_ADMIN" />
+        <node title="Frontpage Manager" link="article.php" target="centro" privilege="ARTICLE_FRONTPAGE" />
+        <node title="Widget Manager" link="widget.php" target="centro" privilege="WIDGET_ADMIN" />
+        <node title="Templates Manager" link="index.php" target="centro" privilege="ARTICLE_FRONTPAGE" />
+
+    </submenu>
+    <submenu title="Contents" link="index.php" target="centro">
+        <node title="Articles" link="article.php?action=list_pendientes" target="centro" privilege="ARTICLE_ADMINPEND" />
+        <node title="Opinions" link="opinion.php" target="centro" privilege="OPINION_ADMIN" />
+        <node title="Comments" link="comment.php" target="centro" privilege="COMMENT_ADMIN" />
+        <node title="Polls" link="poll.php" target="centro" privilege="POLL_ADMIN" />
+        <node title="Advertisements" link="advertisement.php" target="centro" privilege="ADVERTISEMENT_ADMIN" />        
+        <node title="Static Pages" link="static_pages.php" target="centro" privilege="STATIC_ADMIN" />
+        <node title="Library" link="article.php?action=list_hemeroteca" target="centro" privilege="ARCHIVE_ADMIN" />
+        <node title="Sections Manager" link="category.php" target="centro" privilege="WIDGET_ADMIN" />        
     </submenu>
 
-    <submenu title="Multimedia" link="#" target="centro" privilege="IMAGE_ADMIN,VIDEO_ADMIN,ALBUM_ADMIN,FILE_ADMIN,GRAPHICS_ADMIN">
-        <node title="Imágenes" link="mediamanager.php" target="centro" privilege="IMAGE_ADMIN" />
-        <node title="Vídeos" link="video.php" target="centro" privilege="VIDEO_ADMIN" />
-        <node title="Álbum" link="album.php" target="centro" privilege="ALBUM_ADMIN" />
-        <node title="Ficheros" link="ficheros.php" target="centro" privilege="FILE_ADMIN" />
-        <node title="Gráficos" link="mediagraficos.php" target="centro" privilege="GRAPHICS_ADMIN" />
+    <submenu title="Media" link="#" target="centro" privilege="IMAGE_ADMIN,VIDEO_ADMIN,ALBUM_ADMIN,FILE_ADMIN,GRAPHICS_ADMIN">
+        <node title="Images" link="mediamanager.php" target="centro" privilege="IMAGE_ADMIN" />
+        <node title="Files" link="ficheros.php" target="centro" privilege="FILE_ADMIN" />
+        <node title="Videos" link="video.php" target="centro" privilege="VIDEO_ADMIN" />
+        <node title="Albums" link="album.php" target="centro" privilege="ALBUM_ADMIN" />
     </submenu>
     
-    <submenu title="Secciones" link="category.php" target="centro" privilege="CATEGORY_ADMIN"></submenu>
-    
-    <submenu title="Usuarios" link="#" target="centro" privilege="USER_ADMIN">
-        <node title="Usuarios" link="user.php" target="centro" />
-        <node title="Grupos de Usuarios" link="user_groups.php" target="centro" />
-        <node title="Permisos" link="privileges.php" target="centro" />
-        <node title="Autores" link="author.php" target="centro" />
+
+    <submenu title="Users" link="#" target="centro" privilege="USER_ADMIN">
+        <node title="Users" link="user.php" target="centro" />
+        <node title="Group Users" link="user_groups.php" target="centro" />
+        <node title="Permissions" link="privileges.php" target="centro" />
+    </submenu>
+
+    <submenu title="Utilities" link="#" target="centro" privilege="BACKEND_ADMIN,CACHE_ADMIN,SEARCH_ADMIN,TRASH_ADMIN,PCLAVE_ADMIN">
+        <node title="Avanced Search" link="search_advanced.php" target="centro" privilege="SEARCH_ADMIN" />        
+        <node title="Trash" link="litter.php" target="centro" privilege="TRASH_ADMIN" />
+        <node title="Newsstand" link="kiosko.php" target="centro" privilege="CATEGORY_ADMIN" />
+        <node title="Newsletter" link="newsletter.php" target="centro" privilege="NEWSLETTER_ADMIN" />
+        <node title="Key words" link="pclave.php" target="centro" privilege="PCLAVE_ADMIN" />
+        <node title="Control link" link="link_control.php" target="centro" privilege="BACKEND_ADMIN" />
+        <node title="Dashboard" link="dashboard.php" target="centro" privilege="BACKEND_ADMIN" />
     </submenu>
     
-    <submenu title="Boletín" link="#" target="centro" privilege="NEWSLETTER_ADMIN">
-        <node title="Envío de boletín" link="newsletter.php" target="centro" />        
-    </submenu>
-    
-    <submenu title="Utiles" link="#" target="centro" privilege="BACKEND_ADMIN,CACHE_ADMIN,SEARCH_ADMIN,TRASH_ADMIN,PCLAVE_ADMIN">
-        <node link="search_advanced.php" target="centro" title="Busqueda Avanzada" privilege="SEARCH_ADMIN" />
-        <node link="pclave.php" target="centro" title="Palabras clave" privilege="PCLAVE_ADMIN" />
-        <node link="link_control.php" target="centro" title="Control link" privilege="BACKEND_ADMIN" />
-        <node link="litter.php" target="centro" title="Papelera" privilege="TRASH_ADMIN" />
-        <node link="http://piwik.xornal.com" target="centro" title="Webstats" privilege="BACKEND_ADMIN" />
-        <node link="dashboard.php" target="centro" title="Dashboard" privilege="BACKEND_ADMIN" />
-    </submenu>
-    
-    <submenu title="Configuración" link="#" target="centro" privilege="BACKEND_ADMIN,CACHE_ADMIN">
-        <node link="configurator.php" target="centro" title="Config Manager" privilege="BACKEND_ADMIN" />
-        <node link="tpl_manager.php" target="centro" title="Cache Manager" privilege="CACHE_ADMIN" />
-        <node link="apc.php" target="centro" title="APC" privilege="CACHE_ADMIN" />
-        <node link="svn.php" target="centro" title="SVN" privilege="BACKEND_ADMIN" />
-        <node link="mysql-check.php?action=check" target="centro" title="Mysql-Check" privilege="BACKEND_ADMIN" />
+    <submenu title="Configuration" link="#" target="centro" privilege="BACKEND_ADMIN,CACHE_ADMIN">
+        <node title="Settings Manager" link="configurator.php" target="centro"  privilege="BACKEND_ADMIN" />
+        <node title="Cache Manager" link="tpl_manager.php" target="centro" privilege="CACHE_ADMIN" />
+        <node title="Information" link="index.php" target="centro" privilege="BACKEND_ADMIN" />
+        <node title="Help" link="index.php" target="centro" privilege="BACKEND_ADMIN" />
+        <node title="APC" link="index.php" target="centro" privilege="CACHE_ADMIN" />
+        <node title="SVN" link="svn.php" target="centro"  privilege="BACKEND_ADMIN" />
+        <node title="Mysql-Check" link="mysql-check.php?action=check" target="centro" privilege="BACKEND_ADMIN" />
     </submenu>    
 </menu>
 MENUSTRING;
