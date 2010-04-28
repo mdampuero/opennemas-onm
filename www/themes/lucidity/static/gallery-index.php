7c4729226cbe63a372446a7e7b853de0a3db1f30
Fran Dieguez <fran@openhost.es>
2010-04-28 10:32:27 +0000

    Quick version of gallery index page, I have to redising it from scrath
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@399 6b240e21-b319-415b-acc2-1cc10fe194ed

---
+++ www/themes/lucidity/static/gallery-index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+  <head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Xabi Alonso: "Mi relación con CR9 es fantástica" - Deportes - Crónica comarcal</title>
+    <link rel="stylesheet" href="stylesheets/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="stylesheets/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]><link rel="stylesheet" href="stylesheets/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+    <link rel="stylesheet" href="stylesheets/onm-mockup.css" type="text/css" media="screen,projection">
+    <link rel="stylesheet" href="stylesheets/parts/article.css" type="text/css" media="screen,projection">
+    <link rel="stylesheet" href="stylesheets/parts/comments.css" type="text/css" media="screen,projection">
+    <link rel="stylesheet" href="stylesheets/gallery.css" type="text/css" media="screen,projection">
+    
+    <link rel="stylesheet" href="stylesheets/jquery-ui-custom/jquery-ui.css" type="text/css" media="screen,projection">
+
+	<style type="text/css">
+		#main_menu, div.toolbar-bottom a, div.utilities a, .transparent-logo {
+			background-color:#ffbc21;
+		}
+	</style>
+  </head>
+
+  <body>
+
+    <div class="wrapper clearfix ">
+        <div id="publis" class="span-24">
+            <div id="publi_1" class="publi span-10">
+                Lugar para PUBLI
+            </div>
+            <div id="publi_2" class="publi public-corta span-14 last">
+                Lugar para PUBLI
+            </div>
+
+        </div>
+    </div>
+    <div class="wrapper video clearfix">
+        <div class="container clearfix span-24">
+
+            <div id="header" class="">
+                
+                <div id="info-top" class="clearfix span-24">
+
+                    <div id="current-day">
+
+                        19:35 del Jueves, 18 de Febrero de 2010
+                    </div>
+
+                    <div id="widget_weather_top">
+                        <ul>
+                            <li><a href="#">El tiempo</a> | <a href="#">Santiago de Compostela</a></li>
+                            <li><img src="images/weather-mini.png" alt="Vai chover" /></li>
+                            <li><div class="widget-litte-temperatures max_temperature">14º C</div></li>
+
+                            <li><div class="widget-litte-temperatures min_temperature">7º C</div></li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div id="logo" class="clearfix">
+
+                    <div id="logo-image" class="span-8">
+
+                        <a href="#" title="Pulse aquí para ir a la portada de Crónica comarcal">
+                        	<img class="transparent-logo" src="images/main-logo.small.png" alt="Crónica comarcal" />
+
+                        	<img src="images/logo-sections/video.png" alt="Deportes" />  
+                        </a>
+                    </div>
+
+                    <div class="span-16">
+                        
+                    </div>
+
+                </div>
+
+                <div id="menus" class="">
+
+                    <div id="main_menu" class="span-24 clearfix">
+                        <div>
+                            <ul class="clearfix">
+                                <li>Portada</li>
+                                <li>Cultura</li>
+                                <li>Galicia</li>
+
+                                <li>España</li>
+
+                                <li>Mundo</li>
+                                <li>Economía</li>
+                                <li>Sociedad</li>
+                                <li class="active">Deportes</li>
+                            </ul>
+
+                        </div>
+                        
+                    </div>
+
+                    <div id="sub_main_menu" class="span-24">
+                        <ul class="clearfix">
+                            <li>Portada</li>
+                            <li>Cultura</li>
+                            <li>Galicia</li>
+
+                            <li>España</li>
+
+                            <li>Mundo</li>
+                            <li>Economía</li>
+                            <li>Sociedad</li>
+                            <li>Deportes</li>
+                        </ul>
+
+                        <div id="main-search-form">
+                            <form action="#">
+
+                                <input type="text" name="firstname" value="Buscar en la página..."
+                                       onblur="if(this.value=='') this.value='Buscar en la página...';"
+                                       onfocus="if(this.value=='Buscar en la página...') this.value='';" />
+                                <input type="submit" name="lastname" value="Buscar" />
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id="main_content" class="single-article span-24 portada-videos">
+
+                <div class="layout-column first-column span-8 featured-videos">
+                        <h3>Galerias destacadas</h3>
+                        <hr class="new-separator"/>
+                        <div>
+                            <div id="gallery-content" class="clearfix span-8">
+
+                                <img src="/themes/lucidity/static/images/gallery/images/1.jpg" class="image0">
+                            </div>
+
+                            <div class="video-explanation">
+                                <h1><img src="images/utilities/share-black.png" alt="Share" /> Al acecho en el bosque</h1>
+                                <p class="in-subtitle">
+                                Este jueves le han preguntado a Xabi Alonso al respecto y el ex jugador
+                                del Liverpool ha sido tajante: "Cada uno puede ...</p>
+                            </div>
+
+
+                        </div><!-- .main-video -->
+
+                        <div>
+                            <div id="gallery-content" class="clearfix span-8">
+                                <img src="/themes/lucidity/static/images/gallery/images/2.jpg" class="image0">
+                            </div>
+                            <div class="video-explanation">
+                                <h1><img src="images/utilities/share-black.png" alt="Share" /> Vive el Anayet</h1>
+
+                                <p class="in-subtitle">
+
+                                Este jueves le han preguntado a Xabi Alonso al respecto y el ex jugador
+                                del Liverpool ha sido tajante: "Cada uno puede ...</p>
+                            </div>
+
+
+                        </div><!-- .main-video -->
+	                    
+	                    
+                    </div>
+                    <div class="layout-column last-column last span-16 ">
+                        <div class="span-16">
+
+                            <div class="other-interested-gallerys border-dotted">
+
+                                <h3>Otras galerias en esta categoría</h3>
+                                <hr class="new-separator"/>
+                                <div class="clean-paginator">1 de 2 | <a href="#" title="Ir al siguiente">Siguiente »</a></div>
+
+                                <div class="interested-gallery opacity-reduced">
+                                    <div class="capture">
+
+                                        <img src="images/video/video-3.jpg" alt=""/>
+                                        <div class="bar-video-tiny-info"></div>
+
+                                        <div class="bar-video-tiny-info-image-video"><img src="images/video/trailersPlayArrow.gif" /></div>
+                                    </div>
+                                    <div class="info-interested-gallery">
+                                        <div class="category">Sociedad</div>
+                                        <div class="caption">Amor de madre o de esposa, el dilema</div>
+
+                                    </div>
+                                </div>
+
+                                <div class="interested-gallery">
+                                    <div class="capture">
+                                        <img src="images/video/video-2.jpg" alt="" />
+                                        <div class="bar-video-tiny-info"></div>
+                                        <div class="bar-video-tiny-info-image-video"><img alt="" src="images/video/trailersPlayArrow.gif" /></div>
+                                    </div>
+
+                                    <div class="info-interested-gallery  opacity-reduced">
+                                        <div class="category">Cine</div>
+
+                                        <div class="caption">Debatimos el flequillo de los autores</div>
+                                    </div>
+                                </div>
+
+                                <div class="interested-gallery">
+                                    <div class="capture">
+
+                                        <img alt="" src="images/video/video-1.jpg" />
+                                        <div class="bar-video-tiny-info"></div>
+                                        <div class="bar-video-tiny-info-image-video"><img alt="" src="images/video/trailersPlayArrow.gif" /></div>
+
+                                    </div>
+                                    <div class="info-interested-gallery  opacity-reduced">
+                                        <div class="category">Internacional</div>
+                                        <div class="caption">Los panameños atacan la ultima entifada de Chaves</div>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            
+                        </div>
+                        <div class="span-16">
+                            <div class="other-interested-gallerys border-dotted">
+                                <h3>Otras galerias interesantes</h3>
+
+                                <hr class="new-separator"/>
+                                <div class="clean-paginator">1 de 9 | <a href="#" title="Ir al siguiente">Siguiente »</a></div>
+
+                                <div class="interested-gallery">
+
+                                    <div class="capture">
+                                        <img alt="" src="images/video/video-4.jpg" />
+                                        <div class="bar-video-tiny-info"></div>
+                                        <div class="bar-video-tiny-info-image-video"><img src="images/video/trailersPlayArrow.gif" /></div>
+
+                                    </div>
+                                    <div class="info-interested-gallery opacity-reduced">
+                                        <div class="category">Humor</div>
+                                        <div class="caption">Grandes caidas del deporte</div>
+
+                                    </div>
+                                </div>
+
+                                <div class="interested-gallery">
+
+                                    <div class="capture">
+                                        <img alt="" src="images/video/video-5.jpg" />
+                                        <div class="bar-video-tiny-info"></div>
+                                        <div class="bar-video-tiny-info-image-video"><img src="images/video/trailersPlayArrow.gif" /></div>
+                                    </div>
+
+                                    <div class="info-interested-gallery opacity-reduced">
+                                        <div alt="" class="category">Sociedad</div>
+                                        <div class="caption">Tipos de velos del Islam</div>
+
+                                    </div>
+                                </div>
+
+                                <div class="interested-gallery">
+                                    <div class="capture">
+
+                                        <img alt="" src="images/video/video-6.jpg" />
+                                        <div class="bar-video-tiny-info"></div>
+                                        <div class="bar-video-tiny-info-image-video"><img src="images/video/trailersPlayArrow.gif" /></div>
+                                    </div>
+
+                                    <div class="info-interested-gallery opacity-reduced">
+                                        <div class="category">Politica</div>
+                                        <div class="caption">El presidente de polonia se rie de España</div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                </div><!-- fin #main_content -->
+
+            </div><!-- fin .container -->
+    </div><!-- fin .wrapper -->
+
+    <div class="wrapper clearfix">
+
+        <div class="container clearfix span-24">
+
+            <div id="footer" class="">
+
+            </div><!-- fin .footer -->
+
+        </div><!-- fin .container -->
+
+
+    </div>
+
+    
+    <script type="text/javascript" src="javascripts/jquery-1.4.1.min.js"></script>
+    <script type="text/javascript" src="javascripts/jquery-ui.js"></script>
+
+    <script type="text/javascript" src="javascripts/functions.js"></script>
+  </body>
+</html>
