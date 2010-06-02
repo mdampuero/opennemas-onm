216311f464dd7dde266e08d6a50dcc6d4e87704e
Tomás Vilariño <vifito@openhost.es>
2010-06-02 11:27:09 +0000

    frontend controllers
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@590 6b240e21-b319-415b-acc2-1cc10fe194ed

---
+++ www/controllers/IndexController.php
@@ -0,0 +1,50 @@
+<?php
+/* -*- Mode: PHP; tab-width: 4 -*- */
+/**
+ * OpenNeMas project
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   OpenNeMas
+ * @package    OpenNeMas
+ * @copyright  Copyright (c) 2010 Openhost S.L. (http://openhost.es)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+class IndexController extends Onm_Controller_Action
+{
+    
+    public function init()
+    {
+        
+    }
+    
+    
+    /**
+     * Render home page
+     *
+     * Route: index-home
+     *  /
+     */
+    public function homeAction()
+    {
+        // Get "homepage" (root page of tree)
+        $pageMgr = PageManager::getInstance();        
+        $page = $pageMgr->getRoot();        
+        
+        $content = $page->dispatch();        
+        
+        $this->tpl->assign('grid_content', $content);
+        $this->tpl->display('page.tpl');
+    }
+    
+    
+}
---
+++ www/controllers/PageController.php
@@ -0,0 +1,66 @@
+<?php
+/* -*- Mode: PHP; tab-width: 4 -*- */
+/**
+ * OpenNeMas project
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   OpenNeMas
+ * @package    OpenNeMas
+ * @copyright  Copyright (c) 2010 Openhost S.L. (http://openhost.es)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ *
+ */
+class PageController extends Onm_Controller_Action
+{
+    
+    public function init()
+    {
+        
+    }
+    
+    
+    /**
+     * Render page by slug
+     *
+     * Route: page-index
+     *  /
+     */
+    public function indexAction()
+    {
+        $slug = $this->_getParam('slug', 'home');
+        
+        $pageMgr = PageManager::getInstance();
+        $page = $pageMgr->getPageBySlug($slug);                
+        
+        try {
+            $content = $page->dispatch();
+            
+            $this->tpl->assign('grid_content', $content);        
+            $this->tpl->display('page.tpl');
+            
+        } catch(PageNotAvailableException $ex) {
+            
+            if(APPLICATION_ENV == 'production') {
+                $this->redirector->gotoRoute( array(), 'index-home' );
+                exit();
+            } else {
+                Zend_Registry::get('logger')->info($ex->getMessage());
+            }            
+        }
+        
+    }
+    
+    
+}
