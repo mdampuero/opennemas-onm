e0be6a217df3357208650f23dd0c0eeb6e4dcec5
Tomás Vilariño <vifito@openhost.es>
2010-07-16 10:47:54 +0000

    New template system with Onm_View
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@693 6b240e21-b319-415b-acc2-1cc10fe194ed

---
+++ www/controllers/StaticpageController.php
@@ -0,0 +1,43 @@
+<?php
+/* -*- Mode: PHP; tab-width: 4 -*- */
+/**
+ * OpenNeMas project
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @package    Controllers
+ * @copyright  Copyright (c) 2010 Openhost S.L. (http://openhost.es)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+
+class StaticpageController extends Onm_Controller_Action
+{
+    
+    public function init()
+    {
+        
+    }
+    
+    
+    /**
+     * Render staticpage by slug
+     *
+     * Route: staticpage-index
+     *  /
+     */
+    public function indexAction()
+    {        
+        
+    }
+    
+    
+}
