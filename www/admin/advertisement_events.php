b92769c1368260e758dd0c5aac3b6a9d6b7dadd0
David Martinez <macada@openhost.es>
2010-11-04 00:55:23 +0000

    trunk folder is being deleted
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@985 6b240e21-b319-415b-acc2-1cc10fe194ed

--- www/admin/advertisement_events.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (eregi('advertisement_events.php', $_SERVER['PHP_SELF'])) {
-	die();
-}
-
-//$GLOBALS['application']->register('onAfterUpdateAdvertisement',    'onPublish');
-//$GLOBALS['application']->register('onAfterCreateAdvertisement',    'onPublish');
-//$GLOBALS['application']->register('onAfterAvailable',              'onPublish');
--- www/admin/session_bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once( SITE_PATH . 'core/sessionmanager.class.php');
-$GLOBALS['Session'] = SessionManager::getInstance(OPENNEMAS_BACKEND_SESSIONS);
-$GLOBALS['Session']->bootstrap();
-
-if(!isset($_SESSION['userid']) && !preg_match('/bootstrap.php$/', $_SERVER['SCRIPT_FILENAME'])) {
-    header('Location: ' . SITE_URL_ADMIN . 'user/login/');
-    exit(0);
-}
