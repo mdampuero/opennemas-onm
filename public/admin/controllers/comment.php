1e12c3e3d2e765275373e6972c55b6f013e152ee
Sandra Pereira <sandra@openhost.es>
2010-11-26 13:00:20 +0000

    new structure admin controllers
    git-svn-id: http://svn.openhost.es/opennemasdemo/trunk@1045 6b240e21-b319-415b-acc2-1cc10fe194ed

---
+++ public/admin/controllers/comment.php
@@ -0,0 +1,324 @@
+<?php
+/* -*- Mode: PHP; tab-width: 4 -*- */
+/**
+ * OpenNeMas project
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   OpenNeMas
+ * @package    OpenNeMas
+ * @copyright  Copyright (c) 2009 Openhost S.L. (http://openhost.es)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+
+/**
+ * Setup app
+*/
+require_once(dirname(__FILE__).'/../../bootstrap.php');
+require_once(SITE_ADMIN_PATH.'session_bootstrap.php');
+
+ 
+// Check ACL
+require_once(SITE_CORE_PATH.'privileges_check.class.php');
+if(!Acl::_('COMMENT_ADMIN')) {    
+    Acl::deny();
+}
+
+/**
+ * Setup view
+*/
+$tpl = new TemplateAdmin(TEMPLATE_ADMIN);
+
+$tpl->assign('titulo_barra', 'Comment Management');
+
+if(!isset($_REQUEST['category'])) {
+    $_REQUEST['category'] = 'todos';
+}
+$tpl->assign('category', $_REQUEST['category']);
+
+// Initialize $_REQUEST['page']
+$_REQUEST['page'] = (isset($_REQUEST['page']))? $_REQUEST['page']: 0;
+
+if(isset($_REQUEST['action'])) {
+    
+    switch($_REQUEST['action']) {
+        
+        case 'list': {
+            $ccm = ContentCategoryManager::get_instance();
+            list($parentCategories, $subcat, $datos_cat) = $ccm->getArraysMenu();
+            
+            $tpl->assign('subcat', $subcat);
+            $tpl->assign('allcategorys', $parentCategories);
+            $tpl->assign('datos_cat', $datos_cat);
+
+              // Assign a content types who have comments
+            $content_types = array(1 => 'Article' , 7 => 'Album', 9 => 'Video', 4 => 'Opinion', 11 => 'Poll');
+            $tpl->assign('content_types', $content_types);
+            
+            $cm = new ContentManager();
+            if (!isset($_REQUEST['comment_status'])) {
+                $_REQUEST['comment_status'] = 0;
+            }
+            if (!isset($_GET['page']) || empty($_GET['page'])) {$_GET['page'] = 1;}
+
+            $tpl->assign('comment_status', $_REQUEST['comment_status']);
+            $filter="content_status = ".$_REQUEST['comment_status'];
+            
+           if($_REQUEST['category'] == 'encuesta') {
+                $ccm = new ContentManager();
+             //   $comments = $ccm->find('PC_Comment', '  '.$filter.' ', 'ORDER BY content_status, created DESC ');
+             //   $comments = $ccm->paginate($comments);
+                 list($comments, $pager)= $cm->find_pages('Comment',' '.$filter.' ', 'ORDER BY content_status, created DESC ',$_GET['page'],10);
+                    //$comments = $cm->paginate($comments);
+                    $tpl->assign('paginacion', $pager);
+          //      $pager = $ccm->pager;
+                $tpl->assign('paginacion', $pager);
+                $tpl->assign('comments', $comments);
+                
+                $i = 0;
+                $polls = array();
+                $votes = array();
+                foreach( $comments as $prima){
+                    $polls[$i] = new Poll( $prima->fk_content );
+                    $votes[$i] =new Vote( $prima->pk_comment );
+
+                    $i++;
+                }
+                    
+                $tpl->assign('articles', $polls);
+                $tpl->assign('votes', $votes);
+                
+            } else {
+                if($_REQUEST['category'] == 'home') {
+                    $comment = new Comment();
+                    
+                    //Comentarios de las noticias in_home
+                    $comments = $comment->get_home_comments($filter);
+                    if($comments) {
+                        $comments = $cm->paginate_num($comments,10);
+                        $tpl->assign('paginacion', $cm->pager);
+                        $tpl->assign('comments', $comments);
+                    }
+                    
+                } elseif($_REQUEST['category'] == 'todos') {
+                    $comment = new Comment();
+                    // ContentManager::find_pages(<TIPO>, <WHERE>, <ORDER>, <PAGE>, <ITEMS_PER_PAGE>, <CATEGORY>);
+                    list($comments, $pager)= $cm->find_pages('Comment', 'content_status = 0',
+                                                             'ORDER BY  created DESC ',
+                                                             $_REQUEST['page'], 10);
+                    $tpl->assign('paginacion', $pager);
+                    $tpl->assign('comments', $comments);
+                    
+                   
+                } else {
+                    // ContentManager::find_pages(<TIPO>, <WHERE>, <ORDER>, <PAGE>, <ITEMS_PER_PAGE>, <CATEGORY>);
+                    list($comments, $pager) = $cm->find_pages('Comment', ' fk_content_type=6  and '.$filter.' ',
+                                                              'ORDER BY content_status, created DESC ',
+                                                              $_REQUEST['page'], 10, $_REQUEST['category']);
+                    $tpl->assign('paginacion', $pager);
+                    $tpl->assign('comments', $comments);
+                    
+                }
+                
+                $articles = array();
+                $votes = array();
+                $i = 0;  //Sacamos los articulos para el titulo
+                if($comments) {
+                    // sql sobre article y content * IN ($prima->fk_content1, $prima->fk_content2, ...)
+                    $ids = array();
+                   /* foreach($comments as $prima) {
+                        $ids[] = $prima->fk_content;
+                    }
+                    */
+                    foreach($comments as $prima){
+                        $articles[$i] = new Content( $prima->fk_content );
+                        $cat=$articles[$i]->loadCategoryName($prima->fk_content );
+                        if (!$cat) {
+                            $articles[$i]->category_name = 'Opinion';
+                        }
+                        $articles[$i]->category_name = $articles[$i]->loadCategoryName($prima->fk_content);
+                        $votes[$i] =new Vote( $prima->pk_comment );
+                         $i++;
+                    }
+                }
+                $tpl->assign('articles', $articles);
+                $tpl->assign('votes', $votes);
+            }
+        } break;
+        
+        case 'new': {
+            // Nothing
+        } break;
+        
+        case 'read': {
+            //habrá que tener en cuenta el tipo
+            $comment = new Comment( $_REQUEST['id'] );
+            if(is_null($comment->pk_comment)) {                                  
+                $comment = new Comment($_REQUEST['id']);
+                
+                $tpl->assign('comment', $comment);
+                $poll = new Poll( $comment->fk_content );
+                $tpl->assign('article', $poll);
+                
+            } else {
+                $tpl->assign('comment', $comment);
+                
+                $article = new Article( $comment->fk_content );
+                $tpl->assign('article', $article);
+                
+                // ¿?¿?¿?
+                $img1 = $article->img1;
+                if(isset($img1)){
+                    //Buscar foto where pk_foto=img1
+                    $photo1 = new Photo($img1);
+                }
+                $tpl->assign('photo1', $photo1);
+                
+                $img2 = $article->img2;                
+                if(isset($img2)) {
+                    //Buscar foto where pk_foto=img2
+                    $photo2 = new Photo($img2);
+                }                
+                $tpl->assign('photo2', $photo2);
+                
+            }
+        } break;
+        
+        case 'update': {
+            $comment = new Comment($_REQUEST['id']);
+            
+            if(!is_null($comment->pk_comment)) {
+                $comment->update( $_REQUEST );
+                if($_REQUEST['content_status'] == 1) {
+                    $article=new Article($comment->fk_content);
+                    //Para que cambie la fecha changed.
+                    $article->set_status($article->content_status, $article->fk_user_last_editor);
+                }
+            } else {
+                $comment = new PC_Comment();
+                $comment->update( $_REQUEST );
+            }
+            
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+        } break;
+        
+        case 'create': {
+            $comment = new Comment();
+            if($comment->create( $_POST )) {
+                Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                     $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+            } else {
+                $tpl->assign('errors', $comment->errors);
+            }
+        } break;
+        
+        case 'delete': {            
+            
+            if($_REQUEST['category'] == 'encuesta'){
+                $comment = new PC_Comment();
+                $comment->delete($_REQUEST['id']);
+            } else {
+                $comment = new Comment();
+                $comment->delete($_POST['id'], $_SESSION['userid']);
+            }
+            
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+        } break;
+        
+        case 'change_status': {
+            if(($_REQUEST['tipo'] == 'encuesta') || ($_REQUEST['category'] == 'encuesta')){
+                $comment = new PC_Comment($_REQUEST['id']);                     
+                $comment->set_status($_REQUEST['status'], $_SESSION['userid']);
+            } else {
+                $comment = new Comment($_REQUEST['id']);
+                
+                //Publicar o no,
+                if($_REQUEST['status'] == 2) {
+                    $comment->set_status($_REQUEST['status'], $_SESSION['userid']);                               
+                } else {
+                    // Ya se cambia en el set_available    $comment->set_status($status,$_SESSION['userid']);
+                    $comment->set_available($_REQUEST['status'], $_SESSION['userid']);
+                    $article=new Article($comment->fk_content);
+                    //Para que cambie la fecha changed.
+                    $article->set_status($article->content_status, $article->fk_user_last_editor);
+                }
+            }
+            
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&comment_status=' . $_REQUEST['comment_status'] . '&page=' .
+                                 $_REQUEST['page']);
+        } break;
+        
+        case 'mfrontpage': {
+            
+            if(isset($_REQUEST['selected_fld']) && count($_REQUEST['selected_fld']) > 0) {
+                $fields = $_REQUEST['selected_fld'];
+                $status = $_REQUEST['id'];
+                
+                if(is_array($fields)) {
+                    foreach($fields as $i ) {
+                        $comment = new Comment($i);
+                        
+                        if(!is_null($comment->pk_comment)) {
+                            // Ya se cambia en el set_available  $comment->set_status($status,$_SESSION['userid']);
+                            //Se reutiliza el id para pasar el estatus
+                            $comment->set_available($status, $_SESSION['userid']);
+                            $article = new Article($comment->fk_content);
+                            
+                            //Para que cambie la fecha changed.
+                            $article->set_status($article->content_status, $article->fk_user_last_editor);
+                        } else {
+                            $comment = new PC_Comment($i);                                                   
+                            $comment->set_status($status, $_SESSION['userid']);
+                        }
+                    }
+                }
+            }
+            
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+        } break;
+        
+        case 'mdelete': {
+            if(isset($_REQUEST['selected_fld']) && count($_REQUEST['selected_fld']) > 0) {
+                $fields = $_REQUEST['selected_fld'];
+                
+                if(is_array($fields)) {
+                    foreach($fields as $i ) {
+                        $comment = new Comment($i);
+                        if(!is_null($comment->pk_comment)) {
+                            $comment->delete( $i, $_SESSION['userid'] );
+                        } else {
+                            $comment = new PC_Comment($i);
+                            $comment->delete( $i, $_SESSION['userid'] ); 
+                        }
+                    }
+                }
+            }
+            
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+        } break;
+        
+        default: {
+            Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                                 $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+        } break;
+    }
+} else {
+    Application::forward($_SERVER['SCRIPT_NAME'] . '?action=list&category=' .
+                         $_REQUEST['category'] . '&page=' . $_REQUEST['page']);
+}
+
+$tpl->display('comment.tpl');
---
+++ public/admin/controllers/opinion/author.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Setup app
+*/
+require_once(dirname(__FILE__).'/../../../bootstrap.php');
+require_once(SITE_ADMIN_PATH.'session_bootstrap.php');
+
+ 
+/**
+ * Setup view
+*/
+$tpl = new TemplateAdmin(TEMPLATE_ADMIN);
+
+$tpl->assign('titulo_barra', 'Author Opinion Management');
+
+
+/**
+ * Check privileges
+*/
+require_once(SITE_CORE_PATH.'privileges_check.class.php');
+if(!Acl::_('OPINION_ADMIN')) {
+    Acl::Deny();
+}
+
+if( isset($_REQUEST['action']) ) {
+	switch($_REQUEST['action']) {
+		case 'list':
+			$author = new Author();
+			$cm = new ContentManager();
+			$authors = $author->list_authors(NULL,'order by name');			
+			$tpl->assign('authors_list', $authors);										
+			$authors = $cm->paginate_num($authors,16);
+			$tpl->assign('authors', $authors);							
+			$tpl->assign('paginacion', $cm->pager);
+		
+			$_SESSION['_from']='author.php';	
+
+		break;
+
+		case 'new':	
+			//
+		break;
+
+		case 'read':
+			$author = new Author( $_REQUEST['id'] );		
+			$tpl->assign('author', $author);		
+			$photos=$author->get_author_photos($_REQUEST['id']);		
+	
+			$tpl->assign('photos', $photos);
+			
+		break;
+
+		case 'update':	
+			$author = new Author();
+			$author->update( $_REQUEST );	
+	
+			if($_SESSION['_from']=='opinion.php'){
+				Application::forward('controllers/opinion/opinion.php?action=list&page=1');
+			}else{
+				Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$_REQUEST['page']);
+			}
+		break;
+
+		case 'create':
+			$author = new Author();
+			if($author->create( $_POST )) {			
+				Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$_REQUEST['page']);
+			} else {
+				$tpl->assign('errors', $author->errors);
+			}
+		break;
+
+		case 'delete':
+			$author = new Author();
+			$author->delete( $_POST['id'] );
+			Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$_REQUEST['page']);
+		break;
+		
+		case 'validate':
+
+			$author = new Author();
+			if(empty($_POST["id"])) {
+				
+				//Estamos creando un nuevo artículo
+				if(!$author->create( $_POST ))
+					$tpl->assign('errors', $author->errors);		
+			} else {
+				$author = new Author($_POST["id"]);
+				//Estamos atualizando un artículo
+				$author->update( $_REQUEST );
+			}			
+				Application::forward($_SERVER['SCRIPT_NAME'].'?action=read&id='.$author->pk_author);	
+		break;
+
+		case 'check_img_author':
+			$ok='no';
+			$img=$_REQUEST['id_img'];
+			$cm = new ContentManager();
+			$opinions = $cm->find('Opinion', 'fk_content_type=4 and (fk_author_img = '.$img.' OR fk_author_img_widget = '.$img.') ' , 'ORDER BY type_opinion DESC');			
+			if(!empty($opinions)){
+				$ok='si';
+			}
+			Application::ajax_out($ok);
+		break;
+		
+		default:
+			Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$_REQUEST['page']);
+		break;
+	}
+} else {
+	Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$_REQUEST['page']);
+}
+
+$tpl->assign('MEDIA_IMG_PATH_URL', MEDIA_IMG_PATH_WEB);
+
+$tpl->display('opinion/author.tpl');
+ 
