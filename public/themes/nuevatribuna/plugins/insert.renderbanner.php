d705f8ec08ad47a4a8fb10feead16c9edb194a60
Fran Dieguez <fran@openhost.es>
2011-02-16 20:37:33 +0100

    Fix issue with ads when cache is enabled [I guess]

--- public/themes/nuevatribuna/plugins/insert.renderbanner.php
+++ public/themes/nuevatribuna/plugins/insert.renderbanner.php
@@ -2,7 +2,7 @@
 
 /**
  * Smarty plugin to render a banner into OpenNeMas
- * 
+ *
  * @param array $params
  * @param Template $tpl Template class which extends of Smarty
  * @param boolean $cache Use cache
@@ -13,14 +13,14 @@ function smarty_insert_renderbanner($params, &$smarty) {
     // FIXME: include function into Advertisement static method
     if(!function_exists('render_output')) {
         function render_output($content, $banner) {
-            if( /*is_object($banner)
+            if( is_object($banner)
                 && property_exists($banner,'type_advertisement')
-                && */((($banner->type_advertisement + 50)%100) == 0)) {
+                && ((($banner->type_advertisement + 50)%100) == 0)) {
                 $content = json_encode($content);
-                
+
                 $timeout = intval($banner->timeout) * 1000; // convert to ms
                 $pk_advertisement = $banner->pk_advertisement;
-                
+
                 /*
                  * intersticial = new IntersticialBanner({iframeSrc: '/sargadelos.html?cacheburst=1254325526',
                  *                                        timeout: -1,
@@ -37,31 +37,31 @@ function smarty_insert_renderbanner($params, &$smarty) {
 /* ]]> */
 </script>
 JSINTERSTICIAL;
-            
+
                 return $output;
             }
-            
+
             return $content;
         }
     }
-    
+
     $output = '';
     $type = $params['type'];
     if(empty($type) || (defined('ADVERTISEMENT_ENABLE') && !ADVERTISEMENT_ENABLE)) {
         // Banners not enabled
         return $output;
     }
-    
+
     $advertisement = Advertisement::getInstance();
-    $banner = $advertisement->fetch('banner' . $type);    
+    $banner = $advertisement->fetch('banner' . $type);
     $photo  = $advertisement->fetch('photo' . $type);
-    
+
     if(!isset($params['cssclass'])) {
         $smarty->trigger_error("renderbanner: missing 'cssclass' parameter");
         return;
-    }    
+    }
     $cssclass = $params['cssclass'];
-    
+
     // Extract width and height properties from CSS
     if(!isset($params['width']) || !isset($params['height'])) {
         $width  = preg_replace('/^[a-z]+(\d+)x\d+$/i', '\1', $cssclass);
@@ -70,10 +70,10 @@ JSINTERSTICIAL;
         $width  = $params['width'];
         $height = $params['height'];
     }
-    
+
     // If $height is equals to * then calculate using GD
     if($height == '*') {
-        if(file_exists(MEDIA_IMG_PATH. $photo->path_file. $photo->name)) {            
+        if(file_exists(MEDIA_IMG_PATH. $photo->path_file. $photo->name)) {
             list($w, $h, $type, $attr) = getimagesize( MEDIA_IMG_PATH. $photo->path_file. $photo->name );
             if($w == $width) { // La imagen es proporcional
                 $height = $h;
@@ -88,44 +88,44 @@ JSINTERSTICIAL;
             $height = 0;
         }
     }
-    
+
     // Overlap flash?
     $overlap  = (isset($params['overlap']))? $params['overlap']: false;
     $isBastardIE = preg_match('/MSIE /', $_SERVER['HTTP_USER_AGENT']);
-            
+
     if( isset($params['beforeHTML']) ) {
         $output .= $params['beforeHTML'];
     }
-    
+
     // Initial container
-    $output .= '<div class="'.$cssclass.'">';    
-    
-    if(isset($banner->with_script) && $banner->with_script == 1 ) {        
+    $output .= '<div class="'.$cssclass.'">';
+
+    if(isset($banner->with_script) && $banner->with_script == 1 ) {
         // Original method
         // $output .= $banner->script;
-        
+
         // Parallelized method using iframes
         $output .= '<iframe src="/publicidade/get/' . $banner->pk_content  . '.html" ' .
-                   'scrolling="no" frameborder="0" width="' . $width . '" height="' . $height . '" ' . 
+                   'scrolling="no" frameborder="0" width="' . $width . '" height="' . $height . '" ' .
                    'marginwidth="0" marginheight="0" rel="nofollow">Publicidad Xornal.com</iframe>';
-        
-    } elseif( !empty($banner->pk_advertisement) ) {                
-        
+
+    } elseif( !empty($banner->pk_advertisement) ) {
+
         // TODO: controlar los banners swf especiales con div por encima
         if( strtolower($photo->type_img)=='swf' ) {
             if(!$overlap && !$banner->overlap) {
-                // Flash object            
+                // Flash object
                 // FIXME: build flash object with all tags and params
                 $output .= '<a target="_blank" href="/publicidade/'. $banner->pk_advertisement .'.html" rel="nofollow">';
                 $output .= '<object>
                         <param name="wmode" value="transparent" />
                         <param name="movie" value="'. MEDIA_IMG_PATH_WEB. $photo->path_file. $photo->name. '" />
                         <param name="width" value="'.$width.'" />
-                        <param name="height" value="'.$height.'" />            
+                        <param name="height" value="'.$height.'" />
                         <embed src="'. MEDIA_IMG_PATH_WEB. $photo->path_file. $photo->name. '"
                             width="'.$width.'" height="'.$height.'" alt="Publicidad '. $banner->title. '" wmode="transparent"></embed>
                     </object>';
-            } else {                
+            } else {
                 if(!$isBastardIE) {
                     $output .= '<div style="position: relative; width: '.$width.'px; height: '.$height.'px;">
                         <div style="left:0px;top:0px;cursor:pointer;background-color:transparent;position:absolute;z-index:100;width:'.
@@ -137,26 +137,26 @@ JSINTERSTICIAL;
                             $width.'px;height:'.$height.'px;"
                             onclick="javascript:window.open(\'/publicidade/'.$banner->pk_advertisement.'.html\', \'_blank\');return false;"></div>';
                 }
-                
+
                 $output .= '<div style="position: absolute; z-index: 0; width: '.$width.'px; left: 0px;">
                         <object width="'.$width.'" height="'.$height.'">
                             <param name="movie" value="'. MEDIA_IMG_PATH_WEB. $photo->path_file. $photo->name. '" />
                             <param name="wmode" value="opaque" />
                             <param name="width" value="'.$width.'" />
-                            <param name="height" value="'.$height.'" />            
+                            <param name="height" value="'.$height.'" />
                             <embed src="'. MEDIA_IMG_PATH_WEB. $photo->path_file. $photo->name. '" wmode="opaque"
                                 width="'.$width.'" height="'.$height.'" alt="Publicidad '. $banner->title. '"></embed>
                         </object>
                     </div>
                   </div>';
-                  
+
                 $output .= '</div>';
-                
+
                 if( isset($params['afterHTML']) ) {
                     $output .= $params['afterHTML'];
                 }
-                
-                
+
+
                 return render_output($output, $banner);
             }
         } else {
@@ -165,20 +165,20 @@ JSINTERSTICIAL;
             $output .= '<img src="'. MEDIA_IMG_PATH_WEB. $photo->path_file. $photo->name.'"
                     alt="Publicidad '.$banner->title.'" width="'.$width.'" height="'.$height.'" />';
         }
-        
+
         $output .= '</a>';
     } else {
         // Empty banner, don't return anything
         $output = '';
         return render_output($output, $banner);
     }
-    
+
     $output .= '</div>';
-    
+
     // Post content of banner
     if( isset($params['afterHTML']) ) {
         $output .= $params['afterHTML'];
-    }    
+    }
 
     return render_output($output, $banner);
 }
