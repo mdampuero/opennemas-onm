cf655178f0dc81bcc4cd627710b983eab28c16e7
Fran Dieguez <fran@openhost.es>
2011-02-16 20:37:06 +0100

    Little improvements in sitemap.php

--- public/controllers/sitemap.php
+++ public/controllers/sitemap.php
@@ -29,7 +29,7 @@ if(isset($_REQUEST['action']) ) {
 
             $categoriesVideos = array();
             $categoriesGallerys = array();
-            
+
             //FIXME: add this value in a config file for easy editing
             $numArticlesCategory = 250;
             $numContents = 50;
@@ -42,7 +42,7 @@ if(isset($_REQUEST['action']) ) {
                     $categoriesGallerys[$catID_key] = $cm->get_permalinks_by_categoryID($catID_value->pk_content_category, 'available=1 AND fk_content_type=7',' ORDER BY created DESC LIMIT 0 ,'.$numContents);
                 }
             }
-            
+
             $opinions = $cm->getOpinionAuthorsPermalinks('contents.available=1 and contents.content_status=1', 'ORDER BY in_home DESC, position ASC, changed DESC LIMIT 100');
 
             $tpl->assign('categorieswebID',$categorieswebID);
@@ -50,14 +50,14 @@ if(isset($_REQUEST['action']) ) {
             $tpl->assign('categoriesVideos', $categoriesVideos);
             $tpl->assign('categoriesGallerys', $categoriesGallerys);
 
-        
+
         break;
 
         case 'news': {
 
             //FIXME: add this value in a config file for easy editing
             $interval='DATE_SUB(CURDATE(), INTERVAL 700 DAY)';
-            
+
             foreach ($allcategorys as $cID_key => $cID_value) {
                 if ($cID_value->inmenu == 1 && $cID_value->internal_category == 1) {
                     $categoriesnewsID[$cID_key] = $cm->get_permalinks_by_categoryID($cID_value->pk_content_category, 'available=1 AND fk_content_type=1 AND changed >='.$interval.'','ORDER BY changed DESC');
@@ -68,14 +68,14 @@ if(isset($_REQUEST['action']) ) {
             $opinions = $cm->getOpinionAuthorsPermalinks('contents.available=1 AND contents.content_status=1 AND changed >='.$interval.'', 'ORDER BY position ASC, changed DESC LIMIT 100');
 
             $tpl->assign('categoriesnewsID', $categoriesnewsID);
-            $tpl->assign('opinions', $opinions);         
+            $tpl->assign('opinions', $opinions);
 
         }
     }
-    
+
     $tpl->assign('SITE', SITE);
     $tpl->assign('allcategorys', $allcategorys);
     $app->conn->Close();
 }
 
-echo $tpl->fetch('sitemap.tpl');
+echo $tpl->fetch('sitemap/sitemap.tpl');
