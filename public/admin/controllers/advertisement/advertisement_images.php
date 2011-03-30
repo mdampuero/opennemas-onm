7ff47536184c3e4732612a08ef4cea8f5ef07a7d
Sandra Pereira <sandra@openhost.es>
2011-03-30 19:21:38 +0200

    Fixed bug with flash images(swf) on advertisement

--- public/admin/controllers/advertisement/advertisement_images.php
+++ public/admin/controllers/advertisement/advertisement_images.php
@@ -15,6 +15,7 @@ $photos = $cm->find_by_category('Photo',2, 'fk_content_type=8 ', 'ORDER BY creat
 
 $photos = $cm->paginate_num($photos,16);
 $pages=$cm->pager;
+echo "<em>Arrastre y suelte las publicidades para seleccionarlas, use el logo para usar Flash</em><br><br>";
 if($pages->_totalPages>1){
 	echo "<p align='center'>Paginas: ";
 	for($i=1;$i<=$pages->_totalPages;$i++){
@@ -22,22 +23,27 @@ if($pages->_totalPages>1){
 	}
 	echo "</p> ";
 }
-echo "<ul id='thelist' class='gallery_list' style='width:360px;'> ";
+echo "<ul id='thelist' class='gallery_list'> ";
 if($photos){
 $num=1;
 	foreach ($photos as $as) {
 		//if(file_exists(MEDIA_IMG_PATH.$as->path_file.$as->name)){
 				$ph=new Photo($as->pk_photo);
-				
+
 				$ph->set_status(1,$_SESSION['userid']);
-				echo '<li style="height:75px;"> ';
-				if((strtolower($ph->type_img_)=='swf')) {
+				echo '<li style="position:relative"> ';
+				if((strtolower($ph->type_img)=='swf')) {
 					echo'
-						<object style="cursor:default;">
+						<object style="z-index:-3; cursor:default;"  title="Desc: '.$as->description.' Tags: '.$as->metadata.' ">
 							<param name="movie" value="'.MEDIA_IMG_PATH_URL.$as->path_file.$as->name.'"> <param name="autoplay" value="false">  <param name="autoStart" value="0">
-							<embed style="cursor:default;" width="68" height="40"  src="'.MEDIA_IMG_PATH_URL.$as->path_file.$as->name.'" name="'.$as->pk_photo.'" border="0" de:mas="'.$as->name.'" de:url="'.MEDIA_IMG_PATH_URL.$as->path_file.'" de:ancho="'.$as->width.'" de:alto="'.$as->height.'" de:peso="'.$as->size.'" de:created="'.$as->created.'" de:type_img="'.$as->type_img.'" de:description="'.$as->description.'" ></embed>
+							<embed style="cursor:default;" width="68" height="40"  src="'.MEDIA_IMG_PATH_URL.$as->path_file.$as->name.'" name="'.$as->pk_photo.'" border="0" de:mas="'.$as->name.'" de:url="'.MEDIA_IMG_PATH_URL.$as->path_file.'" de:ancho="'.$as->width.'" de:alto="'.$as->height.'" de:peso="'.$as->size.'" de:created="'.$as->created.'" de:type_img="'.$as->type_img.'" de:description="'.$as->description.' title="'.$as->title.'"</embed>
 						</object>
-						<span style="float:right;"><img id="draggable_img'.$num.'" class="draggable" src="themes/default/images/flash.gif" style="width:20px" name="'.$as->pk_photo.'" border="0" de:mas="'.$as->name.'" de:url="'.MEDIA_IMG_PATH_URL.$as->path_file.'" de:ancho="'.$as->width.'" de:alto="'.$as->height.'" de:peso="'.$as->size.'" de:created="'.$as->created.'" de:type_img="'.$as->type_img.'" de:description="'.$as->description.'" ></span>
+						<span style="float:right; clear:none;">
+                        <img id="draggable_img'.$num.'" 
+                             class="draggable" 
+                             src='.SITE_URL_ADMIN.'/themes/default/images/flash.gif
+                             style="width:20px" name="'.$as->pk_photo.'" 
+                             border="0" de:mas="'.$as->name.'" de:url="'.MEDIA_IMG_PATH_URL.$as->path_file.'" de:ancho="'.$as->width.'" de:alto="'.$as->height.'" de:peso="'.$as->size.'" de:created="'.$as->created.'" de:type_img="'.$as->type_img.'" de:description="'.$as->description.'" ></span>
 					';
 
 				} else {
