e232903c3cceb9d14be1f594eafec7c27258e88e
Fran Dieguez <fran@openhost.es>
2011-06-22 17:06:47 +0200

    Moved robots.txt to a controller for avoid site url change-aware.
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

---
+++ public/controllers/robots.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Start up and setup the app
+*/
+require_once('../bootstrap.php');
+header("Content-Type: text/plain");
+?>
+User-Agent: *
+Disallow: /admin/
+Allow: /
+
+Disallow: /harming/humans
+Disallow: /ignoring/human/orders
+Disallow: /harm/to/self
+
+Sitemap: <?php echo SITE_URL; ?>sitemapnews.xml
+Sitemap: <?php echo SITE_URL; ?>sitemapweb.xml
