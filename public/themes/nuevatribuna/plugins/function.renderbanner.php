d8ed5bc0507bed6890ff5980fe1a356cf3de76c1
Alex Rodrigues <alex@openhost.es>
2011-06-30 02:33:12 +0200

    Solved bug of non-object on function.renderbanner
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- public/themes/nuevatribuna/plugins/function.renderbanner.php
+++ public/themes/nuevatribuna/plugins/function.renderbanner.php
@@ -70,64 +70,68 @@ function smarty_function_renderbanner($params, &$smarty, $cache = false) {
     }
     // Initial container
     $output.= '<div class="' . $cssclass . '">';
-    if ($banner->with_script == 1) {
-        // Original method
-        // $output .= $banner->script;
-        // Parallelized method using iframes
-        $output.= '<iframe src="/publicidade/get/' . $banner->pk_content . '.html" ' . 'scrolling="no" frameborder="0" width="' . $width . '" height="' . $height . '" ' . 'marginwidth="0" marginheight="0" rel="nofollow">Publicidad Retrincos.com</iframe>';
-    } elseif (!empty($banner->pk_advertisement)) {
-        // TODO: controlar los banners swf especiales con div por encima
-        if (strtolower($photo->type_img) == 'swf') {
-            if (!$overlap && !$banner->overlap) {
-                // Flash object
-                // FIXME: build flash object with all tags and params
-                $output.= '<a target="_blank" href="/publicidade/' . $banner->pk_advertisement . '.html" rel="nofollow">';
-                $output.= '<object>
-                        <param name="movie" value="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '">
-                        <param name="width" value="' . $width . '" />
-                        <param name="height" value="' . $height . '" />            
-                        <embed src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '"
-                            width="' . $width . '" height="' . $height . '" alt="Publicidad ' . $banner->title . '"></embed>
-                    </object>';
-            } else {
-                if (!$isBastardIE) {
-                    $output.= '<div style="position: relative; width: ' . $width . 'px; height: ' . $height . 'px;">
-                        <div style="left:0px;top:0px;cursor:pointer;background-color:transparent;position:absolute;z-index:100;width:' . $width . 'px;height:' . $height . 'px;"
-                            onclick="javascript:window.open(\'/publicidade/' . $banner->pk_advertisement . '.html\', \'_blank\');return false;"></div>';
-                } else {
-                    $output.= '<div style="position: relative; width: ' . $width . 'px; height: ' . $height . 'px;">
-                        <div style="left:0px;top:0px;cursor:pointer;background-color:#FFF;filter:alpha(opacity=0);position:absolute;z-index:100;width:' . $width . 'px;height:' . $height . 'px;"
-                            onclick="javascript:window.open(\'/publicidade/' . $banner->pk_advertisement . '.html\', \'_blank\');return false;"></div>';
-                }
-                $output.= '<div style="position: absolute; z-index: 0; width: ' . $width . 'px; left: 0px;">
-                        <object width="' . $width . '" height="' . $height . '">
+    
+    if (isset ($banner)) { 
+        if ($banner->with_script == 1) {
+            // Original method
+            // $output .= $banner->script;
+            // Parallelized method using iframes
+            $output.= '<iframe src="/publicidade/get/' . $banner->pk_content . '.html" ' . 'scrolling="no" frameborder="0" width="' . $width . '" height="' . $height . '" ' . 'marginwidth="0" marginheight="0" rel="nofollow">Publicidad Retrincos.com</iframe>';
+        } elseif (!empty($banner->pk_advertisement)) {
+            // TODO: controlar los banners swf especiales con div por encima
+            if (strtolower($photo->type_img) == 'swf') {
+                if (!$overlap && !$banner->overlap) {
+                    // Flash object
+                    // FIXME: build flash object with all tags and params
+                    $output.= '<a target="_blank" href="/publicidade/' . $banner->pk_advertisement . '.html" rel="nofollow">';
+                    $output.= '<object>
                             <param name="movie" value="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '">
-                            <param name="wmode" value="opaque" />
                             <param name="width" value="' . $width . '" />
                             <param name="height" value="' . $height . '" />            
-                            <embed src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '" wmode="opaque"
+                            <embed src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '"
                                 width="' . $width . '" height="' . $height . '" alt="Publicidad ' . $banner->title . '"></embed>
-                        </object>
-                    </div>
-                  </div>';
-                $output.= '</div>';
-                if (isset($params['afterHTML'])) {
-                    $output.= $params['afterHTML'];
+                        </object>';
+                } else {
+                    if (!$isBastardIE) {
+                        $output.= '<div style="position: relative; width: ' . $width . 'px; height: ' . $height . 'px;">
+                            <div style="left:0px;top:0px;cursor:pointer;background-color:transparent;position:absolute;z-index:100;width:' . $width . 'px;height:' . $height . 'px;"
+                                onclick="javascript:window.open(\'/publicidade/' . $banner->pk_advertisement . '.html\', \'_blank\');return false;"></div>';
+                    } else {
+                        $output.= '<div style="position: relative; width: ' . $width . 'px; height: ' . $height . 'px;">
+                            <div style="left:0px;top:0px;cursor:pointer;background-color:#FFF;filter:alpha(opacity=0);position:absolute;z-index:100;width:' . $width . 'px;height:' . $height . 'px;"
+                                onclick="javascript:window.open(\'/publicidade/' . $banner->pk_advertisement . '.html\', \'_blank\');return false;"></div>';
+                    }
+                    $output.= '<div style="position: absolute; z-index: 0; width: ' . $width . 'px; left: 0px;">
+                            <object width="' . $width . '" height="' . $height . '">
+                                <param name="movie" value="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '">
+                                <param name="wmode" value="opaque" />
+                                <param name="width" value="' . $width . '" />
+                                <param name="height" value="' . $height . '" />            
+                                <embed src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '" wmode="opaque"
+                                    width="' . $width . '" height="' . $height . '" alt="Publicidad ' . $banner->title . '"></embed>
+                            </object>
+                        </div>
+                      </div>';
+                    $output.= '</div>';
+                    if (isset($params['afterHTML'])) {
+                        $output.= $params['afterHTML'];
+                    }
+                    return ($output);
                 }
-                return ($output);
+            } else {
+                // Image
+                $output.= '<a target="_blank" href="/publicidade/' . $banner->pk_advertisement . '.html" rel="nofollow">';
+                $output.= '<img src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '"
+                        alt="Publicidad ' . $banner->title . '" width="' . $width . '" height="' . $height . '" />';
             }
+            $output.= '</a>';
         } else {
-            // Image
-            $output.= '<a target="_blank" href="/publicidade/' . $banner->pk_advertisement . '.html" rel="nofollow">';
-            $output.= '<img src="' . MEDIA_IMG_PATH_WEB . $photo->path_file . $photo->name . '"
-                    alt="Publicidad ' . $banner->title . '" width="' . $width . '" height="' . $height . '" />';
+            // Empty banner, don't return anything
+            $output = '';
+            return ($output);
         }
-        $output.= '</a>';
-    } else {
-        // Empty banner, don't return anything
-        $output = '';
-        return ($output);
     }
+    
     $output.= '</div>';
     // Post content of banner
     if (isset($params['afterHTML'])) {
