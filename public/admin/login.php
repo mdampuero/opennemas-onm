378eb7142a0cad54252847a1d34535103bb2a3f7
Fran Dieguez <fran@openhost.es>
2011-07-16 03:31:49 +0200

    Revamped Admin login layout Added Blueprint in admin theme
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- public/admin/login.php
+++ public/admin/login.php
@@ -16,24 +16,24 @@ if( isset($_REQUEST['action'])){
 	switch($_REQUEST['action']) {
         case 'login':
             $user = new User();
-    
+
             $token   = (isset($_REQUEST['token']))?   $_REQUEST['token']:   null;
             $captcha = (isset($_REQUEST['captcha']))? $_REQUEST['captcha']: null;
-    
+
             $result = $user->login($_REQUEST['login'], $_REQUEST['password'], $token, $captcha);
-    
+
             if ($result === true) { // must be same type (===)
-    
+
                 if( isset($_REQUEST['rememberme']) ) {
                     // Use long expression of setcookie to have more security
                     /*
                       setcookie("login", $_REQUEST['login'], time()+3600, "/admin/", ".dominio.com", TRUE, TRUE);
                       setcookie("password", $_REQUEST['password'], time()+3600, "/admin/", ".dominio.com", TRUE, TRUE);
                     */
-    
+
                     $app->setcookie_secure("login_username", $_REQUEST['login'],    time()+60*60*24*30, '/admin/');
                     $app->setcookie_secure("login_password", $_REQUEST['password'], time()+60*60*24*30, '/admin/');
-    
+
                 } else {
                     if( isset($_COOKIE['login_username']) ) {
                         // Caducar a cookie
@@ -41,56 +41,56 @@ if( isset($_REQUEST['action'])){
                         setcookie("login_password", '', time()-(60*60) );
                     }
                 }
-    
+
                 /**
                  * Validar el usuario si esta authorize=1, sino mensaje de usuario de baja.
                  */
-                if($user->authorize == 1){ 
+                if($user->authorize == 1){
 
-                // Load session
-                require_once('session_bootstrap.php');
-    
-                $_SESSION = array(); // Clear before to load
-    
-                $_SESSION['userid']     = $user->id;
-                $_SESSION['username']   = $user->login;
-                $_SESSION['email']      = $user->email;
-                // SYS_NAME_GROUP_ADMIN defined into config.inc.php
-                $_SESSION['isAdmin']    = ( User_group::getGroupName($user->fk_user_group)==SYS_NAME_GROUP_ADMIN );
-                $_SESSION['privileges'] = Privilege::get_privileges_by_user($user->id);
-                $_SESSION['accesscategories'] = $user->get_access_categories_id();
-    
-                // Method authentication: database|google_clientlogin
-                $_SESSION['authMethod'] = $user->authMethod;
-                if($user->authMethod == 'google_clientlogin') {
-                    $_SESSION['authGmail']  = base64_encode($_REQUEST['login'].':'.$_REQUEST['password']);
-                    /* $_SESSION['loginToken'] = $user->clientLoginToken;
-                    $_SESSION['passwd'] = $_REQUEST['password']; */
-                }
-    
-                //Carga en la varible de _SESSION expire el tiempo de expiración del usuario la sesión.
-                $_SESSION['default_expire'] = $user->sessionexpire;
-                $app->setcookie_secure('default_expire', $user->sessionexpire, 0, '/admin/');
-    
-                Privileges_check::loadSessionExpireTime();
-    
-                //initHandleErrorPrivileges();
+					// Load session
+					require_once('session_bootstrap.php');
+
+					$_SESSION = array(); // Clear before to load
+
+					$_SESSION['userid']     = $user->id;
+					$_SESSION['username']   = $user->login;
+					$_SESSION['email']      = $user->email;
+					// SYS_NAME_GROUP_ADMIN defined into config.inc.php
+					$_SESSION['isAdmin']    = ( User_group::getGroupName($user->fk_user_group)==SYS_NAME_GROUP_ADMIN );
+					$_SESSION['privileges'] = Privilege::get_privileges_by_user($user->id);
+					$_SESSION['accesscategories'] = $user->get_access_categories_id();
+
+					// Method authentication: database|google_clientlogin
+					$_SESSION['authMethod'] = $user->authMethod;
+					if($user->authMethod == 'google_clientlogin') {
+						$_SESSION['authGmail']  = base64_encode($_REQUEST['login'].':'.$_REQUEST['password']);
+						/* $_SESSION['loginToken'] = $user->clientLoginToken;
+						$_SESSION['passwd'] = $_REQUEST['password']; */
+					}
+
+					//Carga en la varible de _SESSION expire el tiempo de expiración del usuario la sesión.
+					$_SESSION['default_expire'] = $user->sessionexpire;
+					$app->setcookie_secure('default_expire', $user->sessionexpire, 0, '/admin/');
+
+					Privileges_check::loadSessionExpireTime();
+
+					//initHandleErrorPrivileges();
                     Application::forward(SITE_URL_ADMIN.SS.'index.php');
                 } else{
-                    $tpl->assign('message', 'Este usuario se ha dado de baja. Consulte el administrador.');
+                    $tpl->assign('message', _('This user was deactivated. Please ask your administrator.'));
                 }
             } else {
-                
+
                 // Show google captcha
                 if(isset($result['token'])) {
                     $tpl->assign('token', $result['token']);
                     $tpl->assign('captcha', $result['captcha']);
                 }
-    
-                $tpl->assign('message', 'Nome de usuario ou contrasinal incorrecto.');
+
+                $tpl->assign('message', _('Username or password incorrect.'));
             }
         break;
 	}
 }
-
+$tpl->assign('version', \Onm\Common\Version::VERSION);
 $tpl->display('login.tpl');
