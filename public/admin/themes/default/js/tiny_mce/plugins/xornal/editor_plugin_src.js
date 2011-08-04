ae08f89abb215731daa9422cd690efd18e872d90
Alex Rodrigues <alex@openhost.es>
2011-08-04 20:20:55 +0200

    Fixed url for keywords autolink.
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- public/admin/themes/default/js/tiny_mce/plugins/xornal/editor_plugin_src.js
+++ public/admin/themes/default/js/tiny_mce/plugins/xornal/editor_plugin_src.js
@@ -2,7 +2,7 @@
  * $Id: editor_plugin_src.js 201 2007-02-12 15:56:56Z spocke $
  *
  * @author Moxiecode
- * @copyright Copyright © 2004-2008, Moxiecode Systems AB, All rights reserved.
+ * @copyright Copyright ï¿½ 2004-2008, Moxiecode Systems AB, All rights reserved.
  */
 
 (function() {
@@ -24,7 +24,7 @@
 				var html = tinyMCE.activeEditor.getContent();
 				
 				tinymce.util.XHR.send({
-					url: '/admin/pclave.php?action=autolink',
+					url: '/admin/controllers/keywords/keywords.php?action=autolink',
 					type : "POST",
 					data : tinymce.util.JSON.serialize({
 						'content': html
