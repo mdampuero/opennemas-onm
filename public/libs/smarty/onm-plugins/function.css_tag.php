b61a0f17a173125cda23c8fbf8f50adfe8e970ea
Sandra Pereira <sandra@openhost.es>
2011-09-30 14:58:56 +0200

    added with cherry-pick ba97c6019104d43e6e14c6b128225669b7d75149

---
+++ public/libs/smarty/onm-plugins/function.css_tag.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * -------------------------------------------------------------
+ * File:     	function.script_tag.php
+ * Comprueba el tipo y escribe el nombre o la imag
+ */
+
+function smarty_function_css_tag($params, &$smarty) {
+
+    $output = "";
+    
+    if (empty($params['href'])) {
+        trigger_error("[plugin] css_tag parameter 'href' cannot be empty",E_USER_NOTICE);
+        return;
+    }
+    
+    $href = $params['href'];
+   
+
+    $mtime = '?';        
+    $server = '';
+    if ($smarty->theme == 'default') {
+        $file = TEMPLATE_ADMIN_PATH.'/css'.$href;
+        if (file_exists($file)) {
+            $mtime .= filemtime($file);
+            $server = TEMPLATE_ADMIN_URL.'css';
+        }
+    } else {
+        $file = TEMPLATE_USER_PATH.'/css'.$href;
+        if (file_exists($file)) {
+            $mtime .= filemtime($file);
+            $server = TEMPLATE_USER_URL.'css';
+        }
+    }
+    
+    //Comprobar si tiene type definido
+    if (isset($params['type'])) {
+        $type = "type=\"{$params['type']}\"";
+    } else {
+        $type = "type=\"text/css\"";
+    }
+    
+    //Comprobar si tiene rel definido
+    if (isset($params['rel'])) {
+        $rel = "rel=\"{$params['rel']}\"";
+    } else {
+        $rel = "rel=\"stylesheet\"";
+    }
+    
+    
+    
+    unset($params['rel']);
+    unset($params['href']);
+    unset($params['type']);
+    $properties = '';
+    foreach($params as $key => $value) {
+        $properties .= " {$key}=\"{$value}\"";
+    }
+    
+    $output = "<link {$rel} {$type} href=\"{$server}{$href}{$mtime}\" {$properties}></link>";
+    
+    return $output;
+}
