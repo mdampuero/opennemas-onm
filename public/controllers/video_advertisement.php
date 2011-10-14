c1774457a2e855f3c7b2894e525c8052a83c01b7
Sandra Pereira <sandra@openhost.es>
2011-10-14 13:46:43 +0200

    [Bug #2438] - Frontend: review advertisement positions & add all holes
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- public/controllers/video_advertisement.php
+++ public/controllers/video_advertisement.php
@@ -9,7 +9,7 @@ $advertisement = Advertisement::getInstance();
 
 // Load internal banners, principal banners (1,2,3,11,13) and use cache to performance
 /* $banners = $advertisement->cache->getAdvertisements(array(1, 2, 3, 10, 12, 11, 13), $category); */
-$banners = $advertisement->getAdvertisements(array(201, 202,  209, 210), $category);
+$banners = $advertisement->getAdvertisements(array(201, 202, 203, 209, 210), $category);
  $cm = new ContentManager();
 $banners = $cm->getInTime($banners);
 //$advertisement->render($banners, &$tpl);
