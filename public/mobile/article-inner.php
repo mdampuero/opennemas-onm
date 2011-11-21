296e91914709dd0392a39b02ea4907e0e93f54b6
Alex Rodrigues <alex@openhost.es>
2011-11-21 15:58:23 +0100

    Fix id's on mobile article-inner
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- public/mobile/article-inner.php
+++ public/mobile/article-inner.php
@@ -14,7 +14,17 @@ $tpl = new Template(TEMPLATE_USER);
  * Setup cache
  **/
 $tpl->setConfig('articles-mobile');
-$cacheID = $tpl->generateCacheId('articles-mobile','',$_REQUEST['pk_content']);
+
+/**
+ * Getting request params
+ **/
+$dirtyID = filter_var($_GET['pk_content'], FILTER_SANITIZE_STRING);
+if(empty($dirtyID)) {
+    $dirtyID = filter_input(INPUT_POST,'pk_content',FILTER_SANITIZE_STRING);
+}
+
+$articleID = Content::resolveID($dirtyID);
+$cacheID = $tpl->generateCacheId('articles-mobile','',$articleID);
 
 /**
  * if cache is enabled and this content has an available cache
@@ -22,7 +32,7 @@ $cacheID = $tpl->generateCacheId('articles-mobile','',$_REQUEST['pk_content']);
 if(($tpl->caching == 0)
     || ! $tpl->isCached('mobile/article-inner.tpl', $cacheID)) {
 
-    $articleID = filter_input(INPUT_GET,'pk_content',FILTER_SANITIZE_STRING);
+    //$articleID = filter_input(INPUT_GET,'pk_content',FILTER_SANITIZE_STRING);
 
     // Category manager to retrieve category of article
     $ccm = ContentCategoryManager::get_instance();
@@ -32,14 +42,14 @@ if(($tpl->caching == 0)
 
     require('sections.php');
 
-    $article = new Article($_REQUEST['pk_content']);
+    $article = new Article($articleID);
     $article->category_name = $ccm->get_name($article->category);
 
 
     /******* RELATED  CONTENT *******/
     $rel= new Related_content();
 
-    $relationes = $rel->cache->get_relations_int($_REQUEST['pk_content']);
+    $relationes = $rel->cache->get_relations_int($articleID);
     $relat = $cm->cache->getContents($relationes);
     $relat = $cm->getInTime($relat);
     //Filter availables and not inlitter.
