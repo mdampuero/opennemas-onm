ddea7e0e95fc240f54ce517d872eb0b8ac768f69
Sandra Pereira <sandra@openhost.es>
2011-11-30 13:30:21 +0100

    [ Feature #18] - Import ACL granular schema from Xornal code:  A user can modify its own profile
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- public/admin/controllers/acl/user.php
+++ public/admin/controllers/acl/user.php
@@ -1,23 +1,15 @@
 <?php
-/*
- * This file is part of the onm package.
- * (c) 2009-2011 OpenHost S.L. <contact@openhost.es>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
+use Onm\Settings as s,
+    Onm\Message as m;
+
 /**
  * Setup application
 */
 require_once('../../../bootstrap.php');
 require_once('../../session_bootstrap.php');
 
-// Check ACL {{{
-require_once(SITE_CORE_PATH.'privileges_check.class.php');
-if(!Acl::checkOrForward('USER_ADMIN')) {
-    
-    }
-    // }}}
+ 
 /**
  * Set up view
 */
@@ -25,144 +17,177 @@ $tpl = new \TemplateAdmin(TEMPLATE_ADMIN);
 $tpl->assign('titulo_barra', 'User Management');
 
 $ccm = new ContentCategoryManager();
+
 if (!array_key_exists('page', $_REQUEST)) {
     $page = 0;
 } else {
     $page = $_REQUEST['page'] ?:0;
 }
 
-if( isset($_REQUEST['action']) ) {
-    switch($_REQUEST['action']) {
-        case 'list': {
-            $cm = new ContentManager();
-            $user = new User();
+$action = filter_input( INPUT_POST, 'action' , FILTER_SANITIZE_STRING );
+if (!isset($action)) {
+    $action = filter_input( INPUT_GET, 'action' , FILTER_SANITIZE_STRING, array('options' => array('default' => 'list')) );
+}
 
-            $filters = (isset($_REQUEST['filter']))? $_REQUEST['filter']: null;
-            $users = $user->get_users($filters, ' ORDER BY login ');
 
-            $users = $cm->paginate_num($users,12);
+switch($_REQUEST['action']) {
+    case 'list': 
+        Acl::checkOrForward('USER_ADMIN');
 
-            $user_group = new User_group();
-            $group      = $user_group->get_user_groups();
+        $cm = new ContentManager();
+        $user = new User();
 
-            $groupsOptions = array();
-            $groupsOptions[] = _('-- Select a group --');
-            foreach($group as $cat) {
-                $groupsOptions[$cat->id] = $cat->name;
-            }
+        $filters = (isset($_REQUEST['filter']))? $_REQUEST['filter']: null;
+        $users = $user->get_users($filters, ' ORDER BY login ');
 
-            $tpl->assign( array(
-                'users' => $users,
-                'paginacion' => $cm->pager,
-                'user_groups' => $group,
-                'groupsOptions' => $groupsOptions,
-            ));
+        $users = $cm->paginate_num($users, ITEMS_PAGE);
 
-            $tpl->display('acl/user/list.tpl');
+        $user_group = new User_group();
+        $group      = $user_group->get_user_groups();
 
-        } break;
+        $groupsOptions = array();
+        $groupsOptions[] = _('-- Select a group --');
+        foreach($group as $cat) {
+            $groupsOptions[$cat->id] = $cat->name;
+        }
 
-        case 'new': {
-            $user = new User( $_REQUEST['id'] );
-            $user_group = new User_group();
-            $tpl->assign('user', $user);
-            $tpl->assign('user_groups', $user_group->get_user_groups());
+        $tpl->assign( array(
+            'users' => $users,
+            'paginacion' => $cm->pager,
+            'user_groups' => $group,
+            'groupsOptions' => $groupsOptions,
+        ));
 
-            $tree = $ccm->getCategoriesTree();
-            $tpl->assign('content_categories', $tree);
+        $tpl->display('acl/user/list.tpl');
 
-            $tpl->display('acl/user/new.tpl');
-        } break;
+    break;
 
-        case 'read': {
-            $user = new User( $_REQUEST['id'] );
+    case 'new':  
+        Acl::checkOrForward('USER_CREATE');
+        
+        $user = new User( $_REQUEST['id'] );
+        $user_group = new User_group();
+        $tpl->assign('user', $user);
+        $tpl->assign('user_groups', $user_group->get_user_groups());
 
-            $user_group = new User_group();
-            $tpl->assign('user', $user);
-            $tpl->assign('user_groups', $user_group->get_user_groups());
+        $tree = $ccm->getCategoriesTree();
+        $tpl->assign('content_categories', $tree);
 
-            $tree = $ccm->getCategoriesTree();
-            $tpl->assign('content_categories', $tree);
-            $tpl->assign('content_categories_select', $user->get_access_categories_id());
+        $tpl->display('acl/user/new.tpl');
+    break;
 
-            /*$tpl->assign('categories_options',  $user->get_categories_options());
-            $tpl->assign('categories_selected', $user->get_access_categories_id());*/
+    case 'read': {
+        //user can modify his data
+        if ($_REQUEST['id'] != $_SESSION['userid']) {                
+            Acl::checkOrForward('USER_UPDATE');
+        }
+        $user = new User( $_REQUEST['id'] );
 
-            $tpl->display('acl/user/new.tpl');
-        } break;
+        $user_group = new User_group();
+        $tpl->assign('user', $user);
+        $tpl->assign('user_groups', $user_group->get_user_groups());
 
-        case 'update': {
-            // TODO: validar datos
-            $user = new User();
-            $user->update( $_REQUEST );
+        $tree = $ccm->getCategoriesTree();
+        $tpl->assign('content_categories', $tree);
+        $tpl->assign('content_categories_select', $user->get_access_categories_id());
+
+
+        $tpl->display('acl/user/new.tpl');
+    } break;
+
+    case 'update': 
+        
+        if ($_REQUEST['id'] != $_SESSION['userid']) {                
+            Acl::checkOrForward('USER_UPDATE');
+        }             
+        // TODO: validar datos
+        $user = new User();
+        $user->update( $_REQUEST );
+
+        if ( ($_REQUEST['id'] == $_SESSION['userid']) && !Acl::check('USER_UPDATE') ) {  
+            Application::forward('/admin/');
+        } else {
             Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-        } break;
+        }
+    break;
 
-        case 'create': {
+    case 'create': 
+        Acl::checkOrForward('USER_CREATE');
+        
+        $user = new User();
+        if($user->create( $_POST )) {
+            Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
+        } else {
+            $tpl->assign('errors', $user->errors);
+        }
+        $tpl->display('acl/user/new.tpl');
+    break;
+
+    case 'validate': 
+         
+        if(empty($_POST["id"])) {
+            Acl::checkOrForward('USER_CREATE');
+            
             $user = new User();
-
-            if($user->create( $_POST )) {
-                Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-            } else {
+            if(!$user->create( $_POST )) {
                 $tpl->assign('errors', $user->errors);
             }
-            $tpl->display('acl/user/new.tpl');
-        } break;
+           
+        } else {
+            
+            if ($_POST['id'] != $_SESSION['userid']) {                
+                Acl::checkOrForward('USER_UPDATE');
+            }
 
-        case 'delete': {
             $user = new User();
-            $user->delete( $_POST['id'] );
-            Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-        } break;
+            $user->update( $_REQUEST );
+             
+        }
 
-        case 'change_authorize': {
-            $user = new User( $_REQUEST['id'] );
-            //Autorizar o no , comprobar...
-            $status = ($_REQUEST['status']==1)? 1: 0; // Evitar otros valores
-            if($status == 1){
-                $user->unauthorize_user($user->id);
-            } else {
-                $user->authorize_user($user->id);
-            }
-            Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-        } break;
+        Application::forward($_SERVER['SCRIPT_NAME'].'?action=read&id='.$user->id);
+    break;
+    
+    case 'change_authorize': 
+        
+        $user = new User( $_REQUEST['id'] );
+        //Autorizar o no , comprobar...
+        $status = ($_REQUEST['status']==1)? 1: 0; // Evitar otros valores
+        if($status == 1){
+            $user->unauthorize_user($user->id);
+        } else {
+            $user->authorize_user($user->id);
+        }
+        Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
+    break;    
+    
+    case 'delete': 
+        Acl::checkOrForward('USER_DELETE');
+        
+        $user = new User();
+        $user->delete( $_POST['id'] );
+        Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
+    break;
 
-        case 'validate': {
-            $user = null;
-            if(empty($_POST["id"])) {
-                $user = new User();
-                if(!$user->create( $_POST )) {
-                    $tpl->assign('errors', $user->errors);
-                }
-            } else {
-                $user = new User($_POST["id"]);
-                $user->update( $_REQUEST );
-            }
-            Application::forward($_SERVER['SCRIPT_NAME'].'?action=read&id='.$user->id);
-        } break;
-
-        case 'mdelete':
-            if(isset($_REQUEST['selected_fld']) && count($_REQUEST['selected_fld'])>0)
+    case 'mdelete':
+        Acl::checkOrForward('USER_DELETE');
+        
+        if(isset($_REQUEST['selected_fld']) && count($_REQUEST['selected_fld'])>0)
+        {
+            $fields = $_REQUEST['selected_fld'];
+            if(is_array($fields))
             {
-                $fields = $_REQUEST['selected_fld'];
-                if(is_array($fields))
+                $user = new User();
+                foreach($fields as $i )
                 {
-                    $user = new User();
-                    foreach($fields as $i )
-                    {
-                        $user->delete( $i );
-                    }
+                    $user->delete( $i );
                 }
             }
-            Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-            break;
+        }
+        Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
+        break;
 
-        default: {
-            Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
-        } break;
-    }
-
-} else {
-    $page = (isset($_REQUEST['page'])? $_REQUEST['page'] : 0);
-    Application::forward($_SERVER['SCRIPT_NAME']."?action=list&page={$page}");
+    default: {
+        Application::forward($_SERVER['SCRIPT_NAME'].'?action=list&page='.$page);
+    } break;
 }
+
