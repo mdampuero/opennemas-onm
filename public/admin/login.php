452642f43b098bd7af75990ffaee1b6dde18815d
Fran Dieguez <fran@openhost.es>
2012-01-14 02:04:08 +0100

    Fixed bug in login that redirected to frontend and not backend section

--- public/admin/login.php
+++ public/admin/login.php
@@ -62,7 +62,7 @@ switch ($action) {
                     Privileges_check::loadSessionExpireTime();
 
                     $forwardTo = filter_input(INPUT_POST, 'forward_to');
-                    if (!is_null($forwardTo)) {
+                    if (!is_null($forwardTo) && !empty($forwardTo)) {
                         Application::forward(SITE_URL.SS.$forwardTo);
                     } else {
                         Application::forward(SITE_URL_ADMIN);
