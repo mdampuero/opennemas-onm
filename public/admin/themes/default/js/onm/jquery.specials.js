9ee4caaeef705b95c68ea39e15f99f73436ce36b
Sandra Pereira <sandra@openhost.es>
2012-03-14 11:17:34 +0100

    Right formatting & spaces indent
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- public/admin/themes/default/js/onm/jquery.specials.js
+++ public/admin/themes/default/js/onm/jquery.specials.js
@@ -1,7 +1,8 @@
-
-
+/*
+* jquery functions
+* Get specials contents for save in database.
+*/
 saveSpecialContent = function() {
-  
     var itemsLeft = [];
     var itemsRight= [];
 
@@ -11,15 +12,17 @@ saveSpecialContent = function() {
     jQuery( "div#cates div#column_left ul.content-receiver li" ).each(function(){
         itemsLeft.push(jQuery(this).data("id"));
     });
-//test JSON.stringify
+    //test JSON.stringify
     jQuery('input#noticias_right').val(itemsRight.join(','));
     jQuery('input#noticias_left').val(itemsLeft.join(','));
 
 }
 
-
+/*
+ * Toggle divs with specials content.
+ */
 setOnlyPdf = function(my){
-   jQuery(my).click(function(){
-       jQuery('div.special-container').toggle();
+    jQuery(my).click(function(){
+        jQuery('div.special-container').toggle();
 	});
 }
