bd235f37824bec69dd1180ce6dfb1e3dba00bfab
Fran Dieguez <fran@openhost.es>
2012-04-23 14:56:47 +0200

    Added images to posts in frontpage opinion
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

--- public/controllers/opinion_index.php
+++ public/controllers/opinion_index.php
@@ -8,7 +8,7 @@ require_once('../bootstrap.php');
 /**
  * Redirect Mobile browsers to mobile site unless a cookie exists.
 */
-$app->mobileRouter();
+// $app->mobileRouter();
 
 /**
  * Setup view
@@ -22,10 +22,10 @@ $tpl->setConfig('opinion');
 
 $category_name = $request->query->filter('category_name', 'opinion', FILTER_SANITIZE_STRING);
 
-$action = $request->query->filter('action', 'list' , FILTER_SANITIZE_STRING);
-$page = $request->query->filter('page', 1, FILTER_VALIDATE_INT);
+$action        = $request->query->filter('action', 'list' , FILTER_SANITIZE_STRING);
+$page          = $request->query->filter('page', 1, FILTER_VALIDATE_INT);
 
-$authorID = (int)$request->query->filter('author_id', '' , FILTER_SANITIZE_STRING);
+$authorID      = (int)$request->query->filter('author_id', '' , FILTER_SANITIZE_STRING);
 
 /**
  * Redirect to home if category_name is not opinion
@@ -53,23 +53,27 @@ switch ($action) {
         if (!$tpl->isCached('opinion/opinion_index.tpl', $cacheID)) {
 
             $cm = new ContentManager();
+
             // Fetch last opinions from editorial
-            $editorial = $cm->find('Opinion',
-                                    'opinions.type_opinion=1 '.
-                                    'AND contents.available=1 '.
-                                    'AND contents.in_home=1 '.
-                                    'AND contents.content_status=1 ',
-                                    'ORDER BY position ASC, created DESC '.
-                                    'LIMIT 2');
+            $editorial = $cm->find(
+                'Opinion',
+                'opinions.type_opinion=1 '.
+                'AND contents.available=1 '.
+                'AND contents.in_home=1 '.
+                'AND contents.content_status=1 ',
+                'ORDER BY position ASC, created DESC '.
+                'LIMIT 2'
+            );
 
             // Fetch last opinions from director
-            $director = $cm->find('Opinion',
-                                    'opinions.type_opinion=2 '.
-                                    'AND contents.available=1 '.
-                                    'AND contents.in_home=1 '.
-                                    'AND contents.content_status=1 ',
-                                    'ORDER BY created DESC LIMIT 2');
-
+            $director = $cm->find(
+                'Opinion',
+                'opinions.type_opinion=2 '.
+                'AND contents.available=1 '.
+                'AND contents.in_home=1 '.
+                'AND contents.content_status=1 ',
+                'ORDER BY created DESC LIMIT 2'
+            );
 
             if (isset($director) && !empty($director)) {
                 // Fetch the photo images of the director
@@ -83,35 +87,35 @@ switch ($action) {
                 $tpl->assign('director',  $director[0]);
             }
 
+            $_limit ='LIMIT '.(($page-1)*ITEMS_PAGE).', '.(($page)*ITEMS_PAGE);
+            $url    ='opinion';
 
-            //define('ITEMS_PAGE', 2);
-
-            $_limit='LIMIT '.(($page-1)*ITEMS_PAGE).', '.(($page)*ITEMS_PAGE);
-            // var_dump($_limit);die();
-
-            $url='opinion';
-
-            $total_opinions = $cm->count('Opinion','in_home=1 and available=1 and type_opinion=0',
-                                    'ORDER BY type_opinion DESC, position ASC, created DESC ');
+            $total_opinions = $cm->count(
+                'Opinion','in_home=1 and available=1 and type_opinion=0',
+                'ORDER BY type_opinion DESC, position ASC, created DESC '
+            );
 
             // Fetch last opinions of contributors and paginate them by ITEM_PAGE
-            $opinions = $cm->find('Opinion', 'in_home=1 and available=1 and type_opinion=0',
-                                    'ORDER BY type_opinion DESC, position ASC, created DESC '.$_limit);
+            $opinions = $cm->find(
+                'Opinion',
+                'in_home=1 and available=1 and type_opinion=0',
+                'ORDER BY type_opinion DESC, position ASC, created DESC '.$_limit
+            );
 
-            $improvedOpinions = array();
-            foreach($opinions as $opinion) {
-                $opinion->author = new Author($opinion->fk_author);
-                $opinion->name = $opinion->author->name;
+            foreach ($opinions as &$opinion) {
+                $opinion->author           = new Author($opinion->fk_author);
+                $opinion->author->photo    = $opinion->author->get_photo($opinion->fk_author_img);
+                $opinion->name             = $opinion->author->name;
                 $opinion->author_name_slug = StringUtils::get_title($opinion->name);
-                $improvedOpinions[] = $opinion;
             }
 
             $pagination = $cm->create_paginate($total_opinions, ITEMS_PAGE, 2, 'URL', $url);
 
             $tpl->assign('editorial', $editorial);
-            $tpl->assign('opinions',  $improvedOpinions);
+            $tpl->assign('opinions',  $opinions);
             $tpl->assign('pagination',  $pagination);
             $tpl->assign('page', $page);
+
 
         }
 
@@ -120,7 +124,7 @@ switch ($action) {
     break;
 
     case 'list_op_author':  // Author frontpage
- 
+
         // Don't execute the app logic if there are caches available
         if (!$tpl->isCached('opinion/frontpage_author.tpl', $cacheID)) {
 
@@ -168,9 +172,8 @@ switch ($action) {
                     }
                 }
 
+                $tpl->assign('author_name', $aut->name);
             }
-
-
 
             // If there aren't opinions just redirect to homepage opinion
             if(empty($total_opinions)){ Application::forward301('/seccion/opinion/'); }
@@ -183,7 +186,6 @@ switch ($action) {
 
             $pagination = $cm->create_paginate($total_opinions, ITEMS_PAGE, 2, 'URL', $url);
 
-            $tpl->assign('author_name', $aut->name);
             $tpl->assign('pagination_list', $pagination);
             $tpl->assign('opinions', $opinions);
             $tpl->assign('author_id', $authorID);
