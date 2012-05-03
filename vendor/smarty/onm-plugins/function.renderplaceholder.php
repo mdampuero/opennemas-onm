b04768c3ebdbb36b161fb53979c74dd85cd2215b
Alex Rodrigues <alex@openhost.es>
2012-05-03 20:41:44 +0200

    Fixed renderplaceholder smarty plugin
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- vendor/smarty/onm-plugins/function.renderplaceholder.php
+++ vendor/smarty/onm-plugins/function.renderplaceholder.php
@@ -2,55 +2,36 @@
 /* {renderplaceholder items=$articles tpl='xxx.tpl' placeholder="placeholder_0_0"} */
 function smarty_function_renderplaceholder($params, &$smarty) {
 
-    $outputHTML = '';
+    $outputHTML    = '';
 
     // get all the parameters passed to the function
-    $items   = $params['items'];
-    $tpl     = $params['tpl'];
-    $placeholder  = $params['placeholder'];
-    $cssclass = $params['cssclass'];
+    $items         = $params['items'];
+    $tpl           = $params['tpl'];
+    $placeholder   = $params['placeholder'];
+    $cssclass      = $params['cssclass'];
     $category_name = $smarty->getTemplateVars('category_name');
-    $placeholder_property = ($category_name=='home') ? 'home_placeholder': 'placeholder';
-    $varname = (!isset($params['varname']))? 'item': $params['varname'];
+    $varname       = (!isset($params['varname']))? 'item': $params['varname'];
 
     // Doing some checks if this method was called properly
-    if(!isset($items)) { throw new Exception('RenderPlaceHolder: you must specify the items param'); }
+    if (!isset($items)) {
+        throw new Exception('RenderPlaceHolder: you must specify the items param');
+    }
 
     // Iterate over all the items and try to get its html representation
-    $caching = $smarty->caching;
+    $caching         = $smarty->caching;
     $smarty->caching = 0;
-    if(isset($items) && count($items>0)){
-        $iteration = 0;
-        foreach($items as $i => $item) {
+    if (isset($items) && count($items>0)) {
+        foreach ($items as $i => $item) {
 
-            if( $item->{$placeholder_property} == $placeholder && ($item->available == 1) ) {
-
-                if(method_exists($item, 'render')){
-
-                    $outputHTML .= $item->render($params);
-
-                } else {
-
-                    $smarty->clearAssign($varname);
-                    $smarty->assign($varname, $items[$i]);
-                    $smarty->clearAssign('cssclass');
-                    if($iteration == 0) {
-                        $smarty->assign('cssclass', $cssclass);
-                    }
-                    $outputHTML .= "\n". $smarty->fetch( $tpl, md5(serialize($item)) );
-
-                }
-
-                $iteration++;
-
+            if ($item->placeholder == $placeholder && ($item->available == 1)) {
+                $outputHTML .= $item->render($params, $smarty);
             }
 
         }
-        unset($iteration);
     }
 
     $smarty->caching = $caching;
 
     // return all the html collected
-    return( $outputHTML );
+    return $outputHTML;
 }
