9260da3f6fd3aaccc73f8c86f472e86b76dd9f0d
Alex Rodrigues <alex@openhost.es>
2012-05-03 20:31:37 +0200

    Added onm-smarty plugins on vendor folder
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

---
+++ vendor/smarty/onm-plugins/function.renderTypeRelated.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * -------------------------------------------------------------
+ * File:        function.typecontent.php
+ * Comprueba el tipo y escribe el class adecuado
+ *
+ * Ejemplo:
+ *   <li class="res-file"><a href="#">El viaje al sol, un sueño olvidado (PDF)</a></li>
+ *   <li class="res-image"><a href="#">Fototeca: El viaje al sol, un sueño olvidado</a></li>
+ *   <li class="res-link"><a href="#">Los grandes pelígros de la humanidad</a></li>
+ *   <li class="res-video"><a href="#">Descubre el sistema planetario en este vídeo</a></li>
+ *
+ */
+function smarty_function_renderTypeRelated($params,&$smarty)
+{
+    $content = $params['content'];
+
+    switch ($content->fk_content_type){
+    case 1:
+        $class='class="res-article" ';
+        $content->content_type = 'article';
+
+        break;
+    case 3: //    /.+\.(jpeg|jpg|gif)/
+        if ((preg_match("/.+\.jpeg|jpg|gif/", $ext))) {
+             $class='class="res-image" ';
+        } elseif ((preg_match("/.+\.doc/", $ext))) {
+            $class='class="res-file" ';
+        } elseif ((preg_match("/.+\.pdf/", $ext))) {
+             $class='class="res-file" ';
+        } else {
+            $class='class="res-file" ';
+        }
+
+        break;
+    case 4://Opinion
+         $class='class="res-opinion" ';
+         $content->content_type = 'opinion';
+         $opinion = new Opinion($content->id);
+         $content->uri =   $opinion->uri;
+
+        break;
+    case 7:
+        $class='class="res-image" ';
+        $content->content_type = 'album';
+
+        break;
+    case 8:
+         $class='class="res-image" ';
+         $content->content_type = 'photo';
+
+        break;
+    case 9:
+         $class='class="res-video" ';
+         $content->content_type = 'video';
+        break;
+    default:
+         $class='class="res-link" ';
+        break;
+    }
+
+    $patterns = array('/"/', '/\'/', '/“/');
+    $replace = array('', '','');
+
+    $title_cleaned = preg_replace($patterns, $replace, $content->title);
+    $pathFile = ContentManager::getFilePathFromId($content->id);
+
+    if ($content->content_type == 3 || $content->fk_content_type == 3) {
+        $content->uri = INSTANCE_MEDIA.FILE_DIR . $pathFile;
+        $html=' <a title="Relacionado: '.$title_cleaned.'" href="'.$content->uri .'"';
+    } else {
+        $html=' <a title="Relacionado: '.$title_cleaned.'" href="'. SITE_URL . $content->uri .'"';
+    }
+
+    if ($content->fk_content_type==3) {
+        $html.='target="_blank"';
+    }
+    $html.='><span '.$class.'>&nbsp;</span>'.clearslash($content->title).'</a>';
+
+
+    return $html;
+
+}
