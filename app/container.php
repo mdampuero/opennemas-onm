4c9480970e9cd5dd03b07ed87679259196946eb9
Fran Dieguez <fran@openhost.es>
2012-06-13 15:52:08 +0200

    Make container to load global app configuration file config/app.yml
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>
    
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

--- app/container.php
+++ app/container.php
@@ -7,10 +7,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  **/
-use Symfony\Component\DependencyInjection;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
-$sc = new DependencyInjection\ContainerBuilder();
+$sc = new ContainerBuilder();
+
+// Load parameters from an configuration file
+$loader = new YamlFileLoader($sc, new FileLocator(__DIR__.'/config/'));
+$loader->load('app.yml');
 
 $sc->register('context', 'Symfony\Component\Routing\RequestContext')
     ->addMethodCall('fromRequest', array($request));
@@ -18,6 +24,5 @@ $sc->register('matcher', 'Symfony\Component\Routing\Matcher\UrlMatcher')
     ->setArguments(array($routes, new Reference('context')));
 $sc->register('url_generator', '\Symfony\Component\Routing\Generator\UrlGenerator')
     ->setArguments(array($routes, new Reference('context')));
-$sc->setParameter('dispatcher.exceptionhandler', 'Framework:Controllers:ErrorController:default');
 
 return $sc;
