d29c893b41c9d3d817b332f56c7bbdad5fd15583
Alex Rodrigues <alex@openhost.es>
2012-06-14 11:25:31 +0200

    Fixed code style on redirect-from-editmaker controller
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- public/controllers/redirect-from-editmaker.php
+++ public/controllers/redirect-from-editmaker.php
@@ -10,8 +10,12 @@
 require_once '../bootstrap.php';
 
 // Fetch HTTP variables
-$contentType = $request->query->filter('content_type', null, FILTER_SANITIZE_STRING);
-$contentId   = $request->query->filter('content_id', null, FILTER_SANITIZE_STRING);
+$contentType = $request->query->filter(
+    'content_type', null, FILTER_SANITIZE_STRING
+);
+$contentId   = $request->query->filter(
+    'content_id', null, FILTER_SANITIZE_STRING
+);
 
 $url = SITE_URL;
 
@@ -19,49 +23,51 @@ $url = SITE_URL;
 if (!is_null($contentId)) {
 
     switch ($contentType) {
-    case 'article':
-        list($type,$newContentID) = getOriginalIdAndContentTypeFromID($contentId);
+        case 'article':
+            list($type,$newContentID)
+                = getOriginalIdAndContentTypeFromID($contentId);
 
-        $finalId = Content::resolveID($newContentID);
+            $finalId = Content::resolveID($newContentID);
 
-        if ($type == 'article') {
-            $article = new Article($finalId);
-            $article->category_name = $article->catName;
+            if ($type == 'article') {
+                $article = new Article($finalId);
+                $article->category_name = $article->catName;
 
-            $url .=  Uri::generate(
-                'article',
-                array(
-                    'id'       => $article->id,
-                    'date'     => date('YmdHis', strtotime($article->created)),
-                    'category' => $article->category_name,
-                    'slug'     => $article->slug,
-                )
-            );
-        } elseif ($type == 'opinion') {
+                $url .=  Uri::generate(
+                    'article',
+                    array(
+                        'id'       => $article->id,
+                        'date'     => date('YmdHis', strtotime($article->created)),
+                        'category' => $article->category_name,
+                        'slug'     => $article->slug,
+                    )
+                );
+            } elseif ($type == 'opinion') {
 
-            $opinion = new Opinion($finalId);
-            $url .=  Uri::generate(
-                'opinion',
-                array(
-                    'id'       => $opinion->id,
-                    'date'     => date('YmdHis', strtotime($opinion->created)),
-                    'category' => StringUtils::get_title($opinion->name),
-                    'slug'     => $opinion->slug,
-                )
-            );
-        }
-        break;
+                $opinion = new Opinion($finalId);
+                $url .=  Uri::generate(
+                    'opinion',
+                    array(
+                        'id'       => $opinion->id,
+                        'date'     => date('YmdHis', strtotime($opinion->created)),
+                        'category' => StringUtils::get_title($opinion->name),
+                        'slug'     => $opinion->slug,
+                    )
+                );
+            }
+            break;
 
-    case 'category':
-        $newContentID = getOriginalIDForContentTypeAndID($contentType, $contentId);
+        case 'category':
+            $newContentID
+                = getOriginalIDForContentTypeAndID($contentType, $contentId);
 
-        $cc = new ContentCategory($newContentID);
+            $cc = new ContentCategory($newContentID);
 
-        $url .= Uri::generate('section', array('id' => $cc->name));
-        break;
+            $url .= Uri::generate('section', array('id' => $cc->name));
+            break;
 
-    default:
-        break;
+        default:
+            break;
     }
 }
 
