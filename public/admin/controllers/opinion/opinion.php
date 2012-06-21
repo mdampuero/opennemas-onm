ea6bc82260f9b41aaeda04d3453f4081c7d00515
Alex Rodrigues <alex@openhost.es>
2012-06-21 17:39:21 +0200

    Order frontpage opinions and backend list by created.
    Signed-off-by: Alex Rodrigues <alex@openhost.es>

--- public/admin/controllers/opinion/opinion.php
+++ public/admin/controllers/opinion/opinion.php
@@ -99,7 +99,7 @@ switch ($action) {
             }
         } else {
             list($opinions, $pager)= $cm->find_pages('Opinion', 'in_home=1 and available=1 and type_opinion=0',
-                                     'ORDER BY position ASC, created DESC ', $page, ITEMS_PAGE);
+                                     'ORDER BY created DESC ', $page, ITEMS_PAGE);
             $tpl->assign('paginacion', $pager->links);
 
             // $opinions = $cm->find('Opinion', 'in_home=1 and available=1 and type_opinion=0',
