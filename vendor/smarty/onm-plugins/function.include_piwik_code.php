ba45e708aa81777965d2c6200b0ef986ff84c9e9
Alex Rodrigues <alex@openhost.es>
2012-07-03 13:20:45 +0200

    Allow to specify in include_piwik_code smarty function to only show the image
    Signed-off-by: Alex Rico <alex@openhost.es>

--- vendor/smarty/onm-plugins/function.include_piwik_code.php
+++ vendor/smarty/onm-plugins/function.include_piwik_code.php
@@ -1,14 +1,14 @@
 <?php
-/*
- * -------------------------------------------------------------
- * File:     	function.humandate.php
- */
 use \Onm\Settings as s;
 
 function smarty_function_include_piwik_code($params, &$smarty) {
 
     $output = "";
 
+    // Fetch parameters
+    $onlyImage = (isset($params['onlyimage']) ? $params['onlyimage'] : null);
+
+    // Get piwik config
     $piwikConfig = s::get('piwik');
 
     // Only return anything if the G Analytics is setted in the configuration
@@ -21,21 +21,28 @@ function smarty_function_include_piwik_code($params, &$smarty) {
 
         $httpsHost = preg_replace("@http:@", "https:", $piwikConfig['server_url']);
 
-        $output = '<!-- Piwik -->
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "'.$httpsHost.'" : "'.  $piwikConfig['server_url'] .'");
-document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", '. $piwikConfig['page_id'] .');
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="'. $piwikConfig['server_url'] .'piwik.php?idsite='. $piwikConfig['page_id'] .'" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tracking Code -->';
+        if (!is_null($onlyImage)) {
+            $output = '<img src="'.$piwikConfig['server_url'].
+                      'piwik.php?idsite='.$piwikConfig['page_id'].
+                      '&amp;rec=1&amp;action_name=Newsletter'.
+                      '&amp;urlref='.SITE_URL.'newsletter/'.date("Ymd").
+                      '" style="border:0" alt="" />';
+        } else {
 
+            $output = '<!-- Piwik -->
+            <script type="text/javascript">
+            var pkBaseURL = (("https:" == document.location.protocol) ? "'.$httpsHost.'" : "'.  $piwikConfig['server_url'] .'");
+            document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+            </script><script type="text/javascript">
+            try {
+            var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", '. $piwikConfig['page_id'] .');
+            piwikTracker.trackPageView();
+            piwikTracker.enableLinkTracking();
+            } catch( err ) {}
+            </script><noscript><p><img src="'. $piwikConfig['server_url'] .'piwik.php?idsite='. $piwikConfig['page_id'] .'" style="border:0" alt="" /></p></noscript>
+            <!-- End Piwik Tracking Code -->';
+        }
     }
-
     return $output;
 
 }
