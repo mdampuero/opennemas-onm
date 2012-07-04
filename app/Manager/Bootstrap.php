c6ef18a4382bc984ec2cd1e6f78f45f27dd1e16b
Fran Dieguez <fran@openhost.es>
2012-07-04 14:26:49 +0200

    Add Manager class-based module schema
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

---
+++ app/Manager/Bootstrap.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c)  OpenHost S.L. <developers@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+namespace Manager;
+
+use Onm\Framework\Module\ModuleBootstrap;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Initializes the Backend Module
+ *
+ * @package default
+ * @author
+ **/
+class Bootstrap extends ModuleBootstrap
+{
+    /**
+     * Initialed the custom error handler
+     *
+     * @return void
+     **/
+    public function initErrorHandler()
+    {
+
+        return $this;
+    }
+
+    /**
+     * Starts the authentication system for the backend
+     *
+     * @return Boostrap the boostrap instance
+     **/
+    public function initAuthenticationSystem()
+    {
+        // $session = $this->container->get('session');
+        // $session->start();
+
+        // $request = $this->container->get('request');
+
+        // $isAsset = preg_match('@.*\.(png|gif|jpg|ico|css|js)$@', $request->getPathInfo());
+        // if ($isAsset != 1) {
+
+        //     $GLOBALS['Session'] = \SessionManager::getInstance(OPENNEMAS_BACKEND_SESSIONS);
+        //     $GLOBALS['Session']->bootstrap();
+
+
+        //     if (!isset($_SESSION['userid'])
+        //         && !preg_match('@^/login@', $request->getPathInfo())
+        //     ) {
+        //         $url = $request->getPathInfo();
+
+        //         if (!empty($url)) {
+        //             $redirectTo = urlencode($request->getUri());
+        //         }
+        //         $location = $request->getBaseUrl() .'/login/?forward_to='.$redirectTo;
+
+        //         $response = new RedirectResponse($location, 301);
+        //         $response->send();
+        //         exit(0);
+        //     }
+        // } else {
+        //     // Log this error event to the webserver logging sysmte
+        //     error_log("File does not exist: ".$request->getPathInfo(), 0);
+
+        //     $response = new Response('Content not available', 404);
+        //     $response->send();
+        //     exit();
+        // }
+
+    }
+
+} // END class Bootstrap
---
+++ app/Manager/routes.php
@@ -0,0 +1,21 @@
+<?php
+use Symfony\Component\Routing\Route;
+
+/**
+ * This file is part of the Onm package.
+ *
+ * (c)  OpenHost S.L. <developers@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+$routes->add(
+    'manager_instances',
+    new Route(
+        '/instances/',
+        array(
+            '_controller' => 'Manager:Controllers:InstancesController:list',
+        )
+    ),
+    '/manager'
+);
