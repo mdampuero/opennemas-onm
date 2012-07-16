d7305dca65fa2b49e51b0508a00def6f732f11b4
Sandra Pereira <sandra@openhost.es>
2012-07-16 10:38:52 +0200

    fix bug in count pagination in opinion frontpage
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- public/controllers/opinion_index.php
+++ public/controllers/opinion_index.php
@@ -96,7 +96,7 @@ switch ($action) {
             // Added ITEMS_PAGE for count first page
             $total_opinions =  ITEMS_PAGE + $cm->count(
                 'Opinion',
-                'in_home=1 and available=1 and type_opinion=0',
+                'in_home=0 and available=1 and type_opinion=0',
                 'ORDER BY type_opinion DESC, created DESC '
             );
 
