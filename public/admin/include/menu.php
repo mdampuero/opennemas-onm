2d9180982440e0a78c982800560b544a1e408145
Fran Dieguez <fran@openhost.es>
2012-08-20 02:19:12 +0200

    Change opinions link to opinion listing in backend menu
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- public/admin/include/menu.php
+++ public/admin/include/menu.php
@@ -53,7 +53,7 @@ $menuXml = '<?xml version="1.0"?>
             privilege="OPINION_ADMIN"
             title="'.htmlspecialchars(_("Opinions"), ENT_QUOTES).'"
             id="opinion_manager"
-            link="'.url('admin_opinions_frontpage').'">
+            link="'.url('admin_opinions').'">
             <node
                 module_name="OPINION_MANAGER"
                 privilege="OPINION_ADMIN"
