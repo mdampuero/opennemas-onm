584d21e37d73779440a0bb0158382cbd2391bf36
Sandra Pereira <sandra@openhost.es>
2012-10-02 15:13:40 +0200

    get data for section frontpage customizations
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- public/controllers/index.php
+++ public/controllers/index.php
@@ -64,10 +64,12 @@ if (
     $actualCategoryId = $actual_category_id = $ccm->get_id($actualCategory);
     $tpl->assign(
         array(
-            'actual_category_id' => $actualCategoryId,
+            'actual_category_id'    => $actualCategoryId,
             'actual_category_title' => $ccm->get_title($category_name),
+            'category_data'         => $ccm->categories[$actualCategoryId],
         )
     );
+
 
     $cm = new ContentManager;
 
