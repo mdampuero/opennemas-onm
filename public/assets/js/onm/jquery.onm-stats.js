245bb50bb1336a388bd1b850dace5cbbac9ac514
Fran Dieguez <fran@openhost.es>
2012-10-29 14:33:50 +0100

    Added jquery plugin and controllr for converting setnumviews to AJAX request
    Signed-off-by: Fran Dieguez <fran@openhost.es>

---
+++ public/assets/js/onm/jquery.onm-stats.js
@@ -0,0 +1,35 @@
+/******************************************
+ * Opennemas
+ *
+ * Handles the
+ *
+ * @author          Websanova
+ * @copyright       Copyright (c) 2012 Websanova.
+ * @license         This websanova jQuery boilerplate is dual licensed under the MIT and GPL licenses.
+ * @link            http://www.websanova.com
+ * @github          http://github.com/websanova/boilerplate
+ * @version         1.2.3
+ *
+ ******************************************/
+;(function($){
+    $.extend({
+        onmStats: function( options ) {
+
+            var defaultOptions = {
+                controller: "/controllers/stats.php"
+            };
+
+            var settings = $.extend({}, defaultOptions, options);
+
+            if ("content_id" in settings) {
+                $.ajax({
+                    url: settings.controller,
+                    method: 'GET',
+                    data: {
+                        content_id: settings.content_id,
+                    }
+                });
+            };
+        }
+    });
+})(jQuery);
