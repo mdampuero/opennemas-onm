f96ef9447df51a4fd502fbbf5bb4122194452b54
Fran Dieguez <fran@openhost.es>
2012-11-12 16:26:08 +0100

    Improve error differentiation in error controller of Frontend
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- app/Frontend/Controllers/ErrorController.php
+++ app/Frontend/Controllers/ErrorController.php
@@ -44,26 +44,77 @@ class ErrorController extends Controller
         $category_name = $request->query->filter('category_name', '', FILTER_SANITIZE_STRING);
         $cache_page    = $request->query->filter('page', 0, FILTER_VALIDATE_INT);
 
-        // Look how to get advertisements without this require.
-        require_once 'controllers/statics_advertisement.php';
-        if ($errorCode =='404' || empty($errorCode)) {
-            $code = 404;
-            $content = $this->view->fetch('static_pages/404.tpl');
-        } else {
-            $code = 500;
-            $page = new \stdClass();
+        $error = unserialize($request->get('error'));
 
-            // Dummy content while testing this feature
-            $page->title   = 'No hemos podido encontrar la página que buscas.';
-            $page->content = 'Whoups!';
-
-            $content = $this->renderView('static_pages/statics.tpl', array(
-                'category_real_name' => $page->title,
-                'page'               => $page,
-            ));
+        if ($this->container->hasParameter('environment')) {
+            $environment = $this->container->getParameter('environment');
         }
 
-        return new Response($content, $code);
+        $name = join('', array_slice(explode('\\', get_class($error)), -1));
+
+        $errorID = strtoupper(INSTANCE_UNIQUE_NAME.'_'.uniqid());
+
+
+        switch ($name) {
+            case 'ResourceNotFoundException':
+                $trace = $error->getTrace();
+                $path = $trace[0]['args'][0];
+
+
+                $page = new \stdClass();
+
+                // Dummy content while testing this feature
+                $page->title   = 'No hemos podido encontrar la página que buscas.';
+                $page->content = 'Whoups!';
+
+                $errorMessage = sprintf('Oups! We can\'t find anything at "%s".', $path);
+                error_log('File not found: '.$path.'ERROR_ID: '.$errorID);
+                if ($this->request->isXmlHttpRequest()) {
+                    $content = $errorMesage;
+                } else {
+                    $content = $this->renderView(
+                        'static_pages/404.tpl',
+                        array(
+                            'category_real_name' => $page->title,
+                            'page'               => $page,
+                        )
+                    );
+                }
+
+                return new Response($content, 404);
+                break;
+            default:
+                // Change this handle to a more generic error template
+                $errorMessage = _('Oups! Seems that we had an unknown problem while trying to run your request.');
+                error_log('Unknown error. ERROR_ID: '.$errorID);
+
+                if ($environment == 'development') {
+                    $errorMessage = $error->getMessage();
+                }
+
+                $page = new \stdClass();
+
+                // Dummy content while testing this feature
+                $page->title   = $errorMessage;
+                $page->content = 'Whoups!';
+
+                $content = $this->renderView(
+                    'static_pages/statics.tpl',
+                    array(
+                        'category_real_name' => $page->title,
+                        'page'               => $page,
+                        'error_message' => $errorMessage,
+                        'error'         => $error,
+                        'error_id'      => $errorID,
+                        'environment'   => $environment,
+                        'backtrace'     => array_reverse($error->getTrace()),
+                    )
+                );
+
+                return new Response($content, 500);
+
+                break;
+        }
     }
 }
 
