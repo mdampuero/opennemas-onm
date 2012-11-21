025a188e3192669ea01c2af14af364989f411ab2
Toni Martinez <toni@openhost.es>
2012-11-21 01:44:01 +0100

    Migrate NewStand and NewStandPaypal to Symfony controller
    Signed-off-by: Toni Martinez <toni@openhost.es>

--- app/Frontend/Controllers/NewStandController.php
+++ app/Frontend/Controllers/NewStandController.php
@@ -30,16 +30,267 @@ class NewStandController extends Controller
     public function init()
     {
         $this->view = new \Template(TEMPLATE_USER);
+        $this->view->setConfig('kiosko');
+
+        $this->cm = new \ContentManager();
+        
+        // Esta variable no se utiliza?¿ Ni tp viene por .htaccess
+        // $subcategory_name = $this->request->query->filter('subcategory_name', '', FILTER_SANITIZE_STRING);
+        // solo se usa al cachear en show (tiene sentido?¿) Tp viene por .htaccess
+        // $page  = $this->request->query->getDigits('page', 1);
+        $this->category_name    = $this->request->query->filter('category_name', '', FILTER_SANITIZE_STRING);
+
+        $this->view->assign(array( 'actual_category' => $this->category_name, ));
+
+        if (!defined('KIOSKO_DIR')) {
+            define('KIOSKO_DIR', "kiosko".SS);
+        }
     }
 
     /**
-     * Description of the action
+     * Renders the newstand frontpage
      *
      * @return Response the response object
      **/
-    public function defaultAction(Request $request)
+    public function frontpageAction(Request $request)
+    {
+        /**
+         * Avoid to run the entire app logic if is available a cache for this page
+        */
+        $configurations = s::get('kiosko_settings');
+        $month = $request->query->getDigits('month', date('n'));
+        $year  = $request->query->getDigits('year', date('Y'));
+
+        $order = $configurations['orderFrontpage'];
+
+        if ($order =='grouped') {
+
+            $cacheID = $this->view->generateCacheId('newsstand', $this->category_name, $year);
+            $kiosko =array();
+            if (($this->view->caching == 0)
+               || !$this->view->isCached('newsstand/newsstand.tpl', $cacheID)
+            ) {
+                $ccm = \ContentCategoryManager::get_instance();
+                $contentType = \Content::getIDContentType('kiosko');
+                $category = $ccm->get_id($this->category_name);
+                
+                list($allcategorys, $subcat, $categoryData) =
+                    $ccm->getArraysMenu($category, $contentType);
+
+                foreach ($allcategorys as $theCategory) {
+                    $portadas = $this->cm->find_by_category(
+                        'Kiosko',
+                        $theCategory->pk_content_category,
+                        ' `contents`.`available`=1   '.
+                        'AND YEAR(`kioskos`.date)='.$year.'',
+                        'ORDER BY `kioskos`.date DESC '
+                    );
+                    if (!empty($portadas)) {
+                        $kiosko[] = array (
+                            'category' => $theCategory->title,
+                            'portadas' => $portadas
+                        );
+                    }
+                }
+            }
+
+        } elseif ($order =='sections') {
+            $day        = $request->query->getDigits('day',1);
+            $cache_date = $year.$month.$day;
+            $cacheID = $this->view->generateCacheId('newsstand', $this->category_name, $cache_date);
+            $kiosko =array();
+            if (($this->view->caching == 0)
+               || !$this->view->isCached('newsstand/newsstand.tpl', $cacheID)
+            ) {
+                // $ccm = \ContentCategoryManager::get_instance();
+                // $category = $ccm->get_id($this->category_name);
+                // list($allcategorys, $subcat, $categoryData) =
+                //      $ccm->getArraysMenu($category, $contentType);
+
+                $date = "$year-$month-$day";
+                $portadas = $this->cm->findAll(
+                    'Kiosko',
+                    ' `contents`.`available`=1 AND  `kioskos`.date ="'.$date.'"',
+                    'ORDER BY `kioskos`.date DESC '
+                );
+
+                if (!empty($portadas)) {
+                    $kiosko[] = array (
+                        'portadas' => $portadas
+                    );
+                }
+
+            }
+
+        } else {
+
+            $cacheDate = $year.$month;
+            $cacheID   = $this->view->generateCacheId('newsstand', $this->category_name, $cacheDate);
+            $kiosko     = array();
+            if (($this->view->caching == 0)
+               || !$this->view->isCached('newsstand/newsstand.tpl', $cacheID)
+            ) {
+                $ccm = \ContentCategoryManager::get_instance();
+                $contentType = \Content::getIDContentType('kiosko');
+                $category = $ccm->get_id($this->category_name);
+                list($allcategorys, $subcat, $categoryData) =
+                    $ccm->getArraysMenu($category, $contentType);
+
+                foreach ($allcategorys as $theCategory) {
+                    $portadas = $this->cm->find_by_category(
+                        'Kiosko',
+                        $theCategory->pk_content_category,
+                        ' `contents`.`available`=1   '.
+                        'AND MONTH(`kioskos`.date)='.$month.' AND'.
+                        ' YEAR(`kioskos`.date)='.$year.'',
+                        'ORDER BY `kioskos`.date DESC '
+                    );
+                    if (!empty($portadas)) {
+                        $kiosko[] = array (
+                            'category' => $theCategory->title,
+                            'portadas' => $portadas
+                        );
+                    }
+                }
+
+            }
+        }
+
+        $this->view->assign(
+            array(
+                'KIOSKO_IMG_URL' => INSTANCE_MEDIA.KIOSKO_DIR,
+                'date'           => '1-'.$month.'-'.$year,
+                'MONTH'          => $month,
+                'YEAR'           => $year,
+                'kiosko'         => $kiosko
+            )
+        );
+
+        $this->widgetNewsstandDates();
+        $this->advertisements();
+
+        // Show in Frontpage
+        return $this->render(
+            'newsstand/newsstand.tpl',
+            array(
+                'cache_id' => $cacheID,
+            )
+        );
+
+    }
+
+    /**
+     * Render a particular cover
+     *
+     * @return Response the response object
+     **/
+    public function showAction(Request $request)
     {
 
+        $dirtyID = $request->query->getDigits('id', null);
+
+        $epaperId = \Content::resolveID($dirtyID);
+
+        /**
+         * Redirect to album frontpage if id_album wasn't provided
+         */
+        if (is_null($epaperId)) {
+            return new RedirectResponse($this->generateUrl('frontend_newspaper_frontpage'));
+        }
+
+        $cacheID = $this->view->generateCacheId('newsstand', null, $epaperId);
+
+        if (($this->view->caching == 0)
+            || (!$this->view->isCached('newsstand/newsstand.tpl', $cacheID))
+        ) {
+
+            $epaper = new \Kiosko($epaperId);
+
+            $format_date = strtotime($epaper->date);
+            $month       = date('m', $format_date);
+            $year        = date('Y', $format_date);
+
+            $portadas = $this->cm->find_by_category(
+                'Kiosko',
+                $epaper->category,
+                ' `contents`.`available`=1   ',
+                'ORDER BY `kioskos`.date DESC  LIMIT 4'
+            );
+            $kiosko =array();
+            if (!empty($portadas)) {
+                $kiosko[] = array (
+                    'category' => '',
+                    'portadas' => $portadas
+                );
+            }
+            $this->view->assign(
+                array(
+                    'KIOSKO_IMG_URL' => INSTANCE_MEDIA.KIOSKO_DIR,
+                    'date'           => '1-'.$month.'-'.$year,
+                    'MONTH'          => $month,
+                    'YEAR'           => $year,
+                    'epaper'         => $epaper,
+                    'kiosko'         => $kiosko
+                )
+            );
+        }
+
+        $this->widgetNewsstandDates();
+        $this->advertisements();
+
+        // Show in Frontpage
+        return $this->render(
+            'newsstand/newsstand.tpl',
+            array(
+                'cache_id' => $cacheID,
+            )
+        );
+
+    }
+
+    /**
+     * calculates the months of the covers existing
+     *
+     * @return 
+     **/
+    public function widgetNewsstandDates()
+    {
+        //for widget_newsstand_dates
+        //TODO: intelligent wigget
+        $ki = new \Kiosko();
+        $months_kiosko = $ki->get_months_by_years();
+        $this->view->assign('months_kiosko', $months_kiosko);
+    }
+
+    /**
+     * Fetches the advertisement
+     *
+     * @return
+     **/
+    private function advertisements()
+    {
+        $positions = array(1,2, 3,4, 5,6, 11,12,13,14,15,16, 21,22,24,25, 31,32,33,34,35,36,103,105, 9, 91, 92);
+        $intersticialId = 50;
+
+        // Asignacion de valores y comprobaciones realizadas en init
+        // $ccm = ContentCategoryManager::get_instance();
+        // $category = $ccm->get_id($category_name);
+        $category = (!isset($category) || ($category=='home'))? 0: $category;
+        $advertisement = \Advertisement::getInstance();
+
+        // Load 1-16 banners and use cache to performance
+        //$banners = $advertisement->getAdvertisements(range(1, 16), $category); // 4,9 unused
+        $banners = $advertisement->getAdvertisements( $positions, $category);
+
+        $banners = $this->cm->getInTime($banners);
+        //$advertisement->renderMultiple($banners, &$tpl);
+        $advertisement->renderMultiple($banners, $advertisement);
+
+        // Get intersticial banner
+        $intersticial = $advertisement->getIntersticial( $intersticialId, $category);
+        if (!empty($intersticial)) {
+            $advertisement->renderMultiple(array($intersticial), $advertisement);
+        }
     }
 
 } // END class NewStandController
