48b04e52238ef4597bc733644104e31fa7e17357
Toni Martinez <toni@openhost.es>
2012-11-27 10:39:48 +0100

    Modifying the smarty function include_google_analytics_code to retrieve stats throw a gif image

--- vendor/smarty/onm-plugins/function.include_google_analytics_code.php
+++ vendor/smarty/onm-plugins/function.include_google_analytics_code.php
@@ -9,6 +9,9 @@ function smarty_function_include_google_analytics_code($params, &$smarty) {
 
     $output = "";
 
+    // Fetch parameters
+    $onlyImage = (isset($params['onlyimage']) ? $params['onlyimage'] : null);
+
     $gAnalyticsConfigs = s::get('google_analytics');
 
     if (array_key_exists('api_key', $gAnalyticsConfigs)) {
@@ -19,24 +22,46 @@ function smarty_function_include_google_analytics_code($params, &$smarty) {
 
     // Only return anything if the Ganalytics is setted in the configuration
     if (is_array($gAnalyticsConfigs) && !empty($apiKey))  {
-        $output = "<script type=\"text/javascript\">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', '".$apiKey."']);";
 
-        // If base domain for ganalytics is set append it to the final output.
-        if (array_key_exists('base_domain', $gAnalyticsConfigs)
-            && !empty($gAnalyticsConfigs['base_domain']))
-        {
-            $output .= " _gaq.push(['_setDomainName', '". $gAnalyticsConfigs['base_domain'] ."']); ";
+        if (!is_null($onlyImage) && $onlyImage=="true" ) {
+
+            $utmGifLocation = "http://www.google-analytics.com/__utm.gif";
+
+            // Construct the gif hit url.
+            $utmUrl = $utmGifLocation."?".
+                "utmwv=4".
+                "&utmn=".rand(0, 0x7fffffff).
+                "&utmdt='Newsletter ".urlencode(SITE_URL)." [".date('%d/%m/%Y')."] ".
+                "&utmhn=".urlencode(SITE_URL).
+                "&utmr=".urlencode(SITE_URL.'newsletter/'.date("YmdHis")).
+                "&utmp=".urlencode('newsletter/'.date("YmdHis")) .
+                "&utmac=".$apiKey.
+                "&utmcc=__utma%3D999.999.999.999.999.1%3B";
+
+            $output = '<img src="'.$utmUrl.'" style="border:0" alt="" />';
+        } else {
+
+            $output = "<script type=\"text/javascript\">
+                var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', '".$apiKey."']);";
+
+            // If base domain for ganalytics is set append it to the final output.
+            if (array_key_exists('base_domain', $gAnalyticsConfigs)
+                && !empty($gAnalyticsConfigs['base_domain']))
+            {
+                $output .= " _gaq.push(['_setDomainName', '". $gAnalyticsConfigs['base_domain'] ."']); ";
+            }
+
+            $output .= "_gaq.push(['_trackPageview']);"
+                    ."(function() {\n"
+                    ."var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n"
+                    ."ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n"
+                    ."var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n"
+                    ."})();\n"
+                    ."</script>\n";
+
         }
-
-        $output .= "_gaq.push(['_trackPageview']);"
-                ."(function() {\n"
-                ."var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n"
-                ."ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n"
-                ."var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n"
-                ."})();\n"
-                ."</script>\n";
+        
     }
 
     return $output;
