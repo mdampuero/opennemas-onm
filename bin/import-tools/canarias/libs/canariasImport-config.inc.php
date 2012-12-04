384cf1aea2ad030cdcee2dcf7928625810df6cdd
Sandra Pereira <sandra@openhost.es>
2012-12-04 23:17:41 +0100

    Adapted import functions for other instances in canarias
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- bin/import-tools/canarias/libs/canariasImport-config.inc.php
+++ bin/import-tools/canarias/libs/canariasImport-config.inc.php
@@ -6,7 +6,10 @@
 
 $configOldDB = array(
                           'host' => 'localhost',
-                          'database' => 'ant-canariasAhora',
+                          //'database' => 'ant-canariasAhora',
+                          //'database' => 'ant-fuerteventura',
+                          'database' => 'ant-lanzarote',
+                          //'database' => 'ant-lapalma',
                           'user' => 'root',
                           'password' => 'root' ,
                           'type' => 'mysql' ,
@@ -14,7 +17,10 @@ $configOldDB = array(
 
 $configNewDB = array(
                           'host' => 'localhost',
-                          'database' => 'c-canariasaho',
+                          //'database' => 'c-canariasaho',
+                          //'database' => 'c-fuerteventu',
+                          'database' => 'c-lanzaroteah',
+                          //'database' => 'c-lapalmaahor',
                           'user' => 'root',
                           'password' => 'root' ,
                           'type' => 'mysql' ,
@@ -22,20 +28,25 @@ $configNewDB = array(
 
 
 //define internal name instance from manager
-define('INSTANCE_UNIQUE_NAME', 'canariasAhora');
-
+//define('INSTANCE_UNIQUE_NAME', 'canariasAhora');
+//define('INSTANCE_UNIQUE_NAME', 'fuerteventuraahora');
+define('INSTANCE_UNIQUE_NAME', 'lanzaroteahora');
+//define('INSTANCE_UNIQUE_NAME', 'lapalmaahora');
 //galimundo id.
 define('USER_ID', '135');
 
-define('OLD_MEDIA', '/var/www/canariasSw/canariasahora_20');
+//define('OLD_MEDIA', '/var/www/canariasSw/canariasahora_20');
+//define('OLD_MEDIA', '/var/www/canariasSw/fuerteventura');
+//define('OLD_MEDIA', '/var/www/canariasSw/lanzarote');
+define('OLD_MEDIA', '/var/www/canariasSw/lapalma');
 
 defined('MEDIA_PATH')
     || define('MEDIA_PATH', SITE_PATH. 'media');
 
 defined('IMG_DIR')
-    || define('IMG_DIR', 'canariasahora/images');
+    || define('IMG_DIR', 'INSTANCE_UNIQUE_NAME/images');
 
 
 defined('FILE_DIR')
-    || define('FILE_DIR', 'canariasahora/files');
+    || define('FILE_DIR', 'INSTANCE_UNIQUE_NAME/files');
 
