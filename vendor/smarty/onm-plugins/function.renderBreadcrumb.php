fda95da57cd40a2c7c93dd9c8561ceb58f18cea0
Alex Rodrigues <alex@openhost.es>
2013-03-04 12:24:07 +0100

    Allow smarty plugin breadcrumb to render sync categories
    Signed-off-by: Alex Rico <alex@openhost.es>

--- vendor/smarty/onm-plugins/function.renderBreadcrumb.php
+++ vendor/smarty/onm-plugins/function.renderBreadcrumb.php
@@ -6,7 +6,7 @@
  *
  **/
 
-function smarty_function_renderBreadcrumb($params,&$smarty)
+function smarty_function_renderBreadcrumb($params, &$smarty)
 {
     $output = '';
     if (!array_key_exists('item', $params)) {
@@ -20,6 +20,8 @@ function smarty_function_renderBreadcrumb($params,&$smarty)
     }
 
     $actualCategory = $params['item']->category;
+    $actualCategoryName = $params['item']->category_name;
+    $actualCategoryTitle = $params['item']->category_title;
 
     $ccm = \ContentCategoryManager::get_instance();
 
@@ -28,7 +30,11 @@ function smarty_function_renderBreadcrumb($params,&$smarty)
     $output .= '<a href="/seccion/' . $first->name . '" title="'
         . $first->title . '">' . $first->title . '</a>';
 
-    if (!empty($first->fk_content_category)) {
+    // If is a synchronized category
+    if (substr($_SERVER['REQUEST_URI'], 1, 3) == 'ext') {
+        $output = '<a href="/extseccion/' . $actualCategoryName . '" title="'
+            . $actualCategoryTitle . '">' . $actualCategoryTitle . '</a>';
+    } elseif (!empty($first->fk_content_category)) {
         $second = $ccm->categories[$first->fk_content_category];
         $output = ' <a href="/seccion/' . $second->name
             .'" title="'. $second->title . '">' . $second->title . '</a>'
