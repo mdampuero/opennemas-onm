dff5d7af7541de6a112178cbb44cc0ef926675c0
Fran Dieguez <fran@openhost.es>
2013-03-06 11:14:03 +0100

    Added width/height param translation in gravatar smarty function
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- vendor/smarty/onm-plugins/function.gravatar.php
+++ vendor/smarty/onm-plugins/function.gravatar.php
@@ -1,31 +1,36 @@
 <?php
-function smarty_function_gravatar($params, &$smarty) {
-
-    $url = '';
+function smarty_function_gravatar($params, &$smarty)
+{
+    $html = '';
     if (array_key_exists('email', $params)) {
-
         $size = (isset($params['size'])) ? $params['size'] : '16';
         $d = 'mm'; // mm, identicon, 404, monsterid, wavatar
         $r = 'g';
         $img = false;
         $atts = array();
         $default_icon = "&d=".urlencode($params["image_dir"]."favicon.png");
-        if (array_key_exists('size',$params))  $size = $params['size'];
-        if (array_key_exists('image',$params))  $img = $params['image'];
+        if (array_key_exists('size', $params)) {
+            $size           = $params['size'];
+            $atts['height'] = $size;
+            $atts['width']  = $size;
+        }
+        if (array_key_exists('image', $params)) {
+            $img = $params['image'];
+        }
 
         $url = 'http://www.gravatar.com/avatar/';
 
-        $url .= md5( strtolower( trim( $params['email'] ) ) );
+        $url .= md5(strtolower(trim($params['email'])));
         $url .= "?s=$size&amp;d=$d&amp;r=$r";
-        if ( $img ) {
-            $url = '<img src="' . $url . '"';
-            foreach ( $atts as $key => $val )
-                $url .= ' ' . $key . '="' . $val . '"';
-            $url .= ' />';
+        if ($img) {
+            $html = '<img src="' . $url . '"';
+            foreach ($atts as $key => $val) {
+                $html .= ' ' . $key . '="' . $val . '"';
+            }
+
+            $html .= ' />';
         }
     }
 
-
-    return $url;
+    return $html;
 }
-?>
