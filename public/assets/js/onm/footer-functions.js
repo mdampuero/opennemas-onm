7a809a4b515cb0e1f677d0e1dbb128ac73196156
Fran Dieguez <fran@openhost.es>
2013-03-15 12:19:03 +0100

    Hide alert messages after 5 seconds in backend
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- public/assets/js/onm/footer-functions.js
+++ public/assets/js/onm/footer-functions.js
@@ -53,5 +53,11 @@ jQuery(document).ready(function($) {
         $(this).removeClass('open');
         jQuery(this).find('.dropdown-menu:first').hide();
     });
+
+    window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+        });
+    }, 5000);
 });
 
