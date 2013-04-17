33d643ca280d7b733e96c0049127df25e19e363a
Alex Rodrigues <alex@openhost.es>
2013-04-17 16:18:21 +0200

    Improvements in newsletter subscriptions
    Signed-off-by: Alex Rico <alex@openhost.es>

--- src/Frontend/Controllers/SubscriptionsController.php
+++ src/Frontend/Controllers/SubscriptionsController.php
@@ -102,8 +102,10 @@ class SubscriptionsController extends Controller
                 $name  = $request->request->filter('name', null, FILTER_SANITIZE_STRING);
 
                 if (empty($email) || empty($name)) {
-                    $message = "Lo sentimos, no se ha podido completar su solicitud.\n"
-                            ."Verifique el formulario y vuelva intentarlo.";
+                    $message = _(
+                        "Sorry, we were unable to complete your request.\n"
+                        ."Check the form and try again"
+                    );
                     $class = 'error';
                 } else {
                     // Correct CAPTCHA, correct mail and name not empty
@@ -116,106 +118,114 @@ class SubscriptionsController extends Controller
                     $data['subscritorCountry']   = $request->request->filter('country', null, FILTER_SANITIZE_STRING);
                     $data['subscritorCommunity'] = $request->request->filter('community', null, FILTER_SANITIZE_STRING);
 
-                    switch ($action) {
-                        // Logic for subscription sending a mail to s::get('newsletter_maillist')
-                        case 'submit':
+                    $user = new \Subscriptor();
+                    // Check for repeated e-mail
+                    if ($user->exists_email($data['email'])) {
+                        $message = _("Sorry, that email is already subscribed to our newsletter");
+                        $class = 'error';
+                    } else {
+                        switch ($action) {
+                            // Logic for subscription sending a mail to s::get('newsletter_maillist')
+                            case 'submit':
 
-                            // Build mail body
-                            $formulario= "Nombre y Apellidos: ". $data['name']." \r\n".
-                                "Email: ".$data['email']." \r\n";
-                            if (!empty($data['subscritorEntity'])) {
-                                $formulario.= "Entidad: ".$data['subscritorEntity']." \n";
-                            }
-                            if (!empty($data['subscritorCountry'])) {
-                                $formulario.= "País: ".$data['subscritorCountry']." \n";
-                            }
-                            if (!empty($data['subscritorCommunity'])) {
-                                $formulario.= "Provincia de Origen: ".$data['subscritorCommunity']." \n";
-                            }
+                                // Build mail body
+                                $formulario= "Nombre y Apellidos: ". $data['name']." \r\n".
+                                    "Email: ".$data['email']." \r\n";
+                                if (!empty($data['subscritorEntity'])) {
+                                    $formulario.= "Entidad: ".$data['subscritorEntity']." \n";
+                                }
+                                if (!empty($data['subscritorCountry'])) {
+                                    $formulario.= "País: ".$data['subscritorCountry']." \n";
+                                }
+                                if (!empty($data['subscritorCommunity'])) {
+                                    $formulario.= "Provincia de Origen: ".$data['subscritorCommunity']." \n";
+                                }
 
-                            // Checking the type of action to do (alta/baja)
-                            if ($data['subscription'] == 'alta') {
-                                $subject = utf8_decode("Solicitud de ALTA - Boletín ".$configSiteName);
-                                $body    =  "Solicitud de Alta en el boletín de: \r\n". $formulario;
+                                // Checking the type of action to do (alta/baja)
+                                if ($data['subscription'] == 'alta') {
+                                    $subject = utf8_decode("Solicitud de ALTA - Boletín ".$configSiteName);
+                                    $body    =  "Solicitud de Alta en el boletín de: \r\n". $formulario;
 
-                                $message = _("You have been subscribed to the newsletter.");
-                                $class = 'success';
-                            } else {
-                                $subject = utf8_decode("Solicitud de BAJA - Boletín ".$configSiteName);
-                                $body    =  "Solicitud de Baja en el boletín de: \r\n". $formulario;
-
-                                $message = _("You have been unsusbscribed from the newsletter.");
-                                $class = 'success';
-                            }
-
-                            //Send mail
-                            $to = $configMailTo['subscription'];
-
-                            $mail = new \PHPMailer();
-                            $mail->SetLanguage('es');
-                            $mail->IsSMTP();
-                            $mail->Host = MAIL_HOST;
-                            $mail->Username = MAIL_USER;
-                            $mail->Password = MAIL_PASS;
-
-                            if (!empty($mail->Username) && !empty($mail->Password)) {
-                                $mail->SMTPAuth = true;
-                            } else {
-                                $mail->SMTPAuth = false;
-                            }
-
-                            $mail->Subject = $subject;
-                            $mail->From = $data['email'];
-                            $mail->FromName = utf8_decode($data['name']);
-                            $mail->Body = utf8_decode($body);
-
-                            $mail->AddAddress($to, $to);
-
-                            if (!$mail->Send()) {
-                                $message = "Lo sentimos, no se ha podido '
-                                    .'completar su solicitud.\nVerifique el formulario '
-                                    .'y vuelva intentarlo.";
-                                $class = 'error';
-                            }
-                            break;
-                        case 'create_subscriptor':
-
-                            if ($data['subscription'] == 'alta') {
-                                $data['subscription'] = 1;
-                                $data['status'] = 2;
-
-                                $user = new \Subscriptor();
-
-                                if ($user->create($data)) {
-                                    $message = _("You have been subscribed to our newsletter.");
+                                    $message = _("You have been subscribed to the newsletter.");
                                     $class = 'success';
                                 } else {
-                                    $message = _(
-                                        "Sorry, we were unable to complete your request.\n"
-                                        ."Check the form and try again"
-                                    );
-                                    $class = 'error';
-                                }
-                            } else {
-                                $data['subscription'] = 0;
-                                $data['status'] = 3;
+                                    $subject = utf8_decode("Solicitud de BAJA - Boletín ".$configSiteName);
+                                    $body    =  "Solicitud de Baja en el boletín de: \r\n". $formulario;
 
-                                $user = new \Subscriptor();
-                                $user = $user->getUserByEmail($data['email']);
-                                $data['id'] = $user->id;
-
-                                if ($user->update($data)) {
-                                    $message = _("You have been unsubscribed from our newsletter");
+                                    $message = _("You have been unsusbscribed from the newsletter.");
                                     $class = 'success';
+                                }
+
+                                //Send mail
+                                $to = $configMailTo['subscription'];
+
+                                $mail = new \PHPMailer();
+                                $mail->SetLanguage('es');
+                                $mail->IsSMTP();
+                                $mail->Host = MAIL_HOST;
+                                $mail->Username = MAIL_USER;
+                                $mail->Password = MAIL_PASS;
+
+                                if (!empty($mail->Username) && !empty($mail->Password)) {
+                                    $mail->SMTPAuth = true;
                                 } else {
+                                    $mail->SMTPAuth = false;
+                                }
+
+                                $mail->Subject = $subject;
+                                $mail->From = $data['email'];
+                                $mail->FromName = utf8_decode($data['name']);
+                                $mail->Body = utf8_decode($body);
+
+                                $mail->AddAddress($to, $to);
+
+                                if (!$mail->Send()) {
                                     $message = _(
-                                        "Sorry, we were unable to complete your request.\n"
-                                        ."Check the form and try again"
-                                    );
+                                    "Sorry, we were unable to complete your request.\n"
+                                    ."Check the form and try again"
+                                );
                                     $class = 'error';
                                 }
-                            }
-                            break;
+                                break;
+                            case 'create_subscriptor':
+
+                                if ($data['subscription'] == 'alta') {
+                                    $data['subscription'] = 1;
+                                    $data['status'] = 2;
+
+                                    $user = new \Subscriptor();
+
+                                    if ($user->create($data)) {
+                                        $message = _("You have been subscribed to our newsletter.");
+                                        $class = 'success';
+                                    } else {
+                                        $message = _(
+                                            "Sorry, we were unable to complete your request.\n"
+                                            ."Check the form and try again"
+                                        );
+                                        $class = 'error';
+                                    }
+                                } else {
+                                    $data['subscription'] = 0;
+                                    $data['status'] = 3;
+
+                                    $user = new \Subscriptor();
+                                    $user = $user->getUserByEmail($data['email']);
+                                    $data['id'] = $user->id;
+
+                                    if ($user->update($data)) {
+                                        $message = _("You have been unsubscribed from our newsletter");
+                                        $class = 'success';
+                                    } else {
+                                        $message = _(
+                                            "Sorry, we were unable to complete your request.\n"
+                                            ."Check the form and try again"
+                                        );
+                                        $class = 'error';
+                                    }
+                                }
+                                break;
+                        }
                     }
                 }
             }
