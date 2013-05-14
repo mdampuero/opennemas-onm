a1836c54d3bd080da82f3f9ebbe3c79a201e7e96
Sandra Pereira <sandra@openhost.es>
2013-05-14 16:11:42 +0200

    added newsletter changes in database file
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- db/changes-01102012.sql
+++ db/changes-01102012.sql
@@ -3,6 +3,13 @@
 -- Please check rigth sql, use ; in the end of lines & -- for comments.
 -- Write date with each sentence and with stack method. (last writed in the top)
 
+
+-- 06-05-2013
+ALTER TABLE `newsletter_archive` CHANGE `created` `created` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `newsletter_archive` ADD `updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created`;
+UPDATE `newsletter_archive` SET  `updated` = `created` WHERE `updated`='0000-00-00 00:00:00';
+DROP TABLE `bulletins_archive`;
+
 -- 10-10-2012
 CREATE TABLE IF NOT EXISTS `contentmeta` (
   `fk_content` bigint(20) NOT NULL,
