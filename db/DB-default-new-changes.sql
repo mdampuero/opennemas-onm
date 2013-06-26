36d2cf1d18c8f735887957b954f6fcc6d6d6d6c9
Alex Rodrigues <alex@openhost.es>
2013-06-26 17:23:19 +0200

    Added DB changes for manager users table
    Signed-off-by: Alex Rico <alex@openhost.es>

--- db/DB-default-new-changes.sql
+++ db/DB-default-new-changes.sql
@@ -7,13 +7,6 @@
 -- Please check rigth sql, use ; in the end of lines & -- for comments.
 -- Write date with each sentence and with stack method. (last writed in the top)
 
--- 11-06-2013
-ALTER TABLE `users` CHANGE `login` `username` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-ALTER TABLE `users` CHANGE `authorize` `activated` TINYINT( 1 ) NOT NULL DEFAULT '1' COMMENT '1 activated - 0 deactivated';
-ALTER TABLE `users` ADD `url` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '' AFTER `sessionexpire`;
-ALTER TABLE `users` ADD `bio` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '' AFTER `url`;
-ALTER TABLE `users` ADD `avatar_img_id` BIGINT( 20 ) UNSIGNED NOT NULL DEFAULT  '' AFTER `bio`;
-
 -- 05-30-2013
 DROP TABLE content_types;
 
