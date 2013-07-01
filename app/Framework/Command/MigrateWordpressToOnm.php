f501ce9d71e0d5c623d4754e38c47eb2afd11d2e
Sandra Pereira <sandra@openhost.es>
2013-07-01 16:46:11 +0200

    Load routes and container in console script
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- app/Framework/Command/MigrateWordpressToOnm.php
+++ app/Framework/Command/MigrateWordpressToOnm.php
@@ -74,6 +74,19 @@ EOF
             false
         );
 
+
+       /* $originalMedia = $password = $dialog->askHiddenResponse(
+            $output,
+            'Where is the wordpress media directory?',
+            false
+        );
+
+        define('ORIGINAL_URL', $originalMedia); */
+
+
+        define('ORIGINAL_URL', 'http://mundiario.com');
+        define('ORIGINAL_MEDIA', '/wp-content/uploads/');
+
         define('BD_HOST', $dataBaseHost);
         define('BD_USER', $dataBaseUser);
         define('BD_PASS', $dataBasePass);
@@ -81,18 +94,19 @@ EOF
         define('BD_DATABASE', $dataBaseName);
         define('ORIGIN_BD_DATABASE', $originDataBaseName);
 
-        include_once $basePath.'/app/autoload.php';
-        include_once 'Application.php';
-
         // Initialize internal constants for logger
         // Logger in content class when creating widgets
         define('SYS_LOG_PATH', realpath(SITE_PATH.DS.'..'.DS."tmp/logs"));
-        define('INSTANCE_UNIQUE_NAME', 'opennemas');
+        define('INSTANCE_UNIQUE_NAME', 'mundiario');
+
+        define('IMG_DIR', "images");
+        define('MEDIA_PATH', SITE_PATH."media".DS.INSTANCE_UNIQUE_NAME);
 
         // Initialize Globals and Database
         $GLOBALS['application'] = new \Application();
         \Application::initDatabase();
         \Application::initLogger();
+
 
         $GLOBALS['application']->connOrigin = \ADONewConnection(BD_TYPE);
         $GLOBALS['application']->connOrigin->Connect(BD_HOST, BD_USER, BD_PASS, ORIGIN_BD_DATABASE);
@@ -100,20 +114,20 @@ EOF
         $_SESSION['username'] = 'script';
         $_SESSION['userid'] = 11;
         // Execute functions
-        $output->writeln("\t<fg=blue;bg=white>Migrating: ".$originDataBaseName."->".$dataBaseName."</fg=blue;bg=white>");
+        $output->writeln("\t<fg=blue>Migrating: ".$originDataBaseName."->".$dataBaseName."</fg=blue>");
 
         $this->output = $output;
 
-        $this->prepareDatabase();
+     //   $this->prepareDatabase();
 
-        $this->importUsers();
+     //   $this->importUsers();
 
-     /*   $this->importCategories();
+       $this->importCategories();
         $this->loadCategories();
 
-        $this->importImages();
+      //  $this->importImages();
         $this->importArticles();
-
+/*
         $this->importGalleries();
          $this->importVideos();
 */
@@ -128,9 +142,10 @@ EOF
 
         $sql = "ALTER TABLE `translation_ids` ".
             "ADD `slug`  VARCHAR( 200 ) NOT NULL DEFAULT  '' ";
-
         $rss = $GLOBALS['application']->conn->Execute($sql);
 
+        $sql = "INSERT INTO user_groups (`pk_user_group`, `name`) VALUES (3, 'autores')";
+        $rss = $GLOBALS['application']->conn->Execute($sql);
     }
 
 
@@ -150,10 +165,11 @@ EOF
         $usersMeta = $rs2->getArray();
         $data = array();
         foreach ($usersMeta as $user) {
-            var_dump($user);
             $userID = $user['user_id'];
+
             $key = $user['meta_key'];
             $data[$userID][$key] = $user['meta_value'];
+
         }
 
 
@@ -181,7 +197,15 @@ EOF
 
                     $originalID = $rs->fields['ID'];
 
-                    $data = array(
+                    if (!empty($data[$originalID]['userphoto_image_file'])
+                         && is_null($data[$originalID]['userphoto_image_file'])) {
+
+                        $file = ORIGINAL_MEDIA.'userphoto/'.$data[$originalID]['userphoto_image_file'];
+                        $photoId = $this->uploadUserAvatar($file, $rs->fields['user_nicename']);
+                        $data['avatar_img_id'] = $photoId;
+                    }
+
+                    $values = array(
                         'username' => $rs->fields['user_nicename'],
                         'password' => $rs->fields['user_pass'],
                         'sessionexpire' =>'30',
@@ -195,9 +219,10 @@ EOF
                         'avatar_img_id' => $data[$originalID]['userphoto_image_file'],
                         'bio' => $data[$originalID]['description'],
                         'url' => $rs->fields['user_url'],
+                        'id_user_group' => array('3'),
                     );
 
-                    $userID = $user->create($data);
+                    $userID = $user->create($values);
                     $user->setMeta(array('twitter' => $data[$originalID]['twitter']));
 
                     if (!empty($userID)) {
@@ -218,8 +243,13 @@ EOF
     protected function importCategories()
     {
 
-        $rs = self::$originConn->Execute("SELECT * FROM wp_terms, wp_term_taxonomy ".
-                "WHERE wp_terms.term_id = wp_term_taxonomy.term_id AND taxonomy='category'");
+        $this->output->writeln("Importing Categories");
+
+        $sql ="SELECT * FROM wp_terms, wp_term_taxonomy ".
+                "WHERE wp_terms.term_id = wp_term_taxonomy.term_id AND taxonomy='category'";
+
+        $request = $GLOBALS['application']->connOrigin->Prepare($sql);
+        $rs = $GLOBALS['application']->connOrigin->Execute($request);
 
         $categories = $rs->getArray();
 
@@ -238,7 +268,7 @@ EOF
                     'internal_category' => 0,
                     'color'             => '',
                     'params'            => array(
-                        'title'         => $elem[0],
+                        'title'         => $category['name'],
                         'inrss'         => 1,
                     ),
                 );
@@ -281,12 +311,13 @@ EOF
 
         $sql = "SELECT * FROM translation_ids WHERE type='category'";
 
-        $rs = $GLOBALS['application']->conn->Execute($sql, $values);
+        $rs = $GLOBALS['application']->conn->Execute($sql);
 
         $categories = $rs->GetArray();
 
         foreach ($categories as $category) {
             $this->categories[$category['pk_content_old']] = $category['pk_content'];
+            $this->originalCategories[] = $category['pk_content_old'];
         }
 
         return $this;
@@ -343,10 +374,10 @@ EOF
                         'frontpage' => 0,
                         'in_home' => 0,
                         'title_int' => $this->convertoUTF8($rs->fields['post_title']),
-                        'metadata' => StringUtils::get_tags($this->convertoUTF8($rs->fields['post_title'])),
+                        'metadata' => \Onm\StringUtils::get_tags($this->convertoUTF8($rs->fields['post_title'])),
                         'subtitle' => '',
                         'slug' => $rs->fields['post_name'],
-                        'agency' => self::$configuration['agency']['name'],
+                        'agency' => '',
                         'summary' => $summary,
                         'description' => strip_tags(substr($summary, 0,150)),
                         'body' => $data['body'],
@@ -354,21 +385,26 @@ EOF
                         'id' => 0,
                         'img1' =>$data['img'],
                         'img2' =>$data['img'],
+                        'fk_video' => '',
+                        'fk_video2' => '',
+                        'footer_video2' => '',
                         'created' => $rs->fields['post_date_gmt'],
                         'starttime' => $rs->fields['post_date_gmt'],
                         'changed' => $rs->fields['post_modified'],
-                        'fk_user' => $this->matchCategory($rs->fields['post_author']),
-                        'fk_publisher' => $this->matchCategory($rs->fields['post_author']),
+                        'fk_user' => $this->elementIsImported($rs->fields['post_author'], 'user'),
+                        'fk_author' => $this->elementIsImported($rs->fields['post_author'], 'user'),
+                        'fk_publisher' => $this->elementIsImported($rs->fields['post_author'], 'user'),
+                        'fk_publisher' => $this->elementIsImported($rs->fields['post_author'], 'user'),
                     );
 
-                    $article = new Article();
+                    $article = new \Article();
                     $newArticleID = $article->create($values);
 
-                    if (!empty($articleID)) {
+                    if (!empty($newArticleID)) {
                         $this->insertRefactorID($originalArticleID, $newArticleID,'article', $rs->fields['post_name']);
                         $this->output->writeln('-'. $originalArticleID.'->'. $newArticleID. ' article ok');
                     } else {
-                        $this->output->writeln('Problem inserting article '.$originalArticleID.' - '.$title .'\n');
+                        $this->output->writeln('Problem inserting article '.$originalArticleID.' - '. $rs->fields['post_name'] .'\n');
                     }
                 }
                 $current++;
@@ -394,7 +430,7 @@ EOF
         $request = $GLOBALS['application']->connOrigin->Prepare($sql);
         $rs = $GLOBALS['application']->connOrigin->Execute($request);
 
-        $IDCategory=1; //assign category for media elements
+        $IDCategory=$this->matchCategory('62'); //assign category 'Fotos' for media elements
 
         if (!$rs) {
             $this->output->writeln(self::$originConn->ErrorMsg());
@@ -412,7 +448,7 @@ EOF
                     $originalImageID = $rs->fields['ID'];
 
                     ///http://mundiario.com/wp-content/uploads/2013/06/Brasil-360x225.png
-                    $local_file = str_replace(ORIGINAL_MEDIA,'',$rs->fields['guid']);
+                    $local_file = str_replace(ORIGINAL_URL,'',$rs->fields['guid']);
                   //  $local_file = str_replace(self::$originMedia['originUrl'],'',$rs->fields['guid']);
 
                     $imageData = array(
@@ -423,26 +459,29 @@ EOF
                             'content_status' => 1,
                             'frontpage' => 0,
                             'in_home' => 0,
-                            'metadata' => StringUtils::get_tags($this->convertoUTF8($rs->fields['post_title'].$rs->fields['post_excerpt'])),
+                            'metadata' => \Onm\StringUtils::get_tags($this->convertoUTF8($rs->fields['post_name'].$rs->fields['post_excerpt'])),
                             'description' => $this->convertoUTF8(strip_tags(substr($rs->fields['post_excerpt'], 0, 150))),
                             'id' => 0,
-                            'created' => $rs->fields['post_date'],
-                            'starttime' => $rs->fields['post_date'],
-                            'changed' => $rs->fields['post_modified'],
-                            'fk_user' => self::$configuration['idUser']['id'],
-                            'fk_author' => self::$configuration['idUser']['id'],
-                            'local_file' => self::$originMedia.$local_file,
+                            'created' => $rs->fields['post_date_gmt'],
+                            'starttime' => $rs->fields['post_date_gmt'],
+                            'changed' => $rs->fields['post_modified_gmt'],
+                            'fk_user' =>  $this->elementIsImported($rs->fields['post_author'], 'user'),
+                            'fk_author' =>  $this->elementIsImported($rs->fields['post_author'], 'user'),
+                            'local_file' => $local_file,
+                            'author_name' => '',
                         );
 
-                    $imageID = $photo->create($imageData);
 
-                    if (!empty($imageID)) {
-                        $this->insertRefactorID($rs->fields['pk_content'], $imageID, 'image');
+                    $newimageID= @$photo->createFromLocalFile($imageData);
+
+                    if (!empty($newimageID)) {
+                        $this->insertRefactorID($originalImageID, $imageID, 'image', $rs->fields['post_name']);
                         //$this->updateFields('`available` ='.$rs->fields['available'], $rs->fields['pk_content']);
                         $this->output->writeln('- Image '. $imageID. ' ok');
                     } else {
-                        $this->output->writeln('Problem inserting image '.$rs->fields['pk_content'].' - '.$rs->fields['title'] .'\n');
+                        $this->output->writeln('Problem inserting image '.$originalImageID.' - '.$rs->fields['post_name'] ."\n");
                     }
+
                 }
                 $current++;
                 $rs->MoveNext();
@@ -509,7 +548,7 @@ EOF
                                 'category' => $article->category,
                                 'category_name' =>$article->loadCategoryName($articleID),
                                 'body' =>  strip_tags($rs->fields['comment_content']),
-                                'metadata' => StringUtils::get_tags($title),
+                                'metadata' => \Onm\StringUtils::get_tags($title),
                                 'description' => substr($comment, 0, 150),
                                 'content_status' => 1,
                                 'created' => $rs->fields['comment_date'],
@@ -638,12 +677,12 @@ EOF
 
 
         // Fetch the list of Opinions available for one author in EditMaker
-        $request = self::$originConn->Prepare($sql);
-        $rs = self::$originConn->Execute($request);
+        $request = $GLOBALS['application']->conn->Prepare($sql);
+        $rs = $GLOBALS['application']->conn->Execute($request);
 
         $imageID='';
         if(!$rs) {
-            $this->output->writeln('- User '. $userID. ' ok');
+            $this->output->writeln('- Image '. $guid. ' ok');
         } else {
             $imageID = $this->elementIsImported($rs->fields['ID'], 'image');
 
@@ -660,7 +699,8 @@ EOF
         */
         $result = array();
         #Deleted [caption id="attachment_2302" align="aligncenter" width="300" caption="El partido ultra Jobbik siembra el terror entre las minorías y los extranjeros en Hungría."][/caption]
-
+        //OJO!!<a title="Kobe Bryant" href="http://www.flickr.com/photos/42161969@N03/4067656449/" target="_blank"><img title="Kobe Bryant" alt="Kobe Bryant" src="http://farm3.staticflickr.com/2493/4067656449_a576ba8a59.jpg" /></a>
+        //permitir//
         $newBody='';
         $img='';
         preg_match_all('/(<a .*?href="(.+?)".*?><img .*?src=".+?".*?><\/a>)/', $body, $result);
@@ -668,11 +708,78 @@ EOF
             $guid = $result[2][0];
 
             $newBody =preg_replace('/(<a .*?href="(.+?)".*?><img .*?src=".+?".*?><\/a>)/', '', $body);
-            $newBody = ImportHelper::convertoUTF8(strip_tags($newBody, '<p><a><br>'));
+            $newBody = $this->convertoUTF8(strip_tags($newBody, '<p><a><br>'));
             $img = $this->getOnmIdImage($guid);
         }
-        $newBody= preg_replace('/(\[caption .*?\]\[\/caption\])/', '', $newBody);
+        $newBody = preg_replace('/(\[caption .*?\]\[\/caption\])/', '', $newBody);
         return array('img'=>$img, 'body'=>$newBody);
+
+    }
+
+       /**
+     * Process an uploaded photo for user
+     *
+     * @param Symfony\Component\HttpFoundation\File\UploadedFile $file the uploaded file
+     * @param string $userName the user real name
+     *
+     * @return Response the response object
+     **/
+    protected function uploadUserAvatar($file, $userName)
+    {
+        // Generate image path and upload directory
+        $userNameNormalized = \Onm\StringUtils::normalize_name($userName);
+        $relativeAuthorImagePath ="/images/authors/".$userName;
+        $uploadDirectory =  MEDIA_PATH .$relativeAuthorImagePath;
+
+        // Get original information of the uploaded image
+        $originalFileName = $file;
+        $originalFileData = pathinfo($originalFileName);
+        $fileExtension    = strtolower($originalFileData['extension']);
+
+        // Generate new file name
+        $currentTime = gettimeofday();
+        $microTime   = intval(substr($currentTime['usec'], 0, 5));
+        $newFileName = date("YmdHis").$microTime.".".$fileExtension;
+
+        // Check upload directory
+        if (!is_dir($uploadDirectory)) {
+            \FilesManager::createDirectory($uploadDirectory);
+        }
+
+        // Upload file
+        $fileCopied = copy($uploadDirectory, $newFileName);
+
+        if ($fileCopied) {
+            // Get all necessary data for the photo
+            $infor = new \MediaItem($uploadDirectory.'/'.$newFileName);
+            $data = array(
+                'title'       => $originalFileName,
+                'name'        => $newFileName,
+                'user_name'   => $newFileName,
+                'path_file'   => $relativeAuthorImagePath,
+                'nameCat'     => $userName,
+                'category'    => '',
+                'created'     => $infor->atime,
+                'changed'     => $infor->mtime,
+                'date'        => $infor->mtime,
+                'size'        => round($infor->size/1024, 2),
+                'width'       => $infor->width,
+                'height'      => $infor->height,
+                'type'        => $infor->type,
+                'type_img'    => $fileExtension,
+                'media_type'  => 'image',
+                'author_name' => '',
+            );
+
+            // Create new photo
+            $photo = new \Photo();
+            $photoId = $photo->create($data);
+
+            return $photoId;
+        } else {
+             $this->output->writeln('- No photo move');
+        }
+
 
     }
 
