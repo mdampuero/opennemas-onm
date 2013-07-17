cb2d25ecc360973801c2c2a48510d3aceb923d51
Fran Dieguez <fran@openhost.es>
2013-07-17 12:26:06 +0200

    Change charset in contents table to utf8_general_ci
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- db/DB-default-new-changes.sql
+++ db/DB-default-new-changes.sql
@@ -12,3 +12,12 @@ ALTER TABLE translation_ids ADD `slug` VARCHAR(200) DEFAULT  '' AFTER  `type`;
 ALTER TABLE `contents`
   DROP `placeholder`,
   DROP `home_placeholder`;
+
+ALTER TABLE  `contents`
+CHANGE  `description`  `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE  `metadata`  `metadata` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE  `slug`  `slug` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE  `params`  `params` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+CHANGE  `category_name`  `category_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT  'name category',
+CHANGE  `urn_source`  `urn_source` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+CHANGE  `title`  `title` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
