8470a538efe06302f7f6c4f0a002938f3ff282bb
Sandra Pereira <sandra@openhost.es>
2013-08-07 12:00:00 +0200

    Added config parameters to monetize the newsletter
    Signed-off-by: Sandra Pereira <sandra@openhost.es>

--- db/DB-default-new-changes.sql
+++ db/DB-default-new-changes.sql
@@ -7,3 +7,8 @@
 -- Please check rigth sql, use ; in the end of lines & -- for comments.
 -- Write date with each sentence and with stack method. (last writed in the top)
 ALTER TABLE translation_ids ADD `slug` VARCHAR(200) DEFAULT  '' AFTER  `type`;
+
+-- Added config parameters to monetize the newsletter.
+REPLACE INTO settings (`name`, `value`) VALUES ('newsletter_sender', 's:30:"no-reply@postman.opennemas.com"');
+REPLACE INTO settings (`name`, `value`) VALUES ('max_mailing', 's:1:"0"');
+REPLACE INTO settings (`name`, `value`) VALUES ('last_invoice', 's:19:"2013-07-28 10:00:00"');
