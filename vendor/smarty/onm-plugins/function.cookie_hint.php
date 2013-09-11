5eb1f445acf0ae115416286e90ebf3b95ec322a7
Fran Dieguez <fran@openhost.es>
2013-09-11 11:22:29 +0200

    For user to accept cookies in cookie agreement message
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- vendor/smarty/onm-plugins/function.cookie_hint.php
+++ vendor/smarty/onm-plugins/function.cookie_hint.php
@@ -10,21 +10,20 @@ function smarty_function_cookie_hint($params, &$smarty)
         && !array_key_exists('cookieoverlay_accepted', $_COOKIE)
     ) {
         $message = _(
-            'Utilizamos cookies propias y de terceros para mostrarle publicidad relacionada con sus '
-            .'preferencias según su navegación.  Si continua navegando consideramos que acepta'
-            .' el uso de cookies. <a target="_blank" href="/estaticas/cookies-policies.html"> Más información &gt; </a>'
+            'We use cookies to offer you a better experience. By using this site you agree to our use of cookies. <a target="_blank" href="/estaticas/cookies-policies.html"> Learn more &gt; </a>'
         );
 
         $html = '<link rel="stylesheet" type="text/css" href="/assets/css/cookies_overlay.css"></style>'
                 ."<script type='text/javascript'>$(function() {
                     $('#cookies_overlay').on('click', '.closeover', function(e, ui) {
                         $(this).closest('#cookies_overlay').hide();
+                        $.cookie('cookieoverlay_accepted', true);
                     });
-                    $.cookie('cookieoverlay_accepted', true);
+
                 });
                 </script>"
                 ."<div id='cookies_overlay'><div class='cookies-overlay'>
-                  <button data-dismiss='alert' class='closeover' type='button'>×</button>
+                  <button data-dismiss='alert' class='closeover' type='button'>Aceptar</button>
                   <p>$message</p>
                 </div></div>";
     }
