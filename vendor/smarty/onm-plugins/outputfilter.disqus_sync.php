2fff493367298dbc810145c38ed133fb70eced58
Alex Rodrigues <alex@openhost.es>
2013-11-25 10:37:58 +0100

    Only execute disqus outputfilter when module is activated and configured
    Signed-off-by: Alex Rico <alex@openhost.es>

--- vendor/smarty/onm-plugins/outputfilter.disqus_sync.php
+++ vendor/smarty/onm-plugins/outputfilter.disqus_sync.php
@@ -10,30 +10,35 @@
  */
 function smarty_outputfilter_disqus_sync($output, &$smarty)
 {
-    $url = $_SERVER['REQUEST_URI'];
+    if (\Onm\Module\ModuleManager::isActivated('COMMENT_DISQUS_MANAGER')
+        && (\Onm\Settings::get('disqus_shortname'))
+        && (\Onm\Settings::get('disqus_secret_key'))
+    ) {
+        $url = $_SERVER['REQUEST_URI'];
 
-    $applicableUrl = array_key_exists('content', $smarty->tpl_vars);
+        $applicableUrl = array_key_exists('content', $smarty->tpl_vars);
 
-    // Check if is user template
-    if ($applicableUrl) {
-        global $sc;
-        $cache = $sc->get('cache');
+        // Check if is user template
+        if ($applicableUrl) {
+            global $sc;
+            $cache = $sc->get('cache');
 
-        $syncImg = '';
-        $lastSync = $cache->fetch(CACHE_PREFIX.'disqus_last_sync');
+            $syncImg = '';
+            $lastSync = $cache->fetch(CACHE_PREFIX.'disqus_last_sync');
 
-        if (!$lastSync
-            || ($lastSync + 300) < time()
-        ) {
-            // Generate url
-            $imageUrl = $sc->get('url_generator')
-                 ->generate('frontend_comments_disqus_sync', array(), true);
+            if (!$lastSync
+                || ($lastSync + 300) < time()
+            ) {
+                // Generate url
+                $imageUrl = $sc->get('url_generator')
+                     ->generate('frontend_comments_disqus_sync', array(), true);
 
-            // Generate image to call disqus sync action
-            $syncImg = '<img src="'.$imageUrl.'" style="display:none">';
+                // Generate image to call disqus sync action
+                $syncImg = '<img src="'.$imageUrl.'" style="display:none">';
 
-            // Add image to the end of <body> block
-            $output = str_replace('</body>', $syncImg.'</body>', $output);
+                // Add image to the end of <body> block
+                $output = str_replace('</body>', $syncImg.'</body>', $output);
+            }
         }
     }
 
