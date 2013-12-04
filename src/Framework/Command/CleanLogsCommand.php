bf64977854bc79910e3db583620d3334f2f41918
Fran Dieguez <fran@openhost.es>
2013-12-04 10:52:44 +0100

    Renamed clean:logs command handler class
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Framework/Command/CleanLogsCommand.php
+++ src/Framework/Command/CleanLogsCommand.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class LogsCommand extends Command
+class CleanLogsCommand extends Command
 {
     protected function configure()
     {
