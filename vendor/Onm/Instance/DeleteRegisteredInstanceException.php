815565ebc3c806c3dcd1fd94b995c2d639eef28f
Fran Dieguez <fran@openhost.es>
2013-12-24 16:46:45 +0100

    Move InstanceManager exceptions to its own file
    Signed-off-by: Fran Dieguez <fran@openhost.es>

---
+++ vendor/Onm/Instance/DeleteRegisteredInstanceException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Defines the Onm\Instance\DeleteRegisteredInstanceException
+ *
+ * This file is part of the onm package.
+ * (c) 2009-2011 OpenHost S.L. <contact@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Onm
+ * @subpackage Instance
+ */
+namespace Onm\Instance;
+
+/**
+ * Exceptions for handling unsuccessfull instance deletion
+ **/
+class DeleteRegisteredInstanceException extends \Exception
+{
+}
---
+++ vendor/Onm/Instance/InstanceNotRegisteredException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Defines the Onm\Instance\InstanceNotRegisteredException
+ *
+ * This file is part of the onm package.
+ * (c) 2009-2011 OpenHost S.L. <contact@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Onm
+ * @subpackage Instance
+ */
+namespace Onm\Instance;
+
+/**
+ * Exceptions for handling unsuccessfull instance creation
+ **/
+class InstanceNotRegisteredException extends \Exception
+{
+}
