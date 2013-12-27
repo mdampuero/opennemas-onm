87506e4d173ca55c9e51284d3355f49d6e92b837
Fran Dieguez <fran@openhost.es>
2013-12-27 15:02:40 +0100

    Added more cli options to test-runner.sh script
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- test-runner.sh
+++ test-runner.sh
@@ -5,8 +5,25 @@ if ! which inotifywait > /dev/null; then
     exit 1;
 fi
 
+while getopts “c:r:p:v” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         c)
+             CI="-ci"
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
 while true; do
     inotifywait -qq -r -e modify app/ src/ vendor/Onm/ --excludei "(tpl|js|css|jpg|png|yml|yaml)$" &&
     clear &&
-    ant phpunit;
+    ant phpunit$CI;
 done
