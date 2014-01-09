e25c866a02955c6eda51d0acf64237a72e49c716
Fran Dieguez <fran@openhost.es>
2014-01-09 21:10:56 +0100

    Move service definition from config.yml to OnmFramework bundle DI extension
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Framework/DependencyInjection/OnmFrameworkExtension.php
+++ src/Framework/DependencyInjection/OnmFrameworkExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class FrameworkExtension extends Extension
+class OnmFrameworkExtension extends Extension
 {
     /**
      * {@inheritDoc}
