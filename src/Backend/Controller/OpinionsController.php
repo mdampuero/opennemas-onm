e0b888f809580167629eb4940b1a31238b05e549
Fran Dieguez <fran@openhost.es>
2014-01-22 02:30:10 +0100

    Fix wrong namespace in Backend controllers that caused fatal error on prod environmens
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

--- src/Backend/Controller/OpinionsController.php
+++ src/Backend/Controller/OpinionsController.php
@@ -12,7 +12,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  **/
-namespace Backend\Controllers;
+namespace Backend\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
