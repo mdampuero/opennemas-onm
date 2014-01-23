6de496aed6a42c4d35c46e060a8bbce53015e181
Alex Rodrigues <alex@openhost.es>
2014-01-23 17:31:42 +0100

    Fixed disqus import command to work with new db connection
    Signed-off-by: Alex Rico <alex@openhost.es>

--- src/Framework/Command/DisqusSyncCommand.php
+++ src/Framework/Command/DisqusSyncCommand.php
@@ -14,7 +14,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\EventDispatcher\Event;
 use Onm\Settings as s;
 
 class DisqusSyncCommand extends Command
@@ -24,8 +23,6 @@ class DisqusSyncCommand extends Command
         $this
             ->setDefinition(
                 array(
-                    new InputArgument('user', InputArgument::REQUIRED, 'user'),
-                    new InputOption('password', 'p', InputOption::VALUE_OPTIONAL, 'The database password'),
                     new InputArgument('database', InputArgument::REQUIRED, 'database'),
                 )
             )
@@ -35,7 +32,7 @@ class DisqusSyncCommand extends Command
                 <<<EOF
 The <info>disqus:import</info> executes acomments import action with Disqus Api.
 
-<info>php app/console disqus:import user [-p pass] database</info>
+<info>php app/console disqus:import database</info>
 
 EOF
             );
@@ -43,90 +40,39 @@ EOF
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
-        $basePath = APPLICATION_PATH;
         $phpBinPath = exec('which php');
+        chdir(APPLICATION_PATH);
 
-        chdir($basePath);
-
-        $dataBaseHost = 'localhost';
-        $dataBaseType = 'mysqli';
-        $dataBaseUser = $input->getArgument('user');
-        $dataBaseName = $input->getArgument('database');
-        $dataBasePass = $input->getOption('password');
-
-        define('BD_HOST', $dataBaseHost);
-        define('BD_USER', $dataBaseUser);
-        define('BD_TYPE', $dataBaseType);
-        define('BD_DATABASE', $dataBaseName);
-
-        if (!$dataBasePass) {
-            // Ask password
-            $dialog = $this->getHelperSet()->get('dialog');
-
-            $validator = function ($value) {
-                if (trim($value) == '') {
-                    throw new \Exception('The password can not be empty, please try again');
-                } elseif (!$connect = @mysql_connect('localhost', BD_USER, $value)) {
-                    throw new \Exception('The password is wrong, please try again');
-                }
-
-                // Close connection if opened
-                if ($connect) {
-                    mysql_close($connect);
-                }
-
-                return $value;
-            };
-
-            $dataBasePass = $dialog->askHiddenResponseAndValidate(
-                $output,
-                'What is the password?',
-                $validator,
-                5,
-                false
-            );
-        }
-
-        if (trim($dataBasePass) == '') {
-            throw new \Exception('The password can not be empty, please try again');
-        } elseif (!$connect = @mysql_connect('localhost', BD_USER, $dataBasePass)) {
-            throw new \Exception('The password is wrong, please try again');
-        }
-
-        // Close connection if opened
-        if ($connect) {
-            mysql_close($connect);
-        }
-
-        define('BD_PASS', $dataBasePass);
-
-        // Initialize internal constants for
+        // Initialize internal constants
         define('CACHE_PREFIX', 'disqus');
-        define('SYS_LOG_PATH', realpath(SITE_PATH.DS.'..'.DS."tmp/logs"));
+        define('SYS_LOG_PATH', realpath(SITE_PATH."/".'..'."/"."tmp/logs"));
         define('INSTANCE_UNIQUE_NAME', 'community');
 
-        // Initialize Globals and Database
-        $GLOBALS['application'] = new \Application();
-        \Application::initDatabase();
+        // Get database name from prompt
+        $databaseName = $input->getArgument('database');
 
-        // Init script
+        // Get database connection
+        $databaseConnection = getService('db_conn');
+        $databaseConnection->selectDatabase($databaseName);
+
+        // Load application and initialize Database
+        \Application::load();
+        \Application::initDatabase($databaseConnection);
+
+        // Initialize script
         $output->writeln("\tStart disqus comments import");
 
         // Import
-        $this->fetchDisqusPosts($input, $output);
+        $this->fetchDisqusPosts();
 
         // Finish script
         $output->writeln("\n\tFinished disqus comments import");
-
     }
 
-
-    protected function fetchDisqusPosts($input, $output)
+    protected function fetchDisqusPosts()
     {
-        // Get service container and save disqus_last_sync cache time and uuid
-        global $sc;
-        $cache = $sc->get('cache');
+        // Get cache service and save disqus_last_sync cache time and uuid
+        $cache = getService('cache');
         $cache->save(
             CACHE_PREFIX.'disqus_last_sync',
             array('time' => time(), 'uuid' => uniqid()),
@@ -135,7 +81,5 @@ EOF
 
         // Save disqus comments to database
         \Onm\DisqusSync::saveDisqusCommentsToDatabase();
-
-        $output->writeln("\t<info>Disqus comments imported successfully</info>");
     }
 }
