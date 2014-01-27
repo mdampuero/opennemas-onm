d2387ff3434f04bd787b9be7bac791f108ed9f7a
Fran Dieguez <fran@openhost.es>
2014-01-27 16:16:02 +0100

    Simplify render_widget Smarty plugin by using the EntityRepository
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- vendor/smarty/onm-plugins/function.render_widget.php
+++ vendor/smarty/onm-plugins/function.render_widget.php
@@ -1,36 +1,25 @@
 <?php
-/*
- * -------------------------------------------------------------
- * File:     	function.render_widget.php
- * Comprueba el tipo y escribe el nombre o la imag
- */
-function smarty_function_render_widget($params, &$smarty) {
+function smarty_function_render_widget($params, &$smarty)
+{
+    // Initializing parameters
+    $widgetName = isset($params['name']) ? $params['name'] : null;
+    $widgetID   = isset($params['id'])   ? $params['id']   : null;
 
-	// Initialicing parameters
-	$widgetName = (isset($params['name']) ? $params['name'] : null);
-	$widgetID = (isset($params['id']) ? $params['id'] : null);
+    if (!is_null($widgetName)) {
+        // Initialize widget from name
+        $widget = new Widget();
+        $widget->readIntelligentFromName($widgetName);
+    } else {
+        // Initialize widget from id
+        $er = getService('entity_repository');
+        $widget = $er->find('Widget', $widgetID);
+    }
+
     $output = '';
+    if ($widget->available) {
+        $output = $widget->render($params);
+    }
 
-	// Initialize database access
-	$cm = new ContentManager();
-	$ccm = ContentCategoryManager::get_instance();
-
-	if(!is_null($widgetName)) {
-		// Initialize widget from db
-		$widget = new Widget();
-		$widget->readIntelligentFromName($widgetName);
-	} else {
-
-		// Initialize widget from db
-		$widget = new Widget();
-		$widget->read($widgetID);
-
-	}
-
-	if($widget->available) {
-		$output = $widget->render($params);
-	}
-
-	// Render its contents
-	return $output;
+    // Render its contents
+    return $output;
 }
