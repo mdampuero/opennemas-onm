aacf9dc9d78601f7db8f389471e5ee89346175df
Diego Blanco <diego@openhost.es>
2014-01-30 21:17:45 +0100

    Enabled authentication listener.
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- src/Framework/EventListener/AdminAuthenticationListener.php
+++ src/Framework/EventListener/AdminAuthenticationListener.php
@@ -33,67 +33,66 @@ class AdminAuthenticationListener implements EventSubscriberInterface
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        // if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
-        //     return;
-        // }
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
 
-        // $request = $event->getRequest();
-        // $session = $request->getSession();
-        // $url     = $request->getPathInfo();
+        $request = $event->getRequest();
+        $session = $request->getSession();
+        $url     = $request->getPathInfo();
 
-        // $isAdmin   = strpos($url, '/admin') === 0;
-        // $isManager = preg_match('@^/manager(?!ws)@i', $url, $matches) === 1;
+        $isAdmin   = strpos($url, '/admin') === 0;
+        $isManager = preg_match('@^/manager(?!ws)@i', $url, $matches) === 1;
 
-        // $isAsset = preg_match('@.*\.(png|gif|jpg|ico|css|js)$@', $request->getPathInfo());
+        $isAsset = preg_match('@.*\.(png|gif|jpg|ico|css|js)$@', $request->getPathInfo());
 
-        // if ($isAsset) {
-        //     $isDynAsset = preg_match('@/(asset|nocache)@', $request->getPathInfo());
-        //     if ($isDynAsset) {
-        //         return;
-        //     }
+        if ($isAsset) {
+            $isDynAsset = preg_match('@/(asset|nocache)@', $request->getPathInfo());
+            if ($isDynAsset) {
+                return;
+            }
 
-        //     // Log this error event to the web server logging system
-        //     error_log("File does not exist: ".$request->getPathInfo(), 0);
+            // Log this error event to the web server logging system
+            error_log("File does not exist: ".$request->getPathInfo(), 0);
 
-        //     $event->setResponse(new Response('Content not available', 404));
+            $event->setResponse(new Response('Content not available', 404));
 
-        //     return;
-        // }
-        // if (!isset($_SESSION['userid'])
-        //     && !preg_match('@login@', $request->getPathInfo())
-        // ) {
+            return;
+        }
+        if (!isset($_SESSION['userid'])
+            && !preg_match('@login@', $request->getPathInfo())
+        ) {
 
-        //     if (!empty($url)) {
-        //         $redirectTo = urlencode($request->getRequestUri());
-        //     }
-        //     if ($isAdmin) {
-        //         $location = $request->getBaseUrl() .'/admin/login/?forward_to='.$redirectTo;
-        //     } elseif ($isManager) {
-        //         $location = $request->getBaseUrl() .'/manager/login/?forward_to='.$redirectTo;
-        //     }
+            if (!empty($url)) {
+                $redirectTo = urlencode($request->getRequestUri());
+            }
+            if ($isAdmin) {
+                $location = $request->getBaseUrl() .'/admin/login/?forward_to='.$redirectTo;
+            } elseif ($isManager) {
+                $location = $request->getBaseUrl() .'/manager/login/?forward_to='.$redirectTo;
+            }
 
-        //     if (isset($location)) {
-        //         $event->setResponse(new RedirectResponse($location, 301));
-        //     }
+            if (isset($location)) {
+                $event->setResponse(new RedirectResponse($location, 301));
+            }
 
-        // } elseif (($isAdmin || $isManager) && isset($_SESSION['type']) && $_SESSION['type'] != 0) {
-        //     $event->setResponse(new RedirectResponse('/', 301));
-        // }
-        // else {
-        //     $maxIdleTime = ((int) s::get('max_session_lifetime', 60) * 60);
-        //     $lastUsedSession = $session->getMetadataBag()->getLastUsed();
+        } elseif (($isAdmin || $isManager) && isset($_SESSION['type']) && $_SESSION['type'] != 0) {
+            $event->setResponse(new RedirectResponse('/', 301));
+        }
+        else {
+            $maxIdleTime = ((int) s::get('max_session_lifetime', 60) * 60);
+            $lastUsedSession = $session->getMetadataBag()->getLastUsed();
 
-        //     // If the max idle time is set and the session was used in a time before the max idle time
-        //     // invalidate session and redirect to
-        //     if ($maxIdleTime > 0
-        //         && time() - $lastUsedSession > $maxIdleTime
-        //     ) {
-        //         $session->invalidate();
+            // If the max idle time is set and the session was used in a time before the max idle time
+            // invalidate session and redirect to
+            if ($maxIdleTime > 0
+                && time() - $lastUsedSession > $maxIdleTime
+            ) {
+                $session->invalidate();
 
-        //         $event->setResponse(new RedirectResponse(SITE_URL_ADMIN));
-        //     }
-        // }
-
+                $event->setResponse(new RedirectResponse(SITE_URL_ADMIN));
+            }
+        }
     }
 
     public static function getSubscribedEvents()
