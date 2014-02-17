0cea97a44bad1ddaf359ae5e5bb711892c7e1f64
Diego Blanco <diego@openhost.es>
2014-02-17 16:53:10 +0100

    Removed duplicated getServicePartyName function.
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- vendor/Onm/Import/DataSource/Format/Europapress.php
+++ vendor/Onm/Import/DataSource/Format/Europapress.php
@@ -265,16 +265,6 @@ class Europapress implements FormatInterface
     }
 
     /**
-     * Returns the name of the service that authored this element
-     *
-     * @return string the service name
-     **/
-    public function getServicePartyName()
-    {
-        return 'Europa Press';
-    }
-
-    /**
      * Returns the id of the element
      *
      * @return string the title
