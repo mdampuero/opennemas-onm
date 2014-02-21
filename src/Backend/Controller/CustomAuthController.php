70af3ff1074096b17611dabe9efb53a285ec7dc7
Diego Blanco <diego@openhost.es>
2014-02-21 23:49:55 +0100

    Updated controller to increase login attempts when login fails.
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- src/Backend/Controller/CustomAuthController.php
+++ src/Backend/Controller/CustomAuthController.php
@@ -17,7 +17,7 @@ class CustomAuthController extends Controller
      */
     public function loginAction(Request $request)
     {
-        $error = null;
+        $error   = null;
         $referer = $this->session->get('_security.backend.target_path');
 
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
@@ -30,18 +30,25 @@ class CustomAuthController extends Controller
 
         if ($error) {
             $this->session->getFlashBag()->add('error', $error->getMessage());
+            $_SESSION['failed_login_attempts'] =
+                isset($_SESSION['failed_login_attempts']) ?
+                $_SESSION['failed_login_attempts'] + 1 : 1;
         }
 
         $token = $this->get('form.csrf_provider')
             ->generateCsrfToken('authenticate');
         $currentLanguage  = \Application::$language;
 
+        $failed_login_attempts = isset($_SESSION['failed_login_attempts']) ?
+            $_SESSION['failed_login_attempts'] : 0;
+
         return $this->render(
             'login/login.tpl',
             array(
-                'current_language' => $currentLanguage,
-                'token'            => $token,
-                'referer'          => $referer
+                'failed_login_attempts' => $failed_login_attempts,
+                'current_language'      => $currentLanguage,
+                'token'                 => $token,
+                'referer'               => $referer
             )
         );
     }
