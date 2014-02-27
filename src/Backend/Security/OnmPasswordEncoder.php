a4b646ac5f19de95c62cae734bbc274822fb9867
Diego Blanco <diego@openhost.es>
2014-02-27 22:44:33 +0100

    Created a custom password encoder to handle md5 encoded passwords in backend and manager and plaintext passwords in frontend.
    Signed-off-by: Diego Blanco <diego@openhost.es>

---
+++ src/Backend/Security/OnmPasswordEncoder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Backend\Security;
+
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class OnmPasswordEncoder implements PasswordEncoderInterface
+{
+    /**
+     * Encodes a password.
+     *
+     * @param  string $raw  Password to encode.
+     * @param  string $salt String used while encoding.
+     * @return string       Encoded password.
+     */
+    public function encodePassword($raw, $salt)
+    {
+        if (strpos($raw, 'md5:') === false) {
+            return md5($raw);
+        } else {
+            return substr($raw, strpos($raw, 'md5:') + 4);
+        }
+    }
+
+    /**
+     * Checks if the raw password is equal to the encoded password.
+     *
+     * @param  string $encoded Encoded password.
+     * @param  string $raw     Password to encode.
+     * @param  string $salt    String used while encoding.
+     * @return string          True if $encoded is equal to $raw after encoding.
+     */
+    public function isPasswordValid($encoded, $raw, $salt)
+    {
+        if ($encoded === $this->encodePassword($raw, $salt)) {
+            return true;
+        }
+
+        return false;
+    }
+}
