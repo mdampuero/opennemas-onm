c35a780a23fa590165dae4b8f5f70d33533c590b
Diego Blanco <diego@openhost.es>
2014-03-17 21:16:51 +0100

    Added and enabled JsRoutingBundle to generate symfony routes from javascript.
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             // new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             // new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
 
             new Backend\BackendBundle(),
             new BackendWebService\BackendWebServiceBundle(),
