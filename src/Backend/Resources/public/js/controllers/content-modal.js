bfc6cc498cb8e6364f5a7c2add48a31f4dc67df3
Diego Blanco <diego@openhost.es>
2014-03-20 13:55:07 +0100

    Created generic AngularJS controllers to handle actions in content lists and modals.
    Signed-off-by: Diego Blanco <diego@openhost.es>

---
+++ src/Backend/Resources/public/js/controllers/content-modal.js
@@ -0,0 +1,91 @@
+/**
+ * Controller to handle modal actions.
+ *
+ * Note: Don't use array notation to inject dependencies as controller wouldn't
+ *       recognize the rest of parameters.
+ *
+ * @param int    id       Selected item id.
+ * @param int    index    Index of the selected content in contents array.
+ * @param array  contents Array of contents.
+ * @param string name     Selected item name.
+ * @param array  selected Array of selected items.
+ */
+function ContentModalCtrl($http, $scope, $modalInstance, id, index, contents, name, selected) {
+    $scope.id       = id;
+    $scope.index    = index;
+    $scope.contents = contents;
+    $scope.name     = name;
+    $scope.selected = selected;
+
+    /**
+     * Closes the current modal.
+     */
+    $scope.close = function () {
+        $modalInstance.dismiss('close');
+    };
+
+    /**
+     * Deletes a content on confirmation.
+     *
+     * @param int    id    Item id.
+     * @param int    index Index of the item in the array of contents.
+     * @param string route Route name.
+     */
+    $scope.delete = function (id, index, route) {
+        // Enable spinner
+        $scope.deleting = 1;
+
+        var url = Routing.generate(route, { id: id });
+        $http.post(url).success(function(data) {
+            if (data.status == 'OK') {
+                $scope.contents.splice(index, 1);
+                $modalInstance.close();
+            }
+
+            // Disable spinner
+            $scope.deleting = 0;
+        }).error(function(data) {
+            // Disable spinner
+            $scope.deleting = 0;
+        });
+    };
+
+    /**
+     * Deletes selected contents on confirmation.
+     *
+     * @param string route Route name.
+     */
+    $scope.deleteSelected = function (route) {
+        // Enable spinner
+        $scope.deleting = 1;
+
+        var url = Routing.generate(route);
+        $http.post(url, { ids: $scope.selected }).success(function(response) {
+            if (response.status == 'OK') {
+                // Remove selected items from contents array
+                for (var i = 0; i < $scope.contents.length; i++) {
+                    var j = 0;
+                    while (j < $scope.selected.length
+                        && $scope.contents[i].id != $scope.selected[j]
+                    ) {
+                        j++;
+                    }
+
+                    if (j < $scope.selected.length) {
+                        $scope.contents.splice(i, 1);
+                    }
+                };
+                $modalInstance.close();
+            }
+
+            // Disable spinner
+            $scope.deleting = 0;
+        }).error(function() {
+            // Disable spinner
+            $scope.deleting = 0;
+        });
+    };
+}
+
+// Register ModalCtrl function as AngularJS controller
+angular.module('BackendApp.controllers').controller('ContentModalCtrl', ContentModalCtrl);
---
+++ src/Backend/Resources/public/js/controllers/content.js
@@ -0,0 +1,344 @@
+/**
+ * Controller to handle list actions.
+ */
+function ContentCtrl($http, $location, $modal, $scope, $timeout) {
+    /**
+     * All contents selected flag.
+     *
+     * @type boolean
+     */
+    $scope.allSelected = false;
+
+    /**
+     * Object with the filters of the current list.
+     *
+     * @type object
+     */
+    $scope.filters = {
+        elements_per_page: 10,
+        page:              1,
+        sort_by:           'title',
+        sort_order:        'asc',
+        search: {
+            content_type_name:     -1,
+            available:             -1,
+            category_name:         -1,
+            with_script:           -1,
+        },
+    }
+
+    /**
+     * Array of selected items.
+     *
+     * @type array
+     */
+    $scope.selected = [];
+
+    /**
+     * Returns if the given content is selected.
+     *
+     * @return boolean True if the given content is selected. Otherwise, return
+     *                 false.
+     */
+    $scope.areSelected = function() {
+        var selected = $scope.selected ? $scope.selected.length : 0;
+        var contents = $scope.contents ? $scope.contents.length : 0;
+
+        return selected == contents;
+    };
+
+    /**
+     * Changes the available property for selected contents.
+     *
+     * @param int    available Available value.
+     * @param string route     Route name.
+     */
+    $scope.batchToggleAvailable = function (available, route) {
+        updateAvailable(1);
+
+        var url = Routing.generate(route);
+        $http.post(url, { ids: $scope.selected, available: available })
+            .success(function(response) {
+                if (response.status == 'OK') {
+                    updateAvailable(0, available);
+                }
+            });
+    };
+
+    /**
+     * Removes invalid filters from array of filters.
+     *
+     * @param  Object $filters Filters.
+     * @return Object          Filters after removing invalid properties.
+     */
+    function clearFilters(filters) {
+        var cleaned = {};
+        for (var name in filters) {
+            if (filters[name] != -1 && filters[name] != '') {
+                cleaned[name] = filters[name];
+            }
+        };
+
+        return cleaned;
+    }
+
+    /**
+     * Goes to content edit page.
+     *
+     * @param int    id    Content id.
+     * @param string route Route name.
+     */
+    $scope.edit = function(id, route) {
+        var url = Routing.generate(route, { id: id});
+        document.location = url;
+    }
+
+    /**
+     * Initializes the content type for the current list.
+     *
+     * @param string content Content type.
+     * @param string route   Route name.
+     */
+    $scope.init = function(content, filters, route) {
+        // Initialize filters
+        for (var filter in filters) {
+            $scope.filters.search[filter] = filters[filter];
+        };
+
+        // Load filters from URL
+        var search = $location.search();
+        for (var filter in search) {
+            if (filters[filter] != null) {
+                $scope.filters.search[filter] = search[filter];
+            }
+        };
+
+        // Route for list (required by $watch)
+        $scope.route = route;
+
+        $scope.filters.search.content_type_name = content;
+        $scope.list(route);
+    }
+
+    /**
+     * Returns if the given content is selected.
+     *
+     * @param  int     id Checked item id.
+     * @return boolean    True if the given content is selected. Otherwise,
+     *                    return false.
+     */
+    $scope.isSelected = function(id) {
+        return $scope.selected.indexOf(id) >= 0;
+    };
+
+    /**
+     * Updates the array of contents.
+     *
+     * @param string route Route name.
+     */
+    $scope.list = function(route) {
+        // Enable spinner
+        $scope.loading = 1;
+
+        var postData = {
+            elements_per_page: $scope.filters.elements_per_page,
+            page:              $scope.filters.page,
+            sort_by:           $scope.filters.sort_by,
+            sort_order:        $scope.filters.sort_order,
+            search:            clearFilters($scope.filters.search)
+        }
+
+        var url = Routing.generate(route);
+        $http.post(url, postData).success(function(response) {
+            $scope.total    = response.total;
+            $scope.page     = response.page;
+            $scope.contents = response.results;
+            $scope.map      = response.map;
+
+            // Disable spinner
+            $scope.loading = 0;
+        }).error(function() {
+            // Disable spinner
+            $scope.loading = 0;
+        });
+    }
+
+    /**
+     * Opens new modal window when clicking delete button.
+     *
+     * @param  string template Template id.
+     * @param  int    index    Index of the selected content.
+     */
+    $scope.open = function(template, index) {
+        $modal.open({
+            templateUrl: template,
+            controller: 'ContentModalCtrl',
+            resolve: {
+                contents: function () {
+                    return $scope.contents;
+                },
+                index: function () {
+                    return index;
+                },
+                id: function () {
+                    if (index != null) {
+                        return $scope.contents[index].id;
+                    }
+
+                    return null;
+                },
+                name: function () {
+                    if (index != null) {
+                        return $scope.contents[index].title;
+                    }
+
+                    return null;
+                },
+                selected: function () {
+                    return $scope.selected;
+                }
+            }
+        });
+    }
+
+    /**
+     * Selects all contents in the list.
+     *
+     * @param object $event The event.
+     */
+    $scope.selectAll = function ($event) {
+        var checkbox = $event.target;
+
+        $scope.allSelected = !$scope.areSelected();
+        $scope.selected = [];
+
+        if ($scope.allSelected) {
+            for (var content in $scope.contents) {
+                updateSelected($scope.allSelected, $scope.contents[content].id);
+            };
+        }
+    };
+
+    /**
+     * Changes the page of the list.
+     *
+     * @param  int page Page number.
+     */
+    $scope.selectPage = function(page, route) {
+        if (page != $scope.filters.page) {
+            $scope.filters.page = page;
+            $scope.list(route);
+        }
+    };
+
+    /**
+     * Changes the content available property.
+     *
+     * @param int    id    Content id.
+     * @param int    index Index of content in the array of contents.
+     * @param string route Route name.
+     */
+    $scope.toggleAvailable = function (id, index, route) {
+        // Enable spinner
+        $scope.contents[index].loading = 1;
+
+        var url = Routing.generate(route, { id: id });
+        $http.post(url).success(function(response) {
+            if (response.status == 'OK') {
+                $scope.contents[index].available = response.available;
+
+                // Disable spinner
+                $scope.contents[index].loading = 0;
+            }
+        }).error(function(response) {
+            // Disable spinner
+            $scope.contents[index].loading = 0;
+        });
+    };
+
+    /**
+     * Updates the available property for selected contents.
+     *
+     * @param int loading   Loading flag to use in template.
+     * @param int available Available value.
+     */
+    function updateAvailable(loading, available) {
+        for (var i = 0; i < $scope.contents.length; i++) {
+            var j = 0;
+            while (j < $scope.selected.length
+                && $scope.contents[i].id != $scope.selected[j]
+            ) {
+                j++;
+            }
+
+            if (j < $scope.selected.length) {
+                if (available != null) {
+                    $scope.contents[i].available = available;
+                };
+
+                $scope.contents[i].loading = loading;
+            }
+        };
+    }
+
+    /**
+     * Updates the selected items.
+     *
+     * @param  boolean enabled Whether to enable or disable the select.
+     * @param  int     id      Selected content id.
+     */
+    function updateSelected(enabled, id) {
+        var index = $scope.selected.indexOf(id);
+
+        if (enabled && index === -1) {
+          $scope.selected.push(id);
+        } else {
+          $scope.selected.splice(index, 1);
+        }
+    };
+
+    /**
+     * Updates the selected items on click.
+     *
+     * @param  object $event The event.
+     * @param  int    id     Selected content id.
+     */
+    $scope.updateSelection = function($event, id) {
+        var checkbox = $event.target;
+        var action = checkbox.checked;
+        updateSelected(action, id);
+    };
+
+    var searchTimeout;
+
+    /**
+     * Reloads the list when filters change.
+     *
+     * @param  object newValues New filters values.
+     * @param  object oldValues Old filters values.
+     */
+    $scope.$watch('filters.search', function(newValues, oldValues) {
+        if (searchTimeout) {
+            $timeout.cancel(searchTimeout);
+        }
+
+        if (newValues !== oldValues) {
+            for (var name in newValues) {
+                if (newValues[name] !== oldValues[name]) {
+                    $location.search(name, newValues[name]);
+
+                    if (newValues[name] == '') {
+                        $location.search(name, null);
+                    }
+                }
+            };
+
+            searchTimeout = $timeout(function() {
+                $scope.list($scope.route);
+            }, 500);
+        }
+    }, true);
+}
+
+// Register ContentCtrl function as AngularJS controller
+angular.module('BackendApp.controllers').controller('ContentCtrl', ContentCtrl);
