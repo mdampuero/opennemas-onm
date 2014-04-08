c82489082e7f8ea9017ac6d4f4b3ad641ab7f165
Diego Blanco <diego@openhost.es>
2014-04-08 16:40:52 +0200

    Fixed gravatar directive to return default image when email is empty.
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- src/Backend/Resources/public/js/directives/gravatar.js
+++ src/Backend/Resources/public/js/directives/gravatar.js
@@ -9,35 +9,33 @@ angular.module('BackendApp.directives')
                 var baseUrl;
                 var html = '';
 
-                if ($attrs.email) {
-                    var size = $attrs.size ? $attrs.size : '16';
-                    var d    = 'mm'; // mm, identicon, 404, monsterid, wavatar
-                    var r    = 'g';
-                    var img  = false;
-                    var atts = [];
+                var size = $attrs.size ? $attrs.size : '16';
+                var d    = 'mm'; // mm, identicon, 404, monsterid, wavatar
+                var r    = 'g';
+                var img  = false;
+                var atts = [];
 
-                    var default_icon = "&d="
-                        + encodeURIComponent($attrs["image_dir"] + "favicon.png");
+                var default_icon = "&d="
+                    + encodeURIComponent($attrs["image_dir"] + "favicon.png");
 
-                        atts['height'] = size;
-                        atts['width']  = size;
+                    atts['height'] = size;
+                    atts['width']  = size;
 
-                    if ($attrs.image) {
-                        img = $attrs.image;
+                if ($attrs.image) {
+                    img = $attrs.image;
+                }
+
+                var url = 'http://www.gravatar.com/avatar/';
+
+                url += hex_md5($attrs['email'].trim().toLowerCase());
+                url += "?s=" + size + "&d=" + d + "&r=" + r;
+                if (img) {
+                    html = '<img src="' + url + '"';
+                    for (var i in atts) {
+                        html += ' ' + i + '="' + atts[i] + '"';
                     }
 
-                    var url = 'http://www.gravatar.com/avatar/';
-
-                    url += hex_md5($attrs['email'].trim().toLowerCase());
-                    url += "?s=" + size + "&d=" + d + "&r=" + r;
-                    if (img) {
-                        html = '<img src="' + url + '"';
-                        for (var i in atts) {
-                            html += ' ' + i + '="' + atts[i] + '"';
-                        }
-
-                        html += ' />';
-                    }
+                    html += ' />';
                 }
 
                 // Compile template and replace elements
