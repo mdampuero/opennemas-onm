baaa6d80fa0dd9b46146c17a545476e70e393cd2
Fran Dieguez <fran@openhost.es>
2014-06-20 14:36:23 +0200

    Added default params in parameters.yml.dist to get them autocompleted
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Framework/DependencyInjection/Configuration.php
+++ src/Framework/DependencyInjection/Configuration.php
@@ -30,7 +30,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('onm_framework');
+        $rootNode = $treeBuilder->root('opennemas')->end();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
