e3bc8c4108f14170efc1d0cca81651553e249423
Diego Blanco <diego@openhost.es>
2014-07-07 18:03:10 +0200

    Complete the command to update onm-instances database
    - Add action to get page views from Piwik.
    - Add action to get the number of sent emails from the instance.
    - Add action to get the size of the instance media folder.
    - Add a filter to get only backend users.
    
    Signed-off-by: Diego Blanco <diego@openhost.es>

--- src/Framework/Command/UpdateInstancesCommand.php
+++ src/Framework/Command/UpdateInstancesCommand.php
@@ -66,21 +66,21 @@ class UpdateInstancesCommand extends ContainerAwareCommand
     /**
      * Executes the current command.
      *
-     * @param InputInterface  $input  An InputInterface instance
-     * @param OutputInterface $output An OutputInterface instance
+     * @param InputInterface  $input  An InputInterface instance.
+     * @param OutputInterface $output An OutputInterface instance.
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $alexa = $input->getOption('alexa');
         $views = $input->getOption('views');
 
-        $im = $this->getContainer()->get('instance_manager');
+        $this->im = $this->getContainer()->get('instance_manager');
 
-        $instances = $im->findBy(null, array('id', 'asc'));
+        $instances = $this->im->findBy(null, array('id', 'asc'));
 
         foreach ($instances as $instance) {
-            $this->getInstanceInfo($im, $instance, $alexa, $views);
-            $this->setDatabaseInfo($im);
+            $counters = $this->getInstanceInfo($instance, $alexa, $views);
+            $this->setDatabaseInfo($instance->id, $counters);
         }
     }
 
@@ -115,83 +115,149 @@ class UpdateInstancesCommand extends ContainerAwareCommand
     /**
      * Gets the instance information.
      *
-     * @param  InstanceManager $im    The instance manager.
-     * @param  Instance        $i     The instance.
-     * @param  boolean         $alexa Whether to get the Alexa's rank.
-     * @param  boolean         $views Whether to get the page views.
+     * @param  Instance $i     The instance.
+     * @param  boolean  $alexa Whether to get the Alexa's rank.
+     * @param  boolean  $views Whether to get the page views.
      */
-    private function getInstanceInfo($im, $i, $alexa = false, $views = false)
+    private function getInstanceInfo($i, $alexa = false, $views = false)
     {
-        $im->getConnection()->selectDatabase($i->settings['BD_DATABASE']);
+        $counters = array(
+            'contents'   => 0,
+            'media_size' => 0,
+            'alexa'      => 0,
+            'page_views' => 0,
+            'users'      => 0,
+            'emails'     => 0
+        );
 
+        $this->im->getConnection()->selectDatabase($i->settings['BD_DATABASE']);
+
+        // Count contents
         $sql = 'SELECT COUNT(pk_content) FROM contents';
-        $rs  = $im->getConnection()->fetchArray($sql);
+        $rs  = $this->im->getConnection()->fetchArray($sql);
 
-        if ($rs !== false) {
-            $this->contents = $rs[0];
+        if ($rs !== false && !empty($rs)) {
+            $counters['contents'] = $rs[0];
         }
 
-        $sql = 'SELECT COUNT(id) FROM users';
-        $rs  = $im->getConnection()->fetchArray($sql);
+        // Count users
+        $sql = 'SELECT COUNT(id) FROM users WHERE type = 0';
+        $rs  = $this->im->getConnection()->fetchArray($sql);
 
-        if ($rs !== false) {
-            $this->users = $rs[0];
+        if ($rs !== false && !empty($rs)) {
+            $counters['users'] = $rs[0];
         }
 
         // Check domain's rank in Alexa
         if ($alexa && !empty($i->domains)) {
             $domains = explode(',', $i->domains);
-            $this->alexa = $this->getAlexa($i->domains[0]);
+            $counters['alexa'] = $this->getAlexa($domains[0]);
+        }
+
+        // Count emails
+        $sql = 'SELECT counter FROM action_counters WHERE action_name = \'newsletter\'';
+        $rs  = $this->im->getConnection()->fetchArray($sql);
+
+        if ($rs) {
+            $counters['emails'] = $rs[0];
+        }
+
+        // Get Piwik config and last invoice date
+        $sql = 'SELECT * FROM settings WHERE name=\'piwik\' OR name=\'last_invoice\'';
+        $rs  = $this->im->getConnection()->fetchAll($sql);
+
+        $piwik       = null;
+        $lastInvoice = null;
+        if ($rs !== false && !empty($rs)) {
+            foreach ($rs as $value) {
+                if (array_key_exists('name', $value)
+                    &&  $value['name'] == 'piwik'
+                ) {
+                    $piwik = unserialize($value['value']);
+                } elseif (array_key_exists('name', $value)
+                    &&  $value['name'] == 'last_invoice'
+                ) {
+                    $lastInvoice = unserialize($value['value']);
+                }
+            }
         }
 
         // Get the page views from Piwik
-        if ($views && !empty($i->domains)) {
-            $domains = explode(',', $i->domains);
-            $this->views = $this->getViews($i->domains[0]);
+        if ($views && !empty($piwik) && !empty($lastInvoice)) {
+            $counters['page_views'] = $this->getViews($piwik['page_id'], $lastInvoice);
         }
+
+        // Get media size
+        $size = explode("\t", shell_exec('du -s '.SITE_PATH."media".DS.$i->internal_name.'/'));
+        if (is_array($size)) {
+            $counters['media_size'] = $size[0] / 1024;
+        }
+
+        return $counters;
     }
 
     /**
      * Gets the number of page views from Piwik.
      *
-     * @param  string  $domain The domain to check in Alexa.
+     * @param  integer $siteId The site id in Piwik.
+     * @param  string  $from   Date of the last invoice.
      * @return integer         The number of page views.
      */
-    private function getViews($domain)
+    private function getViews($siteId, $from)
     {
+        if (!$siteId) {
+            return 0;
+        }
+
+        $url   = $this->getContainer()->getParameter('piwik.url');
+        $token = $this->getContainer()->getParameter('piwik.token');
+
+        $from = date_create_from_format('Y-m-d H:i:s', $from)->format('Y-m-d');
+        $to   = date('Y-m-d');
+
+        $url .= "?module=API&method=API.get"
+            . "&apiModule=VisitsSummary&apiAction=get"
+            . "&idSite=$siteId"
+            . "&period=range&date=$from,$to"
+            . "&format=json"
+            . "&showColumns=nb_visits"
+            . "&token_auth=$token";
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $resp = curl_exec($ch);
+        curl_close($ch);
+
+        $views = json_decode($resp, true);
+
+        if (array_key_exists('value', $views)) {
+            return $views['value'];
+        }
+
+        return 0;
     }
 
     /**
      * Updates the instance with the new information.
      *
-     * @param InstanceManager $im The instance manager.
+     * @param array $id       The instance id.
+     * @param array $counters Array of counters.
      */
-    private function setDatabaseInfo($im)
+    private function setDatabaseInfo($id, $counters)
     {
-        if (!$this->contents && !$this->users) {
-            return;
-        }
-
         $inserts = array();
-        if ($this->contents) {
-            $inserts[] = 'contents = ' . $this->contents;
+        foreach ($counters as $key => $value) {
+            $inserts[] = $key . ' = ' . $value;
         }
 
-        if ($this->users) {
-            $inserts[] = 'users = ' . $this->users;
-        }
+        $this->im->getConnection()->selectDatabase('onm-instances');
 
-        if ($this->alexa) {
-            $inserts[] = 'alexa = ' . $this->alexa;
-        }
+        $sql = 'UPDATE instances SET ' . implode(',', $inserts)
+            . ' WHERE id=' . $id;
 
-        if ($this->views) {
-            $inserts[] = 'views = ' . $this->views;
-        }
-
-        $im->getConnection()->selectDatabase('onm-instances');
-
-        $sql = 'UPDATE instances SET ' . implode(',', $inserts);
-        $rs  = $im->getConnection()->executeQuery($sql);
+        $rs  = $this->im->getConnection()->executeQuery($sql);
     }
 }
