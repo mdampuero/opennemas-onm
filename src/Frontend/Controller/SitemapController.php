a800be2501ed122d1cfd4145aa4f83749bbc7a0e
Alex Rodrigues <alex@openhost.es>
2014-07-11 11:01:25 +0200

    Refactored RSS and Sitemap controllers.
    Signed-off-by: Alex Rico <alex@openhost.es>

--- src/Frontend/Controller/SitemapController.php
+++ src/Frontend/Controller/SitemapController.php
@@ -46,7 +46,7 @@ class SitemapController extends Controller
     }
 
     /**
-     * Renders the web sitemap
+     * Renders the sitemap web
      *
      * @param Request $request the request object
      *
@@ -63,54 +63,17 @@ class SitemapController extends Controller
         if (($this->view->caching == 0)
             || !$this->view->isCached('sitemap/sitemap.tpl', $cacheID)
         ) {
-            //TODO: add this value in a config file for easy editing
-            $maxArticlesByCategory = 250;
+            // Fetch contents
+            $contents = $this->fetchContents(array());
 
-            $this->cm  = new \ContentManager();
-            $this->ccm = \ContentCategoryManager::get_instance();
-
-            list($this->availableCategories, $this->subcats, $this->other) =
-                $this->ccm->getArraysMenu(0, 1);
-
-            // Foreach available category retrieve last $maxArticlesByCategory articles in there
-            $articlesByCategory = array();
-            foreach ($this->availableCategories as $category) {
-                if ($category->inmenu == 1
-                    && $category->internal_category == 1
-                ) {
-                    $articlesByCategory[$category->name] = $this->cm->getArrayOfArticlesInCategory(
-                        $category->pk_content_category,
-                        'content_status=1 AND fk_content_type=1',
-                        ' ORDER BY created DESC',
-                        $maxArticlesByCategory
-                    );
-                    $articlesByCategory[$category->name] = $this->cm->getInTime(
-                        $articlesByCategory[$category->name]
-                    );
-
-                    // Exclude articles with external link from RSS
-                    foreach ($articlesByCategory[$category->name] as $key => $article) {
-                        if (!is_null($article['params'])) {
-                            $article['params'] = unserialize($article['params']);
-                            if (isset($article['params']['bodyLink']) && !empty($article['params']['bodyLink'])) {
-                                unset($articlesByCategory[$category->name][$key]);
-                            }
-                        }
-                    }
-
+            // Remove external articles
+            foreach ($contents as $key => &$content) {
+                if (!empty($content->params['bodyLink'])) {
+                    unset($contents[$key]);
                 }
             }
 
-            $opinions = $this->cm->getOpinionAuthorsPermalinks(
-                'contents.content_status=1',
-                'ORDER BY in_home DESC, position ASC, changed DESC LIMIT 100'
-            );
-            foreach ($opinions as &$opinion) {
-                $opinion['author_name_slug'] = \Onm\StringUtils::get_title($opinion['name']);
-            }
-
-            $this->view->assign('articlesByCategory', $articlesByCategory);
-            $this->view->assign('opinions', $opinions);
+            $this->view->assign(array('contents' => $contents));
         }
 
         return $this->buildResponse($format, $cacheID, 'web');
@@ -134,64 +97,118 @@ class SitemapController extends Controller
         if (($this->view->caching == 0)
             || !$this->view->isCached('sitemap/sitemap.tpl', $cacheID)
         ) {
-            $this->cm  = new \ContentManager();
-            $this->ccm = \ContentCategoryManager::get_instance();
+            // Fetch contents
+            $contents = $this->fetchContents(array());
 
-            list($this->availableCategories, $this->subcats, $this->other) =
-                $this->ccm->getArraysMenu(0, 1);
-
-            $articlesByCategory = array();
-            $maxArticlesByCategory = floor(900 / count($this->availableCategories));
-
+            // Fetch images and videos from contents
             $er = getService('entity_repository');
-            // Foreach available category and retrieve articles from 700 days ago
-            foreach ($this->availableCategories as $category) {
-                if ($category->inmenu == 1
-                    && $category->internal_category == 1
-                ) {
-                    $articlesByCategory[$category->name] = $this->cm->getArrayOfArticlesInCategory(
-                        $category->pk_content_category,
-                        'content_status=1 AND fk_content_type=1 ',
-                        'ORDER BY changed DESC',
-                        $maxArticlesByCategory
-                    );
-                    $articlesByCategory[$category->name] =
-                        $this->cm->getInTime($articlesByCategory[$category->name]);
-
-                    foreach ($articlesByCategory[$category->name] as &$value) {
-                        $aux = $er->find('Article', $value['pk_content']);
-                        if (!empty($aux->img1)) {
-                            $value['image_path'] = \Photo::getPhotoPath($aux->img1);
-                        } elseif (!empty($aux->img2)) {
-                            $value['image_path'] = \Photo::getPhotoPath($aux->img2);
-                        }
+            foreach ($contents as $key => &$content) {
+                if (!empty($content->params['bodyLink'])) {
+                    // Remove external articles
+                    unset($contents[$key]);
+                } else {
+                    // Get content image
+                    if (!empty($content->img1)) {
+                        $content->image = $er->find('Photo', $content->img2);
+                    } elseif (!empty($content->img2)) {
+                        $content->image = $er->find('Photo', $content->img2);
+                    }
+                    // Get content video
+                    if (!empty($content->fk_video)) {
+                        $content->video = $er->find('Video', $content->fk_video);
+                    } elseif (!empty($content->fk_video2)) {
+                        $content->video = $er->find('Video', $content->fk_video2);
                     }
                 }
             }
 
-            // Get latest opinions
-            $opinions = $this->cm->getOpinionAuthorsPermalinks(
-                'contents.content_status=1',
-                'ORDER BY position ASC, changed DESC LIMIT 100'
+            $this->view->assign(
+                array(
+                    'contents'   => $contents,
+                    'googleNews' => s::get('google_news_name'),
+                )
             );
-
-            foreach ($opinions as &$opinion) {
-                $opinion['author_name_slug'] = \Onm\StringUtils::get_title($opinion['name']);
-            }
-
-            $this->view->assign('articlesByCategory', $articlesByCategory);
-            $this->view->assign('opinions', $opinions);
         }
 
         return $this->buildResponse($format, $cacheID, 'news');
     }
 
     /**
+     * Renders the image sitemap
+     *
+     * @param Request $request the request object
+     *
+     * @return Response the response object
+     **/
+    public function imageAction(Request $request)
+    {
+        $format = $request->query->filter('_format', 'xml', FILTER_SANITIZE_STRING);
+
+        $this->view = new \Template(TEMPLATE_USER);
+        $this->view->setConfig('sitemap');
+        $cacheID = $this->view->generateCacheId('sitemap', '', 'image');
+
+        if (($this->view->caching == 0)
+            || !$this->view->isCached('sitemap/sitemap.tpl', $cacheID)
+        ) {
+            // Fetch contents
+            $contents = $this->fetchContents(
+                array('content_type_name' => array(array('value' => 'photo')))
+            );
+
+            // Get contents referenced for this image
+            foreach ($contents as $key => &$content) {
+                $content->referenced = $this->getContentsWithReferencedImage($content->id);
+                if (!$content->referenced) {
+                    // Remove images without content referenced
+                    unset($contents[$key]);
+                } elseif (!empty($content->params['bodyLink'])) {
+                    // Remove external articles
+                    unset($contents[$key]);
+                }
+            }
+
+            $this->view->assign(array('contents' => $contents));
+        }
+
+        return $this->buildResponse($format, $cacheID, 'image');
+    }
+
+    /**
+     * Renders the video sitemap
+     *
+     * @param Request $request the request object
+     *
+     * @return Response the response object
+     **/
+    public function videoAction(Request $request)
+    {
+        $format = $request->query->filter('_format', 'xml', FILTER_SANITIZE_STRING);
+
+        $this->view = new \Template(TEMPLATE_USER);
+        $this->view->setConfig('sitemap');
+        $cacheID = $this->view->generateCacheId('sitemap', '', 'video');
+
+        if (($this->view->caching == 0)
+            || !$this->view->isCached('sitemap/sitemap.tpl', $cacheID)
+        ) {
+            // Fetch contents
+            $contents = $this->fetchContents(
+                array('content_type_name' => array(array('value' => 'video')))
+            );
+
+            $this->view->assign(array('contents' => $contents));
+        }
+
+        return $this->buildResponse($format, $cacheID, 'video');
+    }
+
+    /**
      * Formats the response
      *
      * @param string format whether compress the sitemap or not
-     * @param string $cacheID the identificator for this cache
-     * @param string $action the type of sitemap: news or web
+     * @param string $cacheID the identifier for this cache
+     * @param string $action the type of sitemap: news, web, image or video
      *
      * @return Response the response object
      **/
@@ -201,7 +218,7 @@ class SitemapController extends Controller
         $contents = $this->renderView('sitemap/sitemap.tpl', array('cache_id' => $cacheID));
 
         if ($format == 'xml.gz') {
-            // disable ZLIB ouput compression
+            // disable ZLIB output compression
             ini_set('zlib.output_compression', 'Off');
             // compress data
             $contents = gzencode($contents);
@@ -209,7 +226,7 @@ class SitemapController extends Controller
             $headers = array(
                 'Content-Type'     => 'application/x-download',
                 'Content-Encoding' => 'gzip',
-                'Content-Length' => strlen($contents)
+                'Content-Length'   => strlen($contents)
             );
         } else {
             // Return the output as xml
@@ -219,5 +236,79 @@ class SitemapController extends Controller
         $headers['x-tags'] = 'sitemap';
 
         return new Response($contents, 200, $headers);
+    }
+
+    /**
+     * Fetch articles and opinions contents
+     *
+     * @param int Max number of contents
+     *
+     * @return Array all contents
+     **/
+    public function fetchContents($criteria = array(), $limit = 100)
+    {
+        // Set search filters
+        $filters = array(
+            'content_type_name' => array(
+                'union' => 'OR',
+                array('value' => 'article'),
+                array('value' => 'opinion')
+            ),
+            'content_status'    => array(array('value' => 1)),
+            'in_litter'         => array(array('value' => 1, 'operator' => '<>'))
+        );
+
+        if (!empty($criteria)) {
+            $filters = array_merge($filters, $criteria);
+        }
+
+        // Fetch contents
+        $er = getService('entity_repository');
+        $contents = $er->findBy($filters, array('created' => 'desc'), $limit, 1);
+
+        return $contents;
+    }
+
+    /**
+    * Fetches articles or albums associated with the image
+    *
+    * @param $imageId The image id
+    *
+    * @return Array | Contents associated with this image
+    */
+    public function getContentsWithReferencedImage($imageId)
+    {
+        $cache = $this->get('cache');
+
+        $content = $cache->fetch(CACHE_PREFIX."_image_".$imageId);
+
+        if (!$content) {
+            $sql = "SELECT `pk_article` as id FROM `articles`
+                    WHERE `img1` = ? OR `img2` = ?";
+
+            $GLOBALS['application']->conn->SetFetchMode(ADODB_FETCH_ASSOC);
+            $rs = $GLOBALS['application']->conn->Execute($sql, array($imageId, $imageId));
+
+            $er = getService('entity_repository');
+            if (!$rs || $rs->fields === false) {
+                $sql = "SELECT `pk_album` as id FROM `albums_photos`
+                        WHERE `pk_photo` = ?";
+
+                $GLOBALS['application']->conn->SetFetchMode(ADODB_FETCH_ASSOC);
+                $rs = $GLOBALS['application']->conn->Execute($sql, array($imageId));
+
+                if (!$rs || $rs->fields === false) {
+                    return false;
+                } else {
+                    $content = $er->find('Album', $rs->fields['id']);
+                }
+            } else {
+                $content = $er->find('Article', $rs->fields['id']);
+            }
+            // Save the content in cache
+            $cache->save(CACHE_PREFIX."_image_".$imageId, $content, 300);
+        }
+
+        return $content;
     }
 }
