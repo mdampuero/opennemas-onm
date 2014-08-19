51c4a3515d582bc37c97d617d4a38c8e54004348
Diego Blanco <diego@openhost.es>
2014-08-19 10:53:05 +0200

    Update command controller in manager application
    - Rename input variable name to 'data'.
    - Update documentation.
    
    Signed-off-by: Diego Blanco Estévez <diego@openhost.es>

--- public/themes/manager/js/controllers/CommandCtrl.js
+++ public/themes/manager/js/controllers/CommandCtrl.js
@@ -1,9 +1,26 @@
-
-
+/**
+ * Handles all actions in commands listing.
+ *
+ * @param  Object $scope      The current scope.
+ * @param  Object itemService The item service.
+ * @param  Object data        The input data.
+ *
+ * @return Object The command controller.
+ */
 angular.module('ManagerApp.controllers').controller('CommandCtrl',
-    function ($scope, itemService, list) {
+    function ($scope, itemService, data) {
+        /**
+         * List of available commands.
+         *
+         * @type Object
+         */
+        $scope.commands = data.commands;
 
-        $scope.commands = list.commands;
-        $scope.instances = list.instances;
+        /**
+         * List of instances (to clear smarty-cache)
+         *
+         * @type Object
+         */
+        $scope.instances = data.instances;
     }
 );
