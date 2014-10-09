f0752f61220543a219e5e05e47067e4aa16f03eb
Fran Dieguez <fran@openhost.es>
2014-10-09 12:08:15 +0200

    Add edge cache support to frontpage opinion inner

--- src/Frontend/Controller/OpinionsController.php
+++ src/Frontend/Controller/OpinionsController.php
@@ -845,7 +845,8 @@ class OpinionsController extends Controller
             array(
                 'cache_id'        => $cacheID,
                 'actual_category' => 'opinion',
-                //'x-tags'          => 'opinion,'.$opinionID,
+                'x-tags'          => 'opinion,'.$opinionID,
+                'x-cache-for' => '1d'
             )
         );
     }
