a7d58aedd40b1f7252b261240b50831e9df7f6f5
Diego Blanco <diego@openhost.es>
2014-11-13 11:37:47 +0100

    Add animations to show/hide filters-bar on quick return
    Signed-off-by: Diego Blanco Estévez <diego@openhost.es>

--- public/assets/js/manager.js
+++ public/assets/js/manager.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-    var current = 0;
-    var previous = 0;
-
+    var current   = 0;
+    var positions = [];
 
     $(window).scroll(function () {
-        var previous = current;
+        positions.push(current);
         current = $(window).scrollTop();
 
-        if (current > 0 && current < previous) {
-            $('.filters-navbar').addClass("qr");
+        if (positions.length < 10) {
+            return false;
+        }
+
+        positions.splice(0, 1);
+
+        if (!$('.filters-navbar').hasClass('show-qr') && current > 0
+                && current < positions[7]) {
+            $('.filters-navbar').stop(true, true).addClass('show-qr');
+        } else if ($('.filters-navbar').hasClass('show-qr')
+                && current > positions[7]) {
+            $('.filters-navbar').stop(true, true).addClass('hide-qr').delay(250).queue(function() {
+                $(this).removeClass('show-qr').removeClass('hide-qr').dequeue();
+            });
         } else {
-            $('.filters-navbar').removeClass("qr");
+            $(this).removeClass('show-qr').removeClass('hide-qr');
         }
     });
 });
