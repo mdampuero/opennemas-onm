170957e62ddad2f1cbcc5d63296c1a7a06177cb0
Diego Blanco <diego@openhost.es>
2014-12-23 18:19:56 +0100

    Refactor angular application in manager
    - Split ManagerApp.js in app.js, config.js and routing.js
    - Delete Controllers.js and move controllers module definition to app.js.
    
    Signed-off-by: Diego Blanco Estévez <diego@openhost.es>

---
+++ public/themes/manager/js/app.js
@@ -0,0 +1,28 @@
+angular.module('ManagerApp', [
+  'ngAnimate',
+  'webStorageModule',
+  'ngRoute',
+  'ngTouch',
+  'angular-loading-bar',
+  'ui.bootstrap',
+  'ui.select2',
+  'pascalprecht.translate',
+  'ngQuickDate',
+  'ngTagsInput',
+  'checklist-model',
+  'http-interceptor',
+  'googlechart',
+  'vcRecaptcha',
+  'sun.scrollable',
+  'onm.routing',
+  'onm.item',
+  'onm.messenger',
+  'onm.auth',
+  'onm.gravatar',
+  'onm.form-autofill-fix',
+  'onm.history',
+  'onm.resizable',
+  'ManagerApp.controllers'
+]);
+
+angular.module('ManagerApp.controllers', []);
