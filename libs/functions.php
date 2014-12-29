b6d285e26e4bc47f3e0554c84d8a09295d73961d
Fran Dieguez <fran@openhost.es>
2014-12-29 16:29:27 +0100

    Remove stripslashes_deep method (unused)
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- libs/functions.php
+++ libs/functions.php
@@ -180,16 +180,6 @@ function getOriginalIdAndContentTypeFromSlug($slug)
     return $returnValue;
 }
 
-}
-
-function stripslashes_deep($value)
-{
-    $value = is_array($value) ?
-                array_map('stripslashes_deep', $value) :
-                stripslashes($value);
-
-    return $value;
-}
 /**
 * Perform a permanently redirection (301)
 *
