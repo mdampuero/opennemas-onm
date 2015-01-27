0688ab9aa73632d1576a5fd945e69bafe1130ceb
Diego Blanco <diego@openhost.es>
2015-01-27 10:42:22 +0100

    Add icons to sidebar

--- src/Backend/Resources/Menu.php
+++ src/Backend/Resources/Menu.php
@@ -23,6 +23,7 @@ $menuXml = [
                 "id"          => "frontpage_manager",
                 "title"       => _("Frontpage Manager"),
                 "link"        => url('admin_frontpage_list'),
+                'icon'        => 'fa fa-newspaper-o',
                 "module_name" => "FRONTPAGE_MANAGER",
                 "privilege"   => "ARTICLE_FRONTPAGE",
             ],
@@ -31,6 +32,7 @@ $menuXml = [
                 "id"          => "ads_manager",
                 "title"       => _("Advertisements"),
                 "link"        => url('admin_ads'),
+                'icon'        => 'fa fa-bullhorn',
                 "module_name" => "ADS_MANAGER",
                 "privilege"   => "ADVERTISEMENT_ADMIN",
             ],
@@ -39,6 +41,7 @@ $menuXml = [
                 "id"          => "widget_manager",
                 "title"       => _("Widgets"),
                 "link"        => url('admin_widgets'),
+                'icon'        => 'fa fa-gamepad',
                 "module_name" => "WIDGET_MANAGER",
                 "privilege"   => "WIDGET_ADMIN",
             ],
@@ -47,6 +50,7 @@ $menuXml = [
                 "id"          => "sidebar_manager",
                 "title"       => _("Sidebars"),
                 "link"        => url('admin_sidebars'),
+                'icon'        => 'fa fa-indent fa-flip-horizontal',
                 "module_name" => "SIDEBAR_MANAGER",
                 "privilege"   => "SIDEBAR_ADMIN",
             ],
@@ -55,6 +59,7 @@ $menuXml = [
                 "id"          => "menu_manager",
                 "title"       => _("Menus"),
                 "link"        => url('admin_menus'),
+                'icon'        => 'fa fa-list-alt',
                 "module_name" => "MENU_MANAGER",
                 "privilege"   => "MENU_ADMIN",
             ],
@@ -73,6 +78,7 @@ $menuXml = [
                 "title"       => _("Articles"),
                 "link"        => url('admin_articles'),
                 "module_name" => "ARTICLE_MANAGER",
+                'icon'        => 'fa fa-file-text',
                 "privilege"   => "ARTICLE_PENDINGS",
             ],
             [
@@ -80,12 +86,14 @@ $menuXml = [
                 "title"       => _("Opinions"),
                 "link"        => '#',
                 "module_name" => "OPINION_MANAGER",
+                'icon'        => 'fa fa-quote-right',
                 "privilege"   => "OPINION_ADMIN",
                 "submenu"     => [
                     [
                         "id"          => "opinion_manager",
                         "title"       => _("Article opinions"),
                         "link"        => url('admin_opinions'),
+                        'icon' => 'fa fa-quote-right',
                         "module_name" => "OPINION_MANAGER",
                         "privilege"   => "OPINION_ADMIN",
                     ],
@@ -93,6 +101,7 @@ $menuXml = [
                         "id"          => "authors",
                         "title"       => _("Authors"),
                         "link"        => url('admin_opinion_authors'),
+                        'icon'        => 'fa fa-user',
                         "module_name" => "OPINION_MANAGER",
                         "privilege"   => "AUTHOR_ADMIN",
                     ],
@@ -103,6 +112,7 @@ $menuXml = [
                 "title"       => _("Comments"),
                 "link"        => url('admin_comments'),
                 "module_name" => "COMMENT_MANAGER",
+                'icon'        => 'fa fa-comment',
                 "privilege"   => "COMMENT_ADMIN",
             ],
 
@@ -110,6 +120,7 @@ $menuXml = [
                 "id"          => "poll_manager",
                 "title"       => _("Polls"),
                 "link"        => url('admin_polls'),
+                'icon'        => 'fa fa-pie-chart',
                 "module_name" => "POLL_MANAGER",
                 "privilege"   => "POLL_ADMIN",
             ],
@@ -119,6 +130,7 @@ $menuXml = [
                 "title"       => _("Static Pages"),
                 "link"        => url('admin_staticpages'),
                 "module_name" => "STATIC_PAGES_MANAGER",
+                'icon'        => 'fa fa-file',
                 "privilege"   => "STATIC_PAGE_ADMIN",
             ],
 
@@ -127,6 +139,7 @@ $menuXml = [
                 "title"       => _("Specials"),
                 "link"        => url('admin_specials'),
                 "module_name" => "SPECIAL_MANAGER",
+                'icon'        => 'fa fa-star',
                 "privilege"   => "SPECIAL_ADMIN",
             ],
 
@@ -135,6 +148,7 @@ $menuXml = [
                 "title"       => _("Letter to the editor"),
                 "link"        => url('admin_letters'),
                 "module_name" => "LETTER_MANAGER",
+                'icon'        => 'fa fa-envelope',
                 "privilege"   => "LETTER_ADMIN",
             ],
 
@@ -143,6 +157,7 @@ $menuXml = [
                 "title"       => _("Category manager"),
                 "link"        => url('admin_categories'),
                 "module_name" => "CATEGORY_MANAGER",
+                'icon'        => 'fa fa-bookmark',
                 "privilege"   => "CATEGORY_ADMIN",
             ],
 
@@ -161,12 +176,14 @@ $menuXml = [
                 "title"       => _("Images"),
                 "link"        => url('admin_images'),
                 "module_name" => "IMAGE_MANAGER",
+                'icon'        => 'fa fa-picture-o',
                 "privilege"   => "PHOTO_ADMIN",
             ],
             [
                 "id"          => "file_manager",
                 "title"       => _("Files"),
                 "link"        => url('admin_files'),
+                'icon'        => 'fa fa-file',
                 "module_name" => "FILE_MANAGER",
                 "privilege"   => "ATTACHMENT_ADMIN",
             ],
@@ -175,6 +192,7 @@ $menuXml = [
                 "title"       => _("Videos"),
                 "link"        => url('admin_videos'),
                 "module_name" => "VIDEO_MANAGER",
+                'icon'        => 'fa fa-film',
                 "privilege"   => "VIDEO_ADMIN",
             ],
             [
@@ -182,6 +200,7 @@ $menuXml = [
                 "title"       => _("Albums"),
                 "link"        => url('admin_albums'),
                 "module_name" => "ALBUM_MANAGER",
+                'icon'        => 'fa fa-stack-overflow',
                 "privilege"   => "ALBUM_ADMIN",
             ],
             [
@@ -189,6 +208,7 @@ $menuXml = [
                 "title"       => _("News Stand"),
                 "link"        => url('admin_covers'),
                 "module_name" => "KIOSKO_MANAGER",
+                'icon'        => 'fa fa-newspaper-o',
                 "privilege"   => "KIOSKO_ADMIN",
             ],
             [
@@ -196,6 +216,7 @@ $menuXml = [
                 "title"       => _("Books"),
                 "link"        => url('admin_books'),
                 "module_name" => "BOOK_MANAGER",
+                'icon'        => 'fa fa-book',
                 "privilege"   => "BOOK_ADMIN",
             ],
 
@@ -216,6 +237,7 @@ $menuXml = [
                 "title"       => _("Global Search"),
                 "link"        => url('admin_search'),
                 "module_name" => "ADVANCED_SEARCH",
+                'icon'        => 'fa fa-search',
                 "privilege"   => "SEARCH_ADMIN",
             ],
             [
@@ -223,6 +245,7 @@ $menuXml = [
                 "title"       => _("Trash"),
                 "link"        => url('admin_trash'),
                 "module_name" => "TRASH_MANAGER",
+                'icon'        => 'fa fa-trash-o',
                 "privilege"   => "TRASH_ADMIN",
             ],
             [
@@ -230,6 +253,7 @@ $menuXml = [
                 "title"       => _("Newsletter"),
                 "link"        => url('admin_newsletters'),
                 "module_name" => "NEWSLETTER_MANAGER",
+                'icon'        => 'fa fa-rss',
                 "privilege"   => "NEWSLETTER_ADMIN",
             ],
             [
@@ -237,11 +261,13 @@ $menuXml = [
                 "title"       => _("Keywords"),
                 "link"        => url('admin_keywords'),
                 "module_name" => "KEYWORD_MANAGER",
+                'icon'        => 'fa fa-trash-o',
                 "privilege"   => "PCLAVE_ADMIN",
             ],
             [
                 "id"          => "paywall",
                 "title"       => _("Paywall"),
+                'icon'        => 'fa fa-paypal',
                 "link"        => '#',
                 "submenu" => [
                     [
@@ -249,6 +275,7 @@ $menuXml = [
                         "title"       => _("Statistics"),
                         "link"        => url('admin_paywall'),
                         "module_name" => "PAYWALL",
+                        'icon'        => 'fa fa-bar-chart',
                         "privilege"   => "PAYWALL",
                     ],
                     [
@@ -256,6 +283,7 @@ $menuXml = [
                         "title"       => _("Paywall users"),
                         "link"        => url('admin_paywall_users'),
                         "module_name" => "PAYWALL",
+                        'icon'        => 'fa fa-user',
                         "privilege"   => "PAYWALL",
                     ],
                     [
@@ -263,6 +291,7 @@ $menuXml = [
                         "title"       => _("Paywall purchases"),
                         "link"        => url('admin_paywall_purchases'),
                         "module_name" => "PAYWALL",
+                        'icon'        => 'fa fa-shopping-cart',
                         "privilege"   => "PAYWALL",
                     ],
                 ]
@@ -272,6 +301,7 @@ $menuXml = [
                 "title"       => _("Sync Instances"),
                 "link"        => url('admin_instance_sync'),
                 "module_name" => "SYNC_MANAGER",
+                'icon'        => 'fa fa-exchange',
                 "privilege"   => "SYNC_ADMIN",
             ],
             [
@@ -279,6 +309,7 @@ $menuXml = [
                 "title"       => _("News Agency"),
                 "link"        => url('admin_news_agency'),
                 "module_name" => "NEWS_AGENCY_IMPORTER",
+                'icon'        => 'fa fa-cloud-download',
                 "privilege"   => "IMPORT_ADMIN",
             ],
             [
@@ -286,6 +317,7 @@ $menuXml = [
                 "title"       => _("XML Importer"),
                 "link"        => url('admin_importer_xmlfile'),
                 "module_name" => "PAPER_IMPORT",
+                'icon'        => 'fa fa-download',
                 "privilege"   => "IMPORT_XML",
             ],
         ],
@@ -303,6 +335,7 @@ $menuXml = [
                 "title"       => _("General"),
                 "link"        => url('admin_system_settings'),
                 "module_name" => "SETTINGS_MANAGER",
+                'icon'        => 'fa fa-gear',
                 "privilege"   => "ONM_SETTINGS",
             ],
             [
@@ -317,6 +350,7 @@ $menuXml = [
                         "title"       => _("Users"),
                         "link"        => url('admin_acl_user'),
                         "module_name" => "USER_MANAGER",
+                        'icon'        => 'fa fa-user',
                         "privilege"   => "USER_ADMIN",
                     ],
                     [
@@ -324,6 +358,7 @@ $menuXml = [
                         "title"       => _("User Groups"),
                         "link"        => url('admin_acl_usergroups'),
                         "module_name" => "USER_GROUP_MANAGER",
+                        'icon'        => 'fa fa-users',
                         "privilege"   => "USER_ADMIN",
                     ],
                 ],
@@ -333,6 +368,7 @@ $menuXml = [
                 "title"       => _("Cache manager"),
                 "link"        => url('admin_tpl_manager'),
                 "module_name" => "CACHE_MANAGER",
+                'icon'        => 'fa fa-database',
                 "privilege"   => "CACHE_ADMIN",
             ],
             [
@@ -340,6 +376,7 @@ $menuXml = [
                 "title"       => _("SQL error log"),
                 "link"        => url('admin_databaseerrors'),
                 "module_name" => "LOG_SQL",
+                'icon'        => 'fa fa-code',
                 "privilege"   => "ONLY_MASTERS",
             ],
 
@@ -355,6 +392,7 @@ $menuXml = [
                 "id"          => "faq",
                 "title"       => _("FAQ"),
                 "link"        => 'http://help.opennemas.com',
+                'icon'        => 'fa fa-question-circle',
                 "module_name" => "CACHE_MANAGER",
                 "privilege"   => "CACHE_ADMIN",
             ],
@@ -362,7 +400,7 @@ $menuXml = [
                 "id"          => "support",
                 "title"       => _("Contact support"),
                 "link"        => 'javascript:UserVoice.showPopupWidget();',
-                'class'       => 'support-button',
+                'icon'        => 'fa fa-support',
                 "module_name" => "LOG_SQL",
                 "privilege"   => "ONLY_MASTERS",
             ],
