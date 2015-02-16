86d9cdf93f30e929e2d6ec8865d27f72ff13f578
Fran Dieguez <fran@openhost.es>
2015-02-16 23:40:53 +0100

    Add backend web service for keywords
    Signed-off-by: Fran Dieguez <fran@openhost.es>

---
+++ src/BackendWebService/Controller/KeywordsController.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Handles the actions for the keywords
+ *
+ * @package Backend_Controllers
+ **/
+/**
+ * This file is part of the Onm package.
+ *
+ * (c)  OpenHost S.L. <developers@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+namespace BackendWebService\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Backend\Annotation\CheckModuleAccess;
+use Onm\Framework\Controller\Controller;
+
+/**
+ * Handles the actions for the keywords
+ *
+ * @package Backend_Controllers
+ **/
+class KeywordsController extends Controller
+{
+    /**
+     * Lists all the keywords
+     *
+     * @param Request $request the request object
+     *
+     * @return Response
+     *
+     * @Security("has_role('PCLAVE_ADMIN')")
+     *
+     * @CheckModuleAccess(module="KEYWORD_MANAGER")
+     **/
+    public function listAction(Request $request)
+    {
+        $search = $request->request->get('search');
+        $page = $request->query->getDigits('page', 1);
+        $elementsPerPage = $request->request->getDigits('elements_per_page', 10);
+
+        $filter = '';
+        if (array_key_exists('title', $search)) {
+            $name = $search['title'][0]['value'];
+            $filter = '`pclave` LIKE "%' . $name . '%"';
+        }
+
+        $keywordManager = new \PClave();
+        $keywords = $keywordManager->find($filter);
+
+        $results = array_slice($keywords, ($page-1) * $elementsPerPage, ITEMS_PAGE);
+
+        return new JsonResponse(
+            array(
+                'elements_per_page' => 10,
+                'extra'             => array(
+                    'types'      => \PClave::getTypes(),
+                ),
+                'page'              => $page,
+                'results'           => $results,
+                'total'             => count($keywords),
+            )
+        );
+    }
+
+    /**
+     * Deletes a content.
+     *
+     * @param  integer      $id          Content id.
+     * @param  string       $contentType Content type name.
+     * @return JsonResponse              The response object.
+     *
+     * @Security("has_role('PCLAVE_DELETE')")
+     *
+     * @CheckModuleAccess(module="KEYWORD_MANAGER")
+     */
+    public function removePermanentlyAction($id)
+    {
+        $errors  = array();
+        $success = array();
+
+        $keyword = new \PClave();
+        $keyword->read($id);
+
+        if (!is_null($keyword->id)) {
+            $deleted = $keyword->delete($id);
+
+            if ($deleted) {
+                $success[] = array(
+                    'id'      => $id,
+                    'message' => _('Item deleted successfully'),
+                    'type'    => 'success'
+                );
+            } else {
+                $errors[] = array(
+                    'id'      => $id,
+                    'message' => sprintf(_('Unable to delete the item with id "%d"'), $id),
+                    'type'    => 'error'
+                );
+            }
+        } else {
+            $errors[] = array(
+                'id'      => $id,
+                'message' => sprintf(_('Unable to find the item with id "%d"'), $id),
+                'type'    => 'error'
+            );
+        }
+
+        return new JsonResponse(
+            array(
+                'messages'  => array_merge($success, $errors),
+            )
+        );
+    }
+}
