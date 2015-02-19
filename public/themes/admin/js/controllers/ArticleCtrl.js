e1eeb593952128072f2f7ddbc2ba38528211b071
Diego Blanco <diego@openhost.es>
2015-02-20 00:07:56 +0100

    Add actions to insert images in CKEditor instances and to update scope

--- public/themes/admin/js/controllers/ArticleCtrl.js
+++ public/themes/admin/js/controllers/ArticleCtrl.js
@@ -9,11 +9,42 @@ angular.module('BackendApp.controllers').controller('ArticleCtrl', [
     $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
 
     $rootScope.$on('MediaPicker.insert', function (event, args) {
-      for (var i = 0; i < args.items.length; i++) {
-        var html = renderer.renderImage(args.items[i]);
-        console.log(html);
-        onmEditor.get(args.target).insertHtml(html);
-      };
+      if (/editor.*/.test(args.target)) {
+        var target = args.target.replace('editor.', '');
+        return $scope.insertInCKEditor(target, args.items);
+      }
+
+      $scope.insertInModel(args.target, args.items);
     });
+
+    /**
+     * Inserts an array of items in a CKEditor instance.
+     *
+     * @param string target The target id.
+     * @param array  items  The items to insert.
+     */
+    $scope.insertInCKEditor = function(target, items) {
+      if (items instanceof Array) {
+        for (var i = 0; i < items.length; i++) {
+          var html = renderer.renderImage(items[i]);
+          onmEditor.get(target).insertHtml(html);
+        };
+
+        return;
+      }
+
+      var html = renderer.renderImage(items);
+      onmEditor.get(target).insertHtml(html);
+    };
+
+    /**
+     * Updates the scope with the items.
+     *
+     * @param string target The property to update.
+     * @param array  items  The new property value.
+     */
+    $scope.insertInModel = function(target, items) {
+      $scope[target] = items;
+    }
   }
 ]);
