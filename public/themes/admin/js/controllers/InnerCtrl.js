7fb1815276adb812f0d3f071c2a493e157bdd847
Diego Blanco <diego@openhost.es>
2015-02-19 17:02:51 +0100

    Initialize $scope with values from template

--- public/themes/admin/js/controllers/InnerCtrl.js
+++ public/themes/admin/js/controllers/InnerCtrl.js
@@ -4,6 +4,10 @@
 angular.module('BackendApp.controllers').controller('InnerCtrl', [
   '$scope',
   function($scope) {
-
+    // Initialize the scope with the input/select values.
+    $('input, select').each(function() {
+      var name = $(this).attr('name');
+      $scope[name] = $(this).val();
+    });
   }
 ]);
