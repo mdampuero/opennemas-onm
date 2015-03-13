9f75cf477d1db896f56a5541d24aa72877890d01
Diego Blanco <diego@openhost.es>
2015-03-13 12:22:41 +0100

    Add action to open a modal window and remove items from menu in MenuCtrl

--- public/themes/admin/js/controllers/MenuCtrl.js
+++ public/themes/admin/js/controllers/MenuCtrl.js
@@ -8,5 +8,30 @@ angular.module('BackendApp.controllers').controller('MenuCtrl', [
 
     // Initialize the super class and extend it.
     $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
+
+    /**
+     * Deletes an item from the menu.
+     *
+     * @param {integer} index The index of the item to remove.
+     */
+    $scope.removeItem = function(index) {
+      console.log(index);
+      $scope.menu.items.splice(index, 1);
+    };
+
+    /**
+     * Opens a modal window to add item to menu.
+     */
+    $scope.open = function() {
+      var modal = $modal.open({
+        templateUrl: 'modal-add-item',
+        backdrop: 'static',
+        controller: 'MenuModalCtrl'
+      });
+
+      modal.result.then(function(response) {
+        $scope.menu.items = $scope.menu.items.concat(response.items);
+      });
+    };
   }
 ]);
