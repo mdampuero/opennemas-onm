ee9278841a659bb3e39a9d812d677ee26226cb68
Fran Dieguez <fran@openhost.es>
2015-03-27 11:50:24 +0100

    Add dummy MyAccountCtrl.js file
    Signed-off-by: Fran Dieguez <fran@openhost.es>

---
+++ public/themes/admin/js/controllers/MyAccountCtrl.js
@@ -0,0 +1,11 @@
+angular.module('BackendApp.controllers')
+  /**
+   * Handle actions for article inner.
+  */
+  .controller('MyAccountCtrl', ['$rootScope', '$scope',
+  function($rootScope, $scope) {
+    'use strict';
+
+    console.log('test');
+  }
+]);
