935c2124faed4cd1c7c2973b70c8576e5d86c6b5
Diego Blanco <diego@openhost.es>
2015-04-14 18:49:00 +0200

    Fix answers initialization in poll inner when updating a poll and improve documentation

--- public/themes/admin/js/controllers/PollCtrl.js
+++ public/themes/admin/js/controllers/PollCtrl.js
@@ -1,65 +1,74 @@
-/**
- * Handle actions for poll inner form.
- */
-angular.module('BackendApp.controllers').controller('PollCtrl', [
-  '$controller', '$rootScope', '$scope',
-  function($controller, $rootScope, $scope) {
-    'use strict';
+angular.module('BackendApp.controllers')
+  /**
+   * @ngdoc controller
+   * @name  PollCtrl
+   *
+   * @description
+   *   Handles actions for poll inner
+   *
+   * @requires $controller
+   * @requires $rootScope
+   * @requires $scope
+   */
+  .controller('PollCtrl', ['$controller', '$rootScope', '$scope',
+    function($controller, $rootScope, $scope) {
+      'use strict';
 
-    // Initialize the super class and extend it.
-    $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
+      // Initialize the super class and extend it.
+      $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
 
-    /**
-     * Adds an empty answer to the answers list
-     *
-     * @param Object answers The poll answers.
-     */
-    $scope.addAnswer = function () {
-      $scope.answers.push({pk_item: '', votes: 0, item: ''});
-    };
+      /**
+       * @function addAnswer
+       * @memberOf PollCtrl
+       *
+       * @description
+       *   Adds an empty answer to the answer list.
+       */
+      $scope.addAnswer = function () {
+        $scope.answers.push({pk_item: '', votes: 0, item: ''});
+      };
 
-    /**
-     * Removes one answer fron the list given its index
-     *
-     * @param int the answer index in the list
-     */
-    $scope.removeAnswer = function (index) {
-      $scope.answers.splice(index, 1);
-    };
+      /**
+       * @function parseAnswers
+       * @memberOf PollCtrl
+       *
+       * @description
+       *   Parses the answers from the template and initializes the scope.
+       *
+       * @param {Object} name The poll answers.
+       */
+      $scope.parseAnswers = function(answers) {
+        if (answers === null) {
+          $scope.answers = [];
+        } else {
+          $scope.answers = answers;
+        }
+      };
 
-    /**
-     * Parse the answers from template and initialize the scope properly
-     *
-     * @param Object answers The poll answers.
-     */
-    $scope.parseAnswers = function(answers) {
-      if (answers === null) {
-        $scope.answers = [];
-      } else {
-        $scope.answers = answers;
-      }
-    };
+      /**
+       * @function removeAnswer
+       * @memberOf PollCtrl
+       *
+       * @description
+       *   Removes one answer from the answer list given its index.
+       *
+       * @param {Integer} index The index of the answer to remove.
+       */
+      $scope.removeAnswer = function (index) {
+        $scope.answers.splice(index, 1);
+      };
 
-    /**
-     * Updates internal parsedAnswers parameter when answers change.
-     *
-     * @param Object nv The new values.
-     * @param Object ov The old values.
-     */
-    $scope.$watch('answers', function(nv, ov) {
-      if (nv === ov) {
-        return false;
-      }
-
-      $scope.parsedAnswers = [];
-      for (var i = $scope.answers.length - 1; i >= 0; i--) {
-        $scope.parsedAnswers.push({
-          pk_item: $scope.answers[i].pk_item,
-          votes: $scope.answers[i].votes,
-          item: $scope.answers[i].item
-        });
-      }
-      $scope.parsedAnswers = JSON.stringify($scope.parsedAnswers.reverse());
-    }, true);
-  }
-]);
+      // Updates internal parsedAnswers parameter when answers change.
+      $scope.$watch('answers', function(nv, ov) {
+        $scope.parsedAnswers = [];
+        for (var i = $scope.answers.length - 1; i >= 0; i--) {
+          $scope.parsedAnswers.push({
+            pk_item: $scope.answers[i].pk_item,
+            votes: $scope.answers[i].votes,
+            item: $scope.answers[i].item
+          });
+        }
+        $scope.parsedAnswers = JSON.stringify($scope.parsedAnswers.reverse());
+      }, true);
+    }
+  ]);
