f7d4cf4e75b5b2a0dd8168cf5fb85db4f0125e2c
Diego Blanco <diego@openhost.es>
2015-04-21 13:03:02 +0200

    Prevent form submission on enter key press and update documentation

--- public/themes/admin/js/controllers/MenuCtrl.js
+++ public/themes/admin/js/controllers/MenuCtrl.js
@@ -1,48 +1,75 @@
-/**
- * Handle actions for article inner.
- */
-angular.module('BackendApp.controllers').controller('MenuCtrl', [
-  '$controller', '$http', '$modal', '$rootScope', '$scope', 'routing',
-  function($controller, $http, $modal, $rootScope, $scope, routing) {
-    'use strict';
+(function () {
+  'use strict';
 
-    // Initialize the super class and extend it.
-    $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
-
+  angular.module('BackendApp.controllers')
     /**
-     * Deletes an item from the menu.
+     * @ngdoc controller
+     * @name  MenuCtrl
      *
-     * @param {Integer} index The index of the item to remove.
+     * @requires $controller
+     * @requires $http
+     * @requires $modal
+     * @requires $rootScope
+     * @requires $scope
+     * @requires routing
+     *
+     * @description
+     *   Handle actions for article inner.
      */
-    $scope.removeItem = function(index, parentIndex) {
-      if (angular.isUndefined(parentIndex)) {
-        $scope.menu.items.splice(index, 1);
-        return;
+    .controller('MenuCtrl', ['$controller', '$http', '$modal', '$rootScope', '$scope', 'routing',
+      function($controller, $http, $modal, $rootScope, $scope, routing) {
+        // Initialize the super class and extend it.
+        $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
+
+        /**
+         * @function open
+         * @memberOf MenuCtrl
+         *
+         * @description
+         *   Opens a modal window to add item to menu.
+         */
+        $scope.open = function() {
+          var modal = $modal.open({
+            templateUrl: 'modal-add-item',
+            backdrop: 'static',
+            controller: 'MenuModalCtrl'
+          });
+
+          modal.result.then(function(response) {
+            $scope.menu.items = $scope.menu.items.concat(response.items);
+          });
+        };
+
+        /**
+         * @function removeItem
+         * @memberOf MenuCtrl
+         *
+         * @description
+         *   Deletes an item from the menu.
+         *
+         * @param {Integer} index The index of the item to remove.
+         */
+        $scope.removeItem = function(index, parentIndex) {
+          if (angular.isUndefined(parentIndex)) {
+            $scope.menu.items.splice(index, 1);
+            return;
+          }
+
+          $scope.menu.items[parentIndex].submenu.splice(index, 1);
+        };
+
+        // Updates the menu items input value when menu items change.
+        $scope.$watch('menu.items', function() {
+          $scope.menuItems = angular.toJson($scope.menu.items);
+        }, true)
+
+        // Prevent form submit on enter key press
+        $('.menu-items').on('keypress', function(e) {
+          if (e.keyCode === 13) {
+            e.preventDefault();
+          }
+        });
       }
+    ]);
+})();
 
-      $scope.menu.items[parentIndex].submenu.splice(index, 1);
-    };
-
-    /**
-     * Opens a modal window to add item to menu.
-     */
-    $scope.open = function() {
-      var modal = $modal.open({
-        templateUrl: 'modal-add-item',
-        backdrop: 'static',
-        controller: 'MenuModalCtrl'
-      });
-
-      modal.result.then(function(response) {
-        $scope.menu.items = $scope.menu.items.concat(response.items);
-      });
-    };
-
-    /**
-     * Updates the menu items input value when menu items change.
-     */
-    $scope.$watch('menu.items', function() {
-      $scope.menuItems = angular.toJson($scope.menu.items);
-    }, true)
-  }
-]);
