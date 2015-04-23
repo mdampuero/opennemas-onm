8e554e7632e1a4defbbcce85b963c2b92f88adfb
Diego Blanco <diego@openhost.es>
2015-04-23 19:51:06 +0200

    Update controller to handle paywall settings form

--- public/themes/admin/js/controllers/PaywallSettingsCtrl.js
+++ public/themes/admin/js/controllers/PaywallSettingsCtrl.js
@@ -1,25 +1,81 @@
-/**
- * Handle actions for cache config form.
- */
-angular.module('BackendApp.controllers').controller('PaywallSettingsCtrl', [
-  '$controller', '$rootScope', '$scope',
-  function($controller, $rootScope, $scope) {
-    'use strict';
+(function () {
+  'use strict';
 
-    // Initialize the super class and extend it.
-    $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
-
-
-
+  angular.module('BackendApp.controllers')
     /**
-     * Selects/unselects all instances.
+     * @ngdoc controller
+     * @name  PaywallSettingsCtrl
+     *
+     * @requires $controller
+     * @requires $rootScope
+     * @requires $scope
+     *
+     * @description
+     *   Handles actions for paywall settings configuration form.
      */
-    $scope.selectAll = function() {
-      if ($scope.selected.all) {
-        $scope.selected.contents = Object.keys($scope.config);
-      } else {
-        $scope.selected.contents = [];
+    .controller('PaywallSettingsCtrl', ['$controller', '$rootScope', '$scope',
+      function($controller, $rootScope, $scope) {
+        // Initialize the super class and extend it.
+        $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
+
+        /**
+         * @function addPaymentMode
+         * @memberOf PaywallSettingsCtrl
+         *
+         * @description
+         *   Add a new payment mode.
+         */
+        $scope.addPaymentMode = function() {
+          if (!$scope.settings.payment_modes) {
+            $scope.settings.payment_modes = [];
+          }
+
+          $scope.settings.payment_modes.push({
+            time: 'Day',
+            description: '',
+            price: 0
+          });
+        };
+
+        /**
+         * @function parseSettings
+         * @memberOf PaywallSettingsCtrl
+         *
+         * @description
+         *   Parses the current configuration.
+         *
+         * @param {Object} settings The current configuration.
+         */
+        $scope.parseSettings = function(settings) {
+          if (!settings) {
+            return;
+          }
+
+          settings.vat_percentage = parseInt(settings.vat_percentage);
+          for (var i = 0; i < settings.payment_modes.length; i++) {
+            settings.payment_modes[i].price = parseInt(settings.payment_modes[i].price);
+          }
+
+          $scope.settings = settings;
+        };
+
+        /**
+         * @function removePaymentMode
+         * @memberOf PaywallSettingsCtrl
+         *
+         * @description
+         *   Removes a payment mode.
+         *
+         * @param {Integer} index The index of the mode to remove.
+         */
+        $scope.removePaymentMode = function(index) {
+          $scope.settings.payment_modes.splice(index, 1);
+        };
+
+        // Updates the settings to submit in form when settings object changes
+        $scope.$watch('settings', function() {
+          $scope.fsettings = angular.toJson($scope.settings);
+        }, true);
       }
-    };
-  }
-]);
+    ]);
+})();
