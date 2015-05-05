0893b1dabe713c50f5656df93ea4fb26eee1a4cd
Diego Blanco <diego@openhost.es>
2015-05-05 15:28:27 +0200

    Fix cache namespace in OnmUserProvider

--- src/Backend/Security/OnmUserProvider.php
+++ src/Backend/Security/OnmUserProvider.php
@@ -79,7 +79,7 @@ class OnmUserProvider implements UserProviderInterface
 
         if (!$user) {
             $this->um->selectDatabase('onm-instances');
-            $this->cache->setNamespace('instance');
+            $this->cache->setNamespace('onm_manager');
             $GLOBALS['application']->conn->selectDatabase('onm-instances');
 
             $user = $this->um->findOneBy($criteria);
