996e8c09ea54f1b2cb1ab7b50dc026f25c202379
Diego Blanco <diego@openhost.es>
2015-05-14 13:05:47 +0200

    Improve documentation and message handling in angular messenger service

--- public/assets/src/angular-messenger/messenger.js
+++ public/assets/src/angular-messenger/messenger.js
@@ -1,33 +1,105 @@
-/**
- * Service used to show messages.
- */
-angular.module('onm.messenger', []).factory('messenger',  function () {
-    /**
-     * The messenger service.
-     *
-     * @type Object
-     */
-    var messenger = {};
+(function () {
+  'use strict';
 
+  /**
+   * @ngdoc module
+   * @name  onm.messenger
+   *
+   * @description
+   *   The `onm.messenger` module provides a service to show notifications
+   *   (toasts) on the screen.
+   */
+  angular.module('onm.messenger', [])
     /**
-     * Run messenger post action.
+     * @ngdoc service
+     * @name  Messenger
      *
-     * @param array params Array of parameters.
+     * @description
+     *   Service to display notifications (toast) on the screen.
      */
-    messenger.post = function(params) {
-        Messenger.options = {
-            extraClasses: 'messenger-fixed messenger-on-bottom',
+    .service('messenger',  function () {
+        this.hideAfter = 5;
+        this.showCloseButton = true;
+
+        /**
+         * @function createMessage
+         * @memberOf Messenger
+         *
+         * @description
+         *   Creates a new message from a string.
+         *
+         * @param {String} str  The message text.
+         * @param {String} type The message type.
+         *
+         * @return {Object} The message object.
+         */
+        this.createMessage = function(str, type) {
+          return {
+            id:      new Date().getTime(),
+            message: str,
+            type:    type ? type : 'info'
+          };
         };
 
-        params.hideAfter = 5;
-        params.showCloseButton = true;
+        /**
+         * @function post
+         * @memberOf Messenger
+         *
+         * @description
+         *   Run messenger post action.
+         *
+         * @param mixed  message The message to post.
+         * @param string type    The message type.
+         */
+        this.post = function(message, type) {
+          Messenger.options = {
+            extraClasses: 'messenger-fixed messenger-on-bottom'
+          };
 
-        if (!params.id) {
-            params.id = new Date().getTime();
-        }
+          if (typeof message === 'string') {
+            message = this.createMessage(message, type);
+          }
 
-        Messenger().post(params);
-    };
+          // Array of messages
+          if (message instanceof Array) {
+            return this.postMessages(message, type);
+          }
 
-    return messenger;
-});
+          // Single message
+          this.postMessage(message, type);
+        };
+
+        /**
+         * @function postMessage
+         * @memberOf Messenger
+         *
+         * @description
+         *   Posts a message.
+         *
+         * @param {Object} message The message to post.
+         */
+        this.postMessage = function(message) {
+          message.hideAfter       = this.hideAfter;
+          message.showCloseButton = this.showCloseButton;
+
+          Messenger().post(message);
+        };
+
+        /**
+         * @function postMessages
+         * @memberOf Messenger
+         *
+         * @description
+         *   Posts an array of messages.
+         *
+         * @param {Array} messages The array of messages.
+         */
+        this.postMessages = function(messages) {
+          for (var i = 0; i < messages.length; i++) {
+            this.postMessage(messages[i]);
+          }
+        };
+
+        return this;
+    });
+})();
