13b0ed2c711a293b4795f3e86fbac33b158ee449
Diego Blanco <diego@openhost.es>
2015-05-20 17:07:17 +0200

    Add junit configuration to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -38,8 +38,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
 
+    junitReporter: {
+      outputFile: 'build/karma-test-results.xml'
+    },
 
     // web server port
     port: 9876,
