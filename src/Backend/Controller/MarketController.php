33bffe7e250862a684bb92d6018659d15a0bb8d2
Diego Blanco <diego@openhost.es>
2015-06-17 12:54:16 +0200

    Add OTHER pseudo-plan to group single modules

--- src/Backend/Controller/MarketController.php
+++ src/Backend/Controller/MarketController.php
@@ -10,6 +10,11 @@ class MarketController extends Controller
     public function listAction()
     {
         $plans = \Onm\Module\ModuleManager::getAvailablePacks();
+        $plans[] = [
+            'id'   => 'OTHER',
+            'name' => _('Others')
+        ];
+
         return $this->render('market/list.tpl', [ 'plans' => $plans ]);
     }
 }
