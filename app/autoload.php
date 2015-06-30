d3c85716ea63fdc7ae44122a3b1feddbcc64ec52
Fran Dieguez <fran@openhost.es>
2015-07-01 01:08:08 +0200

    Bump composer dependencies versions

--- app/autoload.php
+++ app/autoload.php
@@ -120,6 +120,6 @@ $loader->useIncludePath(true);
 $loader->register();
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-require_once __DIR__.'/../vendor/sensio/framework-extra-bundle/Sensio/Bundle/FrameworkExtraBundle/Configuration/Security.php';
+// require_once __DIR__.'/../vendor/sensio/framework-extra-bundle/Sensio/Bundle/FrameworkExtraBundle/Configuration/Security.php';
 
 return $loader;
