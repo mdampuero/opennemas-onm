ead9312bfb9ba46da3c7659dcd350ddcb5e71ebc
Diego Blanco <diego@openhost.es>
2015-07-03 12:30:27 +0200

    ONM-504: Add repository to find widget by name

--- libs/smarty-onm-plugins/function.render_widget.php
+++ libs/smarty-onm-plugins/function.render_widget.php
@@ -5,13 +5,23 @@ function smarty_function_render_widget($params, &$smarty)
     $widgetName = isset($params['name']) ? $params['name'] : null;
     $widgetID   = isset($params['id'])   ? $params['id']   : null;
 
+    $er = getService('entity_repository');
+
     if (!is_null($widgetName)) {
         // Initialize widget from name
-        $widget = new Widget();
-        $widget->readIntelligentFromName($widgetName);
+        $criteria = [
+            'tables'            => [ 'widgets' ],
+            'pk_content'        => [
+                [ 'value' => 'pk_widget', 'field' => true ]
+            ],
+            'content'           => [ [ 'value' => $widgetName ] ],
+            'content_type_name' => [ [ 'value' => 'widget' ] ],
+            'content_status'    => [ [ 'value' => 1 ] ],
+            'in_litter'         => [ [ 'value' => 0 ] ],
+        ];
+        $widget = $er->findOneBy($criteria, null, 1, 1);
     } else {
         // Initialize widget from id
-        $er = getService('entity_repository');
         $widget = $er->find('Widget', $widgetID);
     }
 
