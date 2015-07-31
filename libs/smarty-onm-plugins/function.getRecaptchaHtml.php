6d3924e1ce3d4d8e5f8accfa9c0e50fdf27b3987
Alex Rodrigues <alex@openhost.es>
2015-07-31 09:17:30 +0200

    ONM-517: Add puglin to generate google recaptcha
    Signed-off-by: Alex Rico <alex@openhost.es>

---
+++ libs/smarty-onm-plugins/function.getRecaptchaHtml.php
@@ -0,0 +1,30 @@
+<?php
+
+use Onm\Settings as s;
+
+function smarty_function_getRecaptchaHtml($params, &$smarty)
+{
+    // Get settings repository
+    $sm = getService('setting_repository');
+
+    // Get system language
+    $lang = CURRENT_LANGUAGE;
+
+    // Get publicKey if is not in params
+    if (empty($params['public_key'])) {
+        $recaptcha = $sm->get('recaptcha');
+        // If not set public key, show a message
+        if (!is_array($recaptcha) || !array_key_exists('public_key', $recaptcha)) {
+            return _('reCaptcha public key is not set');
+        }
+    } else {
+        $recaptcha['public_key'] = $params['public_key'];
+    }
+
+    // Generate reCaptcha html
+    $output =
+        "<script type=\"text/javascript\" src=\"https://www.google.com/recaptcha/api.js?hl=".$lang."\"></script>\n".
+        "<div class=\"g-recaptcha\" data-sitekey=".$recaptcha['public_key']."></div>";
+
+    return $output;
+}
