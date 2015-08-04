9ad6038f6ab6cd8304f4d864eb21159e2a0f2c6f
Diego Blanco <diego@openhost.es>
2015-08-04 16:41:04 +0200

    Create action and route for checkout proccess

--- src/Backend/Controller/MarketController.php
+++ src/Backend/Controller/MarketController.php
@@ -7,6 +7,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MarketController extends Controller
 {
+    public function checkoutAction()
+    {
+        $billing = [];
+
+        $instance = $this->get('instance');
+
+        if (!empty($instance->metas)
+            && array_key_exists('billing', $instance->metas)
+        ) {
+            $billing = $instance->metas['billing'];
+        }
+
+        return $this->render('market/checkout.tpl', [ 'billing' => $billing ]);
+    }
+
     public function listAction()
     {
         $plans = \Onm\Module\ModuleManager::getAvailablePacks();
