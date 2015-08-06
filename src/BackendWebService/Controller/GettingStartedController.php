dc1daaa6ecfa365816a898d701e4e7b55572c9f2
Diego Blanco <diego@openhost.es>
2015-08-06 12:54:57 +0200

    Move web service actions in getting started to its own controller

---
+++ src/BackendWebService/Controller/GettingStartedController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace BackendWebService\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Onm\Framework\Controller\Controller;
+
+/**
+ * Handles requests for Getting Started.
+ */
+class GettingStartedController extends Controller
+{
+    /**
+     * Accept the terms of use for the current user.
+     *
+     * @param Request  $request The request object.
+     *
+     * @return Response The response object.
+     */
+    public function acceptTermsAction(Request $request)
+    {
+        $user = $this->getUser();
+
+        if ($user->isMaster()) {
+            $GLOBALS['application']->conn->selectDatabase('onm-instances');
+        }
+
+        if ($request->get('accept') && $request->get('accept') === 'true') {
+            $date = new \DateTime(null, new \DateTimeZone('UTC'));
+
+            $newMeta = array('terms_accepted' => $date->format('Y-m-d H:i:s'));
+            $user->setMeta($newMeta);
+
+            $user->meta = array_merge($user->meta, $newMeta);
+        } else {
+            $user->deleteMetaKey($user->id, 'terms_accepted');
+        }
+
+        return new JsonResponse();
+    }
+
+    /**
+     * Saves the payment information.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     */
+    public function savePaymentInfoAction(Request $request)
+    {
+        $billing = $request->request->get('billing');
+
+        if (!empty($billing)) {
+            $instance = $this->get('instance');
+            $instance->metas['billing'] = $billing;
+            $this->get('instance_manager')->persist($instance);
+        }
+
+        return new JsonResponse();
+    }
+}
