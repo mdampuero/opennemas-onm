e02c1e8b3829e207fbd072f2cbbc9c5baad913dd
Diego Blanco <diego@openhost.es>
2015-08-12 21:21:28 +0200

    Create a custom InvalidPersisterException exception

---
+++ src/Framework/FreshBooks/Exception/InvalidPersisterException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Framework\FreshBooks\Exception;
+
+class InvalidPersisterException extends \Exception
+{
+    /**
+     * Initializes the exception with a custom message.
+     *
+     * @param string $class The classname of the invalid persister.
+     */
+    public function __construct($class)
+    {
+        $message = "The persister \"$class\" does not exist.";
+
+        parent::__construct($message);
+    }
+}
