a55925b50731792bb3abc1f9468eb1b3dbf93a7c
Diego Blanco <diego@openhost.es>
2015-08-12 16:52:27 +0200

    Improve InvalidRepositoryException to show a custom message

--- src/Framework/FreshBooks/Exception/InvalidRepositoryException.php
+++ src/Framework/FreshBooks/Exception/InvalidRepositoryException.php
@@ -4,4 +4,15 @@ namespace Framework\FreshBooks\Exception;
 
 class InvalidRepositoryException extends \Exception
 {
+    /**
+     * Initializes the exception with a custom message.
+     *
+     * @param string $class The classname of the invalid repository.
+     */
+    public function __construct($class)
+    {
+        $message = "The repository \"$class\" does not exist.";
+
+        parent::__construct($message);
+    }
 }
