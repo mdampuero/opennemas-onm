4edb051cd80ef04121c9184e3e200d7c54fabdbd
Diego Blanco <diego@openhost.es>
2015-08-13 19:11:27 +0200

    Add method to clean Invoice before returning data and update tests

--- src/Framework/Tests/FreshBooks/Entity/InvoiceTest.php
+++ src/Framework/Tests/FreshBooks/Entity/InvoiceTest.php
@@ -17,7 +17,49 @@ class InvoiceTest extends \PHPUnit_Framework_TestCase
         foreach ($data as $key => $value) {
            $this->assertEquals($value, $entity->{$key});
         }
+    }
 
+    public function testCleanWithEmptyData()
+    {
+        $entity = new Invoice();
+
+        $this->assertEquals([], $entity->clean());
+    }
+
+    public function testCleanWithData()
+    {
+        $data = [
+            'foo'   => 'bar',
+            'lines' => [
+                'line' => [
+                    [
+                        'order'    => 1,
+                        'name'     => 'test',
+                        'cost'     => 2,
+                        'quantity' => 3
+                    ]
+                ]
+            ],
+            'url'   => 'http://example.org'
+        ];
+
+        $entity = new Invoice($data);
+
+        $this->assertEquals(
+            [
+                'foo'   => 'bar',
+                'lines' => [
+                    'line' => [
+                        [
+                            'name' => 'test',
+                            'cost' => 2,
+                            'quantity' => 3
+                        ]
+                    ]
+                ]
+            ],
+            $entity->clean()
+        );
     }
 
     public function testExistsWithUnexistingInvoice()
