5e94d35fe0bd2d8200b81c9942d6dcbce59f466a
Diego Blanco <diego@openhost.es>
2015-08-13 19:12:00 +0200

    Remove custom constructor in EntityNotFoundException

--- src/Framework/FreshBooks/Exception/EntityNotFoundException.php
+++ src/Framework/FreshBooks/Exception/EntityNotFoundException.php
@@ -4,15 +4,4 @@ namespace Framework\FreshBooks\Exception;
 
 class EntityNotFoundException extends \Exception
 {
-    /**
-     * Initializes the exception with a custom message.
-     *
-     * @param string $id The entity id.
-     */
-    public function __construct($id)
-    {
-        $message = "The entity with id \"$id\" not found.";
-
-        parent::__construct($message);
-    }
 }
