f136a2fb5b993affa87c6e47a67f1290ac73a392
Diego Blanco <diego@openhost.es>
2015-08-18 20:29:20 +0200

    Create new base repository and persister with chain support

---
+++ src/Framework/ORM/Core/ChainElement.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Framework\ORM\Core;
+
+class ChainElement
+{
+    /**
+     * The next element in chain.
+     *
+     * @var ChainElement
+     */
+    private $_next;
+
+    /**
+     * Returns the next element in chain.
+     *
+     * @return ChainElement The next element in chain.
+     */
+    public function next()
+    {
+        return $this->_next;
+    }
+
+    /**
+     * Checks if there is another element in chain.
+     *
+     * @return boolean True if there is another element in chain. Otherwise,
+     *                 return false.
+     */
+    public function hasNext()
+    {
+        return !empty($this->_next);
+    }
+
+    /**
+     * Sets the next element in chain.
+     *
+     * @param ChainElement $element The next element.
+     */
+    public function add(ChainElement $element)
+    {
+        $last = $this;
+
+        while($this->hasNext()) {
+            $last = $this->next();
+        }
+
+        $last->setNext($element);
+    }
+
+    public function setNext(ChainElement $element)
+    {
+        $this->_next = $element;
+    }
+}
