30c487fa0b4ffe025cf889f1dde344302eb375d0
Diego Blanco <diego@openhost.es>
2015-08-20 17:25:38 +0200

    Add template action to PlaygroundController

--- src/Frontend/Controller/PlaygroundController.php
+++ src/Frontend/Controller/PlaygroundController.php
@@ -1,38 +1,20 @@
 <?php
-/**
- * Playground where to test new functions
- *
- * @package Frontend_Controllers
- **/
-/**
- * This file is part of the Onm package.
- *
- * (c)  OpenHost S.L. <developers@openhost.es>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- **/
+
 namespace Frontend\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Onm\Framework\Controller\Controller;
-use Onm\Settings as s;
 
-/**
- * Playground where to test new functions
- *
- * @package Frontend_Controllers
- **/
 class PlaygroundController extends Controller
 {
     /**
-     * Dispatches the actions through the rest of methods in this class
+     * Dispatches the actions through the rest of methods in this class.
      *
-     * @param Request $request the request object
+     * @param Request $request The request object.
      *
-     * @return Response the response object
-     **/
+     * @return Response The response object.
+     */
     public function defaultAction(Request $request)
     {
         $action = $request->query->get('action', null);
@@ -43,23 +25,31 @@ class PlaygroundController extends Controller
         } else {
             return new Response('not valid action', 400);
         }
-
     }
 
     /**
      * Tests for session in container
      *
-     * @return Response the response object
-     **/
+     * @return Response The response object
+     */
     public function session()
     {
-        $this->get('session')->getFlashBag()->add(
-            'notice',
-            'Your changes were saved!'
-        );
+        $session = $this->get('session');
 
-        foreach ($this->get('session')->getFlashBag()->get('notice', array()) as $message) {
+        $session->getFlashBag()->add( 'notice', 'Your changes were saved!');
+
+        foreach ($session->getFlashBag()->get('notice', array()) as $message) {
             echo "<div class='flash-notice'>$message</div>";
         }
+    }
+
+    /**
+     * Displays the base/playground.tpl content.
+     *
+     * @return Response The response object.
+     */
+    public function template()
+    {
+        return $this->render('playground.tpl');
     }
 }
