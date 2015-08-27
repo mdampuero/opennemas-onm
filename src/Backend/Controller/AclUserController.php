100c4e7cd2912fc9aa391cea7c50f287496f6baf
Fran Dieguez <fran@openhost.es>
2015-08-27 13:46:48 +0200

    Add option to download the list of users
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Backend/Controller/AclUserController.php
+++ src/Backend/Controller/AclUserController.php
@@ -766,7 +766,7 @@ class AclUserController extends Controller
     }
 
     /**
-     * Disconnects the accounts.
+     * Disconnects from social account accounts.
      *
      * @param  Request  $request The request object.
      * @param  integer  $id      The user's id.
