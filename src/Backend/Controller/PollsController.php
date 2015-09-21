38a28636b707a1881b21d9f7e7f5182956fab868
Fran Dieguez <fran@openhost.es>
2015-09-21 13:34:52 +0200

    Avoid notices in Opinions and Polls if params is not set
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Backend/Controller/PollsController.php
+++ src/Backend/Controller/PollsController.php
@@ -144,8 +144,8 @@ class PollsController extends Controller
                 'visualization'  => $request->request->getDigits('visualization', 0),
                 'category'       => $request->request->filter('category', '', FILTER_SANITIZE_STRING),
                 'content_status' => $request->request->filter('content_status', 0, FILTER_SANITIZE_STRING),
-                'item'           => json_decode($request->request->get('parsedAnswers'), true),
-                'params'         => $request->request->get('params'),
+                'item'           => json_decode($request->request->get('parsedAnswers')),
+                'params'         => $request->request->get('params', []),
             );
             $poll = $poll->create($data);
 
