d0a0648eedf16c4bed9fd2db27aa8d36190b041d
Diego Blanco <diego@openhost.es>
2015-09-24 19:09:22 +0200

    Update basic classer for importer
    - Replace FormatInterface and FormatAbstract by Parser.
    - Improve format checking and update documentation for DataSourceFactory

--- libs/Onm/Import/DataSource/DataSourceFactory.php
+++ libs/Onm/Import/DataSource/DataSourceFactory.php
@@ -3,46 +3,44 @@
  * Implements the DataSourceFactory class
  *
  * This file is part of the Onm package.
- * (c)  OpenHost S.L. <developers@openhost.es>
+ * (c) OpenHost S.L. <developers@openhost.es>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @package Onm\Import\DataSouce
- **/
+ */
 namespace Onm\Import\DataSource;
 
 /**
- * Class for initialize a FileImporter class handler
- *
- * @package Onm\Import\DataSouce
- **/
+ * Initializes a Parser to proccess a XML file.
+ */
 class DataSourceFactory
 {
     /**
-     * Returns an instance of the element to import
+     * Returns a parser to parse the given XML object.
      *
-     * @param string $filePath the file path to initialize the element
+     * @param SimpleXMLObject $xml The XML to parse.
      *
-     * @return FormatInterface
-     **/
-    public static function get($filePath)
+     * @return Parser The parser.
+     */
+    public function get($xml)
     {
-        $baseFormatClassPath = __DIR__.'/Format';
-        $availableFormats = glob($baseFormatClassPath.'/*.php');
+        $parsers = glob(__DIR__ . DS .'Parser' . DS . '*.php');
 
-        $dataSource = null;
-        foreach ($availableFormats as $value) {
-            $formatName = basename($value, '.php');
-            $formatClass = __NAMESPACE__."\Format\\".$formatName;
-            try {
-                $dataSource = new $formatClass($filePath);
-                break;
-            } catch (\Exception $e) {
-                continue;
+        // Exclude abstract Parser
+        $parsers = array_filter($parsers, function ($parser) {
+            return !preg_match('/Parser.php$/', $parser);
+        });
+
+        foreach ($parsers as $name) {
+            $class = __NAMESPACE__ . "\\Parser\\" . basename($name, '.php');
+
+            $parser = new $class($this);
+
+            if ($parser->checkFormat($xml)) {
+                return $parser;
             }
         }
 
-        return $dataSource;
+        throw new \Exception(_('The XML could not be parsed'));
     }
 }
