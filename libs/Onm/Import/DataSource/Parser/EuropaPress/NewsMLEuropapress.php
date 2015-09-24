e142f855fb1e1afbdd42219b44e1f6401194b821
Diego Blanco <diego@openhost.es>
2015-09-24 19:37:01 +0200

    Update Europapress custom format parser
    - Remove old MultimediaResource.
    - Update parser to extend Parser class.

---
+++ libs/Onm/Import/DataSource/Parser/EuropaPress/NewsMLEuropapress.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Defines the Onm\Import\DataSource\NewsMLEuropapress class
+ *
+ * This file is part of the Onm package.
+ * (c)  OpenHost S.L. <developers@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Onm\Import\DataSource\Format
+ **/
+namespace Onm\Import\DataSource\Parser;
+
+/**
+ * Implements the handler for the Europapress NewsML format
+ *
+ * @package Onm\Import\DataSource
+ **/
+class NewsMLEuropapress extends NewsMLG1
+{
+    /**
+     * Returns the title of the element
+     *
+     * @return string the title
+     **/
+    public function getTitle()
+    {
+        $title = (string) $this->getData()->NewsItem->NewsComponent->NewsComponent->NewsLines->HeadLine;
+
+        return iconv(mb_detect_encoding($title), "UTF-8", $title);
+    }
+
+    /**
+     * Returns the creation datetime of this element
+     *
+     * @return DateTime the datetime of the element
+     **/
+    public function getCreatedTime()
+    {
+        $originalDate = (string) $this->getData()->NewsEnvelope->DateAndTime;
+
+        // ISO 8601 doesn't match this date 20111211T103900+0000
+        $originalDate = preg_replace('@\+(\d){4}$@', '', $originalDate);
+
+        return \DateTime::createFromFormat(
+            'Y-m-d\TH:i:s',
+            $originalDate,
+            new \DateTimeZone('UTC')
+        );
+    }
+
+    /**
+     * Returns the list of tags of this element
+     *
+     * @return int the priority level
+     **/
+    public function getTags()
+    {
+
+        $topics = $this->getData()->NewsItem->NewsComponent->TopicSet->Topic;
+
+        $tags = array();
+        foreach ($topics as $topic) {
+            $tag = (string) $topic->Description;
+            $tag = ucwords(strtolower($tag));
+
+            if (stripos($tag, 'Servicio') !== 0) {
+                $tags []= $tag;
+            }
+        }
+        $tags = array_unique($tags);
+
+        return $tags;
+    }
+
+    /**
+     * Returns the body of the element
+     *
+     * @return string the body
+     **/
+    public function getBody()
+    {
+        $rawContent = (string) $this->getData()->NewsItem->NewsComponent
+            ->NewsComponent->ContentItem->DataContent;
+
+        preg_match('@<body[^>]*>(.*?)<\/body>@is', $rawContent, $matches);
+
+        $body = '';
+        if (array_key_exists(1, $matches)) {
+            $body = $matches[1];
+        }
+
+        return iconv(mb_detect_encoding($body), "UTF-8", $body);
+    }
+
+    /**
+     * Checks if a XML file could be handled by this class
+     *
+     * @param SimpleXmlElement $data the XML file to parse
+     * @param string $xmlFile the path to the xml file
+     *
+     * @return boolean true if the file could be handle by this class
+     **/
+    public function checkFormat($data, $xmlFile = null)
+    {
+        if ($data->NewsItem->count() <= 0) {
+            throw new \Exception(sprintf(_('File %s is not a valid NewsMLEuropapres file'), $xmlFile));
+        }
+        $provider = (string) $data->NewsItem->Identification->NewsIdentifier->ProviderId;
+
+        if ($provider != 'Europa Press') {
+            throw new \Exception(sprintf(_('File %s is not a valid NewsMLEuropapres file'), $xmlFile));
+        }
+
+        return true;
+    }
+}
