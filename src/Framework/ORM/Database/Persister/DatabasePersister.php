a9e2a7bfcc83bf5a8c70d639c6c3bb3df2984ee1
Diego Blanco <diego@openhost.es>
2015-10-14 19:18:18 +0200

    Create base repository, persister and entity manager for data from database

---
+++ src/Framework/ORM/Database/Persister/DatabasePersister.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Framework\ORM\Database\Persister;
+
+use Framework\ORM\Persister\Persister;
+use Onm\Database\DbalWrapper;
+use Onm\Cache\CacheInterface;
+
+abstract class DatabasePersister extends Persister
+{
+    /**
+     * The cache service for instance.
+     *
+     * @var DbalWrapper
+     */
+    protected $icache;
+
+    /**
+     * The database connection for instance.
+     *
+     * @var DbalWrapper
+     */
+    protected $iconn;
+
+    /**
+     * The cache service for manager.
+     *
+     * @var DbalWrapper
+     */
+    protected $mcache;
+
+    /**
+     * The database connection for manager.
+     *
+     * @var DbalWrapper
+     */
+    protected $mconn;
+
+    /**
+     * The source name.
+     *
+     * @var string
+     */
+    protected $source = 'Database';
+
+    /**
+     * Initializes a new DatabasePersister.
+     *
+     * @param CacheInterface $icache The cache service for instance.
+     * @param DbalWrapper    $iconn  The dabase connection for instance.
+     * @param CacheInterface $mcache The cache service for manager.
+     * @param DbalWrapper    $mconn  The dabase connection for manager
+     * @param DbalWrapper    $source The source name.
+     */
+    public function __construct(CacheInterface $icache, DbalWrapper $iconn, CacheInterface $mcache, DbalWrapper $mconn, $source)
+    {
+        $this->icache = $icache;
+        $this->iconn  = $iconn;
+        $this->mcache = $mcache;
+        $this->mconn  = $mconn;
+        $this->source = $source;
+    }
+}
