edea4cc95855fb5063d87b69fe1806b6e95c4740
Diego Blanco <diego@openhost.es>
2015-11-23 12:53:55 +0100

    Improve list action in ThemeController in backend web service

--- src/BackendWebService/Controller/ThemeController.php
+++ src/BackendWebService/Controller/ThemeController.php
@@ -22,33 +22,11 @@ class ThemeController extends Controller
      */
     public function listAction()
     {
-        $themes = [
-            [
-                'id'                => 'theme1',
-                'name'              => 'Theme 1',
-                'description'       => '<p>Long description for theme 1</p><p>Nunc. Mauris consequat, enim vitae venenatis sollicitudin, dolor orci bibendum enim, a sagittis nulla nunc quis elit. Phasellus augue. Nunc suscipit.</p><ul><li>Feature 1</li><li>Feature 2</li><li>Feature 3</li></ul><p>Nascetur ridiculus mus.</p><p>Aenean risus dui, volutpat non, posuere vitae, sollicitudin in, urna. Nam eget eros a enim pulvinar rhoncus. Cum sociis.</p>',
-                'type'              => 'free',
-                'screenshots'       => [ 1, 2, 3 ],
-                'short_description' => 'Short description for theme 1',
-            ],
+        $themes = $this->get('orm.loader')->getPlugins();
 
-            [
-                'id'                => 'theme2',
-                'name'              => 'Theme 2',
-                'description'       => '<p>Long description for theme 2</p><p>Nunc. Mauris consequat, enim vitae venenatis sollicitudin, dolor orci bibendum enim, a sagittis nulla nunc quis elit. Phasellus augue. Nunc suscipit.</p><ul><li>Feature 1</li><li>Feature 2</li><li>Feature 3</li></ul><p>Nascetur ridiculus mus.</p><p>Aenean risus dui, volutpat non, posuere vitae, sollicitudin in, urna. Nam eget eros a enim pulvinar rhoncus. Cum sociis.</p>',
-                'type'              => 'free',
-                'screenshots'       => [ 1, 2, 3 ],
-                'short_description' => 'Short description for theme 2',
-            ],
-            [
-                'id'                => 'theme3',
-                'name'              => 'Theme 3',
-                'description'       => '<p>Long description for theme 3</p><p>Nunc. Mauris consequat, enim vitae venenatis sollicitudin, dolor orci bibendum enim, a sagittis nulla nunc quis elit. Phasellus augue. Nunc suscipit.</p><ul><li>Feature 1</li><li>Feature 2</li><li>Feature 3</li></ul><p>Nascetur ridiculus mus.</p><p>Aenean risus dui, volutpat non, posuere vitae, sollicitudin in, urna. Nam eget eros a enim pulvinar rhoncus. Cum sociis.</p>',
-                'type'              => 'free',
-                'screenshots'       => [ 1, 2, 3 ],
-                'short_description' => 'Short description for theme 3',
-            ]
-        ];
+        foreach ($themes as &$theme) {
+            $theme = $theme->getData();
+        }
 
         $exclusive = \Onm\Module\ModuleManager::getAvailableThemes();
         array_shift($exclusive);
