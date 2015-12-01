3e0ae7a87506ffecba66d1f2084e0197bd574960
Diego Blanco <diego@openhost.es>
2015-12-02 00:01:30 +0100

    Improve gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,32 @@
   var path       = require('path');
 
   gulp.task('phpunit', function () {
-    exec('./vendor/phpunit/phpunit/phpunit -c app/phpunit.xml.dist | tail -1', function(error, stdout) {
-      var summary = stdout;
+    exec('./vendor/phpunit/phpunit/phpunit -c app/phpunit.xml.dist | tail -1',
+      function(error, stdout) {
+        var icon    = 'pass.png';
+        var summary = stdout;
 
-      if (summary.indexOf('Tests') === -1) {
+        if (summary.indexOf('Tests') === -1 && summary.indexOf('OK') === -1) {
+          notifier.notify({
+            'title':   'Unable to complete the tests!',
+            'icon':    path.join(__dirname, 'public/assets/images/fail.png'),
+            'message': 'There was an error while executing tests'
+          });
+
+          return;
+        }
+
+        if (summary.indexOf('Tests') !== -1) {
+          icon = 'fail.png'
+        }
+
         notifier.notify({
-          'title':   'Unable to complete the tests!',
-          'icon':    path.join(__dirname, 'public/assets/images/fail.png'),
-          'message': 'There was an error while executing tests'
+          'title':   'Tests executed!',
+          'icon':    path.join(__dirname, 'public/assets/images', icon),
+          'message': summary
         });
-
-        return;
       }
-
-      var result  = summary.replace(/,|\./g, '').replace(/: /g,':').split(' ');
-
-      var r = {};
-      for (var i = 0; i < result.length; i++) {
-        var token = result[i].split(':');
-        r[token[0]] = parseInt(token[1]);
-      }
-
-      var icon = 'pass.png';
-
-      if (r.Errors > 0) {
-        icon = 'fail.png';
-      }
-
-      notifier.notify({
-        'title':   'Tests executed!',
-        'icon':    path.join(__dirname, 'public/assets/images', icon),
-        'message': summary
-      });
-    });
+    );
   });
 
   gulp.task('touch', function () {
