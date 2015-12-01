20271511d5574f6df709cb32dbdcfa7b167b1c66
Diego Blanco <diego@openhost.es>
2015-12-01 15:29:44 +0100

    Add gulpfile.js file

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+(function () {
+  'use strict';
+
+  var exec       = require('child_process').exec;
+  var gulp       = require('gulp');
+  var livereload = require('gulp-livereload');
+  var notifier   = require('node-notifier');
+  var path       = require('path');
+
+  gulp.task('phpunit', function () {
+    exec('phpunit -c app/phpunit.xml.dist | tail -1', function(error, stdout) {
+      var result = stdout.split('\n');
+      var summary = result[result.length - 2];
+
+      result = summary.replace(/,|\./g, '').replace(/: /g,':').split(' ');
+
+      var r = {};
+      for (var i = 0; i < result.length; i++) {
+        var token = result[i].split(':');
+        r[token[0]] = parseInt(token[1]);
+      }
+
+      var icon = 'pass.png';
+
+      if (r.Errors > 0) {
+        icon = 'fail.png';
+      }
+
+      notifier.notify({
+        'title':   'Tests executed!',
+        'icon':    path.join(__dirname, 'public/assets/images', icon),
+        'message': summary
+      });
+    });
+  });
+
+  gulp.task('watch', function () {
+    livereload.listen();
+
+    gulp.watch([ 'app/models/**/*.php', 'libs/**/*.php', 'src/**/*.php' ],
+      function () {
+          gulp.start('phpunit');
+          livereload.reload();
+      });
+  });
+
+  gulp.task('default', [ 'watch' ]);
+})();
+
+
