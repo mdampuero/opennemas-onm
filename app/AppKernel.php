7b235e983e717fcce5ed61ed5113bed96bf8a1c3
Fran Dieguez <fran@openhost.es>
2015-12-05 15:29:18 +0100

    ONM-889: Enable the DebugBundle when app is in dev environment
    Symfony\Bundle\DebugBundle\DebugBundle()
    
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,7 +39,8 @@ class AppKernel extends Kernel
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
