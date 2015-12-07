9f54b37f4dee56a7e2827fb1dcceeff8010b31e6
Diego Blanco <diego@openhost.es>
2015-12-07 16:18:59 +0100

    Move EventDispatcher to EventDispatcher folder

--- src/Framework/Component/EventDispatcher/EventDispatcher.php
+++ src/Framework/Component/EventDispatcher/EventDispatcher.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Framework\Component;
+namespace Framework\Component\EventDispatcher;
 
 use Framework\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
