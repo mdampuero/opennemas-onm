b97cf31bdc1e78063540f48eaed1aaa327538e45
Diego Blanco <diego@openhost.es>
2015-12-07 16:43:08 +0100

    Move EventDispatcherTest to Test/Component/EventDispatcher

--- src/Framework/Tests/Component/EventDispatcher/EventDispatcherTest.php
+++ src/Framework/Tests/Component/EventDispatcher/EventDispatcherTest.php
@@ -7,9 +7,9 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Framework\Tests\Component;
+namespace Framework\Tests\Component\EventDispatcher;
 
-use Framework\Component\EventDispatcher;
+use Framework\Component\EventDispatcher\EventDispatcher;
 
 class EventDispatcherTest extends \PHPUnit_Framework_TestCase
 {
