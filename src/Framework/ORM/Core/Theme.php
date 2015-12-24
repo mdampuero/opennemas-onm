c61ab82050cbe8e79d142a3206d1848a7921d9cf
Diego Blanco <diego@openhost.es>
2015-12-24 14:05:37 +0100

    Move and improve Theme entity

--- src/Framework/ORM/Core/Theme.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Framework\ORM\Core;
-
-class Theme extends Entity
-{
-}
---
+++ src/Framework/ORM/Entity/Theme.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\ORM\Entity;
+
+class Theme extends Extension
+{
+}
