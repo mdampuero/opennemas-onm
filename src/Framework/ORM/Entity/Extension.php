4361f34c2441b530c7ff9e879b8ee021e78f9b11
Diego Blanco <diego@openhost.es>
2015-12-24 14:04:12 +0100

    Create Extension entity

---
+++ src/Framework/ORM/Entity/Extension.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\ORM\Entity;
+
+use Framework\ORM\Core\Entity;
+
+class Extension extends Entity
+{
+}
