fe13fed179f9cf6aad602ff79f818db7bc657b50
Diego Blanco <diego@openhost.es>
2015-12-30 00:55:19 +0100

    Create an entity for ORM validations

---
+++ src/Framework/ORM/Core/Validation.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\ORM\Core;
+
+use Doctrine\DBAL\Schema\Schema as DbalSchema;
+
+class Validation extends Entity
+{
+    /**
+     * Returns a schema for doctrine DBAL basing on the current schema.
+     */
+    public function getDbalSchema()
+    {
+        $schema = new DbalSchema();
+
+        foreach ($this->data['parameters'] as $table => $definition) {
+            $table = $schema->createTable($table);
+
+            // Add column definitions
+            foreach ($definition['columns'] as $field => $value) {
+                $options = [];
+
+                if (array_key_exists('options', $value)) {
+                    $options = $value['options'];
+                }
+
+                $table->addColumn($field, $value['type'], $options);
+            }
+
+            // Add index definitions
+            foreach ($definition['index'] as $field => $value) {
+                if (!array_key_exists('name', $value)) {
+                    $value['name'] = null;
+                }
+
+                if (array_key_exists('primary', $value)
+                    && !empty($value['primary'])
+                ) {
+                    $table->setPrimaryKey($value['columns'], $value['name']);
+                } elseif (array_key_exists('unique', $value)
+                    && !empty($value['unique'])
+                ) {
+                    $table->addUniqueIndex($value['columns'], $value['name']);
+                } else {
+                    $table->addIndex($value['columns'], $value['name']);
+                }
+            }
+        }
+
+        return $schema;
+    }
+}
