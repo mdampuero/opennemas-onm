a22e9fa05b86940353c8bbb158a74bf889de1d88
Diego Blanco <diego@openhost.es>
2015-12-30 01:12:11 +0100

    Move Loader to ORM/Core and update tests

--- src/Framework/ORM/Core/Loader/Loader.php
+++ src/Framework/ORM/Core/Loader/Loader.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Framework\ORM\Loader;
+namespace Framework\ORM\Core\Loader;
 
 use Framework\ORM\Core\Validation;
 use Framework\ORM\Core\Connection;
--- tests/Framework/ORM/Core/Loader/LoaderTest.php
+++ tests/Framework/ORM/Core/Loader/LoaderTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Framework\Tests\ORM\Configuration;
+namespace Framework\Tests\ORM\Core\Loader;
 
 use Framework\ORM\Core\Entity;
-use Framework\ORM\Loader\Loader;
+use Framework\ORM\Core\Loader\Loader;
 use Framework\Fixture\FixtureLoader;
 
 class LoaderTest extends \PHPUnit_Framework_TestCase
@@ -18,7 +18,7 @@ class LoaderTest extends \PHPUnit_Framework_TestCase
 
     public function testValidPaths()
     {
-        $loader = new Loader(__DIR__ . '/../../../../app', [ 'config/orm' ]);
+        $loader = new Loader(__DIR__ . '/../../../../../app', [ 'config/orm' ]);
         $this->assertNotEmpty($loader->load());
     }
 }
