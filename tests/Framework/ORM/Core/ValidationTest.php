bf3eb2e3ff8d9f458d9663f9381d720028d0cfa7
Diego Blanco <diego@openhost.es>
2015-12-30 14:05:53 +0100

    Move SchemaTest to ValidationTest

---
+++ tests/Framework/ORM/Core/ValidationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Framework\Tests\ORM\Entity;
+
+use Framework\ORM\Core\Validation;
+
+class ValidationTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->data   = [ 'foo' => 'bar', 'parameters' => [] ];
+        $this->validation = new Validation($this->data);
+    }
+
+    public function testGet()
+    {
+        $this->assertEmpty($this->validation->baz);
+
+        foreach ($this->data as $key => $value) {
+            $this->assertEquals($value, $this->validation->{$key});
+        }
+    }
+
+    public function testGetData()
+    {
+        $this->assertEquals($this->data, $this->validation->getData());
+    }
+
+    public function testGetDbalValidation()
+    {
+        $this->assertInstanceOf(
+            'Doctrine\DBAL\Schema\Schema',
+            $this->validation->getDbalSchema()
+        );
+    }
+
+    public function testSet()
+    {
+        $this->validation->qux = 'norf';
+
+        $this->assertEquals('norf', $this->validation->qux);
+    }
+}
