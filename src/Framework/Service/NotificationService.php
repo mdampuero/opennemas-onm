fd51a211e2aba174afdcd0a6c347db2a78200fdc
Fran Dieguez <fran@openhost.es>
2016-01-11 22:08:04 +0100

    ONM-984: More improvements in instance stats notificatoin
    Signed-off-by: Fran Dieguez <fran@openhost.es>

--- src/Framework/Service/NotificationService.php
+++ src/Framework/Service/NotificationService.php
@@ -104,7 +104,7 @@ class NotificationService
         $notification->end         = date('Y-m-d H:i:s', time() + 86400);
 
         $notification->title = [
-                CURRENT_LANGUAGE_SHORT => _('Instance information')
+                CURRENT_LANGUAGE_SHORT => _('Usage of your newspaper')
         ];
 
         $notification->body = [
@@ -158,20 +158,25 @@ class NotificationService
         $body = '';
 
         if ($instance->users > 1) {
-            $body .= sprintf(
-                _('<li>You have %d activated users. Note that <a href="http://help.opennemas.com/knowledgebase/articles/566172-pricing-opennemas-user-licenses" title="Get more info about user licensing">the cost is %s € user/month</a></li>'),
-                $instance->users,
-                12
-            );
+            $body .=
+                '<li>'
+                .sprintf(
+                    _('You have %d activated users. Note that <a href="http://help.opennemas.com/knowledgebase/articles/566172-pricing-opennemas-user-licenses" title="Get more info about user licensing">the cost is %s € user/month</a>'),
+                    $instance->users,
+                    12
+                )
+                .'</li>';
         }
 
         if ($instance->page_views > 45000) {
-            $body .= sprintf(_('<li>This month you\'re recording %d page views. '), $instance->page_views);
+            $body .=
+                '<li>'
+                .sprintf(_('This month you\'re recording %d page views. '), $instance->page_views);
 
             if ($instance->page_views > 50000) {
                 $body .= sprintf(
                     _('Note that <a href="http://help.opennemas.com/knowledgebase/articles/666994-pricing-opennemas-page-views-and-storage-space" title="Get more info about user licensing">the cost %s € pv/month</a>.'),
-                    0.00009
+                    number_format(0.00009, 5)
                 );
             }
 
@@ -179,9 +184,10 @@ class NotificationService
         }
 
         if ($instance->media_size > 450) {
-            $body .= sprintf(
-                _('<li>Your are using %d Mb of storage. '),
-                round($instance->media_size)
+
+            $body .= '<li>'.sprintf(
+                _('Your are using %d Mb of storage. '),
+                $instance->media_size
             );
 
             if ($instance->media_size > 500) {
