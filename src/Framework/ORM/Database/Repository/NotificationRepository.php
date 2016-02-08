0ce79e84c7610b9c21dc41bef20ac6dcc07d81ee
Diego Blanco <diego@openhost.es>
2016-02-08 18:57:14 +0100

    Add missing license header

--- src/Framework/ORM/Database/Repository/NotificationRepository.php
+++ src/Framework/ORM/Database/Repository/NotificationRepository.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Framework\ORM\Database\Repository;
 
 use Framework\ORM\Core\Entity;
