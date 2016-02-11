e42aa9d83238e70e3d59e30109bbb1150a1064d9
Diego Blanco <diego@openhost.es>
2016-02-11 22:37:15 +0100

    Create Schema class

---
+++ src/Framework/ORM/Core/Schema/Schema.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\ORM\Core\Schema;
+
+use Framework\Component\Data\DataObject;
+use Framework\ORM\Core\Validation\Validable;
+
+class Schema extends DataObject implements Validable
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassName()
+    {
+        return 'Schema';
+    }
+}
