80dbae021fcb7019c3e994ebfae684eb934d21d2
Diego Blanco <diego@openhost.es>
2016-02-17 13:44:42 +0100

    Remove chain support and update action to clean entity in Braintree persisters

--- src/Framework/ORM/Braintree/Persister/ClientPersister.php
+++ src/Framework/ORM/Braintree/Persister/ClientPersister.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Framework\ORM\Braintree\Persister;
 
 use Framework\ORM\Entity\Entity;
@@ -8,78 +15,63 @@ use Framework\ORM\Exception\ClientNotFoundException;
 class ClientPersister extends BraintreePersister
 {
     /**
-     * Saves a new client in FreshBooks.
+     * Saves a new client in Braintree.
      *
-     * @param Entity  $entity The client to save.
-     * @param boolean $next   Whether to continue to the next persister.
+     * @param Entity $entity The client to save.
      *
      * @throws RuntimeException If the the client can not be saved.
      */
-    public function create(Entity &$entity, $next = true)
+    public function create(Entity &$entity)
     {
         $cr   = $this->factory->get('customer');
-        $data = $this->entityToArray($entity);
+        $data = $this->clean($entity);
 
         $response = $cr::create($data);
 
         if ($response->success) {
             $entity->client_id = $response->customer->id;
 
-            if ($next && $this->hasNext()) {
-                $this->next()->create($entity);
-            }
-
-            return $this;
+            return;
         }
 
         throw new \RuntimeException();
     }
 
     /**
-     * Removes the client in FreshBooks.
+     * Removes the client in Braintree.
      *
-     * @param Entity  $entity The client to update.
-     * @param boolean $next   Whether to continue to the next persister.
+     * @param Entity $entity The client to update.
      *
      * @throws ClientNotFoundException If the client does not exist.
      */
-    public function remove(Entity $entity, $next = true)
+    public function remove(Entity $entity)
     {
         $cr       = $this->factory->get('customer');
         $response = $cr::delete($entity->client_id);
 
         if ($response->success) {
-            if ($next && $this->hasNext()) {
-                $this->next()->remove($entity);
-            }
-
-            return $this;
+            return;
         }
 
         throw new ClientNotFoundException($entity->client_id, $this->source);
     }
 
     /**
-     * Updates the client in FreshBooks.
+     * Updates the client in Braintree.
      *
-     * @param Entity  $entity The client to update.
-     * @param boolean $next   Whether to continue to the next persister.
+     * @param Entity $entity The client to update.
      *
      * @throws ClientNotFoundException If the client does not exist.
      */
-    public function update(Entity $entity, $next = true)
+    public function update(Entity $entity)
     {
         $cr   = $this->factory->get('customer');
-        $data = $this->entityToArray($entity);
+        $data = $this->clean($entity);
 
         $response = $cr::update($entity->client_id, $data);
 
         if ($response->success) {
-            if ($next && $this->hasNext()) {
-                $this->next()->remove($entity);
-            }
-
-            return $this;
+            return;
         }
 
         throw new ClientNotFoundException($entity->client_id, $this->source);
@@ -92,7 +84,7 @@ class ClientPersister extends BraintreePersister
      *
      * @return array The array.
      */
-    private function entityToArray($entity)
+    private function clean($entity)
     {
         $data = [
             'id'        => $entity->client_id,
