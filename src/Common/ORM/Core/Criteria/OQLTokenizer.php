66afa4523d2e5613322d5dc268be72880e455d03
Diego Blanco <diego@openhost.es>
2016-02-28 01:21:15 +0100

    Create an OQL tokenizer to parse OQL queries

---
+++ src/Common/ORM/Core/Criteria/OQLTokenizer.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <onm-devs@openhost.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\ORM\Core\Criteria;
+
+/**
+ * OQLTokenizer parses and splits OQL queries into tokens.
+ */
+class OQLTokenizer
+{
+    /**
+     * Array of valid tokens.
+     *
+     * @var array
+     */
+    protected $tokens = [
+        'COMMA'          => '/,/',
+        'C_AND'          => '/\s*and\s*/',
+        'C_OR'           => '/\s*or\s*/',
+        'G_CPARENTHESIS' => '/\)/',
+        'G_CBRACKET'     => '/\]/',
+        'G_OBRACKET'     => '/\[/',
+        'G_OPARENTHESIS' => '/\(/',
+        'M_ASC'          => '/\s*asc\s*/',
+        'M_DESC'         => '/\s*desc\s*/',
+        'M_LIMIT'        => '/\s*limit\s*/',
+        'M_OFFSET'       => '/\s*offset\s*/',
+        'M_ORDER'        => '/\s*order by\s*/',
+        'O_EQUALS'       => '/\s*=\s*/',
+        'O_GREAT'        => '/\s*>\s*/',
+        'O_GREAT_EQUALS' => '/\s*>=\s*/',
+        'O_IN'           => '/\s*in\s*/',
+        'O_LESS'         => '/\s*<\s*/',
+        'O_LESS_EQUALS'  => '/\s*<=\s*/',
+        'O_LIKE'         => '/\s*~\s*/',
+        'O_NOT'          => '/\s*!\s*/',
+        'O_REGEX'        => '/\s*\^\s*/',
+        'T_FLOAT'        => '/-?[0-9]+\.[0-9]+/',
+        'T_INTEGER'      => '/-?[0-9]+/',
+        'T_STRING'       => '/\'[^\']*\'|\"[^\"]*\"/',
+        'T_FIELD'        => '/[a-zA-Z0-9]+/',
+    ];
+
+    /**
+     * Array of valid token combinations.
+     *
+     * @var array
+     */
+    protected $sentences = [
+        'T_CONNECTOR' => '/C_AND|C_OR/',
+        'T_OPERATOR'  => '/O_NOT\s*T_OPERATOR|O_EQUALS|O_GREAT|O_GREAT_EQUALS|O_IN|O_LESS|O_LESS_EQUALS|O_LIKE|O_REGEX/',
+        'T_LITERAL'   => '/T_ARRAY|T_FLOAT|T_INTEGER|T_STRING/',
+        'T_ARRAY'     => '/G_OBRACKET\s*T_LITERAL\s*(COMMA\s*T_LITERAL)*\s*G_CBRACKET/',
+        'S_LIMIT'     => '/M_LIMIT T_INTEGER/',
+        'S_OFFSET'    => '/M_OFFSET T_INTEGER/',
+        'S_ORDER'     => '/M_ORDER\s*T_FIELD\s*(M_ASC|M_DESC)/',
+        'S_MODIFIER'  => '/S_ORDER|S_OFFSET\s*(S_LIMIT)?|S_LIMIT\s*(S_OFFSET)?/',
+        'S_CONDITION' => '/T_FIELD\s*T_OPERATOR\s*(T_FIELD|T_LITERAL)|T_LITERAL\s*T_OPERATOR\s*T_FIELD|G_OPARENTHESIS\s*S_CONDITION\s*G_CPARENTHESIS|S_CONDITION(\s*T_CONNECTOR\s*S_CONDITION)+/',
+        'OQL'         => '/G_OPARENTHESIS\s*OQL\s*G_CPARENTHESIS|OQL(\s*T_CONNECTOR\s*OQL)+|S_CONDITION(\s*S_MODIFIER)*|OQL(\s*S_MODIFIER)+/',
+    ];
+
+    /**
+     * Translates an OQL query to a MySQL query.
+     *
+     * @param string $query The OQL query.
+     *
+     * @return string The MySQL query.
+     */
+    public function tokenize($query)
+    {
+        $tokens = $this->getTokens($query);
+
+        // Translate tokens to internal OQL representation
+        $translated = [];
+        foreach ($tokens as &$token) {
+            $translated[] = $this->translateToken($token);
+        }
+
+        $this->checkOQL($translated);
+
+        $map = [];
+        for ($i = 0; $i < count($tokens); $i++) {
+            $map[] = [ $tokens[$i], $translated[$i] ];
+        }
+
+        return $map;
+    }
+
+    /**
+     * Checks if an OQL query is well-formed basing on the given tokens.
+     *
+     * @param array $tokens The OQL tokens.
+     */
+    protected function checkOQL($tokens)
+    {
+        while ((count($tokens) > 1 || $tokens[0] !== 'OQL')) {
+            $sentence = $this->sentenize($tokens);
+
+            // Can't build sentence from tokens
+            if ($sentence === $tokens) {
+                throw new \Exception('You have an error near ' . implode(' ', $tokens));
+            }
+
+            $tokens = $sentence;
+        }
+    }
+
+    /**
+     * Returns the tokens of the query.
+     *
+     * @param string $query The query to tokenize.
+     *
+     * @return array The list of tokens.
+     *
+     * @throws InvalidTokenException If one or more invalid tokens are found in
+     *                               the query.
+     */
+    protected function getTokens($query)
+    {
+        $end    = strlen($query);
+        $tokens = [];
+
+        while (!empty($query) && $end > 0) {
+            $token = substr($query, 0, $end--);
+
+            if ($this->isToken($token)) {
+                $query = substr($query, strlen($token));
+                $end   = strlen($query);
+
+                $tokens[] = trim($token);
+            }
+        }
+
+        if (!empty($query)) {
+            throw new \Exception('Invalid token ' . $query);
+        }
+
+        return $tokens;
+    }
+
+    /**
+     * Checks if a string is a valid sentence.
+     *
+     * @param string $str The string to check.
+     *
+     * @return boolean True if the string is a valid sentence. Otherwise,
+     *                 returns false.
+     */
+    protected function isSentence($str)
+    {
+        return $this->translateSentence($str) !== false;
+    }
+
+    /**
+     * Checks if a string is a valid token.
+     *
+     * @param string $str The string to check.
+     *
+     * @return boolean True if the string is a valid token. Otherwise, returns
+     *                 false.
+     */
+    protected function isToken($str)
+    {
+        return $this->translateToken($str) !== false;
+    }
+
+    /**
+     * Translates a string basing on the source.
+     *
+     * @param string $str    The string to translate.
+     * @param array  $source The translation source.
+     *
+     * @return mixed The translated string if the token can be translated.
+     *               Otherwise, returns false.
+     */
+    protected function match($str, $source)
+    {
+        foreach ($source as $replacement => $pattern) {
+            preg_match_all($pattern, $str, $matches);
+
+            if (count($matches[0]) === 1 && $matches[0][0] === $str) {
+                return $replacement;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the internal representation of an OQL sentence built with the
+     * given tokens.
+     *
+     * @param string $tokens The list of tokens.
+     *
+     * @return array The sentence.
+     */
+    protected function sentenize($tokens)
+    {
+        $end        = count($tokens);
+        $translated = [];
+
+        while (!empty($tokens)) {
+            $token      = implode(' ', array_slice($tokens, 0, $end--));
+            $isSentence = $this->isSentence($token);
+
+            // Translate token if it is a sentence
+            if ($isSentence) {
+                $token = $this->translateSentence($token);
+            }
+
+            // Remove from list if sentence or end of the list of tokens
+            if ($isSentence || $end === 0) {
+                $translated[] = $token;
+                $tokens       = array_slice($tokens, $end + 1);
+                $end          = count($tokens);
+            }
+        }
+
+        return $translated;
+    }
+
+    /**
+     * Translates a OQL sentence to the internal representation.
+     *
+     * @param string $token The sentence to translate.
+     *
+     * @return string The translated sentence.
+     */
+    protected function translateSentence($sentence)
+    {
+        return $this->match($sentence, $this->sentences);
+    }
+
+    /**
+     * Translates a token to the internal representation.
+     *
+     * @param string $token The token to translate.
+     *
+     * @return string The translated token.
+     */
+    protected function translateToken($token)
+    {
+        return $this->match($token, $this->tokens);
+    }
+}
---
+++ tests/Common/ORM/Core/Criteria/OQLTokenizerTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Tests\Common\ORM\Core\Criteria;
+
+use Common\ORM\Core\Criteria\OQLTokenizer;
+
+class OQLTokenizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->translator = new OQLTokenizer();
+    }
+
+    public function testTokenize()
+    {
+        $map = [
+            [ 'foo', 'T_FIELD' ],
+            [ '=', 'O_EQUALS' ],
+            [ '"bar"', 'T_STRING' ],
+            [ 'and', 'C_AND' ],
+            [ '(', 'G_OPARENTHESIS' ],
+            [ 'baz', 'T_FIELD' ],
+            [ '!', 'O_NOT' ],
+            [ '=', 'O_EQUALS' ],
+            [ '"qux"', 'T_STRING' ],
+            [ 'or', 'C_OR' ],
+            [ 'baz', 'T_FIELD' ],
+            [ 'in', 'O_IN' ],
+            [ '[', 'G_OBRACKET' ],
+            [ '1', 'T_INTEGER' ],
+            [ ',', 'COMMA' ],
+            [ '2', 'T_INTEGER' ],
+            [ ']', 'G_CBRACKET' ],
+            [ ')', 'G_CPARENTHESIS' ],
+            [ 'order by', 'M_ORDER' ],
+            [ 'foo', 'T_FIELD' ],
+            [ 'asc', 'M_ASC' ],
+            [ 'limit', 'M_LIMIT' ],
+            [ '20', 'T_INTEGER' ],
+        ];
+
+        $oql = 'foo="bar" and (baz!="qux" or baz in [1,2]) order by foo asc limit 20';
+
+        $this->assertEquals($map, $this->translator->tokenize($oql));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testCheckOQL()
+    {
+        $method = new \ReflectionMethod($this->translator, 'checkOQL');
+        $method->setAccessible(true);
+
+        $method->invokeArgs($this->translator, [ [ 'foobar' ] ]);
+    }
+
+    public function testGetTokens()
+    {
+        $method = new \ReflectionMethod($this->translator, 'getTokens');
+        $method->setAccessible(true);
+
+        $tokens = [ 'foo', '=', '"bar"', 'and', '(', 'baz', '!', '=', '"qux"', 'or', 'baz' ,'in', '[', '1', ',' ,'2', ']', ')' ];
+        $oql    = 'foo="bar" and (baz!="qux" or baz in [1,2])';
+
+        $this->assertEquals($tokens, $method->invokeArgs($this->translator, [ $oql ]));
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testTokenizeError()
+    {
+        $this->translator->tokenize('foo*bar');
+    }
+
+    public function testIsSentence()
+    {
+        $method = new \ReflectionMethod($this->translator, 'isSentence');
+        $method->setAccessible(true);
+
+        $this->assertTrue($method->invokeArgs($this->translator, [ 'C_AND' ]));
+        $this->assertTrue($method->invokeArgs($this->translator, [ 'T_FIELD T_OPERATOR T_LITERAL' ]));
+        $this->assertTrue($method->invokeArgs($this->translator, [ 'S_CONDITION S_MODIFIER' ]));
+
+        $this->assertFalse($method->invokeArgs($this->translator, [ 'G_OBRACKET T_FIELD' ]));
+    }
+
+    public function testIsToken()
+    {
+        $method = new \ReflectionMethod($this->translator, 'isToken');
+        $method->setAccessible(true);
+
+        $this->assertTrue($method->invokeArgs($this->translator, [ 'foobar' ]));
+        $this->assertTrue($method->invokeArgs($this->translator, [ '"foobar"' ]));
+        $this->assertTrue($method->invokeArgs($this->translator, [ '[' ]));
+
+        $this->assertFalse($method->invokeArgs($this->translator, [ 'foo*bar' ]));
+    }
+
+    public function testTranslateSentence()
+    {
+        $method = new \ReflectionMethod($this->translator, 'translateSentence');
+        $method->setAccessible(true);
+
+        $this->assertEquals('T_CONNECTOR', $method->invokeArgs($this->translator, [ 'C_AND' ]));
+        $this->assertEquals('S_CONDITION', $method->invokeArgs($this->translator, [ 'T_FIELD T_OPERATOR T_LITERAL' ]));
+        $this->assertEquals('OQL', $method->invokeArgs($this->translator, [ 'S_CONDITION S_MODIFIER' ]));
+
+        $this->assertFalse($method->invokeArgs($this->translator, [ 'G_OBRACKET T_FIELD' ]));
+    }
+
+    public function testTranslateToken()
+    {
+        $method = new \ReflectionMethod($this->translator, 'translateToken');
+        $method->setAccessible(true);
+
+        $this->assertEquals('T_FIELD', $method->invokeArgs($this->translator, [ 'foobar' ]));
+        $this->assertEquals('T_STRING', $method->invokeArgs($this->translator, [ '"foobar"' ]));
+        $this->assertEquals('G_OBRACKET', $method->invokeArgs($this->translator, [ '[' ]));
+
+        $this->assertFalse($method->invokeArgs($this->translator, [ 'foo*bar' ]));
+    }
+}
