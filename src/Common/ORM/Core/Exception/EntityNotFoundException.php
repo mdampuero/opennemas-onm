19086e00fc91a3b273565ebe8c724bf2d5dcf89f
Diego Blanco <diego@openhost.es>
2016-03-08 00:59:41 +0100

    Improve id handling in EntityNotFoundException

--- src/Common/ORM/Core/Exception/EntityNotFoundException.php
+++ src/Common/ORM/Core/Exception/EntityNotFoundException.php
@@ -6,7 +6,16 @@ class EntityNotFoundException extends \Exception
 {
     public function __construct($entity, $id, $error = '')
     {
-        $message = _('Unable to find entity of type %s with id %s');
+        $message = _('Unable to find entity of type %s with %s');
+
+        if (is_array($id)) {
+            $str = '';
+            foreach ($id as $key => $value) {
+                $str .= $key . '=' . $value . ',';
+            }
+
+            $id = rtrim($str, ',');
+        }
 
         if (!empty($error)) {
             $message .= ': ' . $error;
