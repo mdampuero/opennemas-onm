cf1b4fce2ea0407c8ce76753a352813618f71cd9
Diego Blanco <diego@openhost.es>
2016-03-16 19:20:40 +0100

    Add assertions for YoutubeUrlFilterTest

--- tests/Common/Migration/Filter/YoutubeUrlFilterTest.php
+++ tests/Common/Migration/Filter/YoutubeUrlFilterTest.php
@@ -17,5 +17,8 @@ class YoutubeUrlFilterTest extends \PHPUnit_Framework_TestCase
         $expected = 'http://www.youtube.com/watch?v=' . $str;
 
         $this->assertEquals($expected, $this->filter->filter($str));
+        $this->assertFalse($this->filter->filter(1));
+        $this->assertFalse($this->filter->filter([]));
+        $this->assertFalse($this->filter->filter(null));
     }
 }
