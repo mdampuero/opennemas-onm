bc203f114d06970bc169e58abf7dba5fb3b245cc
Diego Blanco <diego@openhost.es>
2016-03-21 10:48:42 +0100

    Add actions to convert boolean to/from integer

--- src/Common/ORM/Database/Data/Mapper/BooleanDataMapper.php
+++ src/Common/ORM/Database/Data/Mapper/BooleanDataMapper.php
@@ -28,6 +28,22 @@ class BooleanDataMapper
     }
 
     /**
+     * Unserializes an boolean.
+     *
+     * @param string $value The serialized boolean.
+     *
+     * @return string The boolean.
+     */
+    public function fromInteger($value)
+    {
+        if (empty($value)) {
+            return false;
+        }
+
+        return $value === 1;
+    }
+
+    /**
      * Serializes an boolean.
      *
      * @param array $value The boolean to serialize.
@@ -37,6 +53,22 @@ class BooleanDataMapper
     public function toBoolean($value)
     {
         if (empty($value) || !is_bool($value)) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    /**
+     * Unserializes an boolean.
+     *
+     * @param string $value The serialized boolean.
+     *
+     * @return string The boolean.
+     */
+    public function toInteger($value)
+    {
+        if (empty($value)) {
             return 0;
         }
 
