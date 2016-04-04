d4b026132298179386d931b1ac3dd019b9f15435
Diego Blanco <diego@openhost.es>
2016-04-04 19:26:03 +0200

    Refactor notification section

--- public/themes/manager/js/routing.js
+++ public/themes/manager/js/routing.js
@@ -9,21 +9,6 @@
             .when('/', {
               templateUrl: '/managerws/template/index:index.' + appVersion + '.tpl'
             })
-          .when(routingProvider.ngGenerateShort('manager_notifications_list'), {
-            templateUrl: '/managerws/template/notification:list.' + appVersion + '.tpl',
-            controller: 'NotificationListCtrl',
-            reloadOnSearch: false
-          })
-          .when(routingProvider.ngGenerateShort('manager_notification_create'), {
-            templateUrl: '/managerws/template/notification:item.' + appVersion + '.tpl',
-            controller: 'NotificationCtrl',
-          })
-          .when(routingProvider.ngGenerateShort('manager_notification_show', {
-            id: '\:id'
-          }), {
-            templateUrl: '/managerws/template/notification:item.' + appVersion + '.tpl',
-            controller: 'NotificationCtrl',
-          })
           .when(routingProvider.ngGenerateShort('manager_reports_list'), {
             templateUrl: '/managerws/template/report:list.' + appVersion + '.tpl',
             controller: 'ReportListCtrl',
