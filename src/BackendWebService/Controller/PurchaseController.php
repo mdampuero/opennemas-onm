47fc930980de71100584e3ba7e502d8a3e6a3209
Diego Blanco <diego@openhost.es>
2016-04-07 13:26:47 +0200

    Add action to get invoice PDF from backend

---
+++ src/BackendWebService/Controller/PurchaseController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace BackendWebService\Controller;
+
+use Framework\ORM\Entity\Purchase;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Intl\Intl;
+
+/**
+ * Handles the Purchase resource.
+ */
+class PurchaseController extends Controller
+{
+    /**
+     * @api {get} /purchases/:id.pdf Get PDF for purchase
+     * @apiName GetPurchasePDF
+     * @apiGroup Purchase
+     *
+     * @apiParam {String} id  The purchase id.
+     */
+    public function getPdfAction($id)
+    {
+        $em = $this->get('orm.manager');
+
+        $purchase = $em->getRepository('manager.purchase')->findOneBy([
+            'id'          => [ [ 'value' => $id ] ],
+            'instance_id' => [ [ 'value' => $this->get('instance')->id ] ]
+        ]);
+
+        var_dump($purchase);die();
+        $pdf = $em->getRepository('invoice', 'FreshBooks')
+            ->getPDF($purchase->invoice_id);
+
+        $response = new Response($pdf);
+
+        $response->headers->set('Content-Type', 'application/pdf');
+
+        return $response;
+    }
+}
