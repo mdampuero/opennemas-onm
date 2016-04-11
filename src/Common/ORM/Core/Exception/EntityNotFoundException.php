0dfc4c22f36e6eaa58ebe79afbfee661ddd607b2
Diego Blanco <diego@openhost.es>
2016-04-11 11:19:15 +0200

    Update documentation

--- src/Common/ORM/Core/Exception/EntityNotFoundException.php
+++ src/Common/ORM/Core/Exception/EntityNotFoundException.php
@@ -9,8 +9,18 @@
  */
 namespace Common\ORM\Core\Exception;
 
+/**
+ * Exception thrown when no entities found basing on an id or criteria.
+ */
 class EntityNotFoundException extends \Exception
 {
+    /**
+     * Initializes the exception.
+     *
+     * @param String $entity The entity name.
+     * @param String $id     The entity id.
+     * @param String $error  The error message.
+     */
     public function __construct($entity, $id = '', $error = '')
     {
         $message = _('Unable to find entity of type "%s"');
