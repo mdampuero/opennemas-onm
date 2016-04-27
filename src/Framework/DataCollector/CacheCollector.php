7179abdc9d34cc92c1c22d7edd2c79b052bad142
Diego Blanco <diego@openhost.es>
2016-04-27 18:42:54 +0200

    Improve CacheCollector and update template

--- src/Framework/DataCollector/CacheCollector.php
+++ src/Framework/DataCollector/CacheCollector.php
@@ -44,11 +44,55 @@ class CacheCollector extends DataCollector
     }
 
     /**
+     * Returns the amount of cache queries.
+     *
+     * @return integer The amount of cache queries.
+     */
+    public function getCountCache()
+    {
+        return count($this->getCacheQueries());
+    }
+
+    /**
+     * Returns the amount of MRU queries.
+     *
+     * @return integer The amount of MRU queries.
+     */
+    public function getCountMru()
+    {
+        return count($this->getMruQueries());
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getName()
     {
         return 'cache_collector';
+    }
+
+    /**
+     * Returns the list of cache queries.
+     *
+     * @return array The list of cache queries.
+     */
+    public function getCacheQueries()
+    {
+        return array_filter($this->data, function ($a) {
+            return !array_key_exists('mru', $a) || !$a['mru'];
+        });
+    }
+
+    /**
+     * Returns the list of MRU queries.
+     *
+     * @return array The list of MRU queries.
+     */
+    public function getMruQueries()
+    {
+        return array_filter($this->data, function ($a) {
+            return array_key_exists('mru', $a) && $a['mru'];
+        });
     }
 
     /**
