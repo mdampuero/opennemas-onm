c0c17da66007c9d48a265dc41273399aa6b57372
Diego Blanco <diego@openhost.es>
2016-04-27 19:59:26 +0200

    Add action to render widget to PlaygroundController

--- src/Frontend/Controller/PlaygroundController.php
+++ src/Frontend/Controller/PlaygroundController.php
@@ -53,4 +53,51 @@ class PlaygroundController extends Controller
     {
         return $this->render('playground.tpl');
     }
+
+    /**
+     * Displays a widget basing on the request parameters.
+     *
+     * @param Request $request The request object.
+     *
+     * @param Response The response object.
+     */
+    public function widget(Request $request)
+    {
+        $name = $request->query->get('name');
+        $id   = $request->query->get('id');
+
+        if (empty($id) && empty($name)) {
+            return;
+        }
+
+        $params = $request->query->all();
+        $widget = null;
+
+        unset($params['action'], $params['id'], $params['name']);
+
+        if (!empty($id)) {
+            $widget = $this->get('entity_repository')->find('Widget', $id);
+
+            return $widget->render($params);
+        }
+
+        $criteria = [
+            'tables'            => [ 'widgets' ],
+            'pk_content'        => [
+                [ 'value' => 'pk_widget', 'field' => true ]
+            ],
+            'content'           => [ [ 'value' => $name ] ],
+            'content_type_name' => [ [ 'value' => 'widget' ] ],
+            'content_status'    => [ [ 'value' => 1 ] ],
+            'in_litter'         => [ [ 'value' => 0 ] ],
+        ];
+
+        $widget = $this->get('entity_repository')->findOneBy($criteria);
+
+        if (empty($widget)) {
+            return;
+        }
+
+        return $widget->render($params);
+    }
 }
