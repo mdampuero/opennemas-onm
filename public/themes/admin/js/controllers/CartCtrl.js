7b61e3a4000544f4876c90cb20d235027d4926c6
Diego Blanco <diego@openhost.es>
2016-04-28 20:27:34 +0200

    Create controller and template to manage the cart

---
+++ public/themes/admin/js/controllers/CartCtrl.js
@@ -0,0 +1,86 @@
+(function() {
+  'use strict';
+
+  angular.module('BackendApp.controllers')
+    /**
+     * @ngdoc controller
+     * @name  CartCtrl
+     *
+     * @requires $rootScope
+     * @requires $scope
+     *
+     * @description
+     *   Manages actions for cart.
+     */
+    .controller('CartCtrl', [
+      '$rootScope', '$scope',
+      function ($rootScope, $scope) {
+        /**
+         * @function getPrice
+         * @memberOf CartCtrl
+         *
+         * @description
+         *   Returns the price for an item.
+         *
+         * @param {Integer} index The position of the item.
+         * @param {String}  type  The type of the price.
+         *
+         * @return {Float} The item price.
+         */
+        $scope.getPrice = function (index, type = 'monthly') {
+          if (!$scope.cart[index].price ||
+              $scope.cart[index].price.length === 0) {
+            return { value: 0 };
+          }
+
+          var prices = $scope.cart[index].price.filter(function(a) {
+            return a.type === type;
+          });
+
+          if (prices.length > 0) {
+            return prices[0];
+          }
+
+          return $scope.cart[index].price[0];
+        };
+
+        /**
+         * @function getSubTotal
+         * @memberOf CartCtrl
+         *
+         * @description
+         *   Returns the total price of the items in cart.
+         *
+         * @return {Float} The total price of the items in cart.
+         */
+        $scope.getSubTotal = function () {
+          if (!$scope.cart) {
+            return 0;
+          }
+
+          var subtotal = 0;
+
+          for (var i = 0; i < $scope.cart.length; i++) {
+            subtotal += $scope.getPrice(i).value;
+          }
+
+          return subtotal;
+        };
+
+        // Force float type for prices
+        $scope.$watch('cart', function(nv) {
+          if (!nv || nv.length === 0) {
+            return;
+          }
+
+          for (var i = 0; i < nv.length; i++) {
+            for (var j = 0; j < nv[i].price.length; j++) {
+              nv[i].price[j].value = parseFloat(nv[i].price[j].value);
+            }
+          }
+
+          $rootScope.$broadcast('subtotal-changed', $scope.getSubTotal());
+        }, true);
+      }
+    ]);
+})();
---
+++ public/themes/admin/tpl/store/_cart.tpl
@@ -0,0 +1,32 @@
+<div ng-controller="CartCtrl">
+  <ul class="cart-list cart-list-big ng-cloak">
+    <li ng-repeat="item in cart">
+      <img class="img-responsive pull-left" ng-if="item.images && item.images.length > 0" ng-src="[% '/asset/scale,300,300' + item.path + '/' + item.images[0] %]">
+      <div ng-class="{ 'p-l-15': !item.images || item.images.length === 0, 'p-l-100': item.images && item.images.length > 0 }">
+        <h5 class="no-overflow" ng-bind-html="item.name"></h5>
+        <div class="clearfix">
+          <p class="description pull-left no-margin" ng-bind-html="item.description"></p>
+          <div class="text-right p-r-15 p-b-15">
+            <div class="price">
+              <h4 class="no-margin">
+                <strong>[% getPrice($index).value %]</strong>
+                <small ng-show="getPrice($index).type === 'yearly'">€/{t}year{/t}</small>
+                <small ng-show="getPrice($index).type === 'monthly'">€/{t}month{/t}</small>
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <i class="fa fa-times pull-left" ng-click="cart.splice($index, 1)"></i>
+    </li>
+  </ul>
+  <div class="ng-cloak text-right">
+    <div class="p-r-30 p-t-10">
+      <h4>
+        <span class="m-r-30 uppercase">{t}Total{/t}:</span>
+        <strong>[% getSubTotal() %]</strong>
+        <small>€</small>
+      </h4>
+    </div>
+  </div>
+</div>
