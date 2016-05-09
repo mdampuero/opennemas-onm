7583f8ef8fd8ad154738691cc08ada261e7b8995
Diego Blanco <diego@openhost.es>
2016-05-10 01:11:45 +0200

    Update OnmUserProvider to use new ORM

--- src/Backend/Security/OnmUserProvider.php
+++ src/Backend/Security/OnmUserProvider.php
@@ -1,16 +1,15 @@
 <?php
-
 /**
  * This file is part of the Onm package.
  *
- * (c)  OpenHost S.L. <developers@openhost.es>
+ * (c) Openhost, S.L. <developers@opennemas.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Backend\Security;
 
+use Common\ORM\Core\EntityManager;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
@@ -21,38 +20,20 @@ use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 class OnmUserProvider implements UserProviderInterface
 {
     /**
-     * The cache service.
+     * The entity manager.
      *
-     * @var AbstractCache
+     * @var EntityManager
      */
-    protected $cache;
-
-    /**
-     * The instance manager service.
-     *
-     * @var InstanceManager
-     */
-    protected $im;
-
-    /**
-     * The user repository service
-     *
-     * @var UserManager
-     */
-    protected $um;
+    protected $em;
 
     /**
      * Initializes the current user provider.
      *
-     * @param CacheInterface  $cache The cache object.
-     * @param InstanceManager $im    The instance manager.
-     * @param UserRepository  $um    The user repository.
+     * @param EntityManager $em The entity manager.
      */
-    public function __construct($cache, $im, $um)
+    public function __construct(EntityManager $em)
     {
-        $this->cache = $cache;
-        $this->im = $im;
-        $this->um = $um;
+        $this->em = $em;
     }
 
     /**
@@ -69,22 +50,12 @@ class OnmUserProvider implements UserProviderInterface
      */
     public function loadUserByUsername($username)
     {
-        $criteria = array(
-            'union' => 'OR',
-            'username' => array(array('value' => $username)),
-            'email' => array(array('value' => $username))
-        );
+        $oql = sprintf('username = "%s" or email = "%s"', $username, $username);
 
-        $user = $this->um->findOneBy($criteria);
+        $user = $this->em->getRepository('User', 'instance')->findOneBy($oql);
 
         if (!$user) {
-            $database = $this->um->conn->dbname;
-            $this->um->selectDatabase('onm-instances');
-            $this->cache->setNamespace('manager');
-            $GLOBALS['application']->conn->selectDatabase('onm-instances');
-
-            $user = $this->um->findOneBy($criteria);
-            $this->um->conn->selectDatabase($database);
+            $user = $this->em->getRepository('User', 'manager')->findOneBy($oql);
         }
 
         if (!$user) {
