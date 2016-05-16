d1f72e3249bc3987d4c04adace69fcaf1eba49f6
Diego Blanco <diego@openhost.es>
2016-05-16 17:16:56 +0200

    Update save, delete, deleteSelected and patch actions

--- src/ManagerWebService/Controller/InstanceController.php
+++ src/ManagerWebService/Controller/InstanceController.php
@@ -10,83 +10,13 @@ use Onm\Exception\BackupException;
 use Onm\Exception\DatabaseNotDeletedException;
 use Onm\Exception\DatabaseNotRestoredException;
 use Onm\Instance\InstanceCreator;
-use Onm\Instance\Instance;
-use Onm\Instance\InstanceManager as im;
-use Onm\Module\ModuleManager as mm;
+use Common\ORM\Entity\Instance;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Intl\Intl;
 
 class InstanceController extends Controller
 {
-    /**
-     * Creates a new instance from the request.
-     *
-     * @param Request $request The request object.
-     *
-     * @return Response The response object.
-     */
-    public function createAction(Request $request)
-    {
-        $instance = new Instance();
-        foreach ($request->request as $key => $value) {
-            if (!is_null($value)) {
-                $instance->{$key} =
-                    $request->request->filter($key, null, FILTER_SANITIZE_STRING);
-            }
-        }
-        $instance->created = date('Y-m-d H:i:s');
-
-        $im      = $this->get('instance_manager');
-        $creator = new InstanceCreator($im->getConnection());
-
-        try {
-            $this->get('onm.validator.instance')->validate($instance);
-
-            $im->persist($instance);
-
-            $creator->createDatabase($instance->id);
-            $creator->copyDefaultAssets($instance->internal_name);
-
-            $im->configureInstance($instance);
-
-            $response = new JsonResponse(_('Instance saved successfully'), 201);
-
-            // Add permanent URL for the current instance
-            $response->headers->set(
-                'Location',
-                $this->generateUrl(
-                    'manager_ws_instance_show',
-                    [ 'id' => $instance->id ]
-                )
-            );
-
-            return $response;
-        } catch (InstanceAlreadyExistsException $e) {
-            return new JsonResponse(
-                _('The instance already exists'),
-                409
-            );
-        } catch (DatabaseNotRestoredException $e) {
-            $creator->deleteDatabase($instance->id);
-            $im->remove($instance);
-
-            return new JsonResponse(
-                _('Unable to create the database for the instance'),
-                409
-            );
-        } catch (\Exception $e) {
-            $creator->deleteAssets($instance->internal_name);
-            $creator->deleteDatabase($instance->id);
-            $im->remove($instance);
-
-            return new JsonResponse(
-                _($e->getMessage()),
-                409
-            );
-        }
-    }
-
     /**
      * Deletes an instance.
      *
@@ -106,9 +36,6 @@ class InstanceController extends Controller
             $assetFolder = realpath(
                 SITE_PATH . DS . 'media' . DS . $instance->internal_name
             );
-
-            //$backupPath = BACKUP_PATH . DS . $instance->id . "-"
-                //. $instance->internal_name . DS . "DELETED-" . date("YmdHi");
 
             $backupPath = $this->getParameter('kernel.root_dir')
                 . '/../tmp/backups/' . $instance->internal_name . '/DELETED-'
@@ -133,7 +60,6 @@ class InstanceController extends Controller
             $creator->deleteBackup($backupPath);
             $msg->add($e->getMessage(), 'error', 400);
         } catch (\Exception $e) {
-            var_dump($e->getMessage());die();
             $creator->restoreAssets($backupPath);
             $creator->restoreDatabase($backupPath . DS . 'database.sql');
             $creator->deleteBackup($backupPath);
@@ -157,40 +83,30 @@ class InstanceController extends Controller
      */
     public function deleteSelectedAction(Request $request)
     {
-        $error      = [];
-        $messages   = [];
-        $selected   = $request->request->get('selected', null);
-        $statusCode = 200;
-        $updated    = [];
+        $ids = $request->request->get('ids', []);
+        $msg = $this->get('core.messenger');
 
-        if (!is_array($selected)
-            || (is_array($selected) && count($selected) == 0)
-        ) {
-            return new JsonResponse(
-                _('Unable to find the instances for the given criteria'),
-                404
-            );
+        if (!is_array($ids) || empty($ids)) {
+            $msg->add(_('Bad request'), 'error', 400);
+            return new JsonResponse($msg->getMessages(), $msg->getCode());
         }
 
-        $im      = $this->get('instance_manager');
-        $creator = new InstanceCreator($im->getConnection());
+        $em      = $this->get('orm.manager');
+        $creator = new InstanceCreator($em->getConnection('manager'));
+        $oql     = sprintf('id in [%s]', implode(',', $ids));
 
-        $criteria = [
-            'id' => [
-                [ 'value' => $selected, 'operator' => 'IN']
-            ]
-        ];
+        $instances = $em->getRepository('Instance')->findBy($oql);
 
-        $instances = $im->findBy($criteria);
-
+        $deleted = 0;
         foreach ($instances as $instance) {
             try {
                 $assetFolder = realpath(
                     SITE_PATH . DS . 'media' . DS . $instance->internal_name
                 );
 
-                $backupPath = BACKUP_PATH . DS . $instance->id . "-"
-                    . $instance->internal_name . DS . "DELETED-" . date("YmdHi");
+                $backupPath = $this->getParameter('kernel.root_dir')
+                    . '/../tmp/backups/' . $instance->internal_name . '/DELETED-'
+                    . date('YmdHis');
 
                 $database = $instance->getDatabaseName();
 
@@ -201,73 +117,36 @@ class InstanceController extends Controller
 
                 $creator->deleteAssets($instance->internal_name);
                 $creator->deleteDatabase($database);
-                $im->remove($instance);
+                $em->remove($instance);
 
-                $updated[] = $instance->id;
-            } catch (InstanceNotFoundException $e) {
-                $error[]    = $id;
-                $messages[] = [
-                    'message' => sprintf(_('Unable to find the instance with id "%s"'), $id),
-                    'type'    => 'error'
-                ];
+                $deleted++;
             } catch (BackupException $e) {
-                $message = $e->getMessage();
-
                 $creator->deleteBackup($backupPath);
-
-                $error[]    = $id;
-                $messages[] = [
-                    'message' => sprintf(_($message), $id),
-                    'type'    => 'error'
-                ];
-            } catch (AssetsNotDeletedException $e) {
-                $message = $e->getMessage();
-
-                $creator->restoreAssets($backupPath);
-
-                $error[]    = $id;
-                $messages[] = [
-                    'message' => sprintf(_($message), $id),
-                    'type'    => 'error'
-                ];
+                $msg->add($e->getMessage(), 'error', 400);
             } catch (DatabaseNotDeletedException $e) {
-                $message    = $e->getMessage();
-
+                $creator->deleteBackup($backupPath);
+                $msg->add($e->getMessage(), 'error', 400);
+            } catch (\Exception $e) {
                 $creator->restoreAssets($backupPath);
                 $creator->restoreDatabase($backupPath . DS . 'database.sql');
+                $creator->deleteBackup($backupPath);
 
-                $error[]    = $id;
-                $messages[] = [
-                    'message' => sprintf(_($message), $id),
-                    'type'    => 'error'
-                ];
-            } catch (\Exception $e) {
-                $error[]    = $id;
-                $messages[] = [
-                    'message' => _($e->getMessage()),
-                    'type'    => 'error'
-                ];
+                $msg->add(
+                    sprintf(_('Error while deleting instance with id "%s"'), $id),
+                    'error',
+                    400
+                );
             }
         }
 
-        if (count($updated) > 0) {
-            $messages = [
-                'message' => sprintf(_('%s instances deleted successfully.'), count($updated)),
-                'type'    => 'success'
-            ];
+        if ($deleted > 0) {
+            $msg->add(
+                sprintf(_('%s instances deleted successfully'), $deleted),
+                'success'
+            );
         }
 
-        // Return the proper status code
-        if (count($error) > 0 && count($updated) > 0) {
-            $statusCode = 207;
-        } elseif (count($error) > 0) {
-            $statusCode = 409;
-        }
-
-        return new JsonResponse(
-            [ 'error' => $error, 'messages' => $messages ],
-            $statusCode
-        );
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
     }
 
     /**
@@ -284,13 +163,7 @@ class InstanceController extends Controller
 
         $repository = $this->get('orm.manager')->getRepository('Instance');
         $converter  = $this->get('orm.manager')->getConverter('Instance');
-
-        $instances = $repository->findBy($oql);
-        $total     = $repository->countBy($oql);
-
-        //foreach ($instances as &$instance) {
-            //$im->getExternalInformation($instance);
-        //}
+        $instances  = $repository->findBy($oql);
 
         $this->view = new \TemplateManager(TEMPLATE_MANAGER);
 
@@ -368,36 +241,28 @@ class InstanceController extends Controller
      */
     public function patchAction(Request $request, $id)
     {
-        $im = $this->get('instance_manager');
+        $em  = $this->get('orm.manager');
+        $msg = $this->get('core.messenger');
+        $data = $em->getConverter('Instance')
+            ->objectify($request->request->all());
 
-        try {
-            $instance = $im->find($id);
-            $oldActivated = $instance->activated;
+        $instance = $em->getRepository('Instance')->find($id);
 
-            foreach ($request->request->all() as $key => $value) {
-                $instance->{$key} =
-                    $request->request->filter($key, null, FILTER_SANITIZE_STRING);
-            }
+        $old = $instance->activated;
+        $instance->merge($data);
 
-            $this->get('onm.validator.instance')->validate($instance);
-            $im->persist($instance);
+        $em->persist($instance);
 
-            if ($oldActivated != $instance->activated) {
-                dispatchEventWithParams(
-                    'instance.update',
-                    array('instance' => $instance->internal_name)
-                );
-            }
-
-            return new JsonResponse(_('Instance saved successfully'));
-        } catch (InstanceNotFoundException $e) {
-            return new JsonResponse(
-                sprintf(_('Unable to find the instance with id "%s"'), $id),
-                404
+        if ($old != $instance->activated) {
+            dispatchEventWithParams(
+                'instance.update',
+                [ 'instance' => $instance->internal_name ]
             );
-        } catch (\Exception $e) {
-            return new JsonResponse(_($e->getMessage()), 400);
         }
+
+        $msg->add(_('Instance saved successfully'), 'success');
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
     }
 
     /**
@@ -483,6 +348,72 @@ class InstanceController extends Controller
     }
 
     /**
+     * Creates a new instance from the request.
+     *
+     * @param Request $request The request object.
+     *
+     * @return Response The response object.
+     */
+    public function saveAction(Request $request)
+    {
+        $em   = $this->get('orm.manager');
+        $msg  = $this->get('core.messenger');
+        $data = $em->getConverter('Instance')
+            ->objectify($request->request->all());
+
+        $instance = new Instance($data);
+        $creator  = new InstanceCreator($em->getConnection('manager'));
+
+        $instance->created = new \DateTime('now');
+
+        try {
+            $this->get('core.instance.checker')->check($instance);
+            $em->persist($instance);
+
+            $creator->createDatabase($instance->id);
+            $creator->copyDefaultAssets($instance->internal_name);
+
+            //$im->configureInstance($instance);
+
+            $msg->add(_('Instance saved successfully'), 'success', 201);
+
+            // Add permanent URL for the current instance
+            $response = new JsonResponse($msg->getMessages(), $msg->getCode());
+            $response->headers->set(
+                'Location',
+                $this->generateUrl(
+                    'manager_ws_instance_show',
+                    [ 'id' => $instance->id ]
+                )
+            );
+
+            return $response;
+        } catch (InstanceAlreadyExistsException $e) {
+            return new JsonResponse(
+                _('The instance already exists'),
+                409
+            );
+        } catch (DatabaseNotRestoredException $e) {
+            $creator->deleteDatabase($instance->id);
+            $em->remove($instance);
+
+            return new JsonResponse(
+                _('Unable to create the database for the instance'),
+                409
+            );
+        } catch (\Exception $e) {
+            $creator->deleteAssets($instance->internal_name);
+            $creator->deleteDatabase($instance->id);
+            $em->remove($instance);
+
+            return new JsonResponse(
+                _($e->getMessage()),
+                409
+            );
+        }
+    }
+
+    /**
      * Returns an instance as JSON.
      *
      * @param integer  $id The instance id.
@@ -491,48 +422,32 @@ class InstanceController extends Controller
      */
     public function showAction($id)
     {
-        $im = $this->get('instance_manager');
+        $em        = $this->get('orm.manager');
+        $converter = $em->getConverter('Instance');
+        $instance  = $em->getRepository('Instance')->find($id);
 
-        try {
-            $instance = $im->find($id);
-            $theme    = $instance->settings['TEMPLATE_USER'];
-            $template = $this->templateParams();
+        $instance->settings['TEMPLATE_USER'] = 'es.openhost.theme.'
+            . str_replace('es.openhost.theme.', '', $instance->settings['TEMPLATE_USER']);
 
-            $template['countries']= Intl::getRegionBundle()->getCountryNames();
+        $extra = $this->getExtraData();
 
-            if (strpos($theme, 'es.openhost.theme.') === false) {
-                $theme = 'es.openhost.theme.' . $theme;
+        //$im->getExternalInformation($instance);
+        if (!empty($instance->getClient())) {
+            try {
+                $client = $this->get('orm.manager')
+                    ->getRepository('Client')
+                    ->find($instance->getClient());
 
-                $instance->settings['TEMPLATE_USER'] = $theme;
+                $extra['client']    = $client->getData();
+                $extra['countries'] = Intl::getRegionBundle()->getCountryNames();
+            } catch (\Exception $e) {
             }
-
-            $im->getExternalInformation($instance);
-
-            if (!empty($instance->getClient())) {
-                try {
-                    $client = $this->get('orm.manager')
-                        ->getRepository('client', 'Database')
-                        ->find($instance->getClient());
-
-                    $template['client'] = $client->getData();
-                } catch (\Exception $e) {
-                }
-            }
-
-            return new JsonResponse(
-                array(
-                    'instance' => $instance,
-                    'template' => $template,
-                )
-            );
-        } catch (InstanceNotFoundException $e) {
-            return new JsonResponse(
-                sprintf(_('Unable to find the instance with id "%s"'), $id),
-                404
-            );
-        } catch (\Exception $e) {
-            return new JsonResponse(_($e->getMessage()), 400);
         }
+
+        return new JsonResponse([
+            'extra'    => $extra,
+            'instance' => $converter->responsify($instance->getData())
+        ]);
     }
 
     /**
@@ -600,14 +515,15 @@ class InstanceController extends Controller
      *
      * @return array Array of template parameters.
      */
-    private function templateParams()
+    private function getExtraData()
     {
-        $lang    = $this->get('core.loader')->getLocaleShort();
-        $themes  = $this->get('orm.loader')->getPlugins();
-        $modules = $this->get('orm.manager')
-            ->getRepository('manager.extension')
-            ->findBy([ 'type' => [ 'union' => 'OR', [ 'value' => 'module' ], [ 'value' => 'theme-addon' ] ] ], []);
+        $lang    = $this->get('core.locale')->getLocaleShort();
+        //$themes  = $this->get('orm.loader')->getPlugins();
+        $modules = $this->get('orm.manager')->getRepository('Extension')
+            ->findBy('type = "module" or type = "theme-addon" limit 10');
 
+        //$modules = [];
+        $themes = [];
         $modules = array_map(function (&$a) {
             foreach ([ 'about', 'description', 'name' ] as $key) {
                 if (!empty($a->{$key})) {
@@ -643,7 +559,7 @@ class InstanceController extends Controller
                 'EXPERT',
                 'OTHER',
             ],
-            'templates' => im::getAvailableTemplates(),
+            //'templates' => im::getAvailableTemplates(),
             'themes'    => $themes,
             'timezones' => \DateTimeZone::listIdentifiers(),
             'modules'   => $modules,
