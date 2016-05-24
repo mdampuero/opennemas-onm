ef110151bc73237fa1246442306dcef2ff49acde
Diego Blanco <diego@openhost.es>
2016-05-24 22:55:55 +0200

    Add an action to return the extension price

--- src/Framework/ORM/Entity/Extension.php
+++ src/Framework/ORM/Entity/Extension.php
@@ -4,4 +4,29 @@ namespace Framework\ORM\Entity;
 
 class Extension extends Entity
 {
+    /**
+     * Returns the extension price.
+     *
+     * @param string $type The price type.
+     *
+     * @return float The extension price.
+     */
+    public function getPrice($type = 'monthly')
+    {
+        if (!array_key_exists('price', $this->metas)
+            || empty($this->metas['price'])
+        ) {
+            return 0;
+        }
+
+        $prices = array_filter($this->metas['price'], function ($a) use ($type) {
+            return $a['type'] === $type;
+        });
+
+        if (count($prices)) {
+            return $prices[0]['value'];
+        }
+
+        return $this->metas['price'][0]['value'];
+    }
 }
