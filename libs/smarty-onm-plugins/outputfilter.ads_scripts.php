277b69bfd19ff2ef6c074ab1201914b86ad4372b
Alex Rodrigues <alex@openhost.es>
2016-05-25 14:09:59 +0200

    ONM-1379: Avoid running scripts on undesired pages
    Signed-off-by: Alex rico <alex@openhost.es>

--- libs/smarty-onm-plugins/outputfilter.ads_scripts.php
+++ libs/smarty-onm-plugins/outputfilter.ads_scripts.php
@@ -14,7 +14,16 @@ function smarty_outputfilter_ads_scripts($output, $smarty)
     $sm      = getService('setting_repository');
     $uri     = $request->getUri();
 
-    if (!preg_match('@\.amp\.html$@', $uri)) {
+    if (!preg_match('/\/admin\/frontpages/', $referer)
+        && !preg_match('/\/manager/', $uri)
+        && !preg_match('/\/managerws/', $uri)
+        && !preg_match('/\/share-by-email/', $uri)
+        && !preg_match('/\/sharrre/', $uri)
+        && !preg_match('/\/ads/', $uri)
+        && !preg_match('/\/comments/', $uri)
+        && !preg_match('/\/fb\/instant-articles/', $uri)
+        && !preg_match('@\.amp\.html$@', $uri)
+    ) {
         $settings = $sm->get([ 'header_script', 'body_start_script', 'body_end_script' ]);
 
         if (array_key_exists('header_script', $settings)
