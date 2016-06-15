c282e2c3c08409227650c85fa35c7f6c2d943863
Diego Blanco <diego@openhost.es>
2016-06-15 17:20:07 +0100

    Create ThemeLoaderListener to initialize services basing on theme

---
+++ src/Framework/EventListener/ThemeLoaderListener.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelEvents as SymfonyKernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+use Onm\Cache\AbstractCache;
+use Onm\Instance\InstanceManager;
+use Onm\Exception\InstanceNotFoundException;
+use Onm\Exception\InstanceNotRegisteredException;
+
+/**
+ * Loads and initializes an instance from the request object.
+ */
+class ThemeLoaderListener implements EventSubscriberInterface
+{
+    /**
+     * The current theme.
+     *
+     * @var Theme.
+     */
+    protected $theme;
+
+    /**
+     * Initializes the instance loader.
+     *
+     * @param ServiceContainer $container The service container.
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Loads a theme basing on the request.
+     *
+     * @param GetResponseEvent $event A GetResponseEvent object.
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $this->theme = $this->getActiveTheme();
+
+        if (empty($this->theme)) {
+            return;
+        }
+
+        foreach ($this->theme->parameters as $key => $values) {
+            if (method_exists($this, 'load' . $key)) {
+                $this->{'load' . $key}($values);
+            }
+        }
+    }
+
+    /**
+     * Returns the current theme.
+     *
+     * @return Instance The current theme.
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array The event names to listen to.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            SymfonyKernelEvents::REQUEST => [ [ 'onKernelRequest', 100 ] ],
+        ];
+    }
+
+    /**
+     * Returns the active theme.
+     *
+     * @return mixed The active theme if it exists. False otherwise.
+     */
+    protected function getActiveTheme()
+    {
+        $instance = $this->container->get('instance');
+
+        return $this->getThemeByUuid($instance->settings['TEMPLATE_USER']);
+    }
+
+    /**
+     * TODO: Remove when new ORM is merged or search by uuid is allowed in the
+     *       theme repository.
+     *
+     * Returns a theme given an UUID.
+     *
+     * @param string $uuid The theme UUID.
+     *
+     * @return mixed The theme if it exists. False otherwise.
+     */
+    protected function getThemeByUuid($uuid)
+    {
+        if (empty($this->themes)) {
+            $this->themes = $this->container->get('orm.loader')->getPlugins();
+        }
+
+        $themes = array_filter($this->themes, function ($a) use ($uuid) {
+            $uuid = 'es.openhost.theme.'
+                . str_replace('es.openhost.theme.', '', $uuid);
+
+            return $a->uuid === $uuid;
+        });
+
+        if (empty($themes)) {
+            return false;
+        }
+
+        return array_shift($themes);
+    }
+
+    /**
+     * Adds advertisement positions defined by theme to the advertisement
+     * manager.
+     *
+     * @param array $positions The list of positions.
+     */
+    protected function loadAdvertisements($positions)
+    {
+        $this->container->get('core.manager.advertisement')
+            ->addPositions($positions);
+    }
+
+    /**
+     * Adds layouts defined by theme to the layout manager.
+     *
+     * @param array $positions The list of positions.
+     */
+    protected function loadLayouts($layouts)
+    {
+        $this->container->get('core.manager.layout')->addLayouts($layouts);
+    }
+
+    /**
+     * Adds menu positions defined by theme to the menu manager.
+     *
+     * @param array $menus The list of menu positions.
+     */
+    protected function loadMenus($menus)
+    {
+        $this->container->get('core.manager.menu')->addMenus($menus);
+    }
+}
