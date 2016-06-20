6eaf504c3c522172f5aa2d20de8de9d346c2a39e
Alex Rodrigues <alex@openhost.es>
2016-06-20 17:16:28 +0200

    ONM-1305: Add check for articles with video on structured data plugin
    Signed-off-by: Alex rico <alex@openhost.es>

--- libs/smarty-onm-plugins/function.structured_data_tags.php
+++ libs/smarty-onm-plugins/function.structured_data_tags.php
@@ -35,6 +35,21 @@ function smarty_function_structured_data_tags($params, &$smarty)
             $imageWidth = $photoInt->width;
             $imageHeight = $photoInt->height;
             $imageUrl = MEDIA_IMG_ABSOLUTE_URL.$photoInt->path_file.$photoInt->name;
+        } elseif (array_key_exists('videoInt', $smarty->tpl_vars)) {
+            // Articles with inner video
+            $videoInt = $smarty->tpl_vars['videoInt']->value;
+            if (!empty($videoInt) && strpos($videoInt->thumb, 'http')  === false) {
+                $videoInt->thumb = SITE_URL.$videoInt->thumb;
+            }
+            $imageUrl = $videoInt->thumb;
+            $imageSize = @getimagesize($imageUrl);
+            if (is_array($imageSize) &&
+                array_key_exists(0, $imageSize)
+                && array_key_exists(1, $imageSize)
+            ) {
+                $imageWidth = $imageSize[0];
+                $imageHeight = $imageSize[1];
+            }
         } elseif (array_key_exists('photo', $smarty->tpl_vars)) {
             // Opinions
             $photo = $smarty->tpl_vars['photo']->value;
