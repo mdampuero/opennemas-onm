d6942c5048d3ab8880d48902e22535892b84de82
Diego Blanco <diego@openhost.es>
2016-07-19 12:52:07 +0200

    Create a exception for invalid data sets

---
+++ tests/Common/ORM/Core/Exception/InvalidDataSetExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Common\ORM\Core\Exception;
+
+use Common\ORM\Core\Exception\InvalidDataSetException;
+
+/**
+ * Defines test cases for InvalidDataSetException class.
+ */
+class InvalidDataSetExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests constructor for InvalidDataSetException.
+     */
+    public function testInvalidDataSetException()
+    {
+        $class   = 'thud';
+        $dataset = 'wibble';
+
+        $e = new InvalidDataSetException($class);
+        $this->assertRegexp('/for "' . $class . '"$/', $e->getMessage());
+
+        $e = new InvalidDataSetException($class, $dataset);
+        $this->assertRegexp('/for "' . $class . '"/', $e->getMessage());
+        $this->assertRegexp('/dataset "' . $dataset . '"/', $e->getMessage());
+    }
+}
