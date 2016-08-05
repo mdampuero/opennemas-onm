c1e562e2e7259a0b19c643ebe3fb7602d51ba0e7
Diego Blanco <diego@openhost.es>
2016-08-05 16:05:53 +0200

    Update maintenance controller to get backend theme properly

--- src/Common/Core/Controller/MaintenanceController.php
+++ src/Common/Core/Controller/MaintenanceController.php
@@ -28,10 +28,14 @@ class MaintenanceController extends Controller
     public function defaultAction(Request $request)
     {
         $locale = $request->getPreferredLanguage();
+        $theme  = $this->get('orm.manager')->getRepository('Theme')
+            ->findOneBy('uuid = "es.openhost.theme.admin"');
 
         $this->get('core.locale')->setLocale($locale);
 
-        $this->view = new \TemplateAdmin();
+        $this->view = $this->get('core.template.admin');
+        $this->view->addActiveTheme($theme);
+
         $output = $this->renderView('maintenance/index.tpl');
 
         return new Response($output, 503);
