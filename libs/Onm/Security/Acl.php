3cd987d1e093784d5454d28d4c8a471ef78c333f
Diego Blanco <diego@openhost.es>
2016-08-24 00:59:54 +0200

    ONM-1611: Fix category checking in old Acl.php class

--- libs/Onm/Security/Acl.php
+++ libs/Onm/Security/Acl.php
@@ -46,7 +46,7 @@ class Acl
                 return true;
             }
 
-            $user = getService('security.token_storage')->getToken()->getUser();
+            $user = getService('core.user');
 
             if (is_null($user) || $user == 'anon.') {
                 return false;
@@ -56,9 +56,9 @@ class Acl
                 return true;
             }
 
-            $categories = $user->getAccessCategoryIds();
-
-            if (empty($categories) || !in_array($categoryID, $categories)) {
+            if (empty($user->categories)
+                || !in_array($categoryID, $user->categories)
+            ) {
                 return false;
             }
         } catch (Exception $e) {
