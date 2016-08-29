b72a22322511167fb3376eb214e9b65c3ad65085
Diego Blanco <diego@openhost.es>
2016-08-29 22:55:56 +0200

    Add permissions to template and controller for users

--- src/ManagerWebService/Controller/UserController.php
+++ src/ManagerWebService/Controller/UserController.php
@@ -9,10 +9,12 @@
  */
 namespace ManagerWebService\Controller;
 
+use Common\Core\Annotation\Security;
 use Common\ORM\Entity\User;
 use Onm\Framework\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Displays, saves, modifies and removes users.
@@ -25,6 +27,8 @@ class UserController extends Controller
      * @apiGroup User
      *
      * @apiSuccess {String} message The success message.
+     *
+     * @Security("hasPermission('USER_DELETE')")
      */
     public function deleteAction($id)
     {
@@ -47,6 +51,8 @@ class UserController extends Controller
      * @apiParam {Array} ids The user ids.
      *
      * @apiSuccess {Object} The success message.
+     *
+     * @Security("hasPermission('USER_DELETE')")
      */
     public function deleteSelectedAction(Request $request)
     {
@@ -92,6 +98,8 @@ class UserController extends Controller
      *
      * @apiSuccess {Integer} total   The total number of elements.
      * @apiSuccess {Array}   results The list of users.
+     *
+     * @Security("hasPermission('USER_ADMIN')")
      */
     public function listAction(Request $request)
     {
@@ -123,6 +131,8 @@ class UserController extends Controller
      * Returns the data to create a new user.
      *
      * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('USER_CREATE')")
      */
     public function newAction()
     {
@@ -139,6 +149,8 @@ class UserController extends Controller
      * @param Request $request The request object.
      *
      * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('USER_UPDATE')")
      */
     public function patchAction(Request $request, $id)
     {
@@ -163,6 +175,8 @@ class UserController extends Controller
      * @param Request $request The request object.
      *
      * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('USER_UPDATE')")
      */
     public function patchSelectedAction(Request $request)
     {
@@ -210,6 +224,8 @@ class UserController extends Controller
      * @param Request $request The request object.
      *
      * @return Response The response object.
+     *
+     * @Security("hasPermission('USER_CREATE')")
      */
     public function saveAction(Request $request)
     {
@@ -250,6 +266,15 @@ class UserController extends Controller
      */
     public function showAction($id)
     {
+        $security = $this->get('core.security');
+
+        if (!$security->hasPermission('USER_EDIT')
+            && (!$security->hasPermission('USER_EDIT_OWN_PROFILE')
+            || $id != $this->get('core.user')->id)
+        ) {
+            throw new AccessDeniedException();
+        }
+
         $em        = $this->get('orm.manager');
         $converter = $em->getConverter('User');
         $user      = $em->getRepository('User', 'manager')->find($id);
@@ -262,7 +287,7 @@ class UserController extends Controller
 
         return new JsonResponse([
             'extra' => $extra,
-            'user'  => $converter->responsify($user->getData())
+            'user'  => $converter->responsify($user)
         ]);
     }
 
@@ -276,6 +301,15 @@ class UserController extends Controller
      */
     public function updateAction(Request $request, $id)
     {
+        $security = $this->get('core.security');
+
+        if (!$security->hasPermission('USER_EDIT')
+            && (!$security->hasPermission('USER_EDIT_OWN_PROFILE')
+            || $id != $this->get('core.user')->id)
+        ) {
+            throw new AccessDeniedException();
+        }
+
         $em   = $this->get('orm.manager');
         $msg  = $this->get('core.messenger');
         $data = $em->getConverter('User')
