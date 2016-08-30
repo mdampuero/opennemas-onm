46bad19216080cc7832f6e74e211f61f6f019e6b
Alex Rodrigues <alex@openhost.es>
2016-08-30 17:11:55 +0200

    ONM-1670: Added missing DoubleDataMapper class with test
    Signed-off-by: Alex rico <alex@openhost.es>

---
+++ tests/Common/ORM/Core/Data/Mapper/DoubleDataMapperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Common\ORM\Core\Data\Mapper;
+
+use Common\ORM\Core\Data\Mapper\DoubleDataMapper;
+
+class DoubleDataMapperTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->mapper = new DoubleDataMapper();
+    }
+
+    public function testFromDouble()
+    {
+        $this->assertEquals(1.21, $this->mapper->fromDouble(1.21));
+        $this->assertEquals(1.21, $this->mapper->fromDouble('1.21'));
+        $this->assertEquals(null, $this->mapper->fromDouble(null));
+        $this->assertEquals(null, $this->mapper->fromDouble(''));
+    }
+
+    public function testToDouble()
+    {
+        $this->assertEquals(1.21, $this->mapper->toDouble(1.21));
+        $this->assertEquals(1.21, $this->mapper->toDouble('1.21'));
+        $this->assertEquals(null, $this->mapper->toDouble(null));
+        $this->assertEquals(null, $this->mapper->toDouble(''));
+    }
+}
