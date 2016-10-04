ab83bb43c5bf6b9dd6c83dab0fe9970929c906bc
Diego Blanco <diego@openhost.es>
2016-10-04 18:09:47 +0200

    Dispatch instance.update event to force cache deletion

--- src/BackendWebService/Controller/DomainController.php
+++ src/BackendWebService/Controller/DomainController.php
@@ -187,6 +187,10 @@ class DomainController extends Controller
 
             $ph->enable();
 
+            $this->get('core.dispatcher')->dispatch('instance.update', [
+                'instance' => $this->get('core.instance')
+            ]);
+
             $this->get('application.log')->info(
                 'The user ' . $this->getUser()->username
                 . '(' . $this->getUser()->id  .') has purchased '
--- src/BackendWebService/Controller/StoreController.php
+++ src/BackendWebService/Controller/StoreController.php
@@ -45,6 +45,10 @@ class StoreController extends Controller
 
             $ph->enable();
 
+            $this->get('core.dispatcher')->dispatch('instance.update', [
+                'instance' => $this->get('core.instance')
+            ]);
+
             $this->get('application.log')->info(
                 'The user ' . $this->getUser()->username
                 . '(' . $this->getUser()->id  .') has purchased '
