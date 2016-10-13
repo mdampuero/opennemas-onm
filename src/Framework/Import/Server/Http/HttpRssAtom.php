9e2dfb5f1518c2f8b17de1460e36c46da8551c8d
Alex Rodrigues <alex@openhost.es>
2016-10-13 18:14:01 +0200

    ONM-1862: Separate Rss from Atom in two different sources
    Signed-off-by: Alex rico <alex@openhost.es>

--- src/Framework/Import/Server/Http/HttpRssAtom.php
+++ src/Framework/Import/Server/Http/HttpRssAtom.php
@@ -10,9 +10,9 @@
 namespace Framework\Import\Server\Http;
 
 /**
- * Synchronize local folders with an external RSS-based source server.
+ * Synchronize local folders with an external RSS-Atom-based source server.
  */
-class HttpRss extends Http
+class HttpRssAtom extends HttpRss
 {
     /**
      * {@inheritdoc}
@@ -24,42 +24,12 @@ class HttpRss extends Http
             && strpos(
                 file_get_contents($params['url']),
                 'http://www.w3.org/2005/Atom'
-            ) === false
+            ) !== false
         ) {
-            return true;
+                return true;
         }
 
         return false;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function downloadFiles($files = null)
-    {
-        if (empty($files)) {
-            $files = $this->remoteFiles;
-        }
-
-        if (!is_writable($this->params['path'])) {
-            throw new \Exception(
-                sprintf(
-                    _('Directory %s is not writable.'),
-                    $this->params['path']
-                )
-            );
-        }
-
-        foreach ($files as $file) {
-            $localFile = $this->params['path'] . DS . $file['filename'];
-
-            if (!file_exists($localFile)) {
-                $this->buildContentAndSave($localFile, $file['content']);
-
-                $this->localFiles[] = $localFile;
-                $this->downloaded++;
-            }
-        }
     }
 
     /**
@@ -84,13 +54,15 @@ class HttpRss extends Http
         }
 
         $xml   = simplexml_load_string($content);
-        $files = $xml->xpath('//channel/item');
+
+        $xml->registerXPathNamespace('f', 'http://www.w3.org/2005/Atom');
+        $files = $xml->xpath('/f:feed/f:entry');
 
         foreach ($files as $value) {
             $this->remoteFiles[] = [
                 'content'  => $value,
-                'filename' => md5(urlencode((string) $value->link[0])) . '.xml',
-                'url'      => (string) $value->link[0]
+                'filename' => md5(urlencode((string) $value->link['href'])) . '.xml',
+                'url'      => (string) $value->link['href']
             ];
         }
 
@@ -107,15 +79,12 @@ class HttpRss extends Http
     {
         $article = new \Article();
 
-        $fullBody = (string) htmlentities($content->description) . '<br>'
-            . htmlentities($content->children('content', true));
-
-        $article->id               = md5($path);
-        $article->title            = (string) $content->title;
-        $article->body             = $fullBody;
-        $article->created_datetime = new \DateTime($content->pubDate);
-        $article->updated_datetime = new \DateTime($content->pubDate);
-        $article->category_name    = (string) $content->category;
+        $article->id    = md5($path);
+        $article->title = (string) $content->title;
+        $article->body = (string) $content->content;
+        $article->summary = (string) $content->summary;
+        $article->created_datetime = new \DateTime($content->published);
+        $article->updated_datetime = new \DateTime($content->updated);
 
         $newsMLString = $this->tpl->fetch(
             'news_agency/newsml_templates/base.tpl',
