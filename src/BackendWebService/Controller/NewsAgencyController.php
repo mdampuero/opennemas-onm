cbad07959a469c4ce4b66bbf860e3249ea1d93b7
Alex Rodrigues <alex@openhost.es>
2016-10-21 12:24:09 +0200

    ONM-1886: Use timezone on NewsAgency listing
    Signed-off-by: Alex rico <alex@openhost.es>

--- src/BackendWebService/Controller/NewsAgencyController.php
+++ src/BackendWebService/Controller/NewsAgencyController.php
@@ -187,8 +187,14 @@ class NewsAgencyController extends Controller
             }
         }
 
+        $timezone  = $this->container->get('core.locale')->getTimeZone();
+
         $extra = array_merge(
-            [ 'imported' => $imported, 'related' => $related ],
+            [
+                'imported' => $imported,
+                'related' => $related,
+                'timezone' => $timezone
+            ],
             $this->getTemplateParams()
         );
 
