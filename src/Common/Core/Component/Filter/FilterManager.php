cdf279c4a85149e1446445d77e6d66c9473ac513
Diego Blanco <diego@openhost.es>
2016-11-04 02:02:25 +0100

    Add a filter manager

---
+++ src/Common/Core/Component/Filter/FilterManager.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\Core\Component\Filter;
+
+use Common\Core\Component\Exception\Filter\InvalidFilterException;
+
+/**
+ * The FilterManager applies filters to values.
+ */
+class FilterManager
+{
+    /**
+     * Applies a filter to a value.
+     *
+     * @param string $name  The filter name.
+     * @param mixed  $value The value to filter.
+     * @param mixed  $args  The arguments for filter.
+     *
+     * @return mixed The filtered value.
+     */
+    public function filter($name, $value, $args)
+    {
+        $class = __NAMESPACE__ . '\\' . \classify($name)
+            . 'Filter';
+
+        if (class_exists($class)) {
+            $filter = new $class();
+
+            return $filter->filter($value, $args);
+        }
+
+        throw new InvalidFilterException($name);
+    }
+}
