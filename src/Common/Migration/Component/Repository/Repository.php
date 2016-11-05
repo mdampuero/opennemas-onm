ac33eabe540d56f005e57da34965fb80f776bbb7
Diego Blanco <diego@openhost.es>
2016-11-05 03:26:33 +0100

    Add exception for invalid repository and define repository interface

---
+++ src/Common/Migration/Component/Repository/Repository.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\Migration\Component\Repository;
+
+/**
+ * The Repository interface defines methods that all migration repositories
+ * have to implement.
+ */
+interface Repository
+{
+    /**
+     * Returns the total number of items to migrate.
+     *
+     * @return integer The total number of items to migrate.
+     */
+    public function count();
+
+    /**
+     * Returns the next item to migrate.
+     *
+     * @return array The next item to migrate.
+     */
+    public function next();
+}
