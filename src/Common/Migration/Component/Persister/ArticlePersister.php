fdd20faa19021354629ec4b9fb4d1e4c4cd10fbd
Diego Blanco <diego@openhost.es>
2016-11-08 17:51:02 +0100

    Create a migration persister for articles

---
+++ src/Common/Migration/Component/Persister/ArticlePersister.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\Migration\Component\Persister;
+
+/**
+ * The ArticlePersister class provides methods to save a Article from an
+ * extenal data source.
+ */
+class ArticlePersister extends Persister
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function persist($data)
+    {
+        unset($data['pk_article']);
+
+        try {
+            $article = $this->find($data);
+        } catch (\Exception $e) {
+            $article = new \Article();
+            $article->create($data);
+        }
+
+        return $article->pk_article;
+    }
+
+    /**
+     * Search for articles in target data source with the same information ready
+     * to import.
+     *
+     * @param array $data The article information.
+     *
+     * @return Article The article in target data source.
+     */
+    protected function find($data)
+    {
+        $oql = sprintf(
+            '(description = "%s" or title = "%s") and content_type_name = "%s"',
+            $data['title'],
+            $data['description'],
+            'article'
+        );
+
+        return $this->em->getRepository('Content')->findOneBy($oql);
+    }
+}
