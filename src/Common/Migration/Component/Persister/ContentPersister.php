56d4e84e8122b5da2b1655bb99c95419a2fa1c57
Diego Blanco <diego@openhost.es>
2016-11-14 11:36:48 +0100

    Search for existing contents in ContentPersister to prevent duplicates

--- src/Common/Migration/Component/Persister/ContentPersister.php
+++ src/Common/Migration/Component/Persister/ContentPersister.php
@@ -23,10 +23,36 @@ class ContentPersister extends Persister
     public function persist($data)
     {
         $converter = $this->em->getConverter('Content');
-        $content   = new Content($converter->objectify($data));
+        $data      = $converter->objectify($data);
 
-        $this->em->persist($content);
+        unset($data['pk_content']);
+
+        try {
+            $content = $this->find($data);
+        } catch (\Exception $e) {
+            $content = new Content($data);
+            $this->em->persist($content);
+        }
 
         return $content->pk_content;
+    }
+
+    /**
+     * Search for contents in target data source with the same information ready
+     * to import.
+     *
+     * @param array $data The content information.
+     *
+     * @return Article The content in target data source.
+     */
+    protected function find($data)
+    {
+        $oql = sprintf(
+            'title = "%s" and content_type_name = "%s"',
+            $data['title'],
+            'static_page'
+        );
+
+        return $this->em->getRepository('Content')->findOneBy($oql);
     }
 }
