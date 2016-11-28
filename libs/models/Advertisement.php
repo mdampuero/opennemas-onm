a38cb54ff68294cc666cc0dd7dabd775523c34f9
Alex Rodrigues <alex@openhost.es>
2016-11-28 16:41:40 +0100

    ONM-1989: Improve inline interstitial mobile appearance

--- libs/models/Advertisement.php
+++ libs/models/Advertisement.php
@@ -699,12 +699,12 @@ class Advertisement extends Content
                 }
 
                 $content = '<iframe src="'.$url.'" scrolling="no" style="width:'.$width.'px; '
-                            .'height:'.$height.'px; overflow: hidden;border:none"></iframe>';
+                            .'height:'.$height.'px; max-width:100%; overflow: hidden;border:none"></iframe>';
             }
         } elseif ($this->with_script == 2) {
             if (in_array($this->type_advertisement, array(50,150,250,350,450,550))) {
                 $url = url('frontend_ad_get', array('id' => $this->pk_content));
-                $content = '<iframe src="'.$url.'" style="width:800px; height:600px; overflow: hidden;border:none" '.
+                $content = '<iframe src="'.$url.'" style="width:800px; max-width:100%; height:600px; overflow: hidden;border:none" '.
                 'scrolling="no" ></iframe>';
             } else {
                 $content = "<script type='text/javascript' data-id='{$this->id}'><!--// <![CDATA[
