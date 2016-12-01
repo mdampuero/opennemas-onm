b8be071a1b0e90d24b4e9a4a37358c576f30ca23
Diego Blanco <diego@openhost.es>
2016-12-01 13:28:46 +0100

    Update items in list after import

--- public/themes/admin/js/controllers/NewsAgencyListCtrl.js
+++ public/themes/admin/js/controllers/NewsAgencyListCtrl.js
@@ -64,7 +64,7 @@
           });
 
           modal.result.then(function(response) {
-            if (response) {
+            if (response.sucess) {
               if (response.messages) {
                 messenger.post(response.messages);
               }
