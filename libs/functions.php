bcfe19dc23f7f74c7a85be83f64a23a8b6ea04a6
Alex Rodrigues <alex@openhost.es>
2016-12-12 14:53:02 +0100

    ONM-2031: Use new analytics.js only for FIA

--- libs/functions.php
+++ libs/functions.php
@@ -428,7 +428,7 @@ function getGoogleAnalyticsCode($useImage = false)
     return $code;
 }
 
-function generateGAScriptCode($config, $source = null)
+function generateGAScriptCode($config)
 {
     $code = "\n<script type=\"text/javascript\">\nvar _gaq = _gaq || [];\n";
     foreach ($config as $key => $account) {
@@ -448,10 +448,6 @@ function generateGAScriptCode($config, $source = null)
                 ) {
                     $code .= base64_decode(trim($account['custom_var'])) . "\n";
                 }
-                if ($source == 'fia') {
-                    $code .= "_gaq.push(['_setCampSourceKey', 'Facebook']);\n";
-                    $code .= "_gaq.push(['_setCampMediumKey', 'Instant Articles']);\n";
-                }
                 $code .= "_gaq.push(['_trackPageview']);\n";
             } else {
                 $code .= "_gaq.push(['account{$key}._setAccount', '" . trim($account['api_key']) . "']);\n";
@@ -465,10 +461,6 @@ function generateGAScriptCode($config, $source = null)
                 ) {
                     $code .= base64_decode(trim($account['custom_var'])) . "\n";
                 }
-                if ($source == 'fia') {
-                    $code .= "_gaq.push(['account{$key}._setCampSourceKey', 'Facebook']);\n";
-                    $code .= "_gaq.push(['account{$key}._setCampMediumKey', 'Instant Articles']);\n";
-                }
                 $code .= "_gaq.push(['account{$key}._trackPageview']);\n";
             }
         }
@@ -476,10 +468,6 @@ function generateGAScriptCode($config, $source = null)
 
     // Add opennemas Account
     $code .= "_gaq.push(['onm._setAccount', 'UA-40838799-5']);\n";
-    if ($source == 'fia') {
-        $code .= "_gaq.push(['onm._setCampSourceKey', 'Facebook']);\n";
-        $code .= "_gaq.push(['onm._setCampMediumKey', 'Instant Articles']);\n";
-    }
     $code .= "_gaq.push(['onm._trackPageview']);\n";
 
     // Load ga.js script
