ee3d1738b0bfb37a7a8f97745c546d84f8dc23fb
Diego Blanco <diego@openhost.es>
2016-12-21 12:43:00 +0100

    ONM-1892: Empty custom exceptions for migration components

--- src/Common/Migration/Component/Exception/InvalidPersisterException.php
+++ src/Common/Migration/Component/Exception/InvalidPersisterException.php
@@ -14,16 +14,4 @@ namespace Common\Migration\Component\Exception;
  */
 class InvalidPersisterException extends \Exception
 {
-    /**
-     * Initializes the exception.
-     *
-     * @param string $entity    The entity name.
-     */
-    public function __construct($entity)
-    {
-        $message = _('No persisters found for "%s"');
-        $message = sprintf($message, $entity);
-
-        parent::__construct($message);
-    }
 }
