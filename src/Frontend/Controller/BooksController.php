d315d90ae71743bf39d3e906591281c09419255c
Fran Dieguez <fran@openhost.es>
2017-01-31 17:06:56 +0100

    ONM-2143: Use null as default parameter in Frontend Book show action

--- src/Frontend/Controller/BooksController.php
+++ src/Frontend/Controller/BooksController.php
@@ -94,9 +94,9 @@ class BooksController extends Controller
      **/
     public function showAction(Request $request)
     {
-        $categoryName = $this->request->query->filter('category_name', 'all', FILTER_SANITIZE_STRING);
+        $categoryName = $this->request->query->filter('category_name', null, FILTER_SANITIZE_STRING);
         $dirtyID      = $request->query->filter('id', null, FILTER_SANITIZE_STRING);
-        $urlSlug      = $request->query->filter('slug', '', FILTER_SANITIZE_STRING);
+        $urlSlug      = $request->query->filter('slug', null, FILTER_SANITIZE_STRING);
 
         $book = $this->get('content_url_matcher')
             ->matchContentUrl('book', $dirtyID, $urlSlug, $categoryName);
