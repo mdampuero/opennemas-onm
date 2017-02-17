b3d5888bd7c45b83f8271f7351a4868236339cc7
Diego Blanco <diego@openhost.es>
2017-02-17 16:03:58 +0100

    ONM-1872: Remove frontpage category when advertisement has no category selected

--- public/themes/admin/js/controllers/AdvertisementCtrl.js
+++ public/themes/admin/js/controllers/AdvertisementCtrl.js
@@ -190,6 +190,11 @@
         }
 
         if (categories && angular.isArray(categories)) {
+          // Remove frontpage category
+          categories = categories.filter(function(e) {
+            return parseInt(e) !== 0;
+          });
+
           $scope.ui.categories = categories.map(function (e) {
             return parseInt(e);
           });
