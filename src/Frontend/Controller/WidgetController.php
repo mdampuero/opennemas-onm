add4c6976bc6da4ab3cd42e1c269f3ed4c6cafc1
Diego Blanco <diego@openhost.es>
2017-03-23 10:58:21 +0100

    ONM-2232: Improve routing and clean controller

--- src/Frontend/Controller/WidgetController.php
+++ src/Frontend/Controller/WidgetController.php
@@ -1,70 +1,49 @@
 <?php
 /**
- * Handles the actions for a widget
- *
- * @package Frontend_Controllers
- **/
-/**
  * This file is part of the Onm package.
  *
- * (c)  OpenHost S.L. <developers@openhost.es>
+ * (c) Openhost, S.L. <developers@opennemas.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- **/
+ */
 namespace Frontend\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Common\Core\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Handles the actions to execute for a widget
- *
- * @package Frontend_Controllers
- **/
+ * Checks and executes widget actions.
+ */
 class WidgetController extends Controller
 {
     /**
-     * Get a widget and execute an action
+     * Get a widget and execute an action.
      *
      * @param Request $request The request object.
      *
      * @return Response The response object.
      */
-    public function executeAction(Request $request)
+    public function executeAction($id, $action)
     {
-        // Get url parameters
-        $action = $request->query->filter('action', '', FILTER_SANITIZE_STRING);
-        $id = $request->query->getDigits('id', 0);
-
         $response = new Response(
             _('Sorry, we were unable to complete your request'),
             400
         );
 
-        // Verify parameters
-        if (empty($id) || empty($action)) {
-            return $response;
-        }
-
-        // Get widget object
         $widget = $this->get('entity_repository')->find('Widget', $id);
 
-        // Check if widget exists
         if (is_null($widget)) {
             return $response;
         }
 
-        // Get widget instance
         $widget = $widget->factoryWidget();
 
-        // Validate widget action
         if (!method_exists($widget, $action)) {
             return $response;
         }
 
-        // Execute widget action with data from the request
         return $widget->{$action}($request);
     }
 }
