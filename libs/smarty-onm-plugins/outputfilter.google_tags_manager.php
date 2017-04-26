74541ddc2dca8e23c8b94889bc0200c5a88d4a66
Alex Rodrigues <alex@openhost.es>
2017-04-26 17:57:07 +0200

    ONM-2299: Add service for GoogleTagsManager and testing

--- libs/smarty-onm-plugins/outputfilter.google_tags_manager.php
+++ libs/smarty-onm-plugins/outputfilter.google_tags_manager.php
@@ -5,7 +5,7 @@
  * File:     outputfilter.google_tags_manager.php
  * Type:     outputfilter
  * Name:     google_tags_manager
- * Purpose:  Prints Google Tags Manager code
+ * Purpose:  Prints Google Analytics code
  * -------------------------------------------------------------
  */
 function smarty_outputfilter_google_tags_manager($output, $smarty)
@@ -24,42 +24,20 @@ function smarty_outputfilter_google_tags_manager($output, $smarty)
         && !preg_match('/\/fb\/instant-articles/', $uri)
         && !preg_match('@\.amp\.html$@', $uri)
     ) {
-        $containerId = getService('setting_repository')->get('google_tags_id');
+        $containerId = trim(getService('setting_repository')->get('google_tags_id'));
 
-        if (empty(trim($containerId))) {
+        if (empty($containerId)) {
             return $output;
         }
 
-        $headCode = getHeadGoogleTagsManagerCode(trim($containerId));
-        $bodyCode = getBodyGoogleTagsManagerCode(trim($containerId));
+        $gtm = new \Common\Core\Component\GoogleTagsManager\GoogleTagsManager();
+
+        $headCode = $gtm->getGoogleTagsManagerHeadCode($containerId);
+        $bodyCode = $gtm->getGoogleTagsManagerBodyCode($containerId);
 
         $output = preg_replace('@(</head>)@', $headCode.'${1}', $output);
         $output = preg_replace('@(<body.*>)@', '${1}'."\n".$bodyCode, $output);
     }
 
     return $output;
-}
-
-
-function getHeadGoogleTagsManagerCode($containerId)
-{
-    $code = "<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','" . $containerId . "');</script>
-<!-- End Google Tag Manager -->";
-
-    return $code;
-}
-
-function getBodyGoogleTagsManagerCode($containerId)
-{
-    $code = '<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $containerId . '"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->';
-
-    return $code;
 }
