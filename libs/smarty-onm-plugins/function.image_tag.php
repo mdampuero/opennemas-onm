4bda9ea8cb5a576d4b8ecdf36580ae85d743c94d
Alex Rodrigues <alex@openhost.es>
2017-05-02 13:54:25 +0200

    ONM-2306: Improve image_tag plugin to work with images id

--- libs/smarty-onm-plugins/function.image_tag.php
+++ libs/smarty-onm-plugins/function.image_tag.php
@@ -8,6 +8,11 @@ function smarty_function_image_tag($params, &$smarty)
 {
     $output = "";
 
+    if (!empty($params['id'])) {
+        $photo = getService('entity_repository')->find('Photo', $params['id']);
+        $params['src'] = $photo->path_img;
+    }
+
     if (empty($params['src'])) {
         return;
     }
