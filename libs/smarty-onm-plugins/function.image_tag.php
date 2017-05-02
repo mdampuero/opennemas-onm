786f0fdab9d5d07de5fd25ec1777fe4716dbd2c7
Alex Rodrigues <alex@openhost.es>
2017-05-02 15:37:21 +0200

    ONM-2306: Add array key check for id parameter

--- libs/smarty-onm-plugins/function.image_tag.php
+++ libs/smarty-onm-plugins/function.image_tag.php
@@ -8,7 +8,7 @@ function smarty_function_image_tag($params, &$smarty)
 {
     $output = "";
 
-    if (!empty($params['id'])) {
+    if (array_key_exists('id', $params) && !empty($params['id'])) {
         $photo = getService('entity_repository')->find('Photo', $params['id']);
         $params['src'] = $photo->path_img;
     }
