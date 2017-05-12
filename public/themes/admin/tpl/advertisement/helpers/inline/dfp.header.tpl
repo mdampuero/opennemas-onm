2505fd02fc279fdf7df4fe6758884007504eb0b6
Diego Blanco <diego@openhost.es>
2017-05-12 15:41:03 +0200

    ONM-2329: Move HTML to templates and clean render functions

---
+++ public/themes/admin/tpl/advertisement/helpers/inline/dfp.header.tpl
@@ -0,0 +1,21 @@
+<script async="async" src="https://www.googletagservices.com/tag/js/gpt.js"></script>
+<script>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+  googletag.cmd.push(function() {
+
+    {foreach $zones as $zone}
+      googletag.defineSlot('{$zone['dfpId']}', {$zone['sizes']}, 'zone_{$zone['id']}').addService(googletag.pubads());
+    {/foreach}
+
+    {if !empty($options) && array_key_exists('target') && !empty($options['target'])}
+      googletag.pubads().setTargeting('{$options['target']}', [ '{$category}' ]);
+      googletag.pubads().setTargeting('{$options['module']}', [ '{$extension}' ]);
+    {/if}
+
+    {$customCode}
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.enableServices();
+  });
+</script>
--- src/Common/Core/Component/Renderer/AdvertisementRenderer.php
+++ src/Common/Core/Component/Renderer/AdvertisementRenderer.php
@@ -1,6 +1,4 @@
 <?php
-namespace Common\Core\Component\Renderer;
-
 /**
  * This file is part of the Onm package.
  *
@@ -9,231 +7,204 @@ namespace Common\Core\Component\Renderer;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace Common\Core\Component\Renderer;
+
 class AdvertisementRenderer
 {
     /**
-     * Initializes the renderer
-     * @param  Templating $template the tempalte service object
-     **/
-    public function __construct($template)
+     * Initializes the AdvertisementRenderer
+     *
+     * @param ServiceContainer $container The service container.
+     */
+    public function __construct($container)
     {
-        $this->template = $template;
+        $this->container = $container;
+
+        $this->router = $this->container->get('router');
+        $this->sm     = $this->container->get('setting_repository');
+        $this->tpl    = $this->container->get('core.template.admin');
     }
+
     /**
-     * Renders an advertisement given and $ad
+     * Renders an advertisement.
      *
-     * @param  Advertisement $ad the ad to render
+     * @param Advertisement $ad The advertisement to render.
      *
-     * @return string the HTML for the slot
-     **/
+     * @return string The HTML for the slot.
+     */
     public function render(\Advertisement $ad)
     {
-        $content = '';
         if ($ad->with_script == 1) {
-            $content = $ad->script;
+            return $ad->script;
         } elseif ($ad->with_script == 2) {
-            $content = $this->renderReviveSlot($ad);
+            return $this->renderReviveSlot($ad);
         } elseif ($ad->with_script == 3) {
-            $content = $this->renderDFPSlot($ad);
-        } else {
-            $content = $this->renderImage($ad);
+            return  $this->renderDFPSlot($ad);
         }
 
-        return $content;
+        return $this->renderImage($ad);
     }
 
     /**
-     * Renders a DFP slot given a Advertisement
+     * Renders a DFP advertisement slot.
      *
-     * @param  Advertisement $ad the ad to render
+     * @param Advertisement $ad The advertisement to render.
      *
-     * @return string the HTML content for the DFP slot
-     **/
+     * @return string The HTML content for the DFP advertisement slot.
+     */
     public function renderDFPSlot($ad)
     {
-        return "<div id='zone_{$ad->pk_advertisement}'>"
-           ."<script type='text/javascript'>"
-           ."googletag.cmd.push(function() { googletag.display('zone_{$ad->pk_advertisement}'); });"
-           ."</script></div>";
+        return $this->tpl->fetch('advertisement/helpers/inline/dfp_slot.tpl', [
+            'id' => $ad->pk_advertisement
+        ]);
     }
 
     /**
-     * Generates the HTML header section for the DFP ads
+     * Generates the HTML header section for the DFP ads.
      *
-     * @param  arrray  the list of advertisements to generate the header from
+     * @param array $ads    The list of advertisements to generate the header
+     *                      from.
+     * @param array $params The list of parameters.
      *
-     * @return string the HTML content for the DFP slot
-     **/
+     * @return string the HTML content for the DFP slot.
+     */
     public function renderDFPHeader($ads, $params)
     {
-        $headerContents = '';
-        $dfpZonesInformation = [];
+        $ads = array_filter($ads, function ($a) {
+            return $a->with_script == 3
+                && array_key_exists('googledfp_unit_id', $a->params)
+                && !empty($a->params['googledfp_unit_id']);
+        });
+
+        if (empty($ads)) {
+            return '';
+        }
+
+        $zones = [];
         foreach ($ads as $advertisement) {
-            if ($advertisement->with_script == 3
-                && array_key_exists('googledfp_unit_id', $advertisement->params)
-                && !empty($advertisement->params['googledfp_unit_id'])
-            ) {
-                // TODO: Check Api/AdvertisementController::getSizes.
-                $sizes = array_map(function ($a) {
-                    return "[ {$a['width']}, {$a['height']} ]";
-                }, $advertisement->params['sizes']);
+            // TODO: Check Api/AdvertisementController::getSizes.
+            $sizes = array_map(function ($a) {
+                return "[ {$a['width']}, {$a['height']} ]";
+            }, $advertisement->params['sizes']);
 
-                $sizes     = "[".implode(', ', $sizes)."]";
-                $dfpUnitID = $advertisement->params['googledfp_unit_id'];
-                $adId      = $advertisement->id;
+            $sizes = '[ ' . implode(', ', $sizes) . ' ]';
 
-                $dfpZonesInformation []= "  googletag.defineSlot('{$dfpUnitID}', {$sizes}, 'zone_{$adId}').addService(googletag.pubads());";
-            }
+            $zones[] = [
+                'id'    => $advertisement->id,
+                'dfpId' => $advertisement->params['googledfp_unit_id'],
+                'sizes' => $sizes
+            ];
         }
 
-        if (count($dfpZonesInformation) > 0) {
-            // Check if targeting is set
-            $dfpOptions = getService('setting_repository')->get('dfp_options');
-            $targetingCode = '';
-            if (is_array($dfpOptions) &&
-                array_key_exists('target', $dfpOptions) &&
-                !empty($dfpOptions['target'])
-            ) {
-                $targetingCode = "googletag.pubads().setTargeting('".$dfpOptions['target']."', ['".$params['category']."']);";
-            }
-            if (is_array($dfpOptions) &&
-                array_key_exists('module', $dfpOptions) &&
-                !empty($dfpOptions['module'])
-            ) {
-                $module = '';
-                $content = $params['content'];
-                if (!is_null($content)) {
-                    $module = $content->content_type_name;
-                } elseif ($params['x-tags']->value) {
-                    $xTags = $params['x-tags']->value;
-                    $module = ($xTags == 'frontpage-page,home') ? 'home' : strtok($xTags, ',');
-                } elseif (!empty($params['polls']->value)) {
-                    $module = 'poll-frontpage';
-                }
-                $targetingCode .= "\ngoogletag.pubads().setTargeting('".$dfpOptions['module']."', ['".$module."']);\n";
-            }
+        $options    = $this->sm->get('dfp_options');
+        $customCode = $this->sm->get('dfp_custom_code');
 
-            // Check for custom code
-            $dfpCustomCode = getService('setting_repository')->get('dfp_custom_code');
-            $customCode = '';
-            if (!empty($dfpCustomCode)
-            ) {
-                $customCode = "\n".base64_decode($dfpCustomCode)."\n";
-            }
-
-            $headerContents = "<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>\n"
-                ."<script>\n"
-                ."var googletag = googletag || {};\n"
-                ."googletag.cmd = googletag.cmd || [];\n"
-                ."googletag.cmd.push(function() {\n"
-                .implode("\n", $dfpZonesInformation)
-                .$targetingCode
-                .$customCode
-                ."\n  googletag.pubads().enableSingleRequest();\n"
-                ."  googletag.pubads().collapseEmptyDivs();\n"
-                ."  googletag.enableServices();\n"
-                ."});\n</script>".PHP_EOL;
+        if (!empty($customCode)) {
+            $customCode = base64_decode($dfpCustomCode);
         }
 
-        return $headerContents;
+        return $this->tpl->fetch('advertisement/helpers/inline/dfp_header.tpl', [
+            'category'      => $category,
+            'extension'     => $extension,
+            'customCode'    => $customCode,
+            'options'       => $options,
+            'targetingCode' => $targetingCode,
+            'zones'         => $zones
+        ]);
     }
 
     /**
-     * Renders a Revive slot given a Advertisement
+     * Renders a Revive advertisement.
      *
-     * @param  Advertisement $ad the ad to render
+     * @param Advertisement $ad the ad to render.
      *
-     * @return string the HTML content for the DFP slot
-     **/
+     * @return string the HTML content for the DFP slot.
+     */
     public function renderReviveSlot($ad)
     {
-        if (in_array($ad->type_advertisement, array(50,150,250,350,450,550))) {
-            $url = url('frontend_ad_get', array('id' => $ad->pk_content));
-            $content = '<iframe src="'.$url.'" style="width:800px; max-width:100%; height:600px; overflow: hidden;border:none" '.
-            'scrolling="no" ></iframe>';
-        } else {
-            $content = "<script type='text/javascript' data-id='{$ad->id}'><!--// <![CDATA[
-            OA_show('zone_{$ad->id}');
-            // ]]> --></script>";
-        }
+        $iframe = in_array($ad->type_advertisement, [ 50, 150, 250, 350, 450, 550 ]);
+        $url    = $this->router->generate('frontend_ad_get', [
+            'id' => $ad->pk_content
+        ]);
 
-        return $ad;
+        return $this->tpl
+            ->fetch('advertisement/helpers/inline/revive.slot.tpl', [
+                'id'     => $ad->id,
+                'iframe' => $iframe,
+                'url'    => $url,
+            ]);
     }
 
     /**
-     * Generates the HTML header section for the Revive ads
+     * Generates the HTML code to include in header for Revive advertisements.
      *
-     * @param  arrray  the list of advertisements to generate the header from
+     * @param array The list of advertisements.
      *
-     * @return string the HTML content for the Revive slot
-     **/
-    public function renderReviveHeader($ads, $params)
+     * @return string The HTML code to include in header.
+     */
+    public function renderReviveHeader($ads)
     {
-        $reviveZonesInformation = [];
+        $ads = array_filter($ads, function ($a) {
+            return $a->with_script == 2
+                && array_key_exists('openx_zone_id', $a->params)
+                && !empty($a->params['openx_zone_id']);
+        });
 
-        foreach ($ads as $advertisement) {
-            if ($advertisement->with_script == 2
-                && array_key_exists('openx_zone_id', $advertisement->params)
-                && !empty($advertisement->params['openx_zone_id'])
-            ) {
-                $reviveZonesInformation []= " 'zone_{$advertisement->id}' : ".(int) $advertisement->params['openx_zone_id'];
-            }
+        if (empty($ads)) {
+            return '';
         }
 
-        // Generate revive ads positions
-        if (count($reviveZonesInformation) > 0 && count($adsReviveConfs) > 0) {
-            $adsReviveConfs = getService('setting_repository')->get('revive_ad_server');
+        $config = $this->sm->get('revive_ad_server');
+        $zones  = [];
 
-            $reviveAdsPositions = "\n<script type='text/javascript'><!--// <![CDATA["
-                ."var OA_zones = { \n".implode(",\n", $reviveZonesInformation)."\n}"
-                ."// ]]> --></script>"
-                ."<script type='text/javascript' src='{$adsReviveConfs['url']}/www/delivery/spcjs.php?cat_name={$actual_category}'></script>";
-
-            $output = str_replace('</head>', $reviveAdsPositions.'</head>', $output);
+        foreach ($ads as $ad) {
+            $zones =  [
+                'id'      => $ad->id,
+                'openXId' => (int) $ad->params['openx_zone_id']
+            ];
         }
+
+        return $this->tpl
+            ->fetch('advertisement/helpers/inline/revive.header.tpl', [
+                'config' => $config,
+                'zones'  => $zones
+            ]);
     }
 
     /**
-     * Renders an image/swf slot
+     * Renders an image/swf based advertisement.
      *
-     * @param string $ad the ad object to render from
+     * @param string $ad The advertisement to render.
      *
-     * @return string the HTML code for the ad image
-     **/
+     * @return string The HTML code for the advertisement.
+     */
     public function renderImage($ad)
     {
-        return '<div>image ad not implemented yet</div>';
-        // TODO: Load photo
-        $photo = null;
+        try {
+            $img = $this->container->get('entity_repository')
+                ->find('Photo', $ad->img);
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
 
-        // If the Ad is Flash/Image based try to get the width and height fixed
-        if (isset($photo)) {
-            if (($photo->width <= $width)
-                && ($photo->height <= $height)
-            ) {
-                $width  = $photo->width;
-                $height = $photo->height;
-            }
+            return '';
         }
 
-        // TODO: controlar los banners swf especiales con div por encima
-        if (strtolower($photo->type_img) == 'swf') {
-            // Generate flash object with wmode transparent
-            $flashObject =
-                '<object width="'.$width.'" height="'.$height.'" >
-                    <param name="wmode" value="transparent" />
-                    <param name="movie" value="'.$mediaUrl. '" />
-                    <param name="width" value="'.$width.'" />
-                    <param name="height" value="'.$height.'" />
-                    <embed src="'. $mediaUrl. '" width="'.$width.'" height="'.$height.'" '
-                        .'SCALE="exactfit" wmode="transparent"></embed>
-                </object>';
-        } else {
-            // Image
-            $imageObject = '<img alt="'.$photo->category_name.'" src="'. $mediaUrl.'" '
-                            .'width="'.$width.'" height="'.$height.'" />';
+        $template = 'advertisement/helper/inline/image.tpl';
 
-            $content = '<a target="_blank" href="'.$url.'" rel="nofollow">'.$imageObject.'</a>';
+        if ($img->type_img === 'swf') {
+            $template = 'advertisement/helper/inline/flash.tpl';
         }
+
+        return $this->tpl->fetch($template, [
+            'height'   => $img->height,
+            'mediaUrl' => $img->path_img . $img->path_file,
+            'src'      => SITE_URL . 'media/' . INSTANCE_UNIQUE_NAME
+                . '/images' . $img->path_file . $img->name,
+            'url'      => $this->get('router')
+                ->generate('frontend_ad_redirect', [
+                    'id' => $publicId
+                ]),
+            'width'    => $img->width
+        ]);
     }
 }
