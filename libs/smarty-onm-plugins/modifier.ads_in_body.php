a98612352e1dfa9186d039db12ef87b37fbc1438
Fran Dieguez <fran@openhost.es>
2017-05-22 11:43:16 +0200

    ONM-2329: Implement safeFrame/inline option for ads_in_body

--- libs/smarty-onm-plugins/modifier.ads_in_body.php
+++ libs/smarty-onm-plugins/modifier.ads_in_body.php
@@ -30,15 +30,51 @@ function smarty_modifier_ads_in_body($body, $contentType = 'article')
         $id = 3200;
     }
 
-    $html        = '<div class="ad-slot oat" data-type="%s"></div>';
     $bodyWithAds = [];
 
-    foreach ($paragraphs as $key => $paragraph) {
-        $slot   = '';
-        $slotId = $id + 1 + $key;
+    $safeFrame = getService('setting_repository')->get('ads_settings')['safe_frame'];
+    if (!$safeFrame) {
+        $adsRepository = getService('advertisement_repository');
+        switch ($contentType) {
+            case 'article':
+                $ads = $adsRepository->findByPositionsAndCategory(
+                    [ 2201,2202,2203,2204,2205,2206,2207,2208,2209,2210,2211 ],
+                    $category
+                );
+                break;
+            case 'opinion':
+                $ads = $adsRepository->findByPositionsAndCategory(
+                    [ 3201,3202,3203,3204,3205,3206,3207,3208,3209,3210,3211 ],
+                    4
+                );
+                break;
+        }
 
-        $bodyWithAds[] = $paragraph;
-        $bodyWithAds[] = sprintf($html, $slotId);
+        $adsRenderer    = getService('core.renderer.advertisement');
+        foreach ($paragraphs as $key => $paragraph) {
+            $positionID = $id + 1 + $key;
+
+            $bodyWithAds[] = $paragraph;
+            $adsForPosition = array_filter($ads, function($el) use ($positionID) {
+                return (int) $el->type_advertisement == $positionID;
+            });
+            if (count($adsForPosition) < 1) {
+                continue;
+            }
+            $ad = $adsForPosition[array_rand($adsForPosition)];
+
+            $bodyWithAds[] = $ad->render([]);
+        }
+    } else {
+        $html = '<div class="ad-slot oat" data-type="%s"></div>';
+
+        foreach ($paragraphs as $key => $paragraph) {
+            $slot   = '';
+            $slotId = $id + 1 + $key;
+
+            $bodyWithAds[] = $paragraph;
+            $bodyWithAds[] = sprintf($html, $slotId);
+        }
     }
 
     return implode('', $bodyWithAds);
