9432db64cb40679821c706718d32683b995e9ab9
Sandra Pereira <sandra@openhost.es>
2017-07-20 18:31:08 +0200

    ONM-2633: Added image in twitter cards sharing video and album

--- libs/smarty-onm-plugins/function.meta_twitter_cards.php
+++ libs/smarty-onm-plugins/function.meta_twitter_cards.php
@@ -44,19 +44,46 @@ function smarty_function_meta_twitter_cards($params, &$smarty)
         $output []= '<meta name="twitter:domain"      content="'.$url.'">';
 
         if (array_key_exists('photoInt', $smarty->tpl_vars)) {
+            // Articles with inner photo
             $photoInt = $smarty->tpl_vars['photoInt']->value;
             $imageUrl = MEDIA_IMG_ABSOLUTE_URL.'/'.$photoInt->path_file.'/'.$photoInt->name;
-            $output []= '<meta name="twitter:image:src" content="'.$imageUrl.'">';
-        }
-
-        if ($content->content_type_name == 'opinion'
-            && isset($content->author)
-            && isset($content->author->photo)
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (array_key_exists('videoInt', $smarty->tpl_vars)) {
+            // Articles with inner video
+            $videoInt = $smarty->tpl_vars['videoInt']->value;
+            if (!empty($videoInt) && strpos($videoInt->thumb, 'http')  === false) {
+                $videoInt->thumb = SITE_URL.$videoInt->thumb;
+            }
+            $imageUrl = $videoInt->thumb;
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (isset($content->img1) && ($content->img1 > 0)) {
+            // Front image
+            $photoFront = getService('entity_repository')->find('Photo', $content->img1);
+            $imageUrl = MEDIA_IMG_ABSOLUTE_URL.$photoFront->path_file.$photoFront->name;
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (array_key_exists('photo', $smarty->tpl_vars)) {
+            // Opinions
+            $photo = $smarty->tpl_vars['photo']->value;
+            $imageUrl = MEDIA_IMG_ABSOLUTE_URL.$photo->path_file.$photo->name;
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (($content->content_type_name == 'opinion')            
             && isset($content->author->photo->path_img)
             && !empty($content->author->photo->path_img)
         ) {
+            //Photo author
             $imageUrl = MEDIA_IMG_ABSOLUTE_URL.'/'.$content->author->photo->path_img;
-            $output []= '<meta name="twitter:image:src" content="'.$imageUrl.'">';
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (isset($content->cover) && !empty($content->cover)) {
+            // Album
+            $imageUrl = MEDIA_IMG_ABSOLUTE_URL.'/'.$content->cover;
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
+        } elseif (isset($content->thumb) && !empty($content->thumb)) {
+            // Video
+            if (strpos($content->thumb, 'http')  === false) {
+                $content->thumb = SITE_URL.$content->thumb;
+            }
+            $imageUrl = $content->thumb;
+            $output []= '<meta name="twitter:image" content="'.$imageUrl.'">';
         }
     }
 
