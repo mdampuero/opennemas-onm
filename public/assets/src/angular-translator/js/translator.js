9fd730f17fdea52cc187351242a37e474eb83b37
Adrián Reimunde <adrianrep@openhost.es>
2017-09-19 17:41:37 +0200

    ONM-2862: Binding directive ngmodel with parent controller parameter

--- public/assets/src/angular-translator/js/translator.js
+++ public/assets/src/angular-translator/js/translator.js
@@ -14,10 +14,11 @@ angular.module('onm.translator', [])
   .directive('translator', ['$http', 'routing',
     function ($compile, $http, routing) {
       return {
-        controller: 'TranslatorCtrl',
-        restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        restrict:   'E', // E = Element, A = Attribute, C = Class, M = Comment
+        require:    'ngModel',
         scope: {
           'languageData': '=',
+          ngModel: '=',
           'link': '@',
         },
         template: function(elem, attrs) {
@@ -33,7 +34,7 @@ angular.module('onm.translator', [])
               '<span class=\"caret\"></span>' +
             '</button>' +
             '<ul class=\"dropdown-menu\" role=\"menu\">' +
-              '<li ng-repeat=\"language in languages\" ng-if=\"language.language != ngModel\">' +
+              '<li ng-repeat=\"language in languages\" ng-if=\"language.language != locale\">' +
                 '<a href=\"#\" data-ng-click=\"changeSelected(language.language)\">' +
                   '<span class=\"fa {{language.icon}}\" ></span>' +
                   '{{language.language}}' +
@@ -42,55 +43,31 @@ angular.module('onm.translator', [])
             '</ul>' +
           '</div>';
         },
-        link: function($scope, element, $attrs, $window) {
+        link: function($scope, element, $attrs, ctrl) {
           var data = $scope.languageData;
           var link = $scope.link || null;
-          $scope.ngModel = data.locale || data.default;
+          $scope.locale = data.locale || data.default;
           $scope.languages = {};
           if(data.all) {
             Object.keys(data.all).forEach(function(language) {
-              var icon = $scope.getImg(link, language, data);
+              var icon = (!link && language == data.default)?'fa-exchange':'';
               $scope.languages[language] = {'icon': icon, 'language': language};
               if(link) {
                 $scope.languages[language].link = $attrs.link + '?locale=' + language;
                 $scope.languages[language].btnClass = 'btn ';
               }
-              if(language == $scope.ngModel) {
+              if(language == $scope.locale) {
                 $scope.selected = $scope.languages[language];
               }
             });
           }
+          $scope.changeSelected = function (language) {
+            $scope.locale = language;
+            $scope.ngModel = language;
+            $scope.selected = $scope.languages[language];
+            ctrl.$setViewValue(language);
+          }
         },
       };
-    }
-  ])
-
-  /**
-   * @ngdoc controller
-   * @name  TranslatorCtrl
-   *
-   * @description
-   *   Controller to handle translator actions.
-   *
-   * @requires $http
-   * @requires $rootScope
-   * @requires $scope
-   * @requires routing
-   */
-  .controller('TranslatorCtrl', ['$http', '$rootScope', '$scope', '$window',
-    function($http, $rootScope, $scope, $window) {
-
-      if ($window.languageData) {
-        $scope.languageData = $window.languageData;
-      }
-
-      $scope.changeSelected = function (language) {
-        $scope.ngModel = language;
-        $scope.selected = $scope.languages[language];
-      };
-
-      $scope.getImg = function (link, language, data) {
-        return (!link && language == data.default)?'fa-exchange':'';
-      }
     }
   ]);
