f45d0d8b8e5cf0464100b33b15a79090374d00aa
Alex Rodrigues <alex@openhost.es>
2017-09-26 10:45:50 +0200

    ONM-2884: Merge RTB class on Advertisement and improve routing yml files

--- src/Frontend/Controller/AdvertisementController.php
+++ src/Frontend/Controller/AdvertisementController.php
@@ -33,7 +33,7 @@ class AdvertisementController extends Controller
      *
      * @return Response the response object
      */
-    public function getAction(Request $request)
+    public function showAction(Request $request)
     {
         $id = $request->query->filter('id', null, FILTER_SANITIZE_STRING);
         $ad = $this->get('entity_repository')->find('Advertisement', $id);
@@ -83,7 +83,7 @@ class AdvertisementController extends Controller
      *
      * @return Response The response object.
      */
-    public function adsTxtAction()
+    public function showAdsTxtAction()
     {
         // Check for the module existence and if it is enabled
         if (!$this->get('core.security')->hasExtension('es.openhost.module.advancedAdvertisement')) {
@@ -100,5 +100,90 @@ class AdvertisementController extends Controller
             'x-tags'       => 'instance-' . $instanceName . ',ads,txt',
             'x-instance'   => $instanceName,
         ]);
+    }
+
+    /**
+     * Returns the file contents
+     *
+     * @param Request $request the request object
+     *
+     * @return Response the requested file
+     */
+    public function showRTBFileAction(Request $request)
+    {
+        // Check for the module existence and if it is enabled
+        if (!$this->get('core.security')->hasExtension('es.openhost.module.advancedAdvertisement')) {
+            throw new ResourceNotFoundException();
+        }
+
+        // Search for the
+        $fileName = $request->query->get('filename');
+        $fileId   = $this->checkRTBFileInConfigSettings($fileName);
+        $filePath = $this->getFilePath($fileId);
+
+        if (!file_exists($filePath)) {
+            throw new ResourceNotFoundException();
+        }
+
+        $fileContents = file_get_contents($filePath);
+
+        // Return the resopnse object
+        return new Response($fileContents, 200, [
+            'x-instance'  => $this->get('core.instance')->internal_name,
+            'x-tags'      => 'rtb,',
+            'x-cache-for' => '+1 day',
+            'x-cacheable' => true,
+        ]);
+    }
+
+    /**
+     * This method checks if the file was added to rtb files
+     *
+     * @param string $fileName the file to check
+     *
+     * @return boolean if the file was added
+     */
+    private function checkRTBFileInConfigSettings($fileName)
+    {
+        $configurations = $this->get('setting_repository')->get(['rtb_files']);
+
+        if (!is_array($configurations)
+            || !array_key_exists('rtb_files', $configurations)
+            || !is_array($configurations['rtb_files'])
+        ) {
+            return null;
+        }
+
+        foreach ($configurations['rtb_files'] as $file) {
+            if ($file['filename'] == $fileName) {
+                return $file['id'];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * This method gets from the file path from DB
+     *
+     * @param string $fileId the file Id to recover from Database
+     *
+     * @return string path for the file
+     */
+    private function getFilePath($fileId)
+    {
+        $file = $this->get('entity_repository')->find('Attachment', $fileId);
+
+        if (!is_object($file)) {
+            throw new ResourceNotFoundException();
+        }
+
+        $path = INSTANCE_MEDIA_PATH . FILE_DIR . $file->path;
+
+        if (!file_exists($path) && !is_readable($path)) {
+            throw new ResourceNotFoundException();
+        }
+
+        return $path;
     }
 }
