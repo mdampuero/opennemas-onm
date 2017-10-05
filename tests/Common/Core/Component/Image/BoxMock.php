2331ea73e8aa7561e47771defe8c09bbc85dd13e
Adrián Reimunde <adrianrep@openhost.es>
2017-10-05 10:54:44 +0200

    ONM-2851 Add BoxMock

---
+++ tests/Common/Core/Component/Image/BoxMock.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Test\Common\Core\Component\Image;
+class BoxMock
+{
+    protected $box;
+
+    function __construct() {
+
+   }
+
+    public function __call($name, $args)
+    {
+
+    }
+}
