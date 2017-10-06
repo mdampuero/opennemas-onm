708fd1097d495a30d7c045f5624af2c246de5d6b
Fran Dieguez <fran@openhost.es>
2017-10-06 10:59:24 +0200

    ONM-2862: Use a scope variable for the button collapse with on translator directive

--- public/assets/src/angular-translator/js/translator.js
+++ public/assets/src/angular-translator/js/translator.js
@@ -26,7 +26,7 @@
           },
           template: function(elem, attrs) {
             if (attrs.link) {
-              return '<div class=\"translator btn-group btn-group-xs\" ng-if=\"collapsed || size > 4\">' +
+              return '<div class=\"translator btn-group btn-group-xs\" ng-if=\"collapsed || size > collapse_on_width\">' +
                 '<button type=\"button\" class=\"form-control btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">' +
                   '<i class=\"fa fa-pencil\"></i>' +
                   '{{text}}' +
@@ -34,14 +34,14 @@
                 '</button>' +
                 '<ul class=\"dropdown-menu\" role=\"menu\">' +
                   '<li ng-repeat=\"language in languages\" ng-if=\"language.value != ngModel\">' +
-                    '<a href=\"{{link + \'?locale=\' + language.value}}\" >' +
+                    '<a href=\"{{link + \'?locale=\' + language.value}}\">' +
                       '<i class=\"fa {{language.icon}}\" ng-show=\"language.icon\"></i>' +
                       '{{language.name}}' +
                     '</a>' +
                   '</li>' +
                 '</ul>' +
               '</div>' +
-              '<div class="translator btn-group btn-group-xs" role="group" ng-if=\"!collapsed && size < 5\">' +
+              '<div class="translator btn-group btn-group-xs" role="group" ng-if=\"!collapsed && size < collapse_on_width + 1\">' +
                 '<a ng-repeat=\"language in languages\" href="{{link + \'?locale=\' + language.value}}" class=\"btn {{language.class}}\">' +
                   '<i class=\"fa {{language.icon}}\" ng-show=\"language.icon\"></i>{{language.name}}' +
                 '</a>' +
@@ -65,6 +65,7 @@
             '</div>';
           },
           link: function($scope) {
+            $scope.collapse_on_width = 4;
             $scope.collapsed = $window.innerWidth < 992;
             $scope.languages = {};
             $scope.size      = Object.keys($scope.options.available).length;
