86e57bcd236f104408dfc97f56e7e1ba02291ff8
Diego Blanco <diego@openhost.es>
2017-10-31 18:01:49 +0100

    ONM-2863: Update letters to support OQL

--- src/BackendWebService/Controller/LetterController.php
+++ src/BackendWebService/Controller/LetterController.php
@@ -2,12 +2,11 @@
 /**
  * This file is part of the Onm package.
  *
- * (c)  OpenHost S.L. <developers@openhost.es>
+ * (c) Openhost, S.L. <developers@opennemas.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace BackendWebService\Controller;
 
 use Common\Core\Annotation\Security;
@@ -28,66 +27,56 @@ class LetterController extends ContentController
     {
         $this->hasRoles(__FUNCTION__, $contentType);
 
-        $elementsPerPage = $request->query->getDigits('elements_per_page', 10);
-        $page            = $request->query->getDigits('page', 1);
-        $search          = $request->query->get('search');
-        $sortBy          = $request->query->filter('sort_by', null, FILTER_SANITIZE_STRING);
-        $sortOrder       = $request->query->filter('sort_order', 'asc', FILTER_SANITIZE_STRING);
+        $oql = $request->query->get('oql', '');
+        $em  = $this->get('entity_repository');
 
-        $em = $this->get('entity_repository');
+        list($criteria, $order, $epp, $page) =
+            $this->get('core.helper.oql')->getFiltersFromOql($oql);
 
-        $order = null;
-        if ($sortBy) {
-            $order = '`' . $sortBy . '` ' . $sortOrder;
-        }
-
-        $results = $em->findBy($search, $order, $elementsPerPage, $page);
+        $results = $em->findBy($criteria, $order, $epp, $page);
         $results = \Onm\StringUtils::convertToUtf8($results);
-        $total   = $em->countBy($search);
+        $total   = $em->countBy($criteria);
 
-        return new JsonResponse(
-            array(
-                'elements_per_page' => $elementsPerPage,
-                'extra'             => $this->loadExtraData($results),
-                'page'              => $page,
-                'results'           => $results,
-                'total'             => $total,
-            )
-        );
+        return new JsonResponse([
+            'extra'   => $this->loadExtraData($results),
+            'results' => $results,
+            'total'   => $total,
+        ]);
     }
 
     /**
-     * undocumented function
+     * Returns extra information for letter.
      *
-     * @return void
-     * @author
+     * @param array $results The list of letters.
+     *
+     * @return array The extra information.
      */
     public function loadExtraData($results)
     {
         $data = parent::loadExtraData($results);
 
-        $photoIds = $photos = [];
+        $ids = $photos = [];
 
         foreach ($results as $letter) {
             if (!is_object($letter->photo) && (int) $letter->photo > 0) {
-                $photoIds []= $letter->photo;
+                $ids[] = $letter->photo;
             }
         }
 
-        if (count($photoIds) > 0) {
+        if (count($ids) > 0) {
              $em = getService('entity_repository');
 
             $criteria = [
                 'content_status'    => [ [ 'value' => 1 ] ],
                 'content_type_name' => [ [ 'value' => 'photo' ] ],
-                'pk_content'        => [ [ 'value' => $photoIds, 'operator' => 'IN' ] ],
+                'pk_content'        => [ [ 'value' => $ids, 'operator' => 'IN' ] ],
             ];
 
-            $order = [ 'starttime' => 'desc' ];
-
+            $order     = [ 'starttime' => 'desc' ];
             $photosRAW = $em->findBy($criteria, $order);
+
             foreach ($photosRAW as $photo) {
-                $photos [$photo->pk_content] = $photo;
+                $photos[$photo->pk_content] = $photo;
             }
         }
 
