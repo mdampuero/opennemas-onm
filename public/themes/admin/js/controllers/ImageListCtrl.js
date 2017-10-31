4120a7ba2d5b1950c7d696039218e8cec78fefe2
Diego Blanco <diego@openhost.es>
2017-10-31 18:42:16 +0100

    ONM-2863: Update images to support OQL

---
+++ public/themes/admin/js/controllers/ImageListCtrl.js
@@ -0,0 +1,73 @@
+(function () {
+ 'use strict';
+
+  angular.module('BackendApp.controllers')
+    /**
+     * @ngdoc controller
+     * @name  ImageListCtrl
+     *
+     * @requires $controller
+     * @requires $scope
+     * @requires messenger
+     * @requires oqlEncoder
+     * @requires queryManager
+     *
+     * @description
+     *   Controller for opinion list.
+     */
+    .controller('ImageListCtrl', [
+      '$controller', '$location', '$scope', 'http', 'messenger', 'oqlEncoder',
+      function($controller, $location, $scope, http, messenger, oqlEncoder) {
+
+        // Initialize the super class and extend it.
+        $.extend(this, $controller('ContentListCtrl', { $scope: $scope }));
+
+      /**
+       * Updates the array of contents.
+       *
+       * @param string route Route name.
+       */
+      $scope.list = function(route) {
+        $scope.loading = 1;
+        $scope.selected = { all: false, contents: [] };
+
+        oqlEncoder.configure({
+          placeholder: {
+            title: '(title ~ "%[value]%" or metadata ~ "%[value]%" or' +
+              ' description ~ "%[value]%") ',
+          }
+        });
+
+        var oql   = oqlEncoder.getOql($scope.criteria);
+        var route = {
+          name: $scope.route,
+          params:  {
+            contentType: $scope.criteria.content_type_name,
+            oql: oql
+          }
+        };
+
+        $location.search('oql', oql);
+
+        http.get(route).then(function(response) {
+          $scope.total = parseInt(response.data.total);
+          $scope.contents         = response.data.results;
+          $scope.map              = response.data.map;
+
+          if (response.data.hasOwnProperty('extra')) {
+            $scope.extra = response.data.extra;
+          }
+
+          // Disable spinner
+          $scope.loading = 0;
+        }, function () {
+          $scope.loading = 0;
+
+          messenger.post({
+            message: 'Error while fetching data from backend',
+            type:    'error'
+          });
+        });
+      };
+    }]);
+})();
