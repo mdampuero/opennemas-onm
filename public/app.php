c940747d64dcf6e91b9713167f3c4fa84910cc1c
Alex Rodrigues <alex@openhost.es>
2017-11-15 13:04:17 +0100

    ONM-3162: Fix php-cs errors

--- public/app.php
+++ public/app.php
@@ -5,21 +5,21 @@ use Symfony\Component\Debug\Debug;
 umask(0002);
 
 // Force reset opcache if tmp/restart.txt file is present.
-if (file_exists(__DIR__.'/../tmp/restart.txt')) {
-    @unlink(__DIR__.'/../tmp/restart.txt');
+if (file_exists(__DIR__ . '/../tmp/restart.txt')) {
+    @unlink(__DIR__ . '/../tmp/restart.txt');
     opcache_reset();
 }
 
 /**
  * @var Composer\Autoload\ClassLoader
  */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../tmp/bootstrap.php.cache';
+$loader = include __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../tmp/bootstrap.php.cache';
 
 // Little hack to allow final slashes in the url
 $_SERVER['REQUEST_URI'] = \Onm\StringUtils::normalizeUrl($_SERVER['REQUEST_URI']);
 
-if (file_exists(APPLICATION_PATH.'/.development')
+if (file_exists(APPLICATION_PATH . '/.development')
     || (array_key_exists('OPENNEMAS_ENV', $_ENV) && $_ENV["OPENNEMAS_ENV"] == 'dev')
 ) {
     $kernel = new AppKernel('dev', true);
@@ -30,9 +30,10 @@ if (file_exists(APPLICATION_PATH.'/.development')
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+// When using the HttpCache, you need to call the method in your front controller
+// instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
