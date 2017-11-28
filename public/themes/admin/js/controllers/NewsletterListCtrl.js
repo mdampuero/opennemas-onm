cace99b15fe73a406c46bc3b4febccd41149ec1a
Diego Blanco <diego@openhost.es>
2017-11-28 16:44:13 +0100

    ONM-2438: Improve syntax and add documentation

--- public/themes/admin/js/controllers/NewsletterListCtrl.js
+++ public/themes/admin/js/controllers/NewsletterListCtrl.js
@@ -1,46 +1,70 @@
-/**
- * Controller to handle list actions.
- */
-angular.module('BackendApp.controllers').controller('NewsletterListCtrl', [
-  '$http', '$uibModal', '$scope', '$timeout', 'itemService', 'routing', 'messenger', 'webStorage', 'oqlEncoder', 'queryManager', '$controller',
-  function($http, $uibModal, $scope, $timeout, itemService, routing, messenger, webStorage, oqlEncoder, queryManager, $controller) {
-    'use strict';
+(function() {
+  'use strict';
 
-    // Initialize the super class and extend it.
-    $.extend(this, $controller('ContentListCtrl', { $scope: $scope }));
+  angular.module('BackendApp.controllers')
 
     /**
-     * Permanently removes a contents by using a confirmation dialog
+     * @ngdoc controller
+     * @name  NewsletterListCtrl
+     *
+     * @requires $controller
+     * @requires $http
+     * @requires $scope
+     * @requires $uibModal
+     * @requires messenger
+     * @requires routing
+     *
+     * @description
+     *   Handles actions for newsletter list.
      */
-    $scope.removePermanently = function(content) {
-      var modal = $uibModal.open({
-        templateUrl: 'modal-delete',
-        backdrop: 'static',
-        controller: 'modalCtrl',
-        resolve: {
-          template: function() {
-            return {
-              content: content
-            };
-          },
-          success: function() {
-            return function() {
-              var url = routing.generate('backend_ws_newsletter_delete', { id: content.id });
+    .controller('NewsletterListCtrl', [
+      '$controller', '$http', '$scope', '$uibModal', 'messenger', 'routing',
+      function($controller, $http, $scope, $uibModal, messenger, routing) {
+        'use strict';
 
-              return $http.get(url);
-            };
-          }
-        }
-      });
+        // Initialize the super class and extend it.
+        $.extend(this, $controller('ContentListCtrl', { $scope: $scope }));
 
-      modal.result.then(function(response) {
-        if (response) {
-          $scope.renderMessages(response.data.messages);
+        /**
+         * @function removePermanetly
+         * @memberOf NewsletterListCtrl
+         *
+         * @description
+         *   Removes an item.
+         *
+         * @param {Object} content The content to remove.
+         */
+        $scope.removePermanently = function(content) {
+          var modal = $uibModal.open({
+            templateUrl: 'modal-delete',
+            backdrop: 'static',
+            controller: 'modalCtrl',
+            resolve: {
+              template: function() {
+                return {
+                  content: content
+                };
+              },
+              success: function() {
+                return function() {
+                  var url = routing.generate('backend_ws_newsletter_delete', { id: content.id });
 
-          if (response.status === 200) {
-            $scope.list($scope.route);
-          }
-        }
-      });
-    };
-}]);
+                  return $http.get(url);
+                };
+              }
+            }
+          });
+
+          modal.result.then(function(response) {
+            if (response) {
+              messenger.post(response.data.messages);
+
+              if (response.status === 200) {
+                $scope.list($scope.route);
+              }
+            }
+          });
+        };
+      }
+    ]);
+})();
