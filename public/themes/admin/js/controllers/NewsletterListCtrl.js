92e9dfbc2a26d0419391293bf1fe990c0af7ca7b
Fran Dieguez <fran@openhost.es>
2017-11-28 15:41:31 +0100

    ONM-2438: Remove obsolete removePermanentlySelected from newsletter list

--- public/themes/admin/js/controllers/NewsletterListCtrl.js
+++ public/themes/admin/js/controllers/NewsletterListCtrl.js
@@ -43,45 +43,4 @@ angular.module('BackendApp.controllers').controller('NewsletterListCtrl', [
         }
       });
     };
-
-    /**
-     * Permanently removes a list of contents by using a confirmation dialog
-     */
-    // $scope.removePermanentlySelected = function () {
-    //   // Enable spinner
-    //   $scope.deleting = 1;
-
-    //   var modal = $uibModal.open({
-    //     templateUrl: 'modal-cache-batch-remove',
-    //     backdrop: 'static',
-    //     controller: 'modalCtrl',
-    //     resolve: {
-    //       template: function() {
-    //         return {
-    //           selected: $scope.selected
-    //         };
-    //       },
-    //       success: function() {
-    //         return function() {
-    //           var url = routing.generate('backend_ws_cachemanager_remove');
-
-    //           return $http.post(url, { selected: $scope.selected.contents});
-    //         };
-    //       }
-    //     }
-    //   });
-
-    //   modal.result.then(function(response) {
-    //     if (response) {
-    //       $scope.renderMessages(response.data.messages);
-
-    //       $scope.selected.total = 0;
-    //       $scope.selected.contents = [];
-
-    //       if (response.status == 200) {
-    //         $scope.list($scope.route);
-    //       }
-    //     }
-    //   });
-    // };
 }]);
