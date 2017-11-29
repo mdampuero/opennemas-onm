06b825128f44cee19921a59e712a5556792c2f33
Alex Rodrigues <alex@openhost.es>
2017-11-29 16:07:35 +0100

    ONM-3097: Add smarty plugin to generate AdSense code

---
+++ libs/smarty-onm-plugins/outputfilter.adsense_validator.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     outputfilter.adsense_validator.php
+ * Type:     outputfilter
+ * Name:     adsense_validator
+ * Purpose:  Prints adsense validation script
+ * -------------------------------------------------------------
+ */
+function smarty_outputfilter_adsense_validator($output, $smarty)
+{
+    $request = getService('request');
+    $uri     = $request->getUri();
+
+    if (!preg_match('/\/admin\/frontpages/', $referer)
+        && !preg_match('/\/manager/', $uri)
+        && !preg_match('/\/managerws/', $uri)
+        && !preg_match('/\/share-by-email/', $uri)
+        && !preg_match('/\/sharrre/', $uri)
+        && !preg_match('/\/ads/', $uri)
+        && !preg_match('/\/comments/', $uri)
+        && !preg_match('/\/fb\/instant-articles/', $uri)
+        && !preg_match('@\.amp\.html$@', $uri)
+    ) {
+        $adsenseId = getService('setting_repository')->get('adsense_id');
+
+        if (!empty($adsenseId)) {
+            $code = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({
+google_ad_client: "' . $adsenseId . '",
+enable_page_level_ads: true
+});
+</script>';
+
+            $output = preg_replace('@(</head>)@', "\n" . $code, $output);
+        }
+    }
+
+    return $output;
+}
