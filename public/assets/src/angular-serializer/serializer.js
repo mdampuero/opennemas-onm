3866b2b5d9dca4049b9c3997983fef17b3a30b08
Diego Blanco <diego@openhost.es>
2017-12-12 12:13:48 +0100

    ONM-3278: Convert Date objects to string when serializing

--- public/assets/src/angular-serializer/serializer.js
+++ public/assets/src/angular-serializer/serializer.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   /**
@@ -9,52 +9,62 @@
    *   The `onm.serialize` module provides a service to serialize objects.
    */
   angular.module('onm.serializer', [])
+
     /**
      * @ngdoc provider
-     * @name  serialize
+     * @name  serializer
      *
      * @description
      *   Service to serialize objects.
      */
-    .provider('serializer', function() {
-      /**
-       * Converts an object to x-www-form-urlencoded serialization.
-       *
-       * @param {mixed} value The value to tranform.
-       *
-       * @return {String} The transformed object.
-       */
-      this.serialize = function(value, key) {
-        if (!(value instanceof Array) && !(value instanceof Object)) {
-          var query = key + '=';
+    .provider('serializer', [
+      '$windowProvider',
+      function($windowProvider) {
+        /**
+         * Converts an object to x-www-form-urlencoded serialization.
+         *
+         * @param {mixed} value The value to tranform.
+         *
+         * @return {String} The transformed object.
+         */
+        this.serialize = function(value, key) {
+          if (!(value instanceof Array) && !(value instanceof Object)) {
+            var query = key + '=';
 
-          if (value !== 'undefined' && value !== null) {
-            query += encodeURIComponent(value);
+            if (value !== 'undefined' && value !== null) {
+              query += encodeURIComponent(value);
+            }
+
+            return query;
           }
 
-          return query;
-        }
+          if (value instanceof Date) {
+            return key + '=' + $windowProvider.$get().moment(value)
+              .format('YYYY-MM-DD HH:mm:ss');
+          }
 
-        var query = '';
-        for (var i in value) {
-          query += this.serialize(value[i], key ? key + '[' + i + ']' : i) + '&';
-        }
+          var query = '';
 
-        // Empty Array or Object
-        if (query === '') {
-          return key + '=';
-        }
+          for (var i in value) {
+            query += this.serialize(value[i], key ? key + '[' + i + ']' : i) + '&';
+          }
 
-        return query.substring(0, query.length - 1);
-      };
+          // Empty Array or Object
+          if (query === '') {
+            return key + '=';
+          }
 
-      /**
-       * Returns the current service.
-       *
-       * @return Object The current object.
-       */
-      this.$get = function () {
-        return this;
-      };
-    });
+          return query.substring(0, query.length - 1);
+        };
+
+        /**
+         * Returns the current service.
+         *
+         * @return Object The current object.
+         */
+        this.$get = function() {
+          return this;
+        };
+      }
+    ]);
 })();
