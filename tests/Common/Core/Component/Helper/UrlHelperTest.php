32c95d2658b1240606538193290f9a192b0a650f
Diego Blanco <diego@openhost.es>
2017-12-21 12:12:10 +0100

    ONM-3368: Create a service to parse and generate URLs

---
+++ tests/Common/Core/Component/Helper/UrlHelperTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Common\Core\Component\Helper;
+
+use Common\Core\Component\Helper\UrlHelper;
+
+/**
+ * Defines test cases for UrlHelper class.
+ */
+class UrlHelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Configures the testing environment.
+     */
+    public function setUp()
+    {
+        $this->helper = new UrlHelper();
+    }
+
+    /**
+     * Tests parseUrl with multiple URLs.
+     */
+    public function testParse()
+    {
+        $this->assertEquals([
+            'path' => '/xyzzy/thud'
+        ], $this->helper->parse('/xyzzy/thud'));
+
+        $this->assertEquals([
+            'fragment' => 'corge',
+            'path'     => '/xyzzy/thud'
+        ], $this->helper->parse('/xyzzy/thud#corge'));
+
+        $this->assertEquals([
+            'host'   => 'foobar',
+            'path'   => '/xyzzy/thud',
+            'scheme' => 'http'
+        ], $this->helper->parse('http://foobar/xyzzy/thud'));
+
+        $this->assertEquals([
+            'fragment' => 'fred',
+            'host'     => 'foobar',
+            'pass'     => 'thud',
+            'path'     => '/xyzzy/thud',
+            'query'    => 'flob=bar',
+            'scheme'   => 'http',
+            'user'     => 'quux'
+        ], $this->helper->parse('http://quux:thud@foobar/xyzzy/thud?flob=bar#fred'));
+    }
+
+    /**
+     * Tests unparse with multiple parsed URLs.
+     */
+    public function testUnparse()
+    {
+        $this->assertEquals(
+            '/xyzzy/thud',
+            $this->helper->unparse([
+                'path' => '/xyzzy/thud'
+            ])
+        );
+
+        $this->assertEquals(
+            '/xyzzy/thud#corge',
+            $this->helper->unparse([
+                'fragment' => 'corge',
+                'path'     => '/xyzzy/thud'
+            ])
+        );
+
+        $this->assertEquals(
+            'http://foobar/xyzzy/thud',
+            $this->helper->unparse([
+                'host'   => 'foobar',
+                'path'   => '/xyzzy/thud',
+                'scheme' => 'http'
+            ])
+        );
+
+        $this->assertEquals(
+            'http://quux:thud@foobar/xyzzy/thud?flob=bar#fred',
+            $this->helper->unparse([
+                'fragment' => 'fred',
+                'host'     => 'foobar',
+                'pass'     => 'thud',
+                'path'     => '/xyzzy/thud',
+                'query'    => 'flob=bar',
+                'scheme'   => 'http',
+                'user'     => 'quux'
+            ])
+        );
+    }
+}
