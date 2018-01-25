d0b17d00fa2e6a9524e61cc632a0b3dac74b93da
Diego Blanco <diego@openhost.es>
2018-01-25 19:16:09 +0100

    ONM-3495: Update recaptcha keys when installing databases

--- db/import-databases.sh
+++ db/import-databases.sh
@@ -5,3 +5,5 @@ mysql -h mysql -uroot -proot -e "create database \`onm-instances\`;"
 mysql -h mysql -uroot -proot -e "create database \`c-default\`;"
 mysql -h mysql -uroot -proot onm-instances < $dir/onm-instances.sql
 mysql -h mysql -uroot -proot c-default < $dir/instance-default.sql
+mysql -h mysql -uroot -proot c-default < $dir/instance-default.sql
+mysql -h mysql -uroot -proot c-default -e "REPLACE INTO settings (name, value) VALUES ('recaptcha', 'a:2:{s:10:\"public_key\";s:40:\"6LdWlgkUAAAAADzgu34FyZ-wBSB0xlCUc7UVFWGw\";s:11:\"private_key\";s:40:\"6LdWlgkUAAAAAOUnzzBwHNpPgTBIaLwfDjr6XaeQ\";}')"
