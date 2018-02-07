dc5e9cfcb70ffcdc00d913c32644f636557571a9
Fran Dieguez <fran@openhost.es>
2018-02-07 17:15:12 +0100

    ONM-1646: Improve the Theme entity class and add test cases

--- src/Common/ORM/Entity/Theme.php
+++ src/Common/ORM/Entity/Theme.php
@@ -16,23 +16,72 @@ use Common\ORM\Core\Entity;
  */
 class Theme extends Extension
 {
+    /**
+     * Returns the list of available styles for the Theme.
+     * If no available styles it returns an empty array.
+     *
+     * @return array|null the list of available styles
+     **/
     public function getStyles()
     {
-        if (array_key_exists('styles', $this->data['parameters'])) {
+        if (array_key_exists('parameters', $this->data)
+            && array_key_exists('styles', $this->data['parameters'])
+        ) {
             return $this->data['parameters']['styles'];
         }
+
+        return [];
     }
 
+    /**
+     * Returns the default style for the Theme.
+     * If no default style it returns the first one.
+     * If no styles it returns null.
+     *
+     * @return array|null the defeault style
+     **/
     public function getDefaultStyle()
     {
-        if (!array_key_exists('styles', $this->data['parameters'])) {
+        $styles = $this->getStyles();
+
+        if (empty($styles)) {
             return null;
         }
 
-        $default = array_filter($this->data['parameters']['styles'], function ($el) {
+        $default = array_filter($styles, function ($el) {
             return (array_key_exists('default', $el) && $el['default'] == true);
         });
 
+        // If no default style just pick the first one
+        if (empty($default)) {
+            return array_shift($styles);
+        }
+
         return array_pop($default);
+    }
+
+    /**
+     * Returns the style information given its name.
+     * If the given name is not valid it returns the default style.
+     * If no styles it returns null.
+     *
+     * @param string $name the style name
+     *
+     * @return array|null the defeault style
+     **/
+    public function getCurrentStyle($name)
+    {
+        $styles = $this->getStyles();
+
+        if (empty($styles)) {
+            return null;
+        }
+
+        // Return the default style if the name doesnt exists
+        if (!array_key_exists($name, $styles)) {
+            return $this->getDefaultStyle();
+        }
+
+        return $styles[$name];
     }
 }
