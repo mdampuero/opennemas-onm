b0036eba1d910c921352d9386ad2cc93aac24ce6
Adrián Reimunde <adrianrep@openhost.es>
2018-03-07 20:14:22 +0100

    ONM-3679: Fix error with decode of json

--- src/Backend/Controller/OpinionsController.php
+++ src/Backend/Controller/OpinionsController.php
@@ -597,7 +597,7 @@ class OpinionsController extends Controller
                     'blog_orderFrontpage'   => filter_var($configsRAW['blog_orderFrontpage'], FILTER_SANITIZE_STRING),
                     'blog_itemsFrontpage'   => filter_var($configsRAW['blog_itemsFrontpage'], FILTER_VALIDATE_INT),
                 ],
-                'extraInfoContents.OPINION_MANAGER' => json_decode($extra, true)
+                'extraInfoContents.OPINION_MANAGER' => json_decode($extra)
             ];
 
             foreach ($configs as $key => $value) {
