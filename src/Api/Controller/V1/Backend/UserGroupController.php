c2411f968a3c88ea9a1dc919f14ee39ac589e7b5
Diego Blanco <diego@openhost.es>
2018-03-14 15:53:20 +0100

    ONM-3689: Create route and controller for user groups in API

---
+++ src/Api/Controller/V1/Backend/UserGroupController.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c)  OpenHost S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Api\Controller\V1\Backend;
+
+use Common\Core\Annotation\Security;
+use Common\ORM\Entity\UserGroup;
+use Common\Core\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Displays, saves, modifies and removes user groups.
+ */
+class UserGroupController extends Controller
+{
+    /**
+     * Saves a new user groups.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_CREATE')")
+     */
+    public function createAction(Request $request)
+    {
+        $msg = $this->get('core.messenger');
+
+        $userGroup = $this->get('api.service.user_group')
+            ->createItem($request->request->all());
+        $msg->add(_('UserGroup saved successfully'), 'success', 201);
+
+        $response = new JsonResponse($msg->getMessages(), $msg->getCode());
+        $response->headers->set(
+            'Location',
+            $this->generateUrl(
+                'api_v1_backend_user_group_show',
+                [ 'id' => $userGroup->pk_user_group ]
+            )
+        );
+
+        return $response;
+    }
+
+    /**
+     * Deletes an user group.
+     *
+     * @param integer $id The user group id.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_DELETE')")
+     */
+    public function deleteAction($id)
+    {
+        $msg = $this->get('core.messenger');
+
+        $this->get('api.service.user_group')->deleteItem($id);
+        $msg->add(_('UserGroup deleted successfully'), 'success');
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Deletes the selected user groups.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_DELETE')")
+     */
+    public function deleteSelectedAction(Request $request)
+    {
+        $ids     = $request->request->get('ids', []);
+        $msg     = $this->get('core.messenger');
+        $deleted = $this->get('api.service.user_group')->deleteList($ids);
+
+        if ($deleted > 0) {
+            $msg->add(
+                sprintf(_('%s user groups deleted successfully'), $deleted),
+                'success'
+            );
+        }
+
+        if ($deleted !== count($ids)) {
+            $msg->add(sprintf(
+                _('%s user groups could not be deleted successfully'),
+                count($ids) - $deleted
+            ), 'error');
+        }
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Returns the list of user groups.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_LIST')")
+     */
+    public function listAction(Request $request)
+    {
+        $ss       = $this->get('api.service.user_group');
+        $oql      = $request->query->get('oql', '');
+        $response = $ss->getList($oql);
+
+        $response['results'] = $ss->responsify($response['results']);
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * Returns the data to create a new user group.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_CREATE')")
+     */
+    public function newAction()
+    {
+        return new JsonResponse([ 'extra' => $this->getExtraData() ]);
+    }
+
+    /**
+     * Updates some instance properties.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_UPDATE')")
+     */
+    public function patchAction(Request $request, $id)
+    {
+        $msg = $this->get('core.messenger');
+
+        $this->get('api.service.user_group')
+            ->patchItem($id, $request->request->all());
+        $msg->add(_('UserGroup saved successfully'), 'success');
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Updates some user group properties.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_UPDATE')")
+     */
+    public function patchSelectedAction(Request $request)
+    {
+        $params = $request->request->all();
+        $ids    = $params['ids'];
+        $msg    = $this->get('core.messenger');
+
+        unset($params['ids']);
+
+        $updated = $this->get('api.service.user_group')
+            ->patchList($ids, $params);
+
+        if ($updated > 0) {
+            $msg->add(
+                sprintf(_('%s user groups updated successfully'), $updated),
+                'success'
+            );
+        }
+
+        if ($updated !== count($ids)) {
+            $msg->add(sprintf(
+                _('%s user groups could not be updated successfully'),
+                count($ids) - $updated
+            ), 'error');
+        }
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Returns an user group.
+     *
+     * @param integer $id The group id.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_UPDATE')")
+     */
+    public function showAction($id)
+    {
+        $ss = $this->get('api.service.user_group');
+
+        return new JsonResponse([
+            'user_group' => $ss->responsify($ss->getItem($id)),
+            'extra'      => $this->getExtraData()
+        ]);
+    }
+
+    /**
+     * Updates the user group information given its id and the new information.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     *
+     * @Security("hasPermission('GROUP_UPDATE')")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $msg = $this->get('core.messenger');
+
+        $this->get('api.service.user_group')
+            ->updateItem($id, $request->request->all());
+
+        $msg->add(_('UserGroup saved successfully'), 'success');
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Returns a list of extra data.
+     *
+     * @return array The extra data.
+     */
+    private function getExtraData()
+    {
+        $privilege = new \Privilege();
+
+        return [ 'modules' => $privilege->getPrivilegesByModules() ];
+    }
+}
