5f4caceb2fa0994e7b9f07a26d76c2d6677a13f3
Fran Dieguez <fran@openhost.es>
2018-03-19 16:24:14 +0100

    ONM-2673: Only apply JS tabdrop to elements with the CSS class tabdrop

--- public/themes/admin/js/admin.js
+++ public/themes/admin/js/admin.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
       e.preventDefault();
     });
 
-    $('.nav-pills, .nav-tabs').tabdrop();
+    $('.nav-tabs-tabdrop .nav-pills, .nav-tabs-tabdrop .nav-tabs').tabdrop();
 
     $('#formulario').on('submit', function(){
       var btn = $('.btn.btn-primary');
