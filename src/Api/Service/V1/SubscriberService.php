8c3a56d771ac046e6d76871b0a409eb5b1346ecc
Diego Blanco <diego@openhost.es>
2018-03-22 17:53:05 +0100

    ONM-3692: Update service to convert subscriber + user to user when deleting

--- src/Api/Service/V1/SubscriberService.php
+++ src/Api/Service/V1/SubscriberService.php
@@ -52,6 +52,69 @@ class SubscriberService extends OrmService
     /**
      * {@inheritdoc}
      */
+    public function deleteItem($id)
+    {
+        try {
+            $item = $this->getItem($id);
+
+            // Convert to user if subscriber + user
+            if ($item->type === 2) {
+                $item->type = 0;
+
+                $this->em->persist($item, $item->getOrigin());
+                return;
+            }
+
+            $this->em->remove($item, $item->getOrigin());
+        } catch (\Exception $e) {
+            $this->container->get('error.log')->error($e->getMessage());
+            throw new DeleteItemException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteList($ids)
+    {
+        if (!is_array($ids) || empty($ids)) {
+            throw new DeleteListException('Invalid ids', 400);
+        }
+
+        $oql = $this->getOqlForList($ids);
+
+        try {
+            $response = parent::getList($oql);
+        } catch (\Exception $e) {
+            $this->container->get('error.log')->error($e->getMessage());
+            throw new DeleteListException($e->getMessage(), $e->getCode());
+        }
+
+        $deleted = 0;
+        foreach ($response['results'] as $item) {
+            try {
+                // Convert to user if subscriber + user
+                if ($item->type === 2) {
+                    $item->type = 0;
+
+                    $this->em->persist($item, $item->getOrigin());
+                    $deleted++;
+                    continue;
+                }
+
+                $this->em->remove($item, $item->getOrigin());
+                $deleted++;
+            } catch (\Exception $e) {
+                $this->container->get('error.log')->error($e->getMessage());
+            }
+        }
+
+        return $deleted;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getItem($id)
     {
         try {
