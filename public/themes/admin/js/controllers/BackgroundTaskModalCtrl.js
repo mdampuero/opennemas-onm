90ea544a56b2a06e4bb86d2993a3bc3f6b045822
Fran Dieguez <fran@openhost.es>
2018-04-17 17:11:25 +0200

    ONM-3812: Add BackgroundTaskModalCtrl component

---
+++ public/themes/admin/js/controllers/BackgroundTaskModalCtrl.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  angular.module('BackendApp.controllers')
+
+    /**
+     * @ngdoc controller
+     * @name  BackgroundTaskModalCtrl
+     *
+     * @requires $uibModalInstance
+     * @requires $scope
+     * @requires template
+     * @requires success
+     *
+     * @description
+     *   Handles actions for showing modal windows while running
+     *   a callback in background.
+     */
+    .controller('BackgroundTaskModalCtrl', [
+      '$uibModalInstance', '$scope', 'template', 'callback',
+      function($uibModalInstance, $scope, template, callback) {
+        /**
+         * @memberOf YesNoModalCtrl
+         *
+         * @description
+         *   The template parameters.
+         *
+         * @type {Object}
+         */
+        $scope.template = template;
+
+        callback($uibModalInstance, template);
+
+        /**
+         * @function dismiss
+         * @memberOf YesNoModalCtrl
+         *
+         * @description
+         *   Close the modal without executing any action.
+         */
+        $scope.dismiss = function() {
+          $uibModalInstance.dismiss();
+        };
+
+        // Frees up memory before controller destroy event
+        $scope.$on('$destroy', function() {
+          $scope.template = null;
+        });
+      }
+    ]);
+})();
