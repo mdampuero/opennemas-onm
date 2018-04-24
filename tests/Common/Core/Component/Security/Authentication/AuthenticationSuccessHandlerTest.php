003d8010f0a0de52a4b9c4510f6d20ce5e996b70
Diego Blanco <diego@openhost.es>
2018-04-24 18:43:08 +0200

    ONM-3694: Prevent double message when authentication fails

--- tests/Common/Core/Component/Security/Authentication/AuthenticationSuccessHandlerTest.php
+++ tests/Common/Core/Component/Security/Authentication/AuthenticationSuccessHandlerTest.php
@@ -24,8 +24,8 @@ class AuthenticationSuccessHandlerTest extends \PHPUnit_Framework_TestCase
     {
         $this->auth = $this->getMockBuilder('Authentication')
             ->setMethods([
-                'checkCsrfToken', 'checkRecaptcha', 'failure',
-                'getErrorMessage', 'hasError', 'success'
+                'checkCsrfToken', 'checkRecaptcha', 'getErrorMessage',
+                'hasError', 'success'
             ])->getMock();
 
         $this->user = json_decode(json_encode([
@@ -33,10 +33,6 @@ class AuthenticationSuccessHandlerTest extends \PHPUnit_Framework_TestCase
             'username'      => 'flob',
             'user_language' => 'es'
         ]));
-
-        $this->fb = $this->getMockBuilder('FlashBag')
-            ->setMethods([ 'add' ])
-            ->getMock();
 
         $this->headers = $this->getMockBuilder('Headers')
             ->setMethods([ 'get' ])
@@ -55,7 +51,7 @@ class AuthenticationSuccessHandlerTest extends \PHPUnit_Framework_TestCase
             ->getMock();
 
         $this->session = $this->getMockBuilder('Session')
-            ->setMethods([ 'getFlashBag', 'remove', 'set' ])
+            ->setMethods([ 'remove', 'set' ])
             ->getMock();
 
         $this->ts = $this->getMockBuilder('Symfony\Component\Security\Core\Authentication\Token\TokenInterface')
@@ -73,10 +69,6 @@ class AuthenticationSuccessHandlerTest extends \PHPUnit_Framework_TestCase
             ->willReturn($this->session);
         $this->session->expects($this->at(0))->method('remove')
             ->with('_target');
-        $this->session->expects($this->any())->method('getFlashBag')
-            ->willReturn($this->fb);
-        $this->ts->expects($this->once())->method('getUser')
-            ->willReturn($this->user);
         $this->headers->expects($this->once())->method('get')
             ->with('referer')->willReturn('/mumble/gorp');
 
@@ -92,10 +84,8 @@ class AuthenticationSuccessHandlerTest extends \PHPUnit_Framework_TestCase
     {
         $this->auth->expects($this->once())->method('hasError')
             ->willReturn(true);
-        $this->auth->expects($this->once())->method('failure');
         $this->auth->expects($this->once())->method('getErrorMessage')
             ->willReturn('corge');
-        $this->fb->expects($this->once())->method('add')->with('error', 'corge');
         $this->logger->expects($this->once())->method('info');
 
         $this->request->expects($this->at(0))->method('get')
