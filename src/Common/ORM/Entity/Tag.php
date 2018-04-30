88b55438f3bd0e99e6d994080d12ac164abe2b32
Adrián Reimunde <adrianrep@openhost.es>
2018-04-30 10:50:01 +0200

    ONM-3912: Tags Component

---
+++ src/Common/ORM/Entity/Tag.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\ORM\Entity;
+
+use Common\ORM\Core\Entity;
+
+/**
+ * The Tag class represents a Tag.
+ */
+class Tag extends Entity
+{
+}
