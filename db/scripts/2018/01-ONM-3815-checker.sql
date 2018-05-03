70cff0e1f673d77917ea4c69b38899d209b5a822
Adrián Reimunde <adrianrep@openhost.es>
2018-05-03 15:32:58 +0200

    ONM-3815: Fix tag id

--- db/scripts/2018/01-ONM-3815-checker.sql
+++ db/scripts/2018/01-ONM-3815-checker.sql
@@ -1,1 +1,1 @@
-SELECT tags.name, tags.tag_id, tags.slug, tags.language_id, contents_tags.content_id FROM `tags` inner join contents_tags on tags.tag_id = contents_tags.tag_id WHERE 1 = 1
+SELECT tags.name, tags.id, tags.slug, tags.language_id, contents_tags.content_id FROM `tags` inner join contents_tags on tags.id = contents_tags.tag_id WHERE 1 = 1
