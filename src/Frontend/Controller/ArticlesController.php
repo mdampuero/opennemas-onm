9301adad3e74a0d461e0076989b50f1f7a204a76
Diego Blanco <diego@openhost.es>
2018-05-08 13:01:32 +0200

    ONM-3978: Throw a ResourceNotFoundException when category does not exist

--- src/Frontend/Controller/ArticlesController.php
+++ src/Frontend/Controller/ArticlesController.php
@@ -62,8 +62,12 @@ class ArticlesController extends Controller
             throw new AccessDeniedException();
         }
 
-        $category = $this->get('orm.manager')->getRepository('Category')
-            ->findOneBy(sprintf('name = "%s"', $categoryName));
+        try {
+            $category = $this->get('orm.manager')->getRepository('Category')
+                ->findOneBy(sprintf('name = "%s"', $categoryName));
+        } catch (\Exception $e) {
+            throw new ResourceNotFoundException();
+        }
 
         list($positions, $advertisements) =
             $this->getAds($category->pk_content_category);
