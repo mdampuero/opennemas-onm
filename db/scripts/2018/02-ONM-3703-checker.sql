586a87c82574409a9bde053b688276f9a720224a
Fran Dieguez <fran@openhost.es>
2018-05-16 11:58:56 +0200

    ONM-3703: Format checker script

--- db/scripts/2018/02-ONM-3703-checker.sql
+++ db/scripts/2018/02-ONM-3703-checker.sql
@@ -1,5 +1,11 @@
 SELECT count(id) FROM `users` WHERE id IN (
-    SELECT user_id FROM user_user_group WHERE user_group_id = (select pk_user_group from user_groups WHERE name="Newsletter" LIMIT 1)
+    SELECT user_id
+    FROM user_user_group
+    WHERE user_group_id = (
+        SELECT pk_user_group
+        FROM user_groups
+        WHERE name="Newsletter" LIMIT 1
+    )
 );
 
 SELECT count(pk_pc_user) FROM pc_users;
