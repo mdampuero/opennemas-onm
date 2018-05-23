54c53b472f1a52af121980d42606c865fc5c02c0
Fran Dieguez <fran@openhost.es>
2018-05-23 15:52:34 +0200

    ONM-3806: Add core.validator service

---
+++ src/Common/Core/Component/Validator/Validator.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) OpenHost S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\Core\Component\Validator;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Common\Core\Component\Validator\Constraints as OnmAssert;
+
+class Validator
+{
+    // List of ruleset names
+    const BLACKLIST_RULESET_COMMENTS = 'comment';
+
+    /**
+     * The settings repository
+     *
+     * @var SettingRepository
+     **/
+    private $sm = null;
+
+    /**
+     * The validator service
+     *
+     * @var Symfony\Component\Validator
+     **/
+    private $validator = null;
+
+    /**
+     * Initializes the validator object with dependencies
+     *
+     * @param SettingsRepository $settingsManager the settings repository object
+     * @param Validator          $validator       the validator service
+     *
+     * @return void
+     */
+    public function __construct($settingsManager, $validator)
+    {
+        $this->sm        = $settingsManager;
+        $this->validator = $validator;
+    }
+
+    /**
+     * Validates an array or an object
+     *
+     * @param array|object $entity the data to validate
+     * @param string       $paramname description
+     *
+     * @return mixed the array of violations
+     **/
+    public function validate($entity, $ruleName)
+    {
+        $classReflection = new \ReflectionClass(__CLASS__);
+        $constants       = $classReflection->getConstants();
+
+        $validRuleSet = in_array($ruleName, $constants);
+        $methodName   = 'validate' . ucfirst($ruleName);
+        if ($validRuleSet && method_exists($this, $methodName)) {
+            return $this->{$methodName}($entity);
+        }
+
+        throw new \InvalidArgumentException(sprintf(
+            "The ruleset '%s' is not valid",
+            $ruleName
+        ));
+    }
+
+    /**
+     * Returns the blacklist config for a given ruleSet
+     *
+     * @return void
+     **/
+    public function getConfig($ruleSet)
+    {
+        return $this->sm->get('blacklist.' . $ruleSet);
+    }
+
+    /**
+     * Saves the blacklist config for a given ruleSet
+     *
+     * @param string $ruleSet the name of the rule set
+     * @param mixed  $config  the configuration to save
+     *
+     * @return void
+     **/
+    public function setConfig($ruleSet, $config)
+    {
+        return $this->sm->set('blacklist.' . $ruleSet, $config);
+    }
+
+    /**
+     * Validates comments given an array of data
+     *
+     * @param array $data the comment data
+     *
+     * @return array the list of violations
+     **/
+    private function validateComment($data)
+    {
+        $config = $this->getConfig(self::BLACKLIST_RULESET_COMMENTS);
+
+        $constraint = new Assert\Collection([
+            'author' => new Assert\NotBlank([
+                'message' => _('Please provide a valid author name.')
+            ]),
+            'author_email' => new Assert\Email([
+                'message' => _('Please provide a valid email address')
+            ]),
+            'author_ip'    => new Assert\NotBlank([
+                'message' => _('Your IP address is not valid.')
+            ]),
+            'body'         => [
+                new Assert\Length([
+                    'min'        => 10,
+                    'minMessage' => _('Your comment has no enought contents.')
+                ]),
+                new OnmAssert\BlacklistWords([
+                    'words'   => $config,
+                    'message' => _('Your comment has invalid words')
+                ]),
+            ],
+            'content_id' => new Assert\Range(['min' => 1]),
+        ]);
+
+        $violations = $this->validator->validate($data, $constraint);
+
+        if (count($violations) > 0) {
+            $errors = [];
+
+            foreach ($violations as $el) {
+                $errors[] = $el->getMessage();
+            }
+
+            return $errors;
+        }
+
+        return [];
+    }
+}
