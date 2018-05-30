f6b154cd9b1b8dac7de5690a572fb29937ae069d
Fran Dieguez <fran@openhost.es>
2018-05-30 18:12:19 +0200

    ONM-3898: Refactor Vote class to improve code and remove deprecated

--- libs/models/Vote.php
+++ libs/models/Vote.php
@@ -22,28 +22,28 @@ class Vote
      *
      * @var int
      */
-    public $pk_vote        = null;
+    public $pk_vote = null;
 
     /**
      * Summary of positive votes
      *
      * @var int
      */
-    public $value_pos      = null;
+    public $value_pos = null;
 
     /**
      * Summary of negative votes
      *
      * @var int
      */
-    public $value_neg      = null;
+    public $value_neg = null;
 
     /**
      * Final karma for the content
      *
      * @var
      */
-    public $karma          = null;
+    public $karma = null;
 
     /**
      * Serialized array of ips that voted
@@ -57,7 +57,7 @@ class Vote
      *
      * @var  array
      */
-    private $messages = array('', 'A Favor', 'En Contra');
+    private $messages = [];
 
     /**
      * Initializes the vote given an content id
@@ -68,6 +68,12 @@ class Vote
      */
     public function __construct($id = null)
     {
+        $this->messages = [
+            '',
+            _('A Favor'),
+            _('En Contra'),
+        ];
+
         if (!is_null($id)) {
             $this->read($id);
         }
@@ -84,38 +90,21 @@ class Vote
      */
     public function create($votePK, $vote, $ip)
     {
-        // En contra
-        if ($vote == '2') {
-            $negValue = 1;
-            $karma    = 100 - 1;
-            $posValue = 0;
-        } else {
-            // A favor
-            $posValue = 1;
-            $karma    = 100 + 1;
-            $negValue = 0;
-        }
-        $ipsCountVote[] = array('ip' => $ip, 'count' => 1);
-
         try {
-            $rs = getService('dbal_connection')->insert(
-                "votes",
-                [
-                    'pk_vote'       => $votePK,
-                    'value_pos'     => $posValue,
-                    'value_neg'     => $negValue,
-                    'karma'         => $karma,
-                    'ips_count_vote => serialize($ipsCountVote)'
-                ]
-            );
+            getService('dbal_connection')->insert("votes", [
+                'pk_vote'       => $votePK,
+                'value_pos'     => ($vote == '2') ? 0 : 1,
+                'value_neg'     => ($vote !== '2') ? 0 : 1,
+                'karma'         => ($vote !== '2') ? $karma - 1 : $karma + 1,
+                'ips_count_vote' => serialize([$ip])
+            ]);
 
-            return $this;
+            return true;
         } catch (\Exception $e) {
-            error_log($e->getMessage());
+            getService('error.log')->error($e->getMessage());
+
             return false;
         }
-
-        return true;
     }
 
     /**
@@ -133,43 +122,42 @@ class Vote
                 [ $votePK ]
             );
         } catch (\Exception $e) {
-            error_log('Error on Vote::read: '.$e->getMessage());
+            getService('error.log')->error('Error on Vote::read: ' . $e->getMessage());
+
             return false;
         }
 
-
-        if (!$rs) {
-            try {
-                $rs = getService('dbal_connection')->insert(
-                    "votes",
-                    [
-                      'pk_vote'        => $votePK,
-                      'value_pos'      => 0,
-                      'value_neg'      => 0,
-                      'karma'          => 100,
-                      'ips_count_vote' => serialize(array()),
-                    ]
-                );
-
-                return $this;
-            } catch (\Exception $e) {
-                error_log($e->getMessage());
-                return false;
-            }
-            $this->pk_vote        = $votePK;
-            $this->value_pos      = 0;
-            $this->value_neg      = 0;
-            $this->karma          = 100;
-            $this->ips_count_vote = array();
-        } else {
+        if ($rs) {
             $this->pk_vote        = $votePK;
             $this->value_pos      = $rs['value_pos'];
             $this->value_neg      = $rs['value_neg'];
             $this->karma          = $rs['karma'];
             $this->ips_count_vote = unserialize($rs['ips_count_vote']);
+
+            return $this;
         }
 
-        return $this;
+        try {
+            $rs = getService('dbal_connection')->insert("votes", [
+              'pk_vote'        => $votePK,
+              'value_pos'      => 0,
+              'value_neg'      => 0,
+              'karma'          => 100,
+              'ips_count_vote' => serialize([]),
+            ]);
+
+            $this->pk_vote        = $votePK;
+            $this->value_pos      = 0;
+            $this->value_neg      = 0;
+            $this->karma          = 100;
+            $this->ips_count_vote = [];
+
+            return $this;
+        } catch (\Exception $e) {
+            getService('error.log')->error($e->getMessage());
+
+            return false;
+        }
     }
 
     /**
@@ -188,18 +176,10 @@ class Vote
             return false;
         }
 
+        $values              = [ 'ips_count_vote' => serialize($this->ips_count_vote) ];
+        $values['value_pos'] = ++$this->value_pos;
         if ($vote == '2') {
-            $values = [
-                'value_neg'      => ++$this->value_neg,
-                'ips_count_vote' => serialize($this->ips_count_vote)
-            ];
-            $sql = "UPDATE votes SET  `value_neg`=?,  `ips_count_vote`=?
-                    WHERE pk_vote=" . $this->pk_vote;
-        } else {
-            $values = [
-                'value_pos'      => ++$this->value_pos,
-                'ips_count_vote' => serialize($this->ips_count_vote)
-            ];
+            $values['value_neg'] = ++$this->value_neg;
         }
 
         try {
@@ -208,59 +188,13 @@ class Vote
                 $values,
                 [ 'pk_vote' => (int) $this->pk_vote ]
             );
+
+            return true;
         } catch (\Exception $e) {
-            error_log('Error on Vote:update: '.$e->getMessage());
+            getService('error.log')->error('Error on Vote:update: ' . $e->getMessage());
+
             return false;
         }
-
-        return true;
-    }
-
-    /**
-     * Renders the html for the voting result
-     *
-     * @param string $page the page where the vote takes place
-     * @param string $type the type of the vote
-     * @param string $ajax whether the votes is from an ajax request
-     *
-     * @return string the HTML generated
-     */
-    public function render($page, $type, $ajax = 0)
-    {
-        if (isset($_COOKIE["vote" . $this->pk_vote])) {
-            $type = "result";
-        }
-        $outputHTML = "";
-        $results = "";
-
-        if ($type == "vote") {
-            // Render links
-            for ($i = 1; $i <= 2; $i++) {
-                $results.= $this->renderLink($i, $this->pk_vote);
-            }
-            $outputHTML.= "  <div class=\"CVotos\">";
-            $outputHTML.= $results;
-            $outputHTML.= "  </div>";
-        } elseif ($type == "result") {
-            for ($i = 1; $i <= 2; $i++) {
-                $results.= $this->renderImg($i);
-            }
-            $outputHTML.= "  <div class=\"CVotos\">";
-            $outputHTML.= $results;
-            $outputHTML.= "  </div>";
-            $outputHTML.= "  <div class=\"separadorVotos\"></div>";
-            $outputHTML.= "  <div class=\"CVotos\">";
-            $outputHTML.= "  ¡Gracias por votar!";
-            $outputHTML.= "  </div>";
-        }
-
-        if (!$ajax) {
-            $outputHTML = "<div class=\"CComent_Votos_nota\" id=\"vota"
-                        . $this->pk_vote . "\">" . $outputHTML;
-            $outputHTML.= "</div>";
-        }
-
-        return $outputHTML;
     }
 
     /**
@@ -273,89 +207,32 @@ class Vote
      */
     public function addCount($countIPs, $ip)
     {
-        $ips = array();
-        foreach ($countIPs as $ip_array) {
-            $ips[] = $ip_array['ip'];
+        $ips = [];
+        if (!is_array($countIPs)) {
+            $countIPs = [];
         }
 
-        //Se busca si existe algún voto desde la ip
+        foreach ($countIPs as $ipArray) {
+            $ips[] = $ipArray['ip'];
+        }
+
+        // Se busca si existe algún voto desde la ip
         $countKIP = array_search($ip, $ips);
 
         if ($countKIP === false) {
-            //No se ha votado desde esa ip
-            $countIPs[] = array('ip' => $ip, 'count' => 1);
+            // No se ha votado desde esa ip
+            $countIPs[] = [
+                'ip' => $ip,
+                'count' => 1
+            ];
         } else {
             if ($countIPs[$countKIP]['count'] == 50) {
                 return false;
             }
+
             $countIPs[$countKIP]['count']++;
         }
 
         return $countIPs;
-    }
-
-    /**
-     * Returns the HTML for a vote image
-     *
-     * @param int $i the voting position
-     *
-     * @return string the HTML for the vote image
-     *
-     */
-    private function renderImg($i)
-    {
-        $imgPath = TEMPLATE_USER_URL . "images/utilities/";
-        $imageTpl = '<img src="%s%s.png" style="vertical-align:middle;" alt="%s" title="%s" /> ( %d ) ';
-
-        return sprintf(
-            $imageTpl,
-            $imgPath,
-            ($i % 2 == 0) ?  "vote-up" : "vote-down",
-            $this->messages[$i],
-            $this->messages[$i],
-            ($i % 2 == 1) ? $this->value_pos : $this->value_neg
-        );
-    }
-
-    /**
-     * Returns the HTML for a vote link
-     *
-     * @param int $i the voting position
-     * @param int $votePK the content id to vote
-     * @param int $value the voting value
-     *
-     * @example
-     *  <a href="javascript:vote_comment(IP,1,PK_VOTE)" title="A favor">
-     *    <img id="$this->pk_vote_1"
-     *       src="TEMPLATE_USER_PATH."images/noticias/vote_pos.png "
-     *       alt="A favor" />
-     *  </a>
-     *
-     * @return string the HTML for the vote link
-     *
-     */
-    private function renderLink($i, $votePK)
-    {
-        $imgPath = TEMPLATE_USER_URL . "images/utilities/";
-        $linkTpl = <<< LINKTPLDOC
-            <a href="#votar" onclick="javascript:vote_comment('%s', '%s', '%s'); return false;" title="%s">
-                <img id="%s_%s" style="vertical-align:middle;"
-                     src="{$imgPath}%s.png"
-                     alt="%s" /> </a>   ( %d )
-LINKTPLDOC;
-
-        return sprintf(
-            $linkTpl,
-            $_SERVER['REMOTE_ADDR'],
-            $i,
-            $votePK,
-            $this->messages[$i],
-            // $votePK, $i,
-            $i,
-            $votePK,
-            ($i % 2 == 0) ? "vote-up" : "vote-down",
-            $this->messages[$i],
-            ($i % 2 == 1) ? $this->value_pos : $this->value_neg
-        );
     }
 }
