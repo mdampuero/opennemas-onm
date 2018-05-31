6c705dc70ce9f299045b82390f89e129530dbaa7
Fran Dieguez <fran@openhost.es>
2018-05-31 10:01:30 +0200

    ONM-3898: Add Recaptha in Frontend:CommentsController and refactor save and vote actions

--- src/Frontend/Controller/CommentsController.php
+++ src/Frontend/Controller/CommentsController.php
@@ -12,11 +12,12 @@
  */
 namespace Frontend\Controller;
 
+use Common\Core\Controller\Controller;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Common\Core\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Handles the actions for comments
@@ -68,6 +69,9 @@ class CommentsController extends Controller
             'offset'         => $offset,
             'dark_theme'     => $darkTheme,
             'count'          => $total,
+            'recaptcha'      => $this->get('core.recaptcha')
+                ->configureFromSettings()
+                ->getHtml(),
         ]);
     }
 
@@ -153,17 +157,18 @@ class CommentsController extends Controller
         }
 
         // 1 Vote up - 2 Vote down
-        $voteValue = 2;
+        $vote = 2;
         if ($voteValue == 'up') {
-            $voteValue = 1;
+            $vote = 1;
         }
 
         // Create the vote
-        $vote = new \Vote($commentId);
-        if (is_null($vote)) {
+        $voteObject = new \Vote($commentId);
+        if (is_null($voteObject)) {
             return new Response(_("Error: no vote value!", 400));
         }
-        $update = $vote->update($voteValue, $ip);
+
+        $update = $voteObject->update($vote, $ip);
 
         if ($update) {
             $response = new Response('ok', 200);
@@ -190,8 +195,22 @@ class CommentsController extends Controller
         $authorName  = $request->request->filter('author-name', '', FILTER_SANITIZE_STRING);
         $authorEmail = $request->request->filter('author-email', '', FILTER_SANITIZE_STRING);
         $contentId   = $request->request->getDigits('content-id');
+        $response    = $request->request->filter('g-recaptcha-response', null, FILTER_SANITIZE_STRING);
         $ip          = getUserRealIP();
         $cm          = $this->get('core.helper.comment');
+
+
+        // Check current recaptcha
+        $isValid = $this->get('core.recaptcha')
+            ->configureFromSettings()
+            ->isValid($response, $request->getClientIp());
+
+        if (!$isValid) {
+            return new JsonResponse([
+                'type' => 'error',
+                'message' => _('Please fill the captcha code.'),
+            ], 400);
+        }
 
         $httpCode = 200;
         try {
@@ -206,11 +225,13 @@ class CommentsController extends Controller
 
             $data['body'] = '<p>' . preg_replace('@\\n@', '</p><p>', $data['body']) . '</p>';
 
-
             if ($cm->moderateManually()) {
                 $data['status'] = \Comment::STATUS_PENDING;
 
-                $message = _('Your comment was accepted and now we have to moderate it.');
+                $message = [
+                    'message' => _('Your comment was accepted and now we have to moderate it.'),
+                    'type'    => 'warning',
+                ];
             } else {
                 $errors = $this->get('core.validator')->validate($data, 'comment');
 
@@ -220,14 +241,20 @@ class CommentsController extends Controller
                         : \Comment::STATUS_PENDING;
 
                     $httpCode = 200;
-                    $message  = _('Your comment was accepted.');
+                    $message  = [
+                        'message' => _('Your comment was accepted.'),
+                        'type'    => 'success',
+                    ];
                 } else {
                     $data['status'] = $cm->autoReject()
                         ? \Comment::STATUS_REJECTED
                         : \Comment::STATUS_PENDING;
 
                     $httpCode = 400;
-                    $message  = implode('<br>', $errors);
+                    $message  = [
+                        'message' => implode('<br>', $errors),
+                        'type'    => 'error',
+                    ];
                 }
             }
 
@@ -238,7 +265,14 @@ class CommentsController extends Controller
             $message  = $e->getMessage();
         }
 
-        return new Response($message, $httpCode);
+        $response = new JsonResponse($message, $httpCode);
+        if (!$request->isXmlHttpRequest()) {
+            $response = new RedirectResponse($this->generateUrl('frontend_comments_get', [
+                'id' => $contentId,
+            ]));
+        }
+
+        return $response;
     }
 
     /**
