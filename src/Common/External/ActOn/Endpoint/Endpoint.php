62a42f0949f070539a0c1d65c917bba4fc33251e
Diego Blanco <diego@openhost.es>
2018-06-06 18:25:46 +0200

    ONM-3991: Create base and email campaing endpoint

---
+++ src/Common/External/ActOn/Endpoint/Endpoint.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\External\ActOn\Endpoint;
+
+class Endpoint
+{
+    /**
+     * The endpoint configuration.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Initializes the endpoint.
+     *
+     * @param array $config The endpoint configuration.
+     */
+    public function __construct($config = [])
+    {
+        if (!is_array($config)) {
+            return;
+        }
+
+        $this->config = array_merge($this->config, $config);
+    }
+
+    /**
+     * Checks if parameters are valid.
+     *
+     * @param array  $params The array of parameters.
+     * @param string $action The name of the action.
+     *
+     * @return boolean True parameters are valid. False otherwise.
+     */
+    public function areParametersValid($params, $action)
+    {
+        if (!array_key_exists($action, $this->config['actions'])
+            || !array_key_exists('parameters', $this->config['actions'][$action])
+            || !is_array($params)
+        ) {
+            return false;
+        }
+
+        $parameters = $this->config['actions'][$action]['parameters'];
+        $required   = [];
+        $invalid    = [];
+
+        if (array_key_exists('required', $parameters)) {
+            $required = array_diff($parameters['required'], array_keys($params));
+        }
+
+        if (array_key_exists('optional', $parameters)) {
+            $invalid = array_diff(
+                array_keys($params),
+                $parameters['required'],
+                $parameters['optional']
+            );
+        }
+
+        return empty($required) && empty($invalid);
+    }
+
+    /**
+     * Returns the endpoint configuration.
+     *
+     * @return array The endpoint configuration.
+     */
+    public function getConfiguration()
+    {
+        return $this->config;
+    }
+}
