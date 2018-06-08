905e30be8957bbc416c4913d7db896e5beea2088
Fran Dieguez <fran@openhost.es>
2018-06-08 12:29:57 +0200

    ONM-3898: Add script to remove custom keys

---
+++ db/scripts/2018/03-ONM-4099-checker.sql
@@ -0,0 +1,11 @@
+SELECT * FROM settings
+WHERE
+    `name` LIKE '%recaptcha%'
+    AND (
+        `value` LIKE 'a:2:{s:10:"public_key";s:0:'
+        OR `value` LIKE 'a:2:{s:10:"public_key";s:1:%'
+        OR `value` LIKE '%6LfuqxUUAAAAAHswLzeLd_oK8sNJWRifIssc0CPB%'
+        OR `value` LIKE '%6LfpY8ISAAAAAAuChcU2Agdwg8YzhprxZZ55B7Is%'
+        OR `value` LIKE '%6LeMYAkTAAAAADP0NncqOUIydMymokSI91gFxdJL%'
+        OR `value` LIKE '%6LdWlgkUAAAAADzgu34FyZ-wBSB0xlCUc7UVFWGw%'
+    )
