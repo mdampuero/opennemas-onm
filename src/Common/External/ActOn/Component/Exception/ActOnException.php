e114bd48232a465519a5f9ae357f5d279826fae6
Diego Blanco <diego@openhost.es>
2018-06-13 18:58:13 +0200

    ONM-3991: Create an exception for Act-On

---
+++ src/Common/External/ActOn/Component/Exception/ActOnException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\External\ActOn\Component\Exception;
+
+class ActOnException extends \Exception
+{
+}
