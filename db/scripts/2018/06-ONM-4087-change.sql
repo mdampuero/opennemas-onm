e36cba278dee0c2d0c2dc98926c8aaa0bfc0d831
Fran Dieguez <fran@openhost.es>
2018-06-16 20:25:56 +0200

    ONM-4087: Update newsletter migration script

--- db/scripts/2018/06-ONM-4087-change.sql
+++ db/scripts/2018/06-ONM-4087-change.sql
@@ -6,14 +6,14 @@ ALTER TABLE `newsletters` ADD `status` INT DEFAULT 0 NOT NULL AFTER `type`;
 ALTER TABLE `newsletters` CHANGE `title` `title` VARCHAR(512) NOT NULL;
 ALTER TABLE `newsletters` CHANGE `data` `contents` TEXT DEFAULT NULL;
 ALTER TABLE `newsletters` ADD `recipients` TEXT DEFAULT NULL AFTER `contents`;
-ALTER TABLE `newsletters` CHANGE `html` `generated_html` MEDIUMTEXT DEFAULT NULL;
+ALTER TABLE `newsletters` CHANGE `html` `html` MEDIUMTEXT DEFAULT NULL;
 ALTER TABLE `newsletters` ADD `schedule` TEXT DEFAULT NULL AFTER `recipients`;
 ALTER TABLE `newsletters` CHANGE `updated` `updated` DATETIME DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `newsletters` CHANGE `sent` `sends` INT DEFAULT 0 NOT NULL;
-ALTER TABLE `newsletters` ADD `send_date` DATETIME DEFAULT CURRENT_TIMESTAMP AFTER `sends`;
+ALTER TABLE `newsletters` CHANGE `sent` `sent_items` INT DEFAULT 0 NOT NULL;
+ALTER TABLE `newsletters` ADD `send_date` DATETIME DEFAULT CURRENT_TIMESTAMP AFTER `sent_items`;
 ALTER TABLE `newsletters` ADD `template_id` BIGINT UNSIGNED DEFAULT NULL;
 
 CREATE INDEX newsletter_type ON newsletters (type);
 CREATE INDEX newsletter_template_id ON newsletters (template_id);
 
-UPDATE newsletters SET `send_date`=`updated` WHERE `sends` > 0
+UPDATE newsletters SET `send_date`=`updated` WHERE `sends` > 0;
