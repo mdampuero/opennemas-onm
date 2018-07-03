ccae7c83286e13c6cb68621cfcf8ab88a1ea7aa4
Fran Dieguez <fran@openhost.es>
2018-07-03 13:31:47 +0000

    ONM-4281: Add missing semicolon in newsletter schedule migration SQL

--- db/scripts/2018/08-ONM-4281-change.sql
+++ db/scripts/2018/08-ONM-4281-change.sql
@@ -1,4 +1,4 @@
-SET names 'utf8'
+SET names 'utf8';
 INSERT INTO `extension` (`id`, `uuid`, `name`, `type`, `author`, `description`, `about`, `images`, `created`, `updated`, `enabled`, `url`) VALUES
 (79, 'es.openhost.module.newsletter_scheduling', 'a:3:{s:2:\"en\";s:21:\"Newsletter scheduling\";s:2:\"es\";s:26:\"ProgramaciÃ³n de boletines\";s:2:\"gl\";s:26:\"ProgramaciÃ³n de boletÃ­ns\";}', 'module', 'OpenHost S.L.', 'a:3:{s:2:\"en\";s:0:\"\";s:2:\"es\";s:0:\"\";s:2:\"gl\";s:0:\"\";}', 'a:3:{s:2:\"en\";s:0:\"\";s:2:\"es\";s:0:\"\";s:2:\"gl\";s:0:\"\";}', NULL, '2018-06-15 21:22:33', '2018-07-02 07:25:11', 0, 'http://www.openhost.es');
 INSERT INTO `extension_meta` (`extension_id`, `meta_key`, `meta_value`) VALUES
