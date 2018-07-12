37d561775b6bafee7c05f38587a003ed445c305a
Adrián Reimunde <adrianrep@openhost.es>
2018-07-12 14:13:23 +0200

    ONM-4229: Fix render frontpagemanager in multilanguage

--- src/Backend/Controller/FrontpagesController.php
+++ src/Backend/Controller/FrontpagesController.php
@@ -66,6 +66,7 @@ class FrontpagesController extends Controller
 
         $views = $this->get('content_views_repository')->getViews(array_keys($contents));
 
+        $this->get('core.locale')->setContext('frontend');
         $layout = $lm->render([
             'contents'             => $contents,
             'home'                 => ($categoryId == 0),
@@ -74,6 +75,7 @@ class FrontpagesController extends Controller
             'contentPositionByPos' => $contentPositionByPos,
             'contents'             => $contents
         ]);
+        $this->get('core.locale')->setContext('backend');
 
         $layouts = $this->container->get('core.manager.layout')->getLayouts();
 
