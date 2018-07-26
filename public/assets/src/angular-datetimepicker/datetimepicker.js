c5137005fbe10d5b4135a3b9286b2f1a39246b27
Adrián Reimunde <adrianrep@openhost.es>
2018-07-26 17:56:28 +0200

    ONM-4272: Translate calendar in opennemas

--- public/assets/src/angular-datetimepicker/datetimepicker.js
+++ public/assets/src/angular-datetimepicker/datetimepicker.js
@@ -1,7 +1,8 @@
-(function () {
+(function() {
   'use strict';
 
   angular.module('onm.datetimepicker', [])
+
     /**
      * @ngdoc directive
      * @name  datetime-picker
@@ -23,17 +24,18 @@
      * <!-- Datetime picker with format -->
      * <input datetime-picker="YY-MM-DD" ng-model="date">
      */
-    .directive('datetimePicker', [ '$timeout', '$window',
-      function ($timeout, $window) {
+    .directive('datetimePicker', [
+      '$timeout', '$window',
+      function($timeout, $window) {
         return {
           restrict: 'A',
           scope: {
-            'ngModel': '=',
-            'datetimePicker': '=',
-            'datetimePickerMin': '=?',
-            'datetimePickerMax': '=?'
+            ngModel: '=',
+            datetimePicker: '=',
+            datetimePickerMin: '=?',
+            datetimePickerMax: '=?'
           },
-          link: function ($scope, element, $attrs) {
+          link: function($scope, element, $attrs) {
             var config = {
               format:     $attrs.datetimePickerFormat || 'YYYY-MM-DD HH:mm:ss',
               useCurrent: $attrs.datetimePickerUseCurrent === 'true'
@@ -51,6 +53,26 @@
               config.maxDate = $scope.datetimePickerMax;
             }
 
+            var htmlEle = document.getElementsByTagName('HTML');
+
+            if (
+              htmlEle &&
+              htmlEle.length === 1 &&
+              htmlEle[0].attributes &&
+              htmlEle[0].attributes.length > 0
+            ) {
+              var attributes = htmlEle[0].attributes;
+
+              for (var i = 0; i < attributes.length; i++) {
+                if (attributes[i].name === 'lang') {
+                  config.locale = moment.locales().includes(attributes[i].value) ?
+                    attributes[i].value :
+                    'en';
+                  break;
+                }
+              }
+            }
+
             element.datetimepicker(config);
 
             var picker = element.data('DateTimePicker');
@@ -65,24 +87,25 @@
             }
 
             element.on('dp.change', function() {
-                $scope.ngModel = null;
+              $scope.ngModel = null;
 
-                if (picker.date()) {
-                  $timeout(function() {
-                    var date = $window.moment(picker.date());
-                    $scope.ngModel = date.format(config.format);
-                  });
-                }
+              if (picker.date()) {
+                $timeout(function() {
+                  var date = $window.moment(picker.date());
+
+                  $scope.ngModel = date.format(config.format);
+                });
+              }
             });
 
-            $scope.$watch('datetimePickerMin', function (nv) {
+            $scope.$watch('datetimePickerMin', function(nv) {
               if ($window.moment(nv, config.format, true).isValid() &&
                   $attrs.datetimePickerMin) {
                 element.data('DateTimePicker').minDate(nv);
               }
             }, true);
 
-            $scope.$watch('datetimePickerMax', function (nv) {
+            $scope.$watch('datetimePickerMax', function(nv) {
               if ($window.moment(nv, config.format, true).isValid() &&
                   $attrs.datetimePickerMax) {
                 element.data('DateTimePicker').maxDate(nv);
