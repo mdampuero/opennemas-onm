44a75f0968bddad3cd79a8f0e4335b7f2c46e446
Fran Dieguez <fran@openhost.es>
2018-09-14 00:54:25 +0200

    ONM-649: Fix JS syntax in browser_update

--- libs/smarty-onm-plugins/function.browser_update.php
+++ libs/smarty-onm-plugins/function.browser_update.php
@@ -8,10 +8,12 @@
 function smarty_function_browser_update($params, &$smarty)
 {
     $output = "<script>
-var \$buoop = {vs:{i:9,f:3.5,o:10.6,s:4,n:9}}
+var \$buoop = {vs:{i:9,f:3.5,o:10.6,s:4,n:9}};
+
 \$buoop.ol = window.onload;
 window.onload=function(){
- try {if (\$buoop.ol) \$buoop.ol();}catch (e) {}
+ try {if (\$buoop.ol) \$buoop.ol();}catch (e) {};
+ 
  var e = document.createElement('script');
  e.setAttribute('src', ('https:' == document.location.protocol ? 'https://' : 'http://') +'browser-update.org/update.js');
  document.body.appendChild(e);
