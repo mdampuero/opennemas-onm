29952257459428e35070ab8e53710b64d57fdd00
Fran Dieguez <fran@openhost.es>
2018-09-28 09:22:26 +0200

    ONM-4446: Add function.renderbanner.php to be used in the future

---
+++ libs/smarty-onm-plugins/function.renderbanner.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Smarty plugin for rendering a banner for a position.
+ *
+ * @param array    $params The list of parameters.
+ * @param Template $tpl    The template object.
+ */
+function smarty_function_renderbanner($params, $smarty)
+{
+    $safeframeEnabled = $smarty->getContainer()
+        ->get('orm.manager')
+        ->getDataSet('Settings', 'instance')
+        ->get('ads_settings')['safe_frame'];
+
+    $tpl    = '<div class="ad-slot oat%s">%s</div>';
+    $class  = '" data-type="' . $params['type'];
+    $format = 'safeframe';
+
+    // Filter advertisement by type/position
+    if (array_key_exists('ads_positions', $smarty->tpl_vars)
+        && !in_array($params['type'], $smarty->tpl_vars['ads_positions']->value)) {
+        return '';
+    }
+
+    if (array_key_exists('render_params', $smarty->tpl_vars)
+        && array_key_exists('ads-format', $smarty->tpl_vars['render_params']->value)
+        && !empty($smarty->tpl_vars['render_params']->value['ads-format'])
+    ) {
+        $format = $smarty->tpl_vars['render_params']->value['ads-format'];
+    }
+
+    if (array_key_exists('format', $params) && !empty($params['format'])) {
+        $format = $params['format'];
+    }
+
+    if ($safeframeEnabled && $format === 'safeframe') {
+        return sprintf($tpl, $class, '');
+    }
+
+    $ads  = $smarty->tpl_vars['advertisements']->value;
+    $type = $params['type'];
+
+    if (!is_array($ads)) {
+        return '';
+    }
+
+    $ads = array_filter($ads, function ($ad) use ($type) {
+        return is_array($ad->positions)
+            && in_array($type, $ad->positions)
+            && $ad->isInTime();
+    });
+
+    if (empty($ads)) {
+        return '';
+    }
+
+    $ad = $ads[array_rand($ads)];
+
+    $renderer    = $smarty->getContainer()->get('core.renderer.advertisement');
+    $orientation = empty($ad->params['orientation']) ?
+        'top' : $ad->params['orientation'];
+
+    $content = $renderer->renderInline($ad, $format);
+
+    $class = ' oat-visible oat-' . $orientation . ' ' . $renderer->getDeviceCssClasses($ad);
+    $mark  = $renderer->getMark($ad);
+    if (!empty($mark)) {
+        $class .= '" data-mark="' . $mark . '';
+    }
+
+    return sprintf($tpl, $class, $content);
+}
