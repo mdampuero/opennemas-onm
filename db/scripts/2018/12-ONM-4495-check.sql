fca826870c3eabf9e36fb268a86182354cf78f3a
Diego Blanco <diego@openhost.es>
2018-10-03 15:21:20 +0200

    ONM-4495: Fix SQLs in check.sql

--- db/scripts/2018/12-ONM-4495-check.sql
+++ db/scripts/2018/12-ONM-4495-check.sql
@@ -1,2 +1,2 @@
-SELECT `uuid` FROM `extension` WHERE `id` = 80;
-SELECT `extension_id` FROM `extension_meta` WHERE `extension_id` = 80;
+SELECT CONCAT('extension: ', COUNT(*)) FROM `extension` WHERE `id` = 80;
+SELECT CONCAT('extension_meta: ', COUNT(*)) FROM `extension_meta` WHERE `extension_id` = 80;
