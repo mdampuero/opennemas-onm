6930d8247ca400d1af5d1f9f851b248c03d3e25b
Diego Blanco <diego@openhost.es>
2018-10-04 19:18:52 +0200

    ONM-4550: Prevent infinite refresh for refresh_interval setting

--- libs/smarty-onm-plugins/function.setting.php
+++ libs/smarty-onm-plugins/function.setting.php
@@ -4,23 +4,27 @@
  *
  * Usage:
  *   {setting name=key [field=subkey]}
- *
-*/
+ */
 function smarty_function_setting($params, &$smarty)
 {
-    $request = $smarty->getContainer()->get('request_stack')
-        ->getCurrentRequest();
-
-    if (!array_key_exists('name', $params)
-        || ($params['name'] === 'refresh_interval'
-            && !empty($request)
-            && preg_match('@/admin@', $request->getRequestUri()))
-    ) {
+    if (!array_key_exists('name', $params)) {
         return '';
     }
 
-    $ds    = $smarty->getContainer()->get('orm.manager')->getDataSet('Settings');
-    $value = $ds->get($params['name']);
+    $request = $smarty->getContainer()->get('request_stack')
+        ->getCurrentRequest();
+
+    if ($params['name'] === 'refresh_interval'
+        && !empty($request)
+        && preg_match('@^/admin@', $request->getRequestUri())
+    ) {
+        return -1;
+    }
+
+    $value = $smarty->getContainer()
+        ->get('orm.manager')
+        ->getDataSet('Settings', 'instance')
+        ->get($params['name']);
 
     if (array_key_exists('field', $params)) {
         if (is_array($value) && array_key_exists($params['field'], $value)) {
