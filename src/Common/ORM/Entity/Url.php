d9f82281f9c8b4141cf56c210d132e7bda7a9ed6
Diego Blanco <diego@openhost.es>
2018-10-18 20:16:40 +0200

    ONM-4525: Create class and configuration for Url entity

---
+++ src/Common/ORM/Entity/Url.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\ORM\Entity;
+
+use Common\ORM\Core\Entity;
+
+class Url extends Entity
+{
+}
