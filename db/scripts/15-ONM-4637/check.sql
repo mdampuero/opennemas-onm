415da6e073d287db375e7abf2705cae620fe2985
Fran Dieguez <fran@openhost.es>
2018-11-13 10:06:53 +0100

    ONM-4637: Fix the check sql to create the events module

--- db/scripts/15-ONM-4637/check.sql
+++ db/scripts/15-ONM-4637/check.sql
@@ -1,2 +1,2 @@
 SELECT CONCAT('extension: ', count(*)) FROM `extension` WHERE `id` = 82;
-SELECT CONCAT('extension_meta: ', count(*)) FROM `extension_meta` WHERE `extension_id` = 82 AND `meta_value` LIKE '%es.openhost.module.events%';
+SELECT CONCAT('extension_meta: ', count(*)) FROM `extension_meta` WHERE `extension_id` = 82;
