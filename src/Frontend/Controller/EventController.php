c4baae4b3ad664e9cea9476805e6ced42d66e3b1
Fran Dieguez <fran@openhost.es>
2018-11-15 17:29:42 +0100

    ONM-4637: Add Eventcontroller::frontend action

--- src/Frontend/Controller/EventController.php
+++ src/Frontend/Controller/EventController.php
@@ -12,12 +12,82 @@ namespace Frontend\Controller;
 use Common\Core\Controller\Controller;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Displays static pages.
  */
 class EventController extends Controller
 {
+    /**
+     * Displays the list of the latest events.
+     *
+     * @return Response The response object.
+     */
+    public function frontpageAction(Request $request)
+    {
+        $page  = $request->get('page', 1);
+        $date  = date('Y-m-d H:i:s');
+        $epp   = $this->get('orm.manager')
+            ->getDataSet('Settings', 'instance')
+            ->get('items_per_page');
+        $offset = ($page <= 2) ? 0 : ($page-1) * $epp;
+
+        $eventIds = $this->get('orm.manager')->getConnection('instance')
+            ->executeQuery(
+                "SELECT SQL_CALC_FOUND_ROWS DISTINCT pk_content, contentmeta.meta_value as event_startdate "
+                . "FROM contents join contentmeta "
+                . "ON contentmeta.meta_name = 'event_startdate' "
+                . "AND contents.pk_content = contentmeta.fk_content "
+                . "WHERE fk_content_type = 19 AND content_status = 1 and in_litter = 0 "
+                . "AND (starttime = '0000-00-00 00:00:00' OR starttime IS NULL OR starttime <= ? ) "
+                . "AND (endtime IS NULL OR endtime = '0000-00-00 00:00:00' OR endtime > ?) "
+                . " ORDER BY event_startdate DESC LIMIT ? OFFSET ?",
+                [ $date, $date, $epp, $offset ]
+            )
+            ->fetchAll();
+
+        $sql = 'SELECT FOUND_ROWS()';
+
+        $total = $this->get('dbal_connection')->fetchAssoc($sql);
+        $total = array_pop($total);
+
+
+
+        $eventIds = array_map(function ($event) {
+            return $event['pk_content'];
+        }, $eventIds);
+
+        $contents = $this->get('api.service.content')
+            ->getListByIds($eventIds);
+
+        list($positions, $advertisements) = $this->getAds();
+
+        $pagination = $this->get('paginator')->get([
+            'directional' => true,
+            'epp'         => 5,
+            'page'        => $page,
+            'total'       => $total,
+            'route'       => 'frontend_events_frontpage'
+        ]);
+
+
+        // Setup templating cache layer
+        $this->view->setConfig('articles');
+        $cacheID = $this->view->getCacheId('event', 'frontpage', $page);
+
+        return $this->render('event/frontpage.tpl', [
+            'ads_positions'      => $positions,
+            'advertisements'     => $advertisements,
+            'contents'           => $contents['items'],
+            'page'               => $page,
+            'cache_id'           => $cacheID,
+            'pagination'         => $pagination,
+            'x-tags'             => 'event-frontpage',
+        ]);
+    }
+
+
     /**
      * Displays an event.
      *
@@ -53,7 +123,7 @@ class EventController extends Controller
         $cacheID = $this->view->getCacheId('content', $content->id);
 
         list($positions, $advertisements) = $this->getAds();
-        return $this->render('static_pages/statics.tpl', [
+        return $this->render('event/show.tpl', [
             'ads_positions'      => $positions,
             'advertisements'     => $advertisements,
             'category_real_name' => $content->title,
