b22ab0c871edd2551b04e009a90a16779e07112e
Diego Blanco <diego@openhost.es>
2018-11-29 17:38:41 +0100

    ONM-4753: Fix ? meaning and update tests

--- src/Common/Core/Component/Routing/Redirector.php
+++ src/Common/Core/Component/Routing/Redirector.php
@@ -338,11 +338,7 @@ class Redirector
         }
 
         foreach ($urls['items'] as $url) {
-            $pattern = preg_replace(
-                [ '/\//', '/\?/'],
-                [ '\\/', '\\?' ],
-                $url->source
-            );
+            $pattern = preg_replace([ '/\//' ], [ '\\/' ], $url->source);
 
             if (preg_match('/' . $pattern . '/', $uri)) {
                 return $url;
@@ -396,11 +392,7 @@ class Redirector
         $uri = trim($request->getRequestUri(), '/');
         $uri = $this->replaceInternalVariables($uri);
 
-        $pattern = preg_replace(
-            [ '/\//', '/\?/'],
-            [ '\\/', '\\?' ],
-            $url->source
-        );
+        $pattern = preg_replace([ '/\//' ], [ '\\/' ], $url->source);
 
         preg_match_all(
             '/' . $pattern . '/',
