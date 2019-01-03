e616916eb920205dd1f93a772f1a3bfc07b3e16d
Diego Blanco <diego@openhost.es>
2019-01-03 15:45:30 +0100

    ONM-4884: Check property before converting

--- public/themes/admin/js/controllers/NewsletterTemplateCtrl.js
+++ public/themes/admin/js/controllers/NewsletterTemplateCtrl.js
@@ -91,7 +91,9 @@
 
           // Force integer values in id property
           for (var i = 0; i < data.item.recipients.length; i++) {
-            data.item.recipients[i].id = data.item.recipients[i].id.toString();
+            if (data.item.recipients[i].id) {
+              data.item.recipients[i].id = data.item.recipients[i].id.toString();
+            }
 
             // TODO: Remove when not subscribers stored in settings in production
             delete data.item.recipients[i].subscribers;
