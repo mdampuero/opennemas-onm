827131d4098d402a0e1b47f5c841d80e92b6e26d
Alex Rodrigues <alex@openhost.es>
2019-01-25 11:35:19 +0100

    ONM-4963: Rename CommentHelper::isEmailRequired function

--- src/Frontend/Controller/CommentsController.php
+++ src/Frontend/Controller/CommentsController.php
@@ -64,7 +64,7 @@ class CommentsController extends Controller
             'comments'       => $comments,
             'contentId'      => $contentID,
             'elems_per_page' => $elemsByPage,
-            'required_email' => $this->get('core.helper.comment')->requiredEmail(),
+            'required_email' => $this->get('core.helper.comment')->isEmailRequired(),
             'offset'         => $offset,
             'dark_theme'     => $darkTheme,
             'count'          => $total,
@@ -264,7 +264,7 @@ class CommentsController extends Controller
                     $errorType = $errors['type'];
                     $httpCode  = 400;
 
-                    $handling = ($cm->autoReject() || $cm->requiredEmail())
+                    $handling = ($cm->autoReject() || $cm->isEmailRequired())
                         ? _('Your comment was rejected due to:')
                         : _('Your comment is waiting for moderation due to:');
 
