3f7a41976da63ef1e3387c9b87e7203dbb89cf22
Diego Blanco <diego@openhost.es>
2019-02-07 21:33:21 +0100

    ONM-4969: Update CategoryCtrl

--- public/themes/admin/js/controllers/CategoryCtrl.js
+++ public/themes/admin/js/controllers/CategoryCtrl.js
@@ -2,178 +2,78 @@
   'use strict';
 
   angular.module('BackendApp.controllers')
+
     /**
      * @ngdoc controller
      * @name  SettingsCtrl
      *
      * @requires $controller
-     * @requires $rootScope
      * @requires $scope
      *
      * @description
      *   Handles actions for category edit form.
      */
     .controller('CategoryCtrl', [
-      '$controller', '$rootScope', '$scope', 'http', 'messenger', 'routing',
-      function($controller, $rootScope, $scope, http, messenger, routing) {
+      '$controller', '$scope', '$window', 'http',
+      function($controller, $scope, $window, http) {
         // Initialize the super class and extend it.
-        $.extend(this, $controller('InnerCtrl', { $scope: $scope }));
+        $.extend(this, $controller('RestInnerCtrl', { $scope: $scope }));
 
         /**
-         * @memberOf SettingsCtrl
-         *
-         * @description
-         *  The settings object with default values.
-         *
-         * @type {Object}
+         * @inheritdoc
          */
-        $scope.category = {
-          title: {},
-          internal_category: 1
-        };
-
-        $scope.subcategories = [];
-
-        /**
-         * @function init
-         * @memberOf SettingsCtrl
-         *
-         * @description
-         *   Initizalice the class for .
-         */
-        $scope.init = function(category, extraData, multilanguageEnable, languageData) {
-          $scope.loading = true;
-          $scope.inmenu = false;
-
-          $scope.languageData = languageData;
-
-          $scope.category            = 'id' in category ? category : $scope.category;
-          $scope.subcategories       = extraData.subcategories;
-          $scope.categories          = extraData.categories;
-          $scope.modules             = extraData.modules;
-          $scope.configurations      = extraData.configurations;
-          $scope.categoryUrl         = extraData.image_path + '/sections/' + $scope.category.logo_path;
-          $scope.multilanguageEnable = multilanguageEnable;
-
-          $scope.pre();
-          $scope.loading = false;
-
-          // TODO implement the ajax request for category info
+        $scope.item = {
+          fk_content_category: null
         };
 
         /**
-         * @function save
-         * @memberOf SettingsCtrl
-         *
-         * @description
-         *   Saves settings.
+         * @inheritdoc
          */
-        $scope.save = function() {
-          $scope.preSave();
-          var data = $scope.category;
-
-          $scope.saving = true;
-
-          http.put('backend_ws_category_save', data)
-            .then(function(response) {
-              $scope.saving = false;
-
-              if ($scope.category.internal_category === 0) {
-                $scope.category.internal_category = -1;
-              }
-
-              var reload = response.status === 201 && (
-                !$scope.category.id ||
-                $scope.category.id === '' ||
-                $scope.category.id === null
-              );
-
-              $scope.category.id = response.data.category;
-
-              messenger.post(response.data.message);
-
-              if (reload) {
-                document.location = routing.generate('backend_category_show', { id: $scope.category.id });
-              }
-            }, function(response) {
-              $scope.saving = false;
-
-              if ($scope.category.internal_category === 0) {
-                $scope.category.internal_category = -1;
-              }
-              messenger.post(response.data);
-            });
+        $scope.routes = {
+          create:   'api_v1_backend_category_create',
+          redirect: 'backend_category_show',
+          save:     'api_v1_backend_category_save',
+          show:     'api_v1_backend_category_show',
+          update:   'api_v1_backend_category_update'
         };
 
         /**
-         * Precalculation of params needed
+         * @inheritdoc
          */
-        $scope.pre = function() {
-          $scope.languageData = $scope.languageData || { available: [ 'default' ], locale: 'default' };
+        $scope.getItemId = function() {
+          return $scope.item.pk_content_category;
+        };
 
-          // Initialize all the languages
-          Object.keys($scope.languageData.available).forEach(function(langAux) {
-            if (!$scope.category.title[langAux]) {
-              $scope.category.title[langAux] = '';
-            }
+        /**
+         * @inheritdoc
+         */
+        $scope.itemHasId = function() {
+          return $scope.item.pk_content_category &&
+            $scope.item.pk_content_category !== null;
+        };
+
+        /**
+         * @inheritdoc
+         */
+        $scope.parseItem = function(data) {
+          http.get('api_v1_backend_categories_list').then(function(response) {
+            data.extra.categories = response.data.items;
           });
 
-          if ($scope.category.internal_category === 0) {
-            $scope.category.internal_category = -1;
-          }
-
-          $scope.lang = $scope.languageData.locale || $scope.languageData.default;
-
-          if (!$scope.category.internal_category) {
-            $scope.category.internal_category = $scope.allowedCategories[0].code;
-          }
-          $scope.category.internal_category = $scope.category.internal_category.toString();
-
-          $scope.subsectionCategories = [];
-          for (var key in $scope.categories) {
-            if ($scope.category.id !== $scope.categories[key].id && $scope.categories[key].internal_category === 1) {
-              $scope.subsectionCategories.push({
-                code: $scope.categories[key].id,
-                value: $scope.categories[key].title[$scope.lang]
-              });
-            }
-          }
-
-          $scope.multiLanguageFields = [ 'title' ];
-        };
-
-        /**
-         * Precalculation needed before save.
-         */
-        $scope.preSave = function() {
-          if ($scope.category.internal_category === -1) {
-            $scope.category.internal_category = 0;
+          if (data.item) {
+            $scope.item  = angular.extend($scope.item, data.item);
+            $scope.cover = $scope.item.logo_path.replace($window.instanceMedia, '');
           }
         };
 
-        /**
-         * This method load the slug text when the title value changes
-         */
-        $scope.loadSlug = function() {
-          if ($scope.category.id > 1 || undefined === $scope.category.title[$scope.lang]) {
+        // Updates the logo_path when an image is selected
+        $scope.$watch('cover', function(nv) {
+          if (nv && !angular.isObject(nv)) {
             return;
           }
 
-          $scope.getSlug($scope.category.title[$scope.lang], function(response) {
-            if (response.data.slug !== undefined && $scope.category.name === undefined) {
-              $scope.category.name = response.data.slug;
-            }
-          });
-        };
-
-        $scope.internalCategoriesImgs = {
-          7:  'fa-stack-overflow',
-          9:  'fa-film',
-          11: 'fa-pie-chart',
-          10: 'fa-star',
-          14: 'fa-newspaper-o',
-          15: 'fa-book',
-        };
+          $scope.item.logo_path = nv ? nv.path_img : null;
+        }, true);
       }
     ]);
 })();
