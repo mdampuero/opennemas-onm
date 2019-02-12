f002a62453e1cfff39d3e6b03afbb473d27d7ea9
Diego Blanco <diego@openhost.es>
2019-02-12 20:50:15 +0100

    ONM-4970: Create a mapper for l10n_string type

---
+++ src/Common/ORM/Core/Data/Mapper/L10nStringDataMapper.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\ORM\Core\Data\Mapper;
+
+use Common\Data\Serialize\Serializer\PhpSerializer;
+
+class L10nStringDataMapper
+{
+    /**
+     * Converts an array to a l10n_string.
+     *
+     * @param array $value The array to convert.
+     *
+     * @return string The converted l10n_string value.
+     */
+    public function fromArray($value)
+    {
+        return empty($value) ? null : $value;
+    }
+
+    /**
+     * Converts l10n_string to an array.
+     *
+     * @return string The l10n_string to convert.
+     *
+     * @return array The coverted array.
+     */
+    public function fromString($value)
+    {
+        if (PhpSerializer::isSerialized($value)) {
+            return PhpSerializer::unserialize($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Converts l10n_string to an array.
+     *
+     * @return string The l10n_string to convert.
+     *
+     * @return array The coverted array.
+     */
+    public function toArray($value)
+    {
+        return $this->fromString($value);
+    }
+
+    /**
+     * Converts an array to a l10n_string.
+     *
+     * @param array $value The array to convert.
+     *
+     * @return string The converted l10n_string value.
+     */
+    public function toString($value)
+    {
+        if (is_array($value)) {
+            return PhpSerializer::serialize($value);
+        }
+
+        return $value;
+    }
+}
