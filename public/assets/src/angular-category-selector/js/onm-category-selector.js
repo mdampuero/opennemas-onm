1ae44e4fd3e56ca414255308b613735e81c12521
Diego Blanco <diego@openhost.es>
2019-02-13 20:54:41 +0100

    ONM-4969: Update directive to prevent errors when linking categories

--- public/assets/src/angular-category-selector/js/onm-category-selector.js
+++ public/assets/src/angular-category-selector/js/onm-category-selector.js
@@ -28,7 +28,7 @@
                 '  <strong ng-if="labelText">[% labelText %]: </strong>[% $select.selected.title %]' +
                 '</ui-select-match>' +
                 '<ui-select-choices group-by="groupCategories" ' +
-                  'repeat="item.pk_content_category as item in (loadCategories()| filter: { title: $select.search }) track by item.pk_content_category">' +
+                  'repeat="item.pk_content_category as item in (categories | filter: { title: $select.search }) track by item.pk_content_category">' +
                 '  <div ng-bind-html="item.title | highlight: $select.search"></div>' +
                 '</ui-select-choices>' +
               '</ui-select>';
@@ -37,29 +37,14 @@
             $scope.cssClass = $attrs.class ? $attrs.class : '';
             $scope.required = $attrs.required ? $attrs.required : false;
 
-            /**
-             * @function loadCategories
-             * @memberOf onmCategorySelector
-             *
-             * @description
-             *   Returns the list of categories adding the default at the top
-             *
-             * @return {Array} The list of categories.
-             */
-            $scope.loadCategories = function() {
-              if (!angular.isArray($scope.categories) || $scope.categories.length === 0) {
-                return [];
-              }
-
-              if ($scope.categories[0].pk_content_category !== null) {
-                $scope.categories.unshift({
-                  pk_content_category: null,
-                  title: $scope.defaultValueText
-                });
-              }
-
-              return $scope.categories;
-            };
+            if (angular.isArray($scope.categories) &&
+                $scope.categories.length > 0 &&
+                $scope.categories[0].pk_content_category !== null) {
+              $scope.categories.unshift({
+                pk_content_category: null,
+                title: $scope.defaultValueText
+              });
+            }
 
             /**
              * @function groupCategories
