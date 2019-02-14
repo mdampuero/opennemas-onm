289afae17201c8c2adda54c72594979e6121bfd5
Diego Blanco <diego@openhost.es>
2019-02-14 17:48:45 +0100

    ONM-4969: Update onm-category-selector to get, localize and exclude categories

--- public/assets/src/angular-category-selector/js/onm-category-selector.js
+++ public/assets/src/angular-category-selector/js/onm-category-selector.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('onm.categorySelector', [ 'ui.select' ])
+  angular.module('onm.categorySelector', [ 'onm.http', 'onm.localize', 'ui.select' ])
 
     /**
      * @ngdoc directive
@@ -11,13 +11,15 @@
      *   Directive to create category selector dynamically.
      */
     .directive('onmCategorySelector', [
-      function() {
+      'http', 'localizer', 'linker',
+      function(http, localizer, linker) {
         return {
           restrict: 'E',
           transclude: true,
           scope: {
-            ngModel:    '=',
-            categories: '=',
+            ngModel: '=',
+            locale: '=',
+            exclude: '=',
             labelText: '@',
             placeholder: '@',
             defaultValueText: '@'
@@ -28,7 +30,7 @@
                 '  <strong ng-if="labelText">[% labelText %]: </strong>[% $select.selected.title %]' +
                 '</ui-select-match>' +
                 '<ui-select-choices group-by="groupCategories" ' +
-                  'repeat="item.pk_content_category as item in (categories | filter: { title: $select.search }) track by item.pk_content_category">' +
+                  'repeat="item.pk_content_category as item in (categories | filter: { title: $select.search })">' +
                 '  <div ng-bind-html="item.title | highlight: $select.search"></div>' +
                 '</ui-select-choices>' +
               '</ui-select>';
@@ -37,14 +39,47 @@
             $scope.cssClass = $attrs.class ? $attrs.class : '';
             $scope.required = $attrs.required ? $attrs.required : false;
 
-            if (angular.isArray($scope.categories) &&
-                $scope.categories.length > 0 &&
-                $scope.categories[0].pk_content_category !== null) {
-              $scope.categories.unshift({
-                pk_content_category: null,
-                title: $scope.defaultValueText
+            http.get('api_v1_backend_categories_list').then(function(response) {
+              response.data.items = response.data.items.filter(function(e) {
+                return !$scope.exclude ||
+                  $scope.exclude.indexOf(e.pk_content_category) === -1;
               });
-            }
+
+              if (angular.isArray(response.data.items) &&
+                  response.data.items.length > 0 &&
+                  response.data.items[0].pk_content_category !== null) {
+                response.data.items.unshift({
+                  pk_content_category: null,
+                  title: $scope.defaultValueText
+                });
+              }
+
+              var lz = localizer.get(response.data.extra.locale);
+
+              // Localize items
+              $scope.categories = lz.localize(response.data.items,
+                response.data.extra.keys, response.data.extra.locale);
+
+              // Initialize linker
+              if (!$scope.linker) {
+                $scope.linker = linker.get(response.data.extra.keys,
+                  response.data.extra.locale.default, $scope);
+              }
+
+              // Link original and localized items
+              $scope.linker.setKey($scope.locale);
+              $scope.linker.link(response.data.items, $scope.categories);
+            });
+
+            // Updates linker when locale changes
+            $scope.$watch('locale', function(nv, ov) {
+              if (nv === ov || !$scope.linker) {
+                return;
+              }
+
+              $scope.linker.setKey(nv);
+              $scope.linker.update();
+            }, true);
 
             /**
              * @function groupCategories
