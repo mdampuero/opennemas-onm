cb2deb7c683d96857798353a5656b13a2817b899
Diego Blanco <diego@openhost.es>
2019-03-20 18:42:51 +0100

    ONM-5143: Rename and sort functions

--- src/Api/Controller/V1/Backend/TagController.php
+++ src/Api/Controller/V1/Backend/TagController.php
@@ -49,6 +49,65 @@ class TagController extends ApiController
     protected $service = 'api.service.tag';
 
     /**
+     * Saves configuration for tags.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     */
+    public function saveConfigAction(Request $request)
+    {
+        $this->checkSecurity($this->extension, $this->getActionPermission('list'));
+
+        $settings = $request->request->all();
+
+        if (!array_key_exists('blacklist_tag', $settings)
+            || empty($settings['blacklist_tag'])
+        ) {
+            $settings['blacklist_tag'] = null;
+        }
+
+        $msg = $this->get('core.messenger');
+
+        try {
+            $this->get('orm.manager')->getDataSet('Settings')->set($settings);
+            $msg->add(_('Item saved successfully'), 'success');
+        } catch (\Exception $e) {
+            $msg->add(_('Unable to save settings'), 'error');
+            $this->get('error.log')->error($e->getMessage());
+        }
+
+        return new JsonResponse($msg->getMessages(), $msg->getCode());
+    }
+
+    /**
+     * Get the tag config.
+     *
+     * @param Request $request The request object.
+     *
+     * @return JsonResponse The response object.
+     */
+    public function showConfigAction()
+    {
+        $this->checkSecurity($this->extension, $this->getActionPermission('list'));
+
+        $settings = $this->get('orm.manager')
+            ->getDataSet('Settings')
+            ->get([ 'blacklist_tag', 'tags_maxItems', 'tags_maxResults' ]);
+
+        if (array_key_exists('tags_maxItems', $settings)) {
+            $settings['tags_maxItems'] = (int) $settings['tags_maxItems'];
+        }
+
+        if (array_key_exists('tags_maxResults', $settings)) {
+            $settings['tags_maxResults'] = (int) $settings['tags_maxResults'];
+        }
+
+        return new JsonResponse($settings);
+    }
+
+
+    /**
      * Checks if the information in the request is valid to create a new Tag.
      *
      * @param Request $request The request object.
@@ -75,64 +134,6 @@ class TagController extends ApiController
             $this->get('api.validator.tag')->validate($item);
         } catch (\Exception $e) {
             $msg->add($e->getMessage(), 'error', 400);
-        }
-
-        return new JsonResponse($msg->getMessages(), $msg->getCode());
-    }
-
-    /**
-     * Get the tag config.
-     *
-     * @param Request $request The request object.
-     *
-     * @return JsonResponse The response object.
-     */
-    public function showConfAction()
-    {
-        $this->checkSecurity($this->extension, $this->getActionPermission('list'));
-
-        $settings = $this->get('orm.manager')
-            ->getDataSet('Settings')
-            ->get([ 'blacklist_tag', 'tags_maxItems', 'tags_maxResults' ]);
-
-        if (array_key_exists('tags_maxItems', $settings)) {
-            $settings['tags_maxItems'] = (int) $settings['tags_maxItems'];
-        }
-
-        if (array_key_exists('tags_maxResults', $settings)) {
-            $settings['tags_maxResults'] = (int) $settings['tags_maxResults'];
-        }
-
-        return new JsonResponse($settings);
-    }
-
-    /**
-     * Update tag config.
-     *
-     * @param Request $request The request object.
-     *
-     * @return JsonResponse The response object.
-     */
-    public function updateConfAction(Request $request)
-    {
-        $this->checkSecurity($this->extension, $this->getActionPermission('list'));
-
-        $settings = $request->request->all();
-
-        if (!array_key_exists('blacklist_tag', $settings)
-            || empty($settings['blacklist_tag'])
-        ) {
-            $settings['blacklist_tag'] = null;
-        }
-
-        $msg = $this->get('core.messenger');
-
-        try {
-            $this->get('orm.manager')->getDataSet('Settings')->set($settings);
-            $msg->add(_('Item saved successfully'), 'success');
-        } catch (\Exception $e) {
-            $msg->add(_('Unable to save settings'), 'error');
-            $this->get('error.log')->error($e->getMessage());
         }
 
         return new JsonResponse($msg->getMessages(), $msg->getCode());
