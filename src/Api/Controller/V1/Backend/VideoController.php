1a52feab8bcee18ec8d732dfb194f27b6ee23c79
Fran Dieguez <fran@openhost.es>
2019-03-26 23:15:04 +0100

    ONM-5141: Implement videos api backend controller

---
+++ src/Api/Controller/V1/Backend/VideoController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Api\Controller\V1\Backend;
+
+class VideoController extends ContentOldController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $extension = 'VIDEO_MANAGER';
+
+    /**
+     * The route name to generate URL from when creating a new item.
+     *
+     * @var string
+     */
+    protected $getItemRoute = 'api_v1_backend_video_show';
+
+    /**
+     * Returns the list of l10n keys
+     * @param Type $var Description
+     *
+     * @return array
+     **/
+    public function getL10nKeys()
+    {
+        return $this->get($this->service)->getL10nKeys('video');
+    }
+
+    /**
+     * Returns the list of contents related with items.
+     *
+     * @param Content $content The content.
+     *
+     * @return array The list of photos linked to the content.
+     */
+    protected function getRelatedContents($content)
+    {
+        $em    = $this->get('entity_repository');
+        $extra = [];
+
+        if (empty($content)) {
+            return $extra;
+        }
+
+        if (is_object($content)) {
+            $content = [ $content ];
+        }
+
+        foreach ($content as $element) {
+            foreach (['img1', 'img2'] as $relation) {
+                if (!empty($element->{$relation})) {
+                    $photo = $em->find('Photo', $element->{$relation});
+
+                    $extra[] = \Onm\StringUtils::convertToUtf8($photo);
+                }
+            }
+        }
+
+        return $extra;
+    }
+}
