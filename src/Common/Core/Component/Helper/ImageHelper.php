837907a63e8551db38b2cf03761ad05f63a7c136
Diego Blanco <diego@openhost.es>
2019-03-28 19:16:17 +0100

    ONM-5126: Create core.helper.image service

---
+++ src/Common/Core/Component/Helper/ImageHelper.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Common\Core\Component\Helper;
+
+use Common\Core\Component\Image\Processor;
+use Common\ORM\Entity\Instance;
+use Framework\Component\MIME\MimeTypeTool;
+use Symfony\Component\HttpFoundation\File\File;
+
+class ImageHelper
+{
+    /**
+     * The current instance.
+     *
+     * @var instance
+     */
+    protected $instance;
+
+    /**
+     * The image processor service.
+     *
+     * @var Processor
+     */
+    protected $processor;
+
+    /**
+     * The server public directory.
+     *
+     * @var string
+     */
+    protected $publicDir;
+
+    /**
+     * Initalializes the ImageHelper.
+     *
+     * @param Instance  $instance  The current instance.
+     * @param Processor $processor The image processor service.
+     * @param string    $publicDir The server public directory.
+     */
+    public function __construct(Instance $instance, Processor $processor, string $publicDir)
+    {
+        $this->instance  = $instance;
+        $this->processor = $processor;
+        $this->publicDir = $publicDir;
+    }
+
+    /**
+     * Returns the path where the file should be moved.
+     *
+     * @param File $file The file to generate path to.
+     *
+     * @return string The path where the file should be moved.
+     */
+    public function generatePath($file) : string
+    {
+        return preg_replace('/\/+/', '/', sprintf(
+            '%s/%s/%s/%s%s.%s',
+            $this->publicDir,
+            $this->instance->getImagesShortPath(),
+            date('Y/m/d'),
+            date('YmdHis'),
+            substr(gettimeofday()['usec'], 0, 5),
+            $this->getExtension($file)
+        ));
+    }
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * Returns the extension for a file.
+     *
+     * @param File $file The file to return extension for.
+     *
+     * @return string The file extension.
+     */
+    public function getExtension(File $file) : string
+    {
+        return MimeTypeTool::getExtension($file);
+    }
+
+    /**
+     * Returns the imformation for the image in the provided path.
+     *
+     * @param string $path The path to the image.
+     *
+     * @return array The image information.
+     */
+    public function getInformation(string $path) : array
+    {
+        $this->processor->open($path);
+
+        return [
+            'height' => $this->processor->getHeight(),
+            'size'   => $this->processor->getSize(),
+            'width'  => $this->processor->getWidth()
+        ];
+    }
+
+    /**
+     * Checks if the file is optimizable basing on the file path.
+     *
+     * @param string $path The file path.
+     *
+     * @return boolean True if the file is optimizable. False otherwise.
+     */
+    public function isOptimizable(string $path) : bool
+    {
+        $file = new \SplFileInfo($path);
+
+        return $file->getExtension() !== 'swf';
+    }
+
+    /**
+     * Moves the file to the target path.
+     *
+     * @param File $file The file to move.
+     *
+     * @return string The target path.
+     */
+    public function move(File $file, $target)
+    {
+        $name      = basename($target);
+        $directory = str_replace($name, '', $target);
+
+        $file->move($directory, $name);
+    }
+
+    /**
+     * Optimizes the file in the provided path.
+     *
+     * @param string $path The path to the file to optimize.
+     */
+    public function optimize(string $path) : void
+    {
+        $this->processor->open($path)->optimize()->save($path);
+    }
+}
---
+++ tests/Common/Core/Component/Helper/ImageHelperTest.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Common\Core\Component\Helper;
+
+use Common\Core\Component\Helper\ImageHelper;
+use Common\ORM\Entity\Instance;
+
+/**
+ * Defines test cases for ImageHelper class.
+ */
+class ImageHelperTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Configures the testing environment.
+     */
+    public function setUp()
+    {
+        $this->instance = new Instance([ 'internal_name' => 'bar' ]);
+
+        $this->processor = $this->getMockBuilder('Common\Core\Component\Image\Processor')
+            ->setMethods([
+                'getHeight', 'getSize', 'getWidth', 'open', 'optimize', 'save'
+            ])->getMock();
+
+        $this->helper = $this->getMockBuilder('Common\Core\Component\Helper\ImageHelper')
+            ->setConstructorArgs([ $this->instance, $this->processor, '/waldo/grault' ])
+            ->setMethods([ 'getExtension' ])
+            ->getMock();
+    }
+
+    /**
+     * Tests generatePath.
+     */
+    public function testGeneratePath()
+    {
+        $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\File')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->helper->expects($this->once())->method('getExtension')
+            ->with($file)->willReturn('jpg');
+
+        $this->assertRegexp(
+            '/\/waldo\/grault\/media\/bar\/images\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[0-9]{19}.jpg/',
+            $this->helper->generatePath($file)
+        );
+    }
+
+    /**
+     * Tests getInformation.
+     */
+    public function testGetInformation()
+    {
+        $this->processor->expects($this->once())->method('getHeight')
+            ->willReturn(220);
+        $this->processor->expects($this->once())->method('getSize')
+            ->willReturn(23920);
+        $this->processor->expects($this->once())->method('getWidth')
+            ->willReturn(400);
+
+        $this->assertEquals([
+            'size'   => 23920,
+            'width'  => 400,
+            'height' => 220,
+        ], $this->helper->getInformation('corge/quux/grault.jpg'));
+    }
+
+    /**
+     * Tests isOptimizable with optimizable and non-optimizable values.
+     */
+    public function testIsOptimizable()
+    {
+        $this->assertFalse($this->helper->isOptimizable('corge/quux/grault.swf'));
+        $this->assertTrue($this->helper->isOptimizable('corge/quux/grault.jpg'));
+    }
+
+    /**
+     * Tests move when the target directory does not exist.
+     */
+    public function testMoveWhenDirectoryNotExist()
+    {
+        $file = $this->getMockBuilder('Symfony\Component\HttpFoundation\File\File')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'move' ])
+            ->getMock();
+
+        $file->expects($this->once())->method('move')
+            ->with('/thud/fred/', 'norf.jpg');
+
+        $this->helper->move($file, '/thud/fred/norf.jpg');
+    }
+
+    /**
+     * Tests optimize.
+     */
+    public function testOptimize()
+    {
+        $this->processor->expects($this->once())->method('open')
+            ->with('/plugh/frog.jpg')->willReturn($this->processor);
+        $this->processor->expects($this->once())->method('optimize')
+            ->willReturn($this->processor);
+        $this->processor->expects($this->once())->method('save')
+            ->with('/plugh/frog.jpg')->willReturn($this->processor);
+
+        $this->helper->optimize('/plugh/frog.jpg');
+    }
+}
