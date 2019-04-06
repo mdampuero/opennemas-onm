f136d3e32dd7a7092d17e1b3465934e9654c1d6e
Fran Dieguez <fran@openhost.es>
2019-04-07 00:03:31 +0200

    ONM-5208: Port frontend Album controller to the new schema

---
+++ src/Frontend/Controller/AlbumController.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Frontend\Controller;
+
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Displays an album or a list of albums.
+ */
+class AlbumController extends FrontendController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $caches = [
+        'list'       => 'gallery-frontpage',
+        'listauthor' => 'gallery-frontpage',
+        'show'       => 'gallery-inner',
+        'showamp'    => 'gallery-inner',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $extension = 'ALBUM_MANAGER';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $groups = [
+        'showamp'    => 'amp_inner',
+        'list'       => 'album_frontpage',
+        'listauthor' => 'album_frontpage',
+        'show'       => 'album_inner',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $positions = [
+        'album_frontpage' => [ 7, 9 ],
+        'album_inner'     => [ 7 ],
+    ];
+
+    /**
+     * The list of valid query parameters per action.
+     *
+     * @var array
+     */
+    protected $queries = [
+        'list'       => [ 'page', 'category_name' ],
+        'showamp'    => [ '_format' ],
+    ];
+
+    /**
+     * The list of routes per action.
+     *
+     * @var array
+     */
+    protected $routes = [
+        'list' => 'frontend_album_frontpage'
+    ];
+
+    /**
+     * The list of templates per action.
+     *
+     * @var array
+     */
+    protected $templates = [
+        'list'       => 'album/album_frontpage.tpl',
+        'show'       => 'album/album.tpl',
+        'showamp'    => 'amp/content.tpl',
+    ];
+
+    /**
+     * {@inheritdoc}
+     *
+     * Action specific for the frontpage
+     */
+    public function hydrateList(array &$params = []): void
+    {
+        $category = $params['o_category'];
+        $page     = $params['page'] ?? 1;
+        $date     = date('Y-m-d H:i:s');
+
+        $albumSettings = $this->get('orm.manager')
+            ->getDataSet('Settings', 'instance')
+            ->get('album_settings');
+        $epp           = isset($albumSettings['total_front']) ? $albumSettings['total_front'] : 8;
+        $orderBy       = isset($albumSettings['orderFrontpage']) ? $albumSettings['orderFrontpage'] : 'created';
+        $order         = ($orderBy == 'favorite') ? 'favorite desc, created des' : 'created desc';
+
+        $categoryOQL = ($category)
+            ? sprintf(' and pk_fk_content_category=%d', $category->pk_content_category)
+            : '';
+
+        $response = $this->get('api.service.content_old')->getList(sprintf(
+            'content_type_name="album" and content_status=1 and in_litter=0 %s '
+            . 'and (starttime IS NULL or starttime < "%s") '
+            . 'and (endtime IS NULL or endtime > "%s") '
+            . 'order by %s limit %d offset %d',
+            $categoryOQL,
+            $date,
+            $date,
+            $order,
+            $epp,
+            $page
+        ));
+
+        $params = array_merge($params, [
+            'albums'     => $response['items'],
+            'pagination' => $this->get('paginator')->get([
+                'boundary'    => false,
+                'directional' => true,
+                'maxLinks'    => 0,
+                'epp'         => $epp,
+                'page'        => $page,
+                'total'       => $response['total'],
+                'route'       => [
+                    'name'   => (!$category)
+                        ? 'frontend_album_frontpage'
+                        : 'frontend_album_frontpage_category',
+                    'params' => (!$category)
+                        ? []
+                        : ['category_name' => $category->name],
+                ]
+            ])
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function hydrateShow(array &$params = []):void
+    {
+        $author = $this->get('user_repository')->find((int) $params['content']->fk_author);
+
+        $params['content']->author = $author;
+
+        // Get the other albums for the albums widget
+        $settings = $this->get('orm.manager')
+            ->getDataSet('Settings', 'instance')
+            ->get('album_settings');
+        $epp      = isset($settings['total_front']) ? ($settings['total_front']) : 2;
+
+        $order   = [ 'starttime' => 'DESC' ];
+        $filters = [
+            'pk_content'             => [[ 'value' => $params['content']->id, 'operator' => '<>' ]],
+            'pk_fk_content_category' => [[ 'value' => $params['o_category']->pk_content_category ]],
+            'content_type_name'      => [[ 'value' => 'album' ]],
+            'content_status'         => [[ 'value' => 1 ]],
+            'in_litter'              => [[ 'value' => 1, 'operator' => '!=' ]],
+            'starttime'       => [
+                'union' => 'OR',
+                [ 'value' => '0000-00-00 00:00:00' ],
+                [ 'value' => null, 'operator' => 'IS', 'field' => true ],
+                [ 'value' => date('Y-m-d H:i:s'), 'operator' => '<=' ],
+            ],
+            'endtime'         => [
+                'union' => 'OR',
+                [ 'value' => '0000-00-00 00:00:00' ],
+                [ 'value' => null, 'operator' => 'IS', 'field' => true ],
+                [ 'value' => date('Y-m-d H:i:s'), 'operator' => '>' ],
+            ]
+        ];
+
+        $otherAlbums = $this->get('entity_repository')->findBy($filters, $order, $epp, $params['page']);
+
+        // In order to make subscription module to work remove the attached
+        // album photos when not cacheable
+        $_albumArray = (!isset($params['content']->album_content_replaced))
+            ? $params['content']->_getAttachedPhotos($params['content']->id) : null;
+
+        $_albumArrayPaged = (!isset($params['content']->album_content_replaced))
+            ? $params['content']->getAttachedPhotosPaged($params['content']->id, 8, $params['page']) : null;
+
+        if (count($_albumArrayPaged) > $epp) {
+            array_pop($_albumArrayPaged);
+        }
+
+        $params = array_merge($params, [
+            'album_photos'       => $_albumArray,
+            'album_photos_paged' => $_albumArrayPaged,
+            'items_page'         => $epp,
+            'gallerys'           => $otherAlbums
+        ]);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * This func overrides the parent function just to
+     * propertly generate urls to category frontpages
+     **/
+    public function getRoute($action, $params = [])
+    {
+        if ($action == 'list' && array_key_exists('category_name', $params)) {
+            return 'frontend_album_frontpage_category';
+        }
+
+        return parent::getRoute($action, $params);
+    }
+}
