92616dcf2891699cd0032820f4f6c4593a37d3f1
Fran Dieguez <fran@openhost.es>
2019-04-08 12:33:24 +0200

    ONM-5231: Fix content creation from news agency due to tags not being parsed

--- src/Framework/Import/Importer/Importer.php
+++ src/Framework/Import/Importer/Importer.php
@@ -339,9 +339,11 @@ class Importer
     {
         $ts = $this->container->get('api.service.tag');
 
-        $tags = $ts->responsify(
+        $tags = array_map(function ($tag) {
+            return $tag['id'];
+        }, $ts->responsify(
             $ts->getListByString($resource->title)['items']
-        );
+        ));
 
         $data = [
             'category'            => $category,
