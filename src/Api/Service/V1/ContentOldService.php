cdb7dc8c622eb38bbaa1cddab28e3b9a8efaf00c
Fran Dieguez <fran@openhost.es>
2019-04-09 14:38:40 +0200

    ONM-5141: Minor fixes in video listing

--- src/Api/Service/V1/ContentOldService.php
+++ src/Api/Service/V1/ContentOldService.php
@@ -259,7 +259,7 @@ class ContentOldService implements Service
             $repository = $this->em;
 
             list($criteria, $order, $epp, $page) =
-            $this->container->get('core.helper.oql')->getFiltersFromOql($oql);
+                $this->container->get('core.helper.oql')->getFiltersFromOql($oql);
 
             $criteria          = preg_replace('/fk_author/', 'contents.fk_author', $criteria);
             $response['items'] = $repository->findBy($criteria, $order, $epp, $page);
