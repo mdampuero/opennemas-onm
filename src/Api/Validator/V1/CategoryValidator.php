ce617e9df1a90820a2d344ac6729e13c5ff2e768
Alex Rodrigues <alex@openhost.es>
2019-04-12 12:59:57 +0200

    ONM-5240: Add category validator

---
+++ src/Api/Validator/V1/CategoryValidator.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Api\Validator\V1;
+
+use Api\Exception\InvalidArgumentException;
+use Api\Validator\Validator;
+
+class CategoryValidator extends Validator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($item)
+    {
+        $names = $item->name;
+        if (!is_array($names)) {
+            $names = [ $names ];
+        }
+
+        foreach ($names as $name) {
+            try {
+                $category = $this->container->get('api.service.category')
+                    ->getItemBySlug($name);
+
+                // Update action
+                if ($category->pk_content_category == $item->pk_content_category) {
+                    continue;
+                }
+            } catch (\Exception $e) {
+                continue;
+            }
+
+            throw new InvalidArgumentException('Invalid category', 400);
+        }
+    }
+}
