cb715effa82e098bc60563641b366d400564aee2
Diego Blanco <diego@openhost.es>
2019-05-16 15:54:57 +0200

    ONM-5208: Replace setMode by a watcher

--- public/themes/admin/js/controllers/VideoListCtrl.js
+++ public/themes/admin/js/controllers/VideoListCtrl.js
@@ -80,15 +80,11 @@
           $scope.localize($scope.data.items, 'items');
         };
 
-        /**
-         * Changes the list mode.
-         *
-         * @param {String} mode The new list mode.
-         */
-        $scope.setMode = function(mode) {
-          $scope.mode = mode;
-
-          if (mode !== 'grid') {
+        // Update epp when mode changes
+        $scope.$watch(function() {
+          return $scope.config.mode;
+        }, function(nv) {
+          if (nv !== 'grid') {
             return;
           }
 
@@ -127,7 +123,7 @@
           }
 
           $scope.criteria.epp = rows * cols;
-        };
+        }, true);
       }
     ]);
 })();
