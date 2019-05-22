573e5f440c8e5cf5c570a8a21d10e275b87d32b3
Diego Blanco <diego@openhost.es>
2019-05-22 14:29:29 +0200

    ONM-5238: Rename and update controller to extend FrontendController

---
+++ src/Frontend/Controller/PollController.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Frontend\Controller;
+
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Displays a poll or a list of polls.
+ */
+class PollController extends FrontendController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $caches = [
+        'list'    => 'poll-frontpage',
+        'show'    => 'poll-inner',
+        'showamp' => 'poll-inner',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $extension = 'POLL_MANAGER';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $groups = [
+        'list'    => 'poll_frontpage',
+        'show'    => 'poll_inner',
+        'showamp' => 'amp_inner',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $positions = [
+        'poll_frontpage' => [ 7, 9 ],
+        'poll_inner'     => [ 7 ],
+    ];
+
+    /**
+     * The list of valid query parameters per action.
+     *
+     * @var array
+     */
+    protected $queries = [
+        'list'    => [ 'page', 'category_name' ],
+        'showamp' => [ '_format' ],
+    ];
+
+    /**
+     * The list of routes per action.
+     *
+     * @var array
+     */
+    protected $routes = [
+        'list' => 'frontend_poll_frontpage'
+    ];
+
+    /**
+     * The list of templates per action.
+     *
+     * @var array
+     */
+    protected $templates = [
+        'list'    => 'poll/poll_frontpage.tpl',
+        'show'    => 'poll/poll.tpl',
+        'showamp' => 'amp/content.tpl',
+    ];
+
+    /**
+     * Add vote & show poll result.
+     *
+     * @param Request $request The request object.
+     *
+     * @return Response The response object.
+     */
+    public function voteAction(Request $request)
+    {
+        $answer = (int) $request->request->get('answer');
+        $poll   = $this->getItem($request);
+
+        // Prevent vote when poll is closed
+        if ($poll->status === 'closed') {
+            $this->get('session')->getFlashBag()
+                ->add('error', _('You can\'t vote this poll, it is closed.'));
+
+            return new RedirectResponse(
+                $this->get('core.helper.url_generator')->generate($poll)
+            );
+        }
+
+        $cookieName = 'poll-' . $poll->pk_poll;
+        $cookie     = $request->cookies->get($cookieName);
+
+        // Prevent vote when already voted
+        if (!empty($cookie)) {
+            $this->get('session')->getFlashBag()
+                ->add('error', _('You have voted this poll previously.'));
+
+            return new RedirectResponse(
+                $this->get('core.helper.url_generator')->generate($poll)
+            );
+        }
+
+        try {
+            $poll->vote($answer);
+
+            $this->get('session')->getFlashBag()
+                ->add('success', _('Thanks for participating.'));
+
+            $cookie   = new Cookie($cookieName, 'voted');
+            $response = new RedirectResponse(
+                $this->get('core.helper.url_generator')->generate($poll)
+            );
+
+            $response->headers->setCookie($cookie);
+        } catch (\Exception $e) {
+            $this->get('session')->getFlashBag()
+                ->add('error', _('Error while updating content'));
+        }
+
+        return $response;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function hydrateList(array &$params = []) : void
+    {
+        $category = $params['o_category'];
+        $page     = $params['page'] ?? 1;
+        $date     = date('Y-m-d H:i:s');
+
+        $epp = (int) $this->get('orm.manager')
+            ->getDataSet('Settings', 'instance')
+            ->get('items_per_page', 10);
+
+        $categoryOQL = !empty($category)
+            ? sprintf(' and pk_fk_content_category=%d', $category->pk_content_category)
+            : '';
+
+        $response = $this->get('api.service.content_old')->getList(sprintf(
+            'content_type_name="poll" and content_status=1 and in_litter=0 %s '
+            . 'and (starttime IS NULL or starttime < "%s") '
+            . 'and (endtime IS NULL or endtime > "%s") '
+            . 'order by starttime desc limit %d offset %d',
+            $categoryOQL,
+            $date,
+            $date,
+            $epp,
+            $page
+        ));
+
+        $params = array_merge($params, [
+            'polls'      => $response['items'],
+            'pagination' => $this->get('paginator')->get([
+                'boundary'    => false,
+                'directional' => true,
+                'maxLinks'    => 0,
+                'epp'         => $epp,
+                'page'        => $page,
+                'total'       => $response['total'],
+                'route'       => [
+                    'name'   => empty($category)
+                        ? 'frontend_poll_frontpage'
+                        : 'frontend_poll_frontpage_category',
+                    'params' => empty($category)
+                        ? []
+                        : [ 'category_name' => $category->name ],
+                ]
+
+            ])
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function hydrateShow(array &$params = []) : void
+    {
+        $params['author'] = $this->get('user_repository')
+            ->find($params['content']->fk_author);
+    }
+}
