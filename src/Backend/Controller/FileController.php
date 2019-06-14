215d0a5f0c6ffba08d563e21dec58cfa338985d3
Diego Blanco <diego@openhost.es>
2019-06-14 15:35:00 +0200

    ONM-5453: Rename and clean backend controller for files

---
+++ src/Backend/Controller/FileController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Backend\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class FileController extends BackendController
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $extension = 'FILE_MANAGER';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $permissions = [
+        'create' => 'ATTACHMENT_CREATE',
+        'update' => 'ATTACHMENT_UPDATE',
+        'list'   => 'ATTACHMENT_ADMIN',
+        'show'   => 'ATTACHMENT_UPDATE',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $resource = 'file';
+}
