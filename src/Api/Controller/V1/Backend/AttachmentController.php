ceea1c184d0f0de21f4a71f26185bf9dafbcc707
Diego Blanco <diego@openhost.es>
2019-06-19 10:52:50 +0200

    ONM-5453: Remove unnecessary autocomplete action

--- src/Api/Controller/V1/Backend/AttachmentController.php
+++ src/Api/Controller/V1/Backend/AttachmentController.php
@@ -75,35 +75,6 @@ class AttachmentController extends ContentOldController
     }
 
     /**
-     * Returns a list with the file name and id of files in JSON format.
-     *
-     * @param  Request      $request     The request object.
-     * @param  string       $contentType Content type name.
-     * @return JsonResponse              The response object.
-     */
-    public function autocompleteAction(Request $request)
-    {
-        $search   = $request->query->get('query');
-        $order    = [ 'path' => 'desc' ];
-        $criteria = [
-            'content_type_name' => [ ['value' => 'attachment', 'operator' => '='] ],
-            'in_litter'         => [ ['value' => 0, 'operator' => '='] ],
-            'path'              => [ ['value' => '%' . $search . '%', 'operator' => 'like'] ],
-            'join'              => [ [
-                'table'      => 'attachments',
-                'pk_content' => [ [ 'value' => 'pk_attachment', 'field' => true ] ]
-            ] ]
-        ];
-
-        $results = $this->get('entity_repository')->findBy($criteria, $order, 10);
-        $results = array_map(function ($file) {
-            return ['id' => $file->id, 'filename' => basename($file->path)];
-        }, $results);
-
-        return new JsonResponse([ 'results' => $results ]);
-    }
-
-    /**
      * {@inheritDoc}
      */
     protected function getExtraData($items = null)
