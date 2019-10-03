05d640ec11b63ad234c0152a8ded5d406c4fec3e
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2019-10-03 10:50:53 +0200

    ONM-5173: Remove columns posmenu, internal_category and inmenu on default database

--- db/instance-default.sql
+++ db/instance-default.sql
@@ -326,9 +326,6 @@ CREATE TABLE `content_categories` (
   `title` text NOT NULL,
   `name` text,
   `description` text,
-  `inmenu` int(10) unsigned DEFAULT '0',
-  `posmenu` int(10) unsigned DEFAULT '1',
-  `internal_category` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Equal content_type & global=0 ',
   `fk_content_category` bigint(20) unsigned DEFAULT NULL,
   `logo_path` varchar(200) DEFAULT NULL,
   `color` varchar(10) DEFAULT NULL,
@@ -344,7 +341,7 @@ CREATE TABLE `content_categories` (
 
 LOCK TABLES `content_categories` WRITE;
 /*!40000 ALTER TABLE `content_categories` DISABLE KEYS */;
-INSERT INTO `content_categories` VALUES (20,'Sin categorÃ­a','sin-categoria',NULL,1,0,1,NULL,NULL,NULL,1,0),(22,'Deportes','deportes',NULL,1,10,1,NULL,NULL,NULL,1,0),(23,'EconomÃ­a','economia',NULL,1,10,1,NULL,NULL,NULL,1,0),(24,'PolÃ­tica','politica',NULL,1,10,1,NULL,NULL,NULL,1,0),(25,'Cultura','cultura',NULL,1,10,1,NULL,NULL,NULL,1,0),(26,'Sociedad','sociedad',NULL,1,10,1,NULL,NULL,NULL,1,0),(30,'Curiosidades','curiosidades',NULL,1,10,9,NULL,NULL,NULL,1,0),(31,'Fotos de Hoy','fotos-de-hoy',NULL,1,10,7,NULL,NULL,NULL,1,0),(32,'Portadas','portadas',NULL,1,10,14,NULL,NULL,NULL,1,0),(33,'Ciencia','ciencia',NULL,1,10,1,NULL,NULL,NULL,1,0),(34,'Actualidad','actualidad',NULL,1,10,1,NULL,NULL,NULL,1,0),(35,'Internacional','internacional',NULL,1,10,1,NULL,NULL,NULL,1,0),(36,'TecnologÃ­a','tecnologia',NULL,1,10,1,NULL,NULL,NULL,1,0);
+INSERT INTO `content_categories` VALUES (20,'Sin categorÃ­a','sin-categoria',1,NULL,NULL,NULL,1,0),(22,'Deportes','deportes',NULL,NULL,NULL,NULL,1,0),(23,'EconomÃ­a','economia',1,NULL,NULL,NULL,1,0),(24,'PolÃ­tica','politica',1,NULL,NULL,NULL,1,0),(25,'Cultura','cultura',1,NULL,NULL,NULL,1,0),(26,'Sociedad','sociedad',1,NULL,NULL,NULL,1,0),(30,'Curiosidades','curiosidades',9,NULL,NULL,NULL,1,0),(31,'Fotos de Hoy','fotos-de-hoy',7,NULL,NULL,NULL,1,0),(32,'Portadas','portadas',14,NULL,NULL,NULL,1,0),(33,'Ciencia','ciencia',1,NULL,NULL,NULL,1,0),(34,'Actualidad','actualidad',1,NULL,NULL,NULL,1,0),(35,'Internacional','internacional',1,NULL,NULL,NULL,1,0),(36,'TecnologÃ­a','tecnologia',1,NULL,NULL,NULL,1,0);
 /*!40000 ALTER TABLE `content_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
