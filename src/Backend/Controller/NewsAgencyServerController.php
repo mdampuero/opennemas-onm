e9dd460fd1bf1fc10abb3128eae540efad2480b9
Diego Blanco <diego@openhost.es>
2019-10-11 17:54:29 +0200

    ONM-5611: Clean controller and routes for news agency servers

--- src/Backend/Controller/NewsAgencyServerController.php
+++ src/Backend/Controller/NewsAgencyServerController.php
@@ -9,234 +9,26 @@
  */
 namespace Backend\Controller;
 
-use Common\Core\Annotation\Security;
-use Common\Core\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * Handles the actions for the news agency module
- */
-class NewsAgencyServerController extends Controller
+class NewsAgencyServerController extends BackendController
 {
     /**
-     * Displays a form to create a new news-agency server.
-     *
-     * @return Response The response object.
-     *
-     * @Security("hasExtension('NEWS_AGENCY_IMPORTER')
-     *     and hasPermission('IMPORT_NEWS_AGENCY_CONFIG')")
+     * {@inheritdoc}
      */
-    public function createAction()
-    {
-        return $this->render('news_agency/config/new.tpl', [
-            'authors'   => $this->getAuthors(),
-            'server'    => [],
-            'sync_from' => $this->getSyncFrom()
-        ]);
-    }
+    protected $extension = 'NEWS_AGENCY_IMPORTER';
 
     /**
-     * Shows the list of downloaded newsfiles from Efe service
-     *
-     * @param Request $request the request object
-     *
-     * @return Response the response object
-     *
-     * @Security("hasExtension('NEWS_AGENCY_IMPORTER')
-     *     and hasPermission('IMPORT_ADMIN')")
+     * {@inheritdoc}
      */
-    public function listAction()
-    {
-        return $this->render('news_agency/config/list.tpl');
-    }
+    protected $permissions = [
+        'create' => 'IMPORT_NEWS_AGENCY_CONFIG',
+        'list'   => 'IMPORT_NEWS_AGENCY_CONFIG',
+        'show'   => 'IMPORT_NEWS_AGENCY_CONFIG',
+    ];
 
     /**
-     * Shows and handles the configuration form for Efe module
-     *
-     * @param Request $request the request object
-     *
-     * @return Response the response object
-     *
-     * @Security("hasExtension('NEWS_AGENCY_IMPORTER')
-     *     and hasPermission('IMPORT_NEWS_AGENCY_CONFIG')")
+     * {@inheritdoc}
      */
-    public function saveAction(Request $request)
-    {
-        $ds      = $this->get('orm.manager')->getDataSet('Settings');
-        $servers = $ds->get('news_agency_config');
-
-        if (!is_array($servers)) {
-            $servers = [];
-        }
-
-        $latestServerId = count($servers) <= 0 ? 0 : max(array_keys($servers));
-
-        $server = [
-            'id'             => $latestServerId + 1,
-            'name'           => $request->request->filter('name', '', FILTER_SANITIZE_STRING),
-            'url'            => $request->request->filter('url', '', FILTER_SANITIZE_STRING),
-            'username'       => $request->request->filter('username', '', FILTER_SANITIZE_STRING),
-            'password'       => $request->request->filter('password', '', FILTER_SANITIZE_STRING),
-            'agency_string'  => $request->request->filter('agency_string', '', FILTER_SANITIZE_STRING),
-            'external_link'  => $request->request->filter('external_link', '', FILTER_SANITIZE_STRING),
-            'color'          => $request->request->filter('color', '#424E51', FILTER_SANITIZE_STRING),
-            'sync_from'      => $request->request->filter('sync_from', '', FILTER_SANITIZE_STRING),
-            'activated'      => $request->request->getDigits('activated', 0),
-            'author'         => $request->request->getDigits('author', 0),
-            'source'         => $request->request->getDigits('source', 0),
-            'auto_import'    => $request->request->getDigits('auto_import', 0),
-            'category'       => $request->request->filter('category', '', FILTER_SANITIZE_STRING),
-            'target_author'  => $request->request->filter('target_author', '', FILTER_SANITIZE_STRING),
-            'import_related' => $request->request->filter('import_related', '', FILTER_SANITIZE_STRING),
-            'categories_map' => json_decode($request->request->get('categories_map', [])),
-            'filters'        => $request->request->get('filters', []),
-        ];
-
-        $servers[$server['id']] = $server;
-
-        try {
-            $ds->set('news_agency_config', $servers);
-
-            $this->get('session')->getFlashBag()
-                ->add('success', _('News agency server added.'));
-
-            return $this->redirect($this->generateUrl(
-                'backend_news_agency_server_show',
-                [ 'id' => $server['id'] ]
-            ));
-        } catch (\Exception $e) {
-            $this->get('session')->getFlashBag()
-                ->add('error', _('Unable to save the news agency server.'));
-
-            return $this->redirect($this->generateUrl(
-                'backend_news_agency_server_new'
-            ));
-        }
-    }
-
-    /**
-     * Shows the news agency information
-     *
-     * @param Request $request the request object
-     *
-     * @return Response the response object
-     *
-     * @Security("hasExtension('NEWS_AGENCY_IMPORTER')
-     *     and hasPermission('IMPORT_NEWS_AGENCY_CONFIG')")
-     */
-    public function showAction(Request $request)
-    {
-        $id = $request->query->getDigits('id');
-
-        $servers = $this->get('orm.manager')->getDataSet('Settings')
-            ->get('news_agency_config');
-
-        if (empty($id) || !array_key_exists($id, $servers)) {
-            $this->get('session')->getFlashBag()->add(
-                'error',
-                sprintf(_('Unable to find the news agency source with the id "%d"'), $id)
-            );
-
-            return $this->redirect($this->generateUrl('backend_news_agency_servers_list'));
-        }
-
-        return $this->render('news_agency/config/new.tpl', [
-            'authors'   => $this->getAuthors(),
-            'server'    => $servers[$id],
-            'sync_from' => $this->getSyncFrom()
-        ]);
-    }
-
-    /**
-     * Updates a news agency server.
-     *
-     * @param Request $request The request object.
-     *
-     * @return Response The response object.
-     *
-     * @Security("hasExtension('NEWS_AGENCY_IMPORTER')
-     *     and hasPermission('IMPORT_NEWS_AGENCY_CONFIG')")
-     */
-    public function updateAction(Request $request)
-    {
-        $id      = $request->query->getDigits('id');
-        $servers = $this->get('orm.manager')->getDataSet('Settings')
-            ->get('news_agency_config');
-
-        $server = [
-            'id'             => $id,
-            'name'           => $request->request->filter('name', '', FILTER_SANITIZE_STRING),
-            'url'            => $request->request->filter('url', '', FILTER_SANITIZE_STRING),
-            'username'       => $request->request->filter('username', '', FILTER_SANITIZE_STRING),
-            'password'       => $request->request->filter('password', '', FILTER_SANITIZE_STRING),
-            'agency_string'  => $request->request->filter('agency_string', '', FILTER_SANITIZE_STRING),
-            'external_link'  => $request->request->filter('external_link', '', FILTER_SANITIZE_STRING),
-            'color'          => $request->request->filter('color', '#424E51', FILTER_SANITIZE_STRING),
-            'sync_from'      => $request->request->filter('sync_from', '', FILTER_SANITIZE_STRING),
-            'activated'      => $request->request->getDigits('activated', 0),
-            'author'         => $request->request->getDigits('author', 0),
-            'source'         => $request->request->getDigits('source', 0),
-            'auto_import'    => $request->request->getDigits('auto_import', 0),
-            'category'       => $request->request->filter('category', '', FILTER_SANITIZE_STRING),
-            'target_author'  => $request->request->filter('target_author', '', FILTER_SANITIZE_STRING),
-            'import_related' => $request->request->filter('import_related', '', FILTER_SANITIZE_STRING),
-            'categories_map' => json_decode($request->request->get('categories_map', [])),
-            'filters'        => $request->request->get('filters', [])
-        ];
-
-        $servers[$id] = $server;
-
-        $this->get('orm.manager')->getDataSet('Settings')
-            ->set('news_agency_config', $servers);
-
-        $this->get('session')->getFlashBag()
-            ->add('success', _('News agency server updated.'));
-
-        return $this->redirect($this->generateUrl(
-            'backend_news_agency_server_show',
-            [ 'id' => $id ]
-        ));
-    }
-
-    /**
-     * Returns the list of authors for the selector.
-     *
-     * @return array The list of authors.
-     */
-    protected function getAuthors()
-    {
-        $authors = [];
-        $users   = $this->get('api.service.author')
-            ->getList('order by name asc');
-
-        foreach ($users['items'] as $user) {
-            $authors[$user->id] = $user->name;
-        }
-
-        return $authors;
-    }
-
-    /**
-     * Returns the list of of hours for the selector.
-     *
-     * @return array The lisf of hours.
-     */
-    protected function getSyncFrom()
-    {
-        return [
-            '3600'         => sprintf(_('%d hour'), '1'),
-            '10800'         => sprintf(_('%d hours'), '3'),
-            '21600'         => sprintf(_('%d hours'), '6'),
-            '43200'         => sprintf(_('%d hours'), '12'),
-            '86400'         => _('1 day'),
-            '172800'        => sprintf(_('%d days'), '2'),
-            '259200'        => sprintf(_('%d days'), '3'),
-            '345600'        => sprintf(_('%d days'), '4'),
-            '432000'        => sprintf(_('%d days'), '5'),
-            '518400'        => sprintf(_('%d days'), '6'),
-            '604800'        => sprintf(_('%d week'), '1'),
-            '1209600'       => sprintf(_('%d weeks'), '2'),
-            'no_limits'     => _('No limit'),
-        ];
-    }
+    protected $resource = 'news_agency/server';
 }
