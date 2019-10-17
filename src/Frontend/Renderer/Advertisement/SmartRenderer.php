856a4ab1b5f98157396e76fef37816e7fc74e4bc
Alex Rodrigues <alex@openhost.es>
2019-10-17 15:40:52 +0200

    ONM-5496: Add FIA support on SmartRenderer

--- src/Frontend/Renderer/Advertisement/SmartRenderer.php
+++ src/Frontend/Renderer/Advertisement/SmartRenderer.php
@@ -18,10 +18,32 @@ use Frontend\Renderer\AdvertisementRenderer;
 class SmartRenderer extends AdvertisementRenderer
 {
     /**
-     * Renders an advertisement.
+     * Returns the HTML for instant articles advertisements.
      *
      * @param \Advertisement $ad The advertisement to render.
-     * @param array          $params The list of parameters
+     *
+     * @return string The HTML for the advertisement.
+     */
+    public function renderFia($ad, $params)
+    {
+        $size   = $this->getDeviceAdvertisementSize($ad, 'phone');
+        $config = $this->ds->get('smart_ad_server');
+
+        return $this->tpl->fetch('advertisement/helpers/fia/smart.tpl', [
+            'config'    => $config,
+            'page_id'   => $config['page_id']['article_inner'],
+            'format_id' => (int) $ad->params['smart_format_id'],
+            'width'     => $size['width'],
+            'height'    => $size['height'],
+            'default'   => $params['current_position'] === 1075 ? true : false,
+        ]);
+    }
+
+    /**
+     * Renders an advertisement.
+     *
+     * @param \Advertisement $ad     The advertisement to render.
+     * @param array          $params The list of parameters.
      *
      * @return string The HTML for the slot.
      */
@@ -51,12 +73,12 @@ class SmartRenderer extends AdvertisementRenderer
     }
 
     /**
-     * Renders a SafeFrame document for a Smart advertisement
+     * Renders a SafeFrame document for a Smart advertisement.
      *
      * @param \Advertisement $ad     The ad to render.
-     * @param array          $params The list of parameters
+     * @param array          $params The list of parameters.
      *
-     * @return string the HTML generated
+     * @return string The generated HTML.
      */
     public function renderSafeFrame(\Advertisement $ad, $params)
     {
@@ -74,7 +96,7 @@ class SmartRenderer extends AdvertisementRenderer
      * Generates the HTML code to include in header for Smart advertisements.
      *
      * @param array $ads    The list of advertisements.
-     * @param array $params The list of parameters
+     * @param array $params The list of parameters.
      *
      * @return string The HTML code to include in header.
      */
@@ -130,7 +152,7 @@ class SmartRenderer extends AdvertisementRenderer
     /**
      * Returns the targeting-related JS code for google DFP.
      *
-     * @param string  $category The current category.
+     * @param string  $category  The current category.
      * @param string  $module    The current module.
      * @param integer $contentId The id of the content current.
      *
