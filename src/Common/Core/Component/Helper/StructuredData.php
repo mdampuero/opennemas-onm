984ac8bb3fc3b9550b163c66956da1f14e779bf1
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2019-11-26 15:26:25 +0100

    ONM-5815: Fix on articles

--- src/Common/Core/Component/Helper/StructuredData.php
+++ src/Common/Core/Component/Helper/StructuredData.php
@@ -249,7 +249,7 @@ class StructuredData
             "datePublished" : "' . $data['created'] . '",
             "dateModified": "' . $data['changed'] . '",
             "articleSection" : "' . $data['category']->title . '",
-            "keywords": "' . strip_tags(htmlspecialchars(html_entity_decode($keywords, ENT_COMPAT, 'UTF-8'))) . '",
+            "keywords": "' . $keywords . '",
             "url": "' . $data['url'] . '",
             "wordCount": ' . str_word_count($data['content']->body) . ',
             "description": "' . $data['summary'] . '",
