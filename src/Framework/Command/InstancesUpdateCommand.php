eb1ec812e580955308f8b6ee5f90e413393683e3
Diego Blanco <diego@openhost.es>
2019-11-26 19:20:32 +0100

    ONM-4721: Update command to use core.helper.instance

--- src/Framework/Command/InstancesUpdateCommand.php
+++ src/Framework/Command/InstancesUpdateCommand.php
@@ -42,8 +42,14 @@ class InstancesUpdateCommand extends ContainerAwareCommand
                 "Updates the counters in instances table in onm-instances by collecting data from different sources."
             )
             ->addOption(
-                'instance-stats',
-                'i',
+                'media',
+                'm',
+                InputOption::VALUE_NONE,
+                'If set, the command will gather the media sixe for the instance.'
+            )
+            ->addOption(
+                'stats',
+                's',
                 InputOption::VALUE_NONE,
                 'If set, the command will gather content, users, sent emails info.'
             )
@@ -52,23 +58,6 @@ class InstancesUpdateCommand extends ContainerAwareCommand
                 'p',
                 InputOption::VALUE_NONE,
                 'If set, the command will gather the page views from Piwik.'
-            )
-            ->addOption(
-                'media-size',
-                'm',
-                InputOption::VALUE_NONE,
-                'If set, the command will gather the media sixe for the instance.'
-            )
-            ->addOption(
-                'created',
-                'r',
-                InputOption::VALUE_NONE,
-                'If set, the command will gather the created date from instance.'
-            )->addOption(
-                'offset',
-                'o',
-                InputOption::VALUE_OPTIONAL,
-                'If set, this command will only be run in 30 instances from page [offset].'
             );
     }
 
@@ -85,343 +74,61 @@ class InstancesUpdateCommand extends ContainerAwareCommand
         $loader = $this->getContainer()->get('core.loader');
         $loader->init();
 
-        $options = [
-            'instance_stats'  => $input->getOption('instance-stats'),
-            'views'           => $input->getOption('views'),
-            'media_size'      => $input->getOption('media-size'),
-            'created'         => $input->getOption('created'),
-        ];
+        $instances = $this->getInstances();
 
-        $offset       = $input->getOption('offset');
-        $this->input  = $input;
-        $this->output = $output;
-
-        if (!$options['instance_stats']
-            && !$options['views']
-            && !$options['media_size']
-            && !$options['created']
-        ) {
-            $this->output->writeln(
-                '<error>Please provide --instance-stats,'
-                    . ' --views, --media-size or --created</error>'
-            );
-
-            return 1;
-        }
-
-        $amount = !is_null($offset) ? 30 : null;
-
-        $this->getContainer()->get('cache_manager')->setNamespace('manager');
-        $this->em = $this->getContainer()->get('orm.manager');
-        $oql      = 'order by id asc';
-
-        if ($amount) {
-            $oql = ' limit ' . $amount;
-        }
-
-        if ($offset) {
-            $oql .= ' offset ' . ($offset - 1) * 30;
-        }
-
-        $instances = $this->em->getRepository('Instance')->findBy($oql);
-
-        if (count($instances) == 0) {
+        if (empty($instances)) {
             $output->writeln('No instances');
             return 1;
         }
 
+        $helper = $this->getContainer()->get('core.helper.instance');
+        $stats  = [];
+        $sizes  = $input->getOption('media')
+            ? $helper->getMediaSize()
+            : [];
+
         foreach ($instances as $instance) {
-            if ($output->isVerbose()) {
-                $output->writeln('Getting info about \'' . $instance->internal_name . '\'');
+            $stats[$instance->internal_name]['created']    = $helper->getCreated($instance);
+            $stats[$instance->internal_name]['last_login'] = $helper->getLastActivity($instance);
+            $stats[$instance->internal_name]['users']      = $helper->countUsers($instance);
+            $stats[$instance->internal_name]['page_views'] = $helper->getPageViews($instance);
+
+            $contents = $helper->countContents($instance);
+
+            foreach ($contents as $type => $total) {
+                $stats[$instance->internal_name][$type . 's'] = $total;
             }
 
-            try {
-                $this->getInstanceInfo($instance, $options);
-                $this->em->persist($instance);
-            } catch (\Exception $e) {
-                $this->getContainer()->get('error.log')
-                    ->error($e->getMessage());
+            $stats[$instance->internal_name]['contents'] = array_sum($contents);
 
-                $output->writeln(
-                    '<error>Error while getting info about \''
-                    . $instance->internal_name . '\': ' . $e->getMessage() . '</>'
-                );
-            }
-        }
-
-        // No errors
-        return 0;
-    }
-
-    /**
-     * Gets the instance information.
-     *
-     * @param  Instance $i       The instance.
-     * @param  array    $options The list of command options.
-     *
-     * @return boolean
-     */
-    private function getInstanceInfo(&$i, $options)
-    {
-        if (empty($i->getDatabaseName())) {
-            return false;
-        }
-        $conn = $this->getContainer()->get('orm.manager')->getConnection('instance');
-        $conn->selectDatabase($i->getDatabaseName());
-
-        // Update instance stats
-        if ($options['instance_stats']) {
-            if ($this->output->isVeryVerbose()) {
-                $this->output->write("\t- Getting instance stats (emails, users, content numbers) ");
-            }
-
-            $this->getInstanceStats($i);
-
-            if ($this->output->isVeryVerbose()) {
-                $this->output->writeln("<fg=green>DONE</>");
+            if (array_key_exists($instance->internal_name, $sizes)) {
+                $stats[$instance->internal_name]['media_size'] =
+                    $sizes[$instance->internal_name];
             }
         }
 
-        // Update created data
-        if ($options['created']) {
-            if ($this->output->isVeryVerbose()) {
-                $this->output->write("\t- Getting instance creational date ");
+        foreach ($instances as $instance) {
+            foreach ($stats[$instance->internal_name] as $key => $value) {
+                $instance->{$key} = $value;
             }
 
-            $this->getCreatedDate($i);
-
-            if ($this->output->isVeryVerbose()) {
-                $this->output->writeln("<fg=green>DONE</>");
-            }
-        }
-
-        // Get the page views from Piwik
-        if ($options['views']) {
-            if ($this->output->isVeryVerbose()) {
-                $this->output->write("\t- Getting page num views ");
-            }
-            $sql = 'SELECT value FROM settings WHERE name=\'piwik\'';
-            $rs  = $conn->fetchAll($sql);
-
-            if ($rs !== false && !empty($rs)) {
-                $piwik = unserialize($rs[0]['value']);
-
-                if (is_array($piwik) && array_key_exists('page_id', $piwik)) {
-                    $i->page_views = $this->getPageViews($piwik['page_id']);
-                }
-
-                $message = "<fg=green>DONE</>";
-            } else {
-                $message = "<error>FAILED</error>" . "Piwik code not available";
-            }
-
-            if ($this->output->isVeryVerbose()) {
-                $this->output->writeln($message);
-            }
-        }
-
-        $conn->close();
-
-        // Get media size
-        if ($options['media_size']) {
-            if ($this->output->isVeryVerbose()) {
-                $this->output->write("\t- Getting media size ");
-            }
-
-            $this->getMediaSize($i);
-
-            if ($this->output->isVeryVerbose()) {
-                $this->output->writeln("<fg=green>DONE</>");
-            }
+            $this->getContainer()->get('orm.manager')->persist($instance);
         }
     }
 
     /**
-     * Fetches the instance stats ()
+     * Returns the list of instances to process.
      *
-     * @param Instance $i The instance to get stats from
+     * @return array The list of instances to proccess.
      */
-    public function getInstanceStats(&$i)
+    protected function getInstances() : array
     {
-        $conn = $this->getContainer()->get('orm.manager')->getConnection('instance');
-
-        // Count contents
-        $sql = 'SELECT count(*) as total FROM contents';
-        $rs  = $conn->fetchAll($sql);
-
-        if (!empty($rs)) {
-            $i->contents = $rs[0]['total'];
+        try {
+            return $this->getContainer()->get('orm.manager')
+                ->getRepository('Instance')
+                ->findBy('order by id asc');
+        } catch (\Exception $e) {
+            return [];
         }
-
-        $sql = 'SELECT content_type_name as type, count(*) as total '
-            . 'FROM contents WHERE in_litter != 1 '
-            . 'GROUP BY `fk_content_type`, `content_type_name`';
-
-        $rs = $conn->fetchAll($sql);
-
-        if (!empty($rs)) {
-            foreach ($rs as $value) {
-                $allowedContentTypes = [
-                    'advertisement',
-                    'attachment',
-                    'album',
-                    'article',
-                    'letter',
-                    'opinion',
-                    'photo',
-                    'poll',
-                    'static_page',
-                    'video',
-                    'widget'
-                ];
-
-                if (!in_array($value['type'], $allowedContentTypes)) {
-                    continue;
-                }
-
-                $type = $value['type'] . 's';
-
-                $i->{$type} = $value['total'];
-            }
-        }
-
-        // Count users
-        $sql = "SELECT COUNT(id) FROM users WHERE activated = 1 AND type IN (0,2)";
-        $rs  = $conn->fetchArray($sql);
-
-        if ($rs !== false && !empty($rs)) {
-            $i->users = $rs[0];
-        }
-
-        // Count emails
-        $sql = 'SELECT counter FROM action_counters WHERE action_name = \'newsletter\'';
-        $rs  = $conn->fetchArray($sql);
-
-        if ($rs) {
-            $i->emails = $rs[0];
-        }
-
-        // Get last login date
-        $sql = 'SELECT * FROM settings WHERE name=\'last_login\' or name=\'time_zone\'';
-        $rs  = $conn->fetchAll($sql);
-        $tz  = new \DateTimeZone(date_default_timezone_get());
-
-        $i->last_login = null;
-
-        if ($rs !== false && !empty($rs)) {
-            $settings = [];
-            foreach ($rs as $r) {
-                $settings[$r['name']] = unserialize($r['value']);
-            }
-
-            if (array_key_exists('time_zone', $settings) && !empty($settings['time_zone'])) {
-                $this->getContainer()->get('core.locale')->setTimeZone($settings['time_zone']);
-                $tz = $this->getContainer()->get('core.locale')->getTimeZone();
-            }
-
-            if (array_key_exists('last_login', $settings) && !empty($settings['last_login'])) {
-                $i->last_login = new \DateTime($settings['last_login']);
-            }
-        }
-
-        // Get the creation date of the last created content
-        $sql = 'SELECT created FROM contents ORDER BY created desc LIMIT 1 ';
-        $rs  = $conn->fetchAll($sql);
-
-        if (!empty($rs) && !empty($rs[0]['created'])) {
-            $created = new \DateTime($rs[0]['created'], $tz);
-            $created->setTimeZone(new \DateTimeZone('UTC'));
-
-            if (empty($i->last_login) || $created > $i->last_login) {
-                $i->last_login = $created;
-            }
-        }
-    }
-
-    /**
-     * Fetches the date of creation of the instance
-     *
-     * @param Instance $i The instance to get stats from
-     */
-    public function getCreatedDate(&$i)
-    {
-        $sql  = 'SELECT * FROM settings WHERE name=\'site_created\'';
-        $conn = $this->getContainer()->get('orm.manager')->getConnection('instance');
-        $rs   = $conn->fetchAssoc($sql);
-
-        if ($rs !== false && !empty($rs)) {
-            $created = new \DateTime(unserialize($rs['value']));
-            $created->setTimeZone(new \DateTimeZone('UTC'));
-
-            $i->created = $created;
-        }
-    }
-
-    /**
-     * Caculates the amount of Mb that an instance has
-     *
-     * @param Instance $i The instance to get stats from
-     */
-    public function getMediaSize(&$i)
-    {
-        $size      = 0;
-        $mediaPath = realpath(SITE_PATH . "media" . DS . $i->internal_name);
-
-        if ($mediaPath) {
-            $size = (int) shell_exec('du -s ' . $mediaPath . '/ | awk \'{ print $1}\'');
-        }
-
-        $i->media_size = $size / 1024;
-    }
-
-    /**
-     * Gets the number of page views from Piwik.
-     *
-     * @param  integer $siteId The site id in Piwik.
-     * @return integer         The number of page views.
-     */
-    private function getPageViews($siteId)
-    {
-        if (!$siteId) {
-            return 0;
-        }
-
-        $url   = $this->getContainer()->getParameter('piwik.url');
-        $token = $this->getContainer()->getParameter('piwik.token');
-
-        $from = new \DateTime('now');
-
-        if ($from->format('d') <= '27') {
-            $from->modify('-1 month');
-        }
-
-        $from->setDate($from->format('Y'), $from->format('m'), 27);
-
-        $from = $from->format('Y-m-d');
-        $to   = date('Y-m-d');
-
-        $url .= "?module=API&method=API.get"
-            . "&apiModule=VisitsSummary&apiAction=get"
-            . "&idSite=$siteId"
-            . "&period=range&date=$from,$to"
-            . "&format=json"
-            . "&showColumns=nb_pageviews"
-            . "&token_auth=$token";
-
-        $ch = curl_init($url);
-
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $resp = curl_exec($ch);
-        curl_close($ch);
-
-        $views = json_decode($resp, true);
-
-        if (is_array($views) && array_key_exists('value', $views)) {
-            return $views['value'];
-        }
-
-        return 0;
     }
 }
