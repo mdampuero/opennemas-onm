678a06455b285a607e7daa96939d355472bc39a1
Jorge Fuentes <jorge@openhost.es>
2019-12-19 09:17:25 +0100

    ONM-5821: Add 'id' parameter support to dynamic_image

--- libs/smarty-onm-plugins/function.dynamic_image.php
+++ libs/smarty-onm-plugins/function.dynamic_image.php
@@ -9,6 +9,11 @@
  */
 function smarty_function_dynamic_image($params, &$smarty)
 {
+    if (array_key_exists('id', $params) && !empty($params['id'])) {
+        $photo         = getService('entity_repository')->find('Photo', $params['id']);
+        $params['src'] = $photo->path_img;
+    }
+
     if (empty($params['src'])) {
         return;
     }
