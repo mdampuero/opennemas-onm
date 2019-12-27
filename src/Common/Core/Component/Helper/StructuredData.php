f9ce2635032a0c636fe08de43fcd2b6c5bf4074e
Alex Rodrigues <alex@openhost.es>
2019-12-27 12:31:32 +0100

    ONM-5877: Improve structured data code from plugin to class

--- src/Common/Core/Component/Helper/StructuredData.php
+++ src/Common/Core/Component/Helper/StructuredData.php
@@ -39,24 +39,57 @@ class StructuredData
 
 
     /**
-     * Transform tags from array of ids to String and add some data to the array
+     * Extract parameters from data.
      *
-     * @param array $data The array of data
+     * @param array $data The array of data.
      *
-     * @return array The array with Strings of tags and more data
+     * @return array The array with processed data.
      */
     public function extractParamsFromData($data)
     {
-        $data['keywords'] = empty($data['content']->tags) ? '' : $this->getTags($data['content']->tags);
+        // Get content summary, body or description.
+        $data['summary'] = $data['content']->summary;
+        if (empty($data['summary'])) {
+            $data['summary'] = mb_substr($data['content']->body, 0, 120) . "...";
+            if (empty($data['content']->body)) {
+                $data['summary'] = mb_substr($data['content']->description, 0, 120) . "...";
+            }
+        }
+
+        // Count description data words
+        $data['wordCount'] = str_word_count($data['summary']);
+
+        // Encode content data
+        $data['title']   = htmlspecialchars(
+            html_entity_decode($data['content']->title, ENT_COMPAT, 'UTF-8')
+        );
+        $data['summary'] = htmlspecialchars(
+            html_entity_decode($data['summary'], ENT_COMPAT, 'UTF-8')
+        );
+
+        // Logo, author and media information
+        $data['logo']   = $this->getLogoData();
+        $data['author'] = $this->getAuthorData($data['content']);
+        $media          = $this->getMediaData($data['content']);
+        $data['image']  = $media['image'];
+        $data['video']  = $media['video'];
+
+        // Get datetime values
+        $data['created'] = $data['content']->created instanceof \DateTime ?
+            $data['content']->created->format('Y-m-d H:i:s') : $data['content']->created;
+        $data['changed'] = $data['content']->changed instanceof \DateTime ?
+            $data['content']->changed->format('Y-m-d H:i:s') : $data['content']->changed;
+
+        // Content keywords
+        $data['keywords'] = empty($data['content']->tags) ? ''
+            : $this->getTags($data['content']->tags);
 
         if (!empty($data['video'])) {
-            $data['videokeywords'] = empty($data['video']->tags) ? '' : $this->getTags($data['video']->tags);
+            $data['videokeywords'] = empty($data['video']->tags) ? ''
+                : $this->getTags($data['video']->tags);
         }
 
-        if (!empty($data['content']->body)) {
-            $data['wordCount'] = str_word_count($data['content']->body);
-        }
-
+        // Site information
         $data['sitename'] = $this->ds->get('site_name');
         $data['siteurl']  = SITE_URL;
 
@@ -66,30 +99,104 @@ class StructuredData
     /**
      * Generate specific JSON code based on the type of content: album, video or article.
      *
-     * @param array $data The array of data
+     * @param array $data The array of data.
      *
-     * @return string $output The JSON code specific for the data
+     * @return string $output The JSON code specific for the data.
      */
     public function generateJsonLDCode($data)
     {
         $params = $this->extractParamsFromData($data);
 
-        if ($data['content']->content_type_name == 'album') {
+        $output = $this->tpl->fetch('common/helpers/structured_article_data.tpl', $params);
+        if ($params['content']->content_type_name == 'album') {
             $output = $this->tpl->fetch('common/helpers/structured_gallery_data.tpl', $params);
-        } elseif (!empty($data['video'])) {
+        } elseif (!empty($params['video'])) {
             $output = $this->tpl->fetch('common/helpers/structured_video_data.tpl', $params);
-        } else {
-            $output = $this->tpl->fetch('common/helpers/structured_article_data.tpl', $params);
         }
 
         return $output;
     }
+
     /**
-     *  Method to retrieve the tags for a list of tag ids
+     * Method to retrieve the author information.
      *
-     * @param array $ids List of ids we want to retrieve
+     * @param \Content $content The content object.
      *
-     * @return string List of tags fo this tags.
+     * @return string The author information.
+     */
+    protected function getAuthorData($content)
+    {
+        // Get author if exists or agency. Otherwise get site name.
+        $user = $this->container->get('user_repository')
+            ->find($content->fk_author);
+
+        $author = !is_null($user) ? $user->name : $content->agency;
+        if (empty($author)) {
+            $author = $this->ds->get('site_name');
+        }
+
+        return $author;
+    }
+
+    /**
+     * Method to retrieve the logo information.
+     *
+     * @return array Array with logo properties.
+     */
+    protected function getLogoData()
+    {
+        // Default logo information
+        $logo = [
+            'url'    => SITE_URL . 'assets/images/logos/opennemas-powered-horizontal.png',
+            'width'  => '350',
+            'height' => '60'
+        ];
+
+        $siteLogo = $this->ds->get('site_logo');
+        if (!empty($siteLogo)) {
+            $logo = [
+                'url'    => SITE_URL
+                    . 'asset/thumbnail%252C260%252C60%252Ccenter%252Ccenter'
+                    . $this->container->get('core.instance')->getMediaShortPath()
+                    . '/sections/' . $siteLogo,
+                'width'  => '260',
+                'height' => '60'
+            ];
+        }
+
+        return $logo;
+    }
+
+    /**
+     * Method to retrieve the media information.
+     *
+     * @param \Content $content The content object.
+     *
+     * @return array Array with media information.
+     */
+    protected function getMediaData($content)
+    {
+        $mediaObject = $this->container->get('core.helper.content_media')
+            ->getContentMediaObject($content);
+
+        $media = [
+            'image' =>
+                (is_object($mediaObject) && get_class($mediaObject) == 'Photo')
+                    ? $mediaObject : null,
+            'video' =>
+                (is_object($mediaObject) && get_class($mediaObject) == 'Video')
+                    ? $mediaObject : null,
+        ];
+
+        return $media;
+    }
+
+    /**
+     * Method to retrieve the tags for a list of tag ids.
+     *
+     * @param array $ids List of ids we want to retrieve.
+     *
+     * @return string    List of tags fo this tags.
      */
     protected function getTags($ids)
     {
