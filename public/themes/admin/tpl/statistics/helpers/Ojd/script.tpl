3f4f482e03a5b05fadcd77c75ee052d79f1d91f2
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-03-24 18:22:15 +0100

    ONM-2804: Parametrize Ojd code templates

--- public/themes/admin/tpl/statistics/helpers/Ojd/script.tpl
+++ public/themes/admin/tpl/statistics/helpers/Ojd/script.tpl
@@ -1,13 +1,13 @@
 <!-- START Nielsen//NetRatings SiteCensus V5.3 -->
 <!-- COPYRIGHT 2007 Nielsen//NetRatings -->
 <script>
-  var _rsCI="' . $config['page_id'] . '";
+  var _rsCI="{$page_id}";
   var _rsCG="0";
   var _rsDN="//secure-uk.imrworldwide.com/";
   var _rsCC=0;
 </script>
 <script src="//secure-uk.imrworldwide.com/v53.js"></script>
 <noscript>
-  <div><img src="//secure-uk.imrworldwide.com/cgi-bin/m?ci=$config['page_id'] . '&amp;cg=0" alt=""/></div>
+  <div><img src="//secure-uk.imrworldwide.com/cgi-bin/m?ci={$page_id}&amp;cg=0" alt=""/></div>
 </noscript>
 <!-- END Nielsen//NetRatings SiteCensus V5.3 -->
--- src/Frontend/Renderer/Statistics/OjdRenderer.php
+++ src/Frontend/Renderer/Statistics/OjdRenderer.php
@@ -14,15 +14,7 @@ use Frontend\Renderer\StatisticsRenderer;
 class OjdRenderer extends StatisticsRenderer
 {
     /**
-     * Get code of google analytics for amp pages
-     */
-    public function getAmp()
-    {
-        return $this->tpl->fetch('statistics/helpers/Ojd/amp.tpl', []);
-    }
-
-    /**
-     * Get script code for google analytics
+     * Get script code for ojd
      */
     public function getScript()
     {
@@ -30,18 +22,29 @@ class OjdRenderer extends StatisticsRenderer
     }
 
     /**
-     * Get image code for google analytics
-     */
-    public function getImage()
-    {
-        return $this->tpl->fetch('statistics/helpers/Ojd/image.tpl', []);
-    }
-
-    /**
      * Return if ojd is correctly configured or not
      */
     public function validate()
     {
+        $config = $this->em->getDataSet('Settings', 'instance')->get('ojd');
+
+        if (!is_array($config)
+            || !array_key_exists('page_id', $config)
+            || empty(trim($config['page_id']))
+        ) {
+            return false;
+        }
+
         return true;
+    }
+
+    /**
+     * Return parameters needed to generate ojd code
+     */
+    protected function prepareParams()
+    {
+        $config = $this->em->getDataSet('Settings', 'instance')->get('ojd');
+
+        return ['page_id' => $config['page_id']];
     }
 }
