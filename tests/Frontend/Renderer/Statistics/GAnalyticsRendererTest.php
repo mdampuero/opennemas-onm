e021ea0a31aa2fbb7e46a2542dfa180404df0e53
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-03-26 17:48:42 +0100

    ONM-2804: Add tests for google analytics renderer

---
+++ tests/Frontend/Renderer/Statistics/GAnalyticsRendererTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Frontend\Renderer;
+
+use PHPUnit\Framework\TestCase;
+use Frontend\Renderer\Statistics\GAnalyticsRenderer;
+
+/**
+ * Defines test cases for GAnalyticsRenderer class.
+ */
+class GAnalyticsRendererTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->container = $this->getMockForAbstractClass('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $this->ds = $this->getMockForAbstractClass('Common\ORM\Core\DataSet');
+
+        $this->em = $this->getMockBuilder('Common\ORM\Core\EntityManager')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getDataSet' ])
+            ->getMock();
+
+        $this->global = $this->getMockBuilder('Common\Core\Component\Core\GlobalVariables')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getContainer', 'getSection', 'getExtension' ])
+            ->getMock();
+
+        $this->stack = $this->getMockBuilder('Symfony\Component\HttpFoundation\RequestStack')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getCurrentRequest' ])
+            ->getMock();
+
+        $this->tpl = $this->getMockBuilder('Common\Core\Component\Template\Template')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'fetch' ])
+            ->getMock();
+
+        $this->container->expects($this->any())->method('get')
+            ->will($this->returnCallback([ $this, 'serviceContainerCallback' ]));
+
+        $this->em->expects($this->any())->method('getDataSet')
+            ->willReturn($this->ds);
+
+        $this->global->expects($this->any())->method('getContainer')
+            ->willReturn($this->container);
+
+        $this->renderer = new GAnalyticsRenderer($this->global, $this->tpl);
+    }
+
+    public function serviceContainerCallback($name)
+    {
+        switch ($name) {
+            case 'orm.manager':
+                return $this->em;
+
+            case 'request_stack':
+                return $this->stack;
+        }
+
+        return null;
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\GAnalyticsRenderer::validate
+     */
+    public function testValidate()
+    {
+        $this->assertTrue($this->renderer->validate());
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\GAnalyticsRenderer::prepareParams
+     */
+    public function testPrepareParams()
+    {
+        $this->assertIsArray($this->renderer->prepareParams());
+    }
+}
