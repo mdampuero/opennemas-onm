9b8de52d1121be0098ecf8f346b5fddd485117db
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-03-26 17:26:38 +0100

    ONM-2804: Add tests for ComscoreRenderer

---
+++ tests/Frontend/Renderer/Statistics/ComscoreRendererTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Frontend\Renderer;
+
+use PHPUnit\Framework\TestCase;
+use Frontend\Renderer\Statistics\ComscoreRenderer;
+
+/**
+ * Defines test cases for ComscoreRenderer class.
+ */
+class ComscoreRendererTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->container = $this->getMockForAbstractClass('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $this->ds = $this->getMockForAbstractClass('Common\ORM\Core\DataSet');
+
+        $this->em = $this->getMockBuilder('Common\ORM\Core\EntityManager')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getDataSet' ])
+            ->getMock();
+
+        $this->global = $this->getMockBuilder('Common\Core\Component\Core\GlobalVariables')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getContainer' ])
+            ->getMock();
+
+        $this->stack = $this->getMockBuilder('Symfony\Component\HttpFoundation\RequestStack')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getCurrentRequest' ])
+            ->getMock();
+
+        $this->tpl = $this->getMockBuilder('Common\Core\Component\Template\Template')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'fetch' ])
+            ->getMock();
+
+        $this->container->expects($this->any())->method('get')
+            ->will($this->returnCallback([ $this, 'serviceContainerCallback' ]));
+
+        $this->em->expects($this->any())->method('getDataSet')
+            ->willReturn($this->ds);
+
+        $this->global->expects($this->any())->method('getContainer')
+            ->willReturn($this->container);
+
+        $this->renderer = new ComscoreRenderer($this->global, $this->tpl);
+    }
+
+    public function serviceContainerCallback($name)
+    {
+        switch ($name) {
+            case 'orm.manager':
+                return $this->em;
+
+            case 'request_stack':
+                return $this->stack;
+        }
+
+        return null;
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ComscoreRenderer::validate
+     */
+    public function testValidateWhenCorrectConfiguration()
+    {
+        $this->ds->expects($this->once())->method('get')
+            ->with('comscore')
+            ->willReturn([ 'page_id' => 9999 ]);
+
+        $this->assertTrue($this->renderer->validate());
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ComscoreRenderer::validate
+     */
+    public function testValidateWhenIncorrectConfiguration()
+    {
+        $this->ds->expects($this->once())->method('get')
+            ->with('comscore')
+            ->willReturn([]);
+
+        $this->assertFalse($this->renderer->validate());
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ComscoreRenderer::prepareParams
+     */
+    public function testPrepareParams()
+    {
+        $this->assertIsArray($this->renderer->prepareParams());
+    }
+}
