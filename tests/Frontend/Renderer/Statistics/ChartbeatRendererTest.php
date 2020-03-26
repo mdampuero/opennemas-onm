52638c93603ce312c605bbec4b4962373b78b3fa
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-03-26 19:00:41 +0100

    ONM-2804: Add tests for chartbeat renderer

---
+++ tests/Frontend/Renderer/Statistics/ChartbeatRendererTest.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Frontend\Renderer;
+
+use PHPUnit\Framework\TestCase;
+use Frontend\Renderer\Statistics\ChartbeatRenderer;
+use Common\ORM\Entity\Content;
+
+/**
+ * Defines test cases for ChartbeatRenderer class.
+ */
+class ChartbeatRendererTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->api = $this->getMockBuilder('Api\Service\V1\AuthorService')
+            ->disableOriginalConstructor()
+            ->setMethods(['getItem'])
+            ->getMock();
+
+        $this->container = $this->getMockForAbstractClass('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $this->ds = $this->getMockForAbstractClass('Common\ORM\Core\DataSet');
+
+        $this->em = $this->getMockBuilder('Common\ORM\Core\EntityManager')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getDataSet' ])
+            ->getMock();
+
+        $this->global = $this->getMockBuilder('Common\Core\Component\Core\GlobalVariables')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getContainer', 'getSection' ])
+            ->getMock();
+
+        $this->stack = $this->getMockBuilder('Symfony\Component\HttpFoundation\RequestStack')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getCurrentRequest' ])
+            ->getMock();
+
+        $this->tpl = $this->getMockBuilder('Common\Core\Component\Template\Template')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'fetch', 'getValue' ])
+            ->getMock();
+
+        $this->container->expects($this->any())->method('get')
+            ->will($this->returnCallback([ $this, 'serviceContainerCallback' ]));
+
+        $this->em->expects($this->any())->method('getDataSet')
+            ->willReturn($this->ds);
+
+        $this->global->expects($this->any())->method('getContainer')
+            ->willReturn($this->container);
+
+        $this->renderer = new ChartbeatRenderer($this->global, $this->tpl);
+    }
+
+    public function serviceContainerCallback($name)
+    {
+        switch ($name) {
+            case 'orm.manager':
+                return $this->em;
+
+            case 'request_stack':
+                return $this->stack;
+
+            case 'api.service.author':
+                return $this->api;
+        }
+
+        return null;
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ChartbeatRenderer::validate
+     */
+    public function testValidateWhenCorrectConfiguration()
+    {
+        $this->ds->expects($this->once())->method('get')
+            ->with('chartbeat')
+            ->willReturn([ 'id' => 9999, 'domain' => 'default.dev.opennemas.com' ]);
+
+        $this->assertTrue($this->renderer->validate());
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ChartbeatRenderer::validate
+     */
+    public function testValidateWhenIncorrectConfiguration()
+    {
+        $this->ds->expects($this->once())->method('get')
+            ->with('chartbeat')
+            ->willReturn([]);
+
+        $this->assertFalse($this->renderer->validate());
+    }
+
+    /**
+     * @covers \Frontend\Renderer\Statistics\ChartbeatRenderer::prepareParams
+     */
+    public function testPrepareParamsWhenContent()
+    {
+        $content = new Content([
+            'pk_content' => 1,
+            'fk_author'  => 2
+        ]);
+
+        $this->tpl->expects($this->once())->method('getValue')
+            ->with('content')
+            ->willReturn($content);
+
+        $this->api->expects($this->once())->method('getItem')
+            ->with($content->fk_author);
+
+        $this->assertIsArray($this->renderer->prepareParams());
+    }
+}
