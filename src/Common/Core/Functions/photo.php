426628d7f4be6beb24e6c4ebbee1703b668508b8
Sandra Pereira <sandra@openhost.es>
2020-05-08 11:42:24 +0200

    ONM-3940: Include Content and Photo functions and tests

---
+++ src/Common/Core/Functions/photo.php
@@ -0,0 +1,15 @@
+<?php
+
+function get_photo_path($item, string $transform = null, array $params = [])
+{
+    $url = getService('core.helper.url_generator')->generate($item);
+
+    if (empty($transform)) {
+        return $url;
+    }
+
+    return getService('router')->generate('asset_image', [
+        'params' => implode(',', array_merge([ $transform ], $params)),
+        'path'   => $url
+    ]);
+}
