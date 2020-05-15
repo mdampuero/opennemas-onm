dd24787300302825aaceecefd44cf2e9b7c41594
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-05-15 12:26:56 +0200

    ONM-6124: Add tests to ContentRenderer

---
+++ tests/Frontend/Renderer/Content/ContentRendererTest.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * This file is part of the Onm package.
+ *
+ * (c) Openhost, S.L. <developers@opennemas.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Frontend\Renderer\Content;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Frontend\Renderer\Content\ContentRenderer;
+use Mockery as m;
+use ReflectionClass;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ */
+class ContentRendererTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->container = $this->getMockBuilder('ServiceContainer')
+            ->setMethods([ 'get' ])
+            ->getMock();
+
+        $this->logger = $this->getMockBuilder('Logger')
+            ->setMethods([ 'error' ])
+            ->getMock();
+
+        $this->renderer = $this->getMockBuilder('Frontend\Renderer\Content\ContentRenderer')
+            ->setConstructorArgs([ $this->container ])
+            ->setMethods([ 'getTemplate' ])
+            ->getMock();
+
+        $this->template = $this->getMockBuilder('Common\Core\Component\Template\Template')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'fetch' ])
+            ->getMock();
+
+        $this->content = new \Content();
+
+        $this->container->expects($this->any())->method('get')
+            ->will($this->returnCallback([ $this, 'serviceContainerCallback' ]));
+    }
+
+
+    public function serviceContainerCallback($name)
+    {
+        switch ($name) {
+            case 'core.template':
+                return $this->template;
+            case 'error.log':
+                return $this->logger;
+        }
+
+        return null;
+    }
+
+    /**
+     * Tests render when template exists.
+     */
+    public function testRenderWhenTemplate()
+    {
+        $this->renderer->expects($this->at(0))->method('getTemplate')
+            ->with([ 'item' => $this->content ])
+            ->willReturn('template.tpl');
+
+        $this->template->expects($this->at(0))->method('fetch')
+            ->with('template.tpl');
+
+        $this->renderer->render($this->content, []);
+    }
+
+    /**
+     * Tests render when template doesn't exists.
+     */
+    public function testRenderWhenNoTemplate()
+    {
+        $this->renderer->expects($this->at(0))->method('getTemplate')
+            ->with([ 'item' => $this->content ])
+            ->willReturn('template.tpl');
+
+        $this->template->expects($this->at(0))->method('fetch')
+            ->with('template.tpl')
+            ->will($this->throwException(new Exception()));
+
+        $this->assertEquals(_('Content not available'), $this->renderer->render($this->content, []));
+    }
+
+    /**
+     * Tests getTemplate when content is an article.
+     */
+    public function testGetTemplateWhenArticle()
+    {
+        $article        = new \Article();
+        $params['item'] = $article;
+        $params['tpl']  = 'frontpage/contents/_custom_article.tpl';
+
+        $renderer = new ContentRenderer($this->container);
+        $method   = new \ReflectionMethod($renderer, 'getTemplate');
+        $method->setAccessible(true);
+
+        $this->assertEquals('frontpage/contents/_custom_article.tpl', $method->invokeArgs($renderer, [ &$params ]));
+    }
+
+    /**
+     * Tests getTemplate when content is an opinion.
+     */
+    public function testGetTemplateWhenOpinion()
+    {
+        $opinion        = new \Opinion();
+        $params['item'] = $opinion;
+        $tpl            = 'frontpage/contents/_opinion.tpl';
+
+        $renderer = new ContentRenderer($this->container);
+        $method   = new \ReflectionMethod($renderer, 'getTemplate');
+        $method->setAccessible(true);
+
+        $this->assertEquals($tpl, $method->invokeArgs($renderer, [ &$params ]));
+    }
+
+    /**
+     * Test getTemplate when content is a letter.
+     */
+    public function testGetTemplateWhenLetter()
+    {
+        $letter         = new \Letter();
+        $params['item'] = $letter;
+        $tpl            = 'frontpage/contents/_content.tpl';
+
+        $renderer = new ContentRenderer($this->container);
+        $method   = new \ReflectionMethod($renderer, 'getTemplate');
+        $method->setAccessible(true);
+
+        $this->assertEquals($tpl, $method->invokeArgs($renderer, [ &$params ]));
+    }
+}
