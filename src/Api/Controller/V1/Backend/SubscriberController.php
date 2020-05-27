ceca5a76fdc7ffbaced42d12b27f70d24b7a3995
Alex Rodrigues <alex@openhost.es>
2020-05-27 17:38:51 +0200

    ONM-6150: Remove phpdoc for license

--- src/Api/Controller/V1/Backend/SubscriberController.php
+++ src/Api/Controller/V1/Backend/SubscriberController.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * This file is part of the Onm package.
- *
- * (c)  OpenHost S.L. <developers@opennemas.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 namespace Api\Controller\V1\Backend;
 
 use Api\Controller\V1\ApiController;
