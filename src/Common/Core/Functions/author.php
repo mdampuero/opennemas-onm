153f4fce03e1f36d47e20be2d752710ef07aa610
Sandra Pereira <sandra@openhost.es>
2020-07-02 15:10:53 +0200

    ONM-3940: Sort the author functions alfabetically

--- src/Common/Core/Functions/author.php
+++ src/Common/Core/Functions/author.php
@@ -34,22 +34,6 @@ function get_author($item = null) : ?\Common\Model\Entity\User
 }
 
 /**
- * Returns the author id.
- *
- * @param mixed $item The item to get author. If not
- *                    provided, the function will try to search the item in the
- *                    template.
- *
- * @return int The author avatar id.
- */
-function get_author_id($item = null) : ?int
-{
-    $author = get_author($item);
-
-    return !empty($author) ? $author->id : null;
-}
-
-/**
  * Returns the id for the author avatar.
  *
  * @param mixed $item The item to get author avatar for or an author. If not
@@ -63,6 +47,52 @@ function get_author_avatar($item = null) : ?int
     $author = get_author($item);
 
     return !empty($author) ? $author->avatar_img_id : null;
+}
+
+/**
+ * Returns the long author biography for the provided item.
+ *
+ * @param Content $item The item to get author biography for. If not provided, the
+ *                      function will try to search the item in the template.
+ *
+ * @return string The author bio if author is present. Null otherwise.
+ */
+function get_author_bio_body($item = null) : ?string
+{
+    $author = get_author($item);
+
+    return !empty($author->bio_description) ? $author->bio_description : null;
+}
+
+/**
+ * Returns the short author bio for the provided item.
+ *
+ * @param Content $item The item to get author slug for. If not provided, the
+ *                      function will try to search the item in the template.
+ *
+ * @return string The author bio if author is present. Null otherwise.
+ */
+function get_author_bio_summary($item = null) : ?string
+{
+    $author = get_author($item);
+
+    return !empty($author->bio) ? $author->bio : null;
+}
+
+/**
+ * Returns the author id.
+ *
+ * @param mixed $item The item to get author. If not
+ *                    provided, the function will try to search the item in the
+ *                    template.
+ *
+ * @return int The author avatar id.
+ */
+function get_author_id($item = null) : ?int
+{
+    $author = get_author($item);
+
+    return !empty($author) ? $author->id : null;
 }
 
 /**
@@ -100,6 +130,35 @@ function get_author_name($item = null) : ?string
 }
 
 /**
+ * Returns the relative URL to the RSS page of the author for the
+ * provided item.
+ *
+ * @param Content $item The item to get URL for. If not provided, the
+ *                      function will try to search the item in the template.
+ *
+ * @return string The rss URL page to the automatic frontpage of the author.
+ */
+function get_author_rss_url($item = null) : ?string
+{
+    $author = get_author($item);
+
+    if (!empty($author)) {
+        $routeName   = 'frontend_rss_author';
+        $routeParams = [
+            'author_slug' => $author->slug,
+        ];
+
+        if ($author->inrss) {
+            $url = getService('router')->generate($routeName, $routeParams);
+
+            return !empty($url) ? $url : null;
+        }
+    }
+
+    return null;
+}
+
+/**
  * Returns the author slug for the provided item.
  *
  * @param Content $item The item to get author slug for. If not provided, the
@@ -118,33 +177,18 @@ function get_author_slug($item = null) : ?string
 }
 
 /**
- * Returns the short author bio for the provided item.
+ * Returns the author twitter url for the provided item.
  *
- * @param Content $item The item to get author slug for. If not provided, the
+ * @param Content $item The item to get author facebook url for. If not provided, the
  *                      function will try to search the item in the template.
  *
- * @return string The author bio if author is present. Null otherwise.
+ * @return string The facebook url if author is present. Null otherwise.
  */
-function get_author_bio_summary($item = null) : ?string
+function get_author_social_facebook_url($item = null) : ?string
 {
     $author = get_author($item);
 
-    return !empty($author->bio) ? $author->bio : null;
-}
-
-/**
- * Returns the long author biography for the provided item.
- *
- * @param Content $item The item to get author biography for. If not provided, the
- *                      function will try to search the item in the template.
- *
- * @return string The author bio if author is present. Null otherwise.
- */
-function get_author_bio_body($item = null) : ?string
-{
-    $author = get_author($item);
-
-    return !empty($author->bio_description) ? $author->bio_description : null;
+    return !empty($author->facebook) ? ("https://www.facebook.com/" . $author->facebook) : null;
 }
 
 /**
@@ -160,21 +204,6 @@ function get_author_social_twitter_url($item = null) : ?string
     $author = get_author($item);
 
     return !empty($author->twitter) ? ("https://www.twitter.com/" . $author->twitter) : null;
-}
-
-/**
- * Returns the author twitter url for the provided item.
- *
- * @param Content $item The item to get author facebook url for. If not provided, the
- *                      function will try to search the item in the template.
- *
- * @return string The facebook url if author is present. Null otherwise.
- */
-function get_author_social_facebook_url($item = null) : ?string
-{
-    $author = get_author($item);
-
-    return !empty($author->facebook) ? ("https://www.facebook.com/" . $author->facebook) : null;
 }
 
 /**
@@ -209,86 +238,6 @@ function get_author_url($item = null) : ?string
 }
 
 /**
- * Returns the relative URL to the RSS page of the author for the
- * provided item.
- *
- * @param Content $item The item to get URL for. If not provided, the
- *                      function will try to search the item in the template.
- *
- * @return string The rss URL page to the automatic frontpage of the author.
- */
-function get_author_rss_url($item = null) : ?string
-{
-    $author = get_author($item);
-
-    if (!empty($author)) {
-        $routeName   = 'frontend_rss_author';
-        $routeParams = [
-            'author_slug' => $author->slug,
-        ];
-
-        if ($author->inrss) {
-            $url = getService('router')->generate($routeName, $routeParams);
-
-            return !empty($url) ? $url : null;
-        }
-    }
-
-    return null;
-}
-
-/**
- * Checks if if the author is configured as blogger based on a content or author
- * provided as parameter
- *
- * @param mixed $item The item to get author is_blog property. If not
- *                    provided, the function will try to search the item in the
- *                    template.
- *
- * @return int The author avatar id.
- */
-function is_blog($item = null) : bool
-{
-    $author = get_author($item);
-
-    return !empty($author->is_blog) ? $author->is_blog : false;
-}
-
-/**
- * Checks if if the author has a slug defined based on a content or author
- * provided as parameter
- *
- * @param mixed $item The item to get author slug property. If not
- *                    provided, the function will try to search the item in the
- *                    template.
- *
- * @return int The author avatar id.
- */
-function has_author_slug($item = null) : bool
-{
-    $slug = get_author_slug($item);
-
-    return !empty($slug);
-}
-
-/**
- * Checks if if the author has a rss url defined based on a content or author
- * provided as parameter
- *
- * @param mixed $item The item to get author rss url property. If not
- *                    provided, the function will try to search the item in the
- *                    template.
- *
- * @return int The author avatar id.
- */
-function has_author_rss_url($item = null) : bool
-{
-    $url = get_author_rss_url($item);
-
-    return !empty($url);
-}
-
-/**
  * Checks if there is an author based on a content or author provided as
  * parameter.
  *
@@ -301,21 +250,6 @@ function has_author_rss_url($item = null) : bool
 function has_author($item = null) : bool
 {
     return !empty(get_author_name($item));
-}
-
-/**
- * Checks if the author has an url based on a content or author provided as
- * parameter.
- *
- * @param mixed $item The item to check author for or the author. If
- *                    not provided, the function will try to search the item in
- *                    the template.
- *
- * @return bool True if the author exists. False otherwise.
- */
-function has_author_url($item = null) : bool
-{
-    return !empty(get_author_url($item));
 }
 
 /**
@@ -337,6 +271,21 @@ function has_author_avatar($item = null) : bool
  * Checks if the author has a bio defined as
  * parameter.
  *
+ * @param mixed $item The item to check author's long bio for or the author. If
+ *                    not provided, the function will try to search the item in
+ *                    the template.
+ *
+ * @return bool True if the author has a long biography defined. False otherwise.
+ */
+function has_author_bio_body($item = null) : bool
+{
+    return !empty(get_author_bio_body($item));
+}
+
+/**
+ * Checks if the author has a bio defined as
+ * parameter.
+ *
  * @param mixed $item The item to check author's bio for or the author. If
  *                    not provided, the function will try to search the item in
  *                    the template.
@@ -349,18 +298,52 @@ function has_author_bio_summary($item = null) : bool
 }
 
 /**
- * Checks if the author has a bio defined as
+ * Checks if if the author has a rss url defined based on a content or author
+ * provided as parameter
+ *
+ * @param mixed $item The item to get author rss url property. If not
+ *                    provided, the function will try to search the item in the
+ *                    template.
+ *
+ * @return int The author avatar id.
+ */
+function has_author_rss_url($item = null) : bool
+{
+    $url = get_author_rss_url($item);
+
+    return !empty($url);
+}
+
+/**
+ * Checks if if the author has a slug defined based on a content or author
+ * provided as parameter
+ *
+ * @param mixed $item The item to get author slug property. If not
+ *                    provided, the function will try to search the item in the
+ *                    template.
+ *
+ * @return int The author avatar id.
+ */
+function has_author_slug($item = null) : bool
+{
+    $slug = get_author_slug($item);
+
+    return !empty($slug);
+}
+
+/**
+ * Checks if the author has a facebook account provided as
  * parameter.
  *
- * @param mixed $item The item to check author's long bio for or the author. If
+ * @param mixed $item The item to check author's facebook account for or the author. If
  *                    not provided, the function will try to search the item in
  *                    the template.
  *
- * @return bool True if the author has a long biography defined. False otherwise.
+ * @return bool True if the author has a facebook account defined. False otherwise.
  */
-function has_author_bio_body($item = null) : bool
+function has_author_social_facebook_url($item = null) : bool
 {
-    return !empty(get_author_bio_body($item));
+    return !empty(get_author_social_facebook_url($item));
 }
 
 /**
@@ -379,16 +362,33 @@ function has_author_social_twitter_url($item = null) : bool
 }
 
 /**
- * Checks if the author has a facebook account provided as
+ * Checks if the author has an url based on a content or author provided as
  * parameter.
  *
- * @param mixed $item The item to check author's facebook account for or the author. If
+ * @param mixed $item The item to check author for or the author. If
  *                    not provided, the function will try to search the item in
  *                    the template.
  *
- * @return bool True if the author has a facebook account defined. False otherwise.
+ * @return bool True if the author exists. False otherwise.
  */
-function has_author_social_facebook_url($item = null) : bool
+function has_author_url($item = null) : bool
 {
-    return !empty(get_author_social_facebook_url($item));
+    return !empty(get_author_url($item));
+}
+
+/**
+ * Checks if if the author is configured as blogger based on a content or author
+ * provided as parameter
+ *
+ * @param mixed $item The item to get author is_blog property. If not
+ *                    provided, the function will try to search the item in the
+ *                    template.
+ *
+ * @return int The author avatar id.
+ */
+function is_blog($item = null) : bool
+{
+    $author = get_author($item);
+
+    return !empty($author->is_blog) ? $author->is_blog : false;
 }
