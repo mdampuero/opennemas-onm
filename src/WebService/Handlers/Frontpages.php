7a92148a09ae122323f54d4a5adc2809c6651ce4
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-07-10 14:50:46 +0200

    ONM-6008: Fix indentation

--- src/WebService/Handlers/Frontpages.php
+++ src/WebService/Handlers/Frontpages.php
@@ -146,7 +146,7 @@ class Frontpages
             'content_type_name' => [[ 'value' => 'article' ]],
             'content_status'    => [[ 'value' => 1 ]],
             'in_litter'         => [[ 'value' => 1, 'operator' => '!=' ]],
-            'category_id'              => [ [ 'value' => $category->id ] ],
+            'category_id'       => [ [ 'value' => $category->id ] ],
             'starttime'         => [
                 'union' => 'OR',
                 [ 'value' => '0000-00-00 00:00:00' ],
