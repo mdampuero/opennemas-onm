3b42fe812dc74af205bbe52af95b4ec0737515e7
Sandra Pereira <sandra@openhost.es>
2020-09-01 11:41:47 +0200

    ONM-6154: Amend get photo path function

--- src/Common/Core/Functions/photo.php
+++ src/Common/Core/Functions/photo.php
@@ -2,8 +2,6 @@
 
 function get_photo_path($item, string $transform = null, array $params = [])
 {
-    $item = get_content($item, 'Photo');
-
     $url = getService('core.helper.url_generator')->generate($item);
 
     if (empty($transform)) {
