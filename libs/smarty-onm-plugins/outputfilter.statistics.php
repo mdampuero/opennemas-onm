258dfb878dfbf2e84e245d2b207125ee24cc4d9c
Alex Rodrigues <alex@openhost.es>
2020-09-02 09:47:39 +0200

    ONM-6344: Improve statistics filter regex

--- libs/smarty-onm-plugins/outputfilter.statistics.php
+++ libs/smarty-onm-plugins/outputfilter.statistics.php
@@ -13,7 +13,7 @@ function smarty_outputfilter_statistics($output, $smarty)
     $content = $smarty->getValue('content');
 
     if (is_null($request)
-        || !preg_match('/<html.*?>.*<head>.*<\/head>.*<\/html>/s', $output)) {
+        || !preg_match('/<!doctype html>.*<\/html>/s', $output)) {
         return $output;
     }
 
