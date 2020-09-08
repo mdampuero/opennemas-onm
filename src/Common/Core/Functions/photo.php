04f485d2da7192ce3f0695d04ab5c1174831d81d
Diego Blanco <diego@openhost.es>
2020-09-08 14:56:31 +0200

    ONM-6309: Update get_photo_path to support strings and do not transform URLs

--- src/Common/Core/Functions/photo.php
+++ src/Common/Core/Functions/photo.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * Returns the URL for a photo.
+ *
+ * @param Content $item      The photo to generate path for.
+ * @param string  $transform The transform to apply.
+ * @param array   $params    The list of parameters for the transform.
+ *
+ * @return string The URL for the image.
+ */
 function get_photo_path($item, string $transform = null, array $params = [])
 {
+    if (is_string($item)) {
+        return $item;
+    }
+
     $url = getService('core.helper.url_generator')->generate($item);
 
-    if (empty($transform)) {
+    // Do not transform if empty or external photo
+    if (empty($transform) || preg_match('/^https?.*/', $url)) {
         return $url;
     }
 
