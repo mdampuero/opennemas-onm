8588ed2652a08b175c1f9dcc54f82a176c1d3445
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-10-20 11:58:20 +0200

    ONM-6154: Fix logic problems

--- src/Common/Core/Functions/content.php
+++ src/Common/Core/Functions/content.php
@@ -265,8 +265,13 @@ function get_related($item, string $type) : array
 
     return array_filter(array_map(function ($a) {
         $content = get_content($a['target_id'], $a['content_type_name']);
+
         //TODO: Remove this on ticket ONM-6166
-        return !empty($content) && $content instanceof \Content ? $content->loadAttachedVideo() : null;
+        if ($content instanceof \Content) {
+            $content = $content->loadAttachedVideo();
+        }
+
+        return !empty($content) ? $content : null;
     }, $items));
 }
 
