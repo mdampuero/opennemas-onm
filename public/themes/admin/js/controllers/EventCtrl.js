03d7503b99b7f7776980f9d3ad04af02fb6660d3
Diego Blanco <diego@openhost.es>
2020-11-19 16:32:45 +0100

    ONM-6183: Use service and template component for featured media in event

--- public/themes/admin/js/controllers/EventCtrl.js
+++ public/themes/admin/js/controllers/EventCtrl.js
@@ -15,8 +15,8 @@
      *   Check billing information when saving user.
      */
     .controller('EventCtrl', [
-      '$controller', '$scope', 'linker', 'localizer', 'routing',
-      function($controller, $scope, linker, localizer, routing) {
+      '$controller', '$scope', 'related', 'routing',
+      function($controller, $scope, related, routing) {
         $.extend(this, $controller('ContentRestInnerCtrl', { $scope: $scope }));
 
         /**
@@ -57,26 +57,6 @@
          * @memberOf EventCtrl
          *
          * @description
-         *  The map for related contents.
-         *
-         * @type {Object}
-         */
-        $scope.relatedMap = {
-          featured_frontpage: {
-            name:        'featuredFrontpage',
-            replicateOn: 'featured_inner',
-            simple:      true
-          },
-          featured_inner: {
-            name:   'featuredInner',
-            simple: true
-          },
-        };
-
-        /**
-         * @memberOf EventCtrl
-         *
-         * @description
          *  The list of routes for the controller.
          *
          * @type {Object}
@@ -92,132 +72,19 @@
         };
 
         /**
-         * @function buildRelated
-         * @memberOf EventCtrl
-         *
-         * @description
-         *   Initializes the scope with the list of related contents and
-         *   defines watchers to update the model on changes.
-         */
-        $scope.buildRelated = function() {
-          $scope.item.related_contents = [];
-
-          for (var i = 0; i < $scope.data.item.related_contents.length; i++) {
-            var related = $scope.data.item.related_contents[i];
-
-            $scope.item.related_contents.push($scope.localizeRelated(related, i));
-
-            var simple = $scope.relatedMap[related.type].simple;
-            var name   = $scope.relatedMap[related.type].name;
-            var item   = $scope.data.extra.related_contents[related.target_id];
-
-            if (!simple) {
-              if (!$scope.name) {
-                $scope[name] = [];
-              }
-
-              $scope[name].push(item);
-              continue;
-            }
-
-            $scope[name] = item;
-          }
-
-          // Updates related contents after insertion via content picker
-          $scope.$watch('[ featuredFrontpage, featuredInner ]', function(nv, ov) {
-            for (var i = 0; i < nv.length; i++) {
-              var type = Object.keys($scope.relatedMap)[i];
-
-              if (angular.equals(nv[i], ov[i])) {
-                continue;
-              }
-
-              var caption     = null;
-              var removedItem = null;
-
-              if ($scope.relatedMap[type].simple) {
-                if (ov[i]) {
-                  // Try to keep caption from old item
-                  removedItem = $scope.data.item.related_contents.filter(function(e) {
-                    return e.type === type;
-                  }).shift();
-
-                  caption     = removedItem ? removedItem.caption : null;
-                  removedItem = removedItem ?
-                    $scope.data.extra.related_contents[removedItem.target_id] :
-                    null;
-                }
-
-                // Remove from unlocalized
-                $scope.data.item.related_contents =
-                  $scope.data.item.related_contents.filter(function(e) {
-                    return e.type !== type;
-                  });
-
-                // Remove from localized
-                $scope.item.related_contents =
-                  $scope.item.related_contents.filter(function(e) {
-                    return e.type !== type;
-                  });
-              }
-
-              if (!nv[i]) {
-                continue;
-              }
-
-              var items = $scope.relatedMap[type].simple ? [ nv[i] ] : nv[i];
-
-              for (var j = 0; j < items.length; j++) {
-                // Add content to map of contents
-                if (!$scope.data.extra.related_contents[items[j].pk_content]) {
-                  $scope.data.extra.related_contents[items[j].pk_content] = items[j];
-                }
-
-                /**
-                 * Override caption when adding new item or when caption matches
-                 * the removed item description
-                 */
-                if (!removedItem || removedItem.description === caption) {
-                  caption = items[j].description;
-                }
-
-                var related = {
-                  caption:           caption,
-                  content_type_name: items[j].content_type_name,
-                  position:          j,
-                  target_id:         items[j].pk_content,
-                  type:              type
-                };
-
-                $scope.data.item.related_contents.push(related);
-                $scope.item.related_contents.push($scope.localizeRelated(related, j));
-              }
-
-              // Copy current item to another item
-              if ($scope.relatedMap[type].replicateOn) {
-                var replicated = $scope.relatedMap[$scope.relatedMap[type].replicateOn];
-
-                if (!$scope[replicated.name] ||
-                    angular.equals($scope[replicated.name], ov[i])) {
-                  $scope[replicated.name] = angular.copy(nv[i]);
-                }
-              }
-            }
-          }, true);
-        };
-
-        /**
          * @inheritdoc
          */
         $scope.buildScope = function() {
           $scope.localize($scope.data.item, 'item', true, [ 'related_contents' ]);
 
-          // Check if item is new (created) or existing for use default value or not
+          // Use default value for new items
           if (!$scope.data.item.pk_content) {
-            $scope.item.with_comment = $scope.data.extra.comments_enabled ? 1 : 0;
+            $scope.data.item.with_comment =
+              $scope.data.extra.comments_enabled ? 1 : 0;
           }
 
-          $scope.buildRelated();
+          related.init($scope);
+          related.watch();
         };
 
         /**
@@ -237,54 +104,6 @@
               slug: item.slug,
             })
           );
-        };
-
-        /**
-         * @function getRelated
-         * @memberOf EventCtrl
-         *
-         * @description
-         *   Returns the related content based on the type.
-         *
-         * @param {String} type The related type.
-         *
-         * @return {Object} The related content.
-         */
-        $scope.getRelated = function(type) {
-          for (var i = 0; i < $scope.item.related_contents.length; i++) {
-            if ($scope.item.related_contents[i].type === type) {
-              return $scope.item.related_contents[i];
-            }
-          }
-
-          return null;
-        };
-
-        /**
-         * @function localizeRelated
-         * @memberOf EventCtrl
-         *
-         * @description
-         *   Localizes a related content.
-         *
-         * @param {Object} original The content to localize.
-         *
-         * @return {Object} The localized content.
-         */
-        $scope.localizeRelated = function(original, index) {
-          var localized = localizer.get($scope.config.locale).localize(original,
-            [ 'caption' ], $scope.config.locale);
-
-          // Initialize linker
-          delete $scope.config.linkers[index];
-          $scope.config.linkers[index] = linker.get([ 'caption' ],
-            $scope.config.locale.default, $scope, true);
-
-          // Link original and localized items
-          $scope.config.linkers[index].setKey($scope.config.locale.selected);
-          $scope.config.linkers[index].link(original, localized);
-
-          return localized;
         };
       }
     ]);
