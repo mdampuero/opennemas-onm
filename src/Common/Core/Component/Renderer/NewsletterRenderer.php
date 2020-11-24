1d17a33fa020f3535a20347abdb8d0fe39d8a0a5
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2020-11-24 17:26:14 +0100

    ONM-6166: Remove unnecessary hydrate method

--- src/Common/Core/Component/Renderer/NewsletterRenderer.php
+++ src/Common/Core/Component/Renderer/NewsletterRenderer.php
@@ -94,8 +94,6 @@ class NewsletterRenderer
                     if (!is_object($content) || is_null($content->id)) {
                         continue;
                     }
-
-                    $item = $this->hydrateContent($content);
                 }
             }
 
@@ -133,36 +131,6 @@ class NewsletterRenderer
             'conf'              => $configurations,
             'URL_PUBLIC'        => 'http://' . $publicUrl,
         ]);
-    }
-
-    /**
-     * Completes the content information from an object from repository
-     *
-     * @param Content $item    The item to complete
-     * @param Content $content The content from the repository
-     *
-     * @return Content The complete item
-     */
-    public function hydrateContent($content)
-    {
-        $content->name  = (isset($content->name)) ? $content->name : '';
-        $content->image = (isset($content->cover)) ? $content->cover : '';
-
-        // Fetch images of articles if exists
-        if (!empty($content->fk_video)) {
-            $content->video = $this->er->find('Video', $content->fk_video);
-        }
-
-        if (!isset($content->summary)) {
-            $content->summary = substr(strip_tags($content->body), 0, 250) . '...';
-        }
-
-        // Fetch opinion author photos
-        if ($content->content_type == '4') {
-            $content->author = $this->er->find('User', $content->fk_author);
-        }
-
-        return $content;
     }
 
     /**
@@ -271,10 +239,6 @@ class NewsletterRenderer
         }
 
         $contents = $this->er->findBy($searchCriteria, $orderBy, $total, 1);
-
-        foreach ($contents as &$content) {
-            $content = $this->hydrateContent($content);
-        }
 
         return $contents;
     }
