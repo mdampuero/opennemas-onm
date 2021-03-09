2d5014f2e5881084f241ee57ec22a93d1214c180
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2021-03-09 11:59:23 +0100

    ONM-6810: Fix problem with meta and update tests

--- libs/smarty-onm-plugins/outputfilter.meta_amphtml.php
+++ libs/smarty-onm-plugins/outputfilter.meta_amphtml.php
@@ -13,19 +13,14 @@ function smarty_outputfilter_meta_amphtml($output, $smarty)
 {
     $container           = $smarty->getContainer();
     $request             = $container->get('request_stack')->getCurrentRequest();
-    $templateVars        = $smarty->getTemplateVars();
+    $content             = $smarty->getValue('o_content');
     $allowedContentTypes = [ 'album', 'article', 'opinion', 'poll', 'video' ];
-    $content             =
-        is_array($templateVars) && array_key_exists('o_content', $templateVars)
-        ? $templateVars['o_content']
-        : null;
     $tpl                 = '<link rel="amphtml" href="%s"/>';
 
-    if (!$container->get('core.security')->hasExtension('AMP_MODULE')
+    if (empty($content)
+        || !$container->get('core.security')->hasExtension('AMP_MODULE')
         || empty($request)
         || strpos($request->getRequestUri(), 'amp.html') !== false
-        || !is_object($content)
-        || !property_exists($content, 'pk_content')
         || !in_array($content->content_type_name, $allowedContentTypes)
     ) {
         return $output;
