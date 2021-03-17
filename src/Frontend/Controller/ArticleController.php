7752542a92b02ab48a6a2d8feb4f9fe394b0d78c
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2021-03-17 17:06:47 +0100

    ONM-6781: Remove unused parameter

--- src/Frontend/Controller/ArticleController.php
+++ src/Frontend/Controller/ArticleController.php
@@ -117,14 +117,6 @@ class ArticleController extends FrontendController
     {
         $params = parent::getParameters($params, $item);
 
-        if (array_key_exists('o_category', $params) && !empty($params['o_category'])) {
-            $params['o_layout'] = $this->get('orm.manager')
-                ->getDataSet('Settings', 'instance')->get(
-                    'frontpage_layout_' . $params['o_category']->id,
-                    'default'
-                );
-        }
-
         if (!empty($item)) {
             $params[$item->content_type_name] = $item;
 
