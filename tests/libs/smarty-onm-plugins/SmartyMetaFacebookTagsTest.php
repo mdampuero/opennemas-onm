6b0177eb69ab5d35b29cd817fe4f9caf813e2da3
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2021-05-11 12:54:17 +0200

    ONM-6869: Complete tests coverage

--- tests/libs/smarty-onm-plugins/SmartyMetaFacebookTagsTest.php
+++ tests/libs/smarty-onm-plugins/SmartyMetaFacebookTagsTest.php
@@ -55,6 +55,11 @@ class SmartyMetaFacebookTagsTest extends \PHPUnit\Framework\TestCase
             ->setMethods([ 'getPhotoPath' ])
             ->getMock();
 
+        $this->videoHelper = $this->getMockBuilder('Common\Core\Component\Helper\VideoHelper')
+            ->disableOriginalConstructor()
+            ->setMethods([ 'getVideoThumbnail' ])
+            ->getMock();
+
         $this->requestStack = $this->getMockBuilder('RequestStack')
             ->setMethods([ 'getCurrentRequest' ])
             ->getMock();
@@ -112,6 +117,9 @@ class SmartyMetaFacebookTagsTest extends \PHPUnit\Framework\TestCase
 
             case 'core.helper.photo':
                 return $this->photoHelper;
+
+            case 'core.helper.video':
+                return $this->videoHelper;
 
             case 'core.instance':
                 return $this->instance;
@@ -303,6 +311,63 @@ class SmartyMetaFacebookTagsTest extends \PHPUnit\Framework\TestCase
         $photo->height = 400;
 
         $this->helper->expects($this->once())->method('getMedia')
+            ->willReturn($photo);
+
+        $this->photoHelper->expects($this->once())->method('getPhotoPath')
+            ->with($photo, null, [], true)
+            ->willReturn('http://route/to/file.name');
+
+        $output = "<meta property=\"og:type\" content=\"website\" />\n"
+            . "<meta property=\"og:title\" content=\"This is the title\" />\n"
+            . "<meta property=\"og:description\" content=\"This is the summary\" />\n"
+            . "<meta property=\"og:url\" content=\"http://route/to/content.html\" />\n"
+            . "<meta property=\"og:site_name\" content=\"Site Name\" />\n"
+            . "<meta property=\"og:image\" content=\"http://route/to/file.name\" />\n"
+            . "<meta property=\"og:image:width\" content=\"600\"/>\n"
+            . "<meta property=\"og:image:height\" content=\"400\"/>";
+
+        $this->assertEquals(
+            $output,
+            smarty_function_meta_facebook_tags(null, $this->smarty)
+        );
+    }
+
+    /**
+     * Tests smarty_function_meta_facebook_tags when content and video
+     */
+    public function testMetaFacebookWhenContentAndVideo()
+    {
+        $content          = new \Content();
+        $content->title   = 'This is the title';
+        $content->summary = 'This is the summary';
+        $content->body    = 'This is the body';
+
+        $this->smarty->expects($this->at(1))->method('getValue')
+            ->with('content')
+            ->willReturn($content);
+
+        $this->ds->expects($this->at(0))->method('get')->with('site_title')
+            ->willReturn('Site title');
+        $this->ds->expects($this->at(1))->method('get')->with('site_description')
+            ->willReturn('Site description');
+        $this->ds->expects($this->at(2))->method('get')->with('site_name')
+            ->willReturn('Site Name');
+
+        // Video object
+        $video                    = new \Content();
+        $video->width             = 600;
+        $video->height            = 400;
+        $video->content_type_name = 'video';
+
+        // Photo object
+        $photo      = new \Content();
+        $photo->url = 'http://route/to/file.name';
+
+        $this->helper->expects($this->once())->method('getMedia')
+            ->willReturn($video);
+
+        $this->videoHelper->expects($this->once())->method('getVideoThumbnail')
+            ->with($video)
             ->willReturn($photo);
 
         $this->photoHelper->expects($this->once())->method('getPhotoPath')
