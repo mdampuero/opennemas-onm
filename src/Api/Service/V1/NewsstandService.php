858e4afd4b7e532e02d0d57fe50d586248fbaf58
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2021-08-27 10:20:56 +0200

    ONM-7030: Remove thumbnail property from item

--- src/Api/Service/V1/NewsstandService.php
+++ src/Api/Service/V1/NewsstandService.php
@@ -35,15 +35,14 @@ class NewsstandService extends ContentService
 
             $item = new $this->class($data);
 
-            $nh            = $this->container->get('core.helper.newsstand');
-            $filePath      = $nh->generatePath($file, $item->created);
+            $nh       = $this->container->get('core.helper.newsstand');
+            $filePath = $nh->generatePath($file, $item->created);
 
             if ($nh->exists($filePath)) {
                 throw new FileAlreadyExistsException();
             }
 
-            $item->path      = $nh->getRelativePath($filePath);
-            $item->thumbnail = str_replace('pdf', 'jpg', $item->path);
+            $item->path = $nh->getRelativePath($filePath);
 
             $photo = $this->container->get('api.service.photo')->createItem([], $thumbnail);
 
@@ -96,12 +95,9 @@ class NewsstandService extends ContentService
                 $data['path'] = $nh->getRelativePath($filePath);
 
                 $nh->remove($item->path);
-//                $nh->remove($item->thumbnail);
                 $nh->move($file, $filePath);
-//                $nh->move($thumbnail, $thumbnailPath);
 
-                $item->path      = $nh->getRelativePath($filePath);
-                $item->thumbnail = str_replace('pdf', 'jpg', $item->path);
+                $item->path = $nh->getRelativePath($filePath);
 
                 $photo = $this->container->get('api.service.photo')->createItem([], $thumbnail);
 
