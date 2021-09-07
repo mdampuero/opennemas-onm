a06154247ce55e03f601d3f9a52d21a43f38734d
Alex Rodrigues <alex@openhost.es>
2021-09-07 17:00:20 +0200

    ONM-7166: Fix cache delete for non content_type object

--- src/Framework/EventListener/HooksSubscriber.php
+++ src/Framework/EventListener/HooksSubscriber.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * This file is part of the Onm package.
- *
- * (c)  OpenHost S.L. <developers@openhost.es>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 namespace Framework\EventListener;
 
 use Api\Exception\GetItemException;
@@ -230,7 +223,7 @@ class HooksSubscriber implements EventSubscriberInterface
                 return;
             }
 
-            $this->cache->delete(get_class($object) . '-' . $object->id);
+            $this->cache->delete(\underscore(get_class($object)) . '-' . $object->id);
         }
     }
 
