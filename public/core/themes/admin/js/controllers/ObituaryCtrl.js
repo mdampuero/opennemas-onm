83bb6c4b3583bb0fd796ac7497edf443138cc251
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2021-11-23 12:05:39 +0100

    ONM-7342: Add angular controllers for obituaries

---
+++ public/core/themes/admin/js/controllers/ObituaryCtrl.js
@@ -0,0 +1,194 @@
+(function() {
+  'use strict';
+
+  angular.module('BackendApp.controllers')
+
+    /**
+     * @ngdoc controller
+     * @name  ObituaryCtrl
+     *
+     * @requires $controller
+     * @requires $scope
+     * @requires $timeout
+     * @requires $uibModal
+     * @requires $window
+     * @requires cleaner
+     * @requires http
+     * @requires linker
+     * @requires localizer
+     * @requires messenger
+     * @requires webStorage
+     *
+     * @description
+     *   Provides actions to edit, save and update obituaries.
+     */
+    .controller('ObituaryCtrl', [
+      '$controller', '$scope', '$timeout', '$uibModal', '$window', 'cleaner',
+      'http', 'related', 'routing', 'translator',
+      function($controller, $scope, $timeout, $uibModal, $window, cleaner,
+          http, related, routing, translator) {
+        // Initialize the super class and extend it.
+        $.extend(this, $controller('ContentRestInnerCtrl', { $scope: $scope }));
+
+        /**
+         * @inheritdoc
+         */
+        $scope.draftEnabled = true;
+
+        /**
+         * @inheritdoc
+         */
+        $scope.draftKey = 'obituary-draft';
+
+        /**
+         * @inheritdoc
+         */
+        $scope.dtm = null;
+
+        /**
+         * @inheritdoc
+         */
+        $scope.incomplete = true;
+
+        /**
+         * @memberOf ObituaryCtrl
+         *
+         * @description
+         *  The article object.
+         *
+         * @type {Object}
+         */
+        $scope.item = {
+          body: '',
+          content_type_name: 'obituary',
+          fk_content_type: 1,
+          content_status: 0,
+          description: '',
+          frontpage: 0,
+          created: null,
+          starttime: null,
+          endtime: null,
+          title: '',
+          type: 0,
+          with_comment: 0,
+          related_contents: [],
+          tags: [],
+          agency: '',
+        };
+
+        /**
+         * @memberOf ObituaryCtrl
+         *
+         * @description
+         *  The related service.
+         *
+         * @type {Object}
+         */
+        $scope.related = related;
+
+        /**
+         * @memberOf ObituaryCtrl
+         *
+         * @description
+         *  The list of routes for the controller.
+         *
+         * @type {Object}
+         */
+        $scope.routes = {
+          createItem:  'api_v1_backend_obituary_create_item',
+          getItem:     'api_v1_backend_obituary_get_item',
+          getPreview:  'api_v1_backend_obituary_get_preview',
+          list:        'backend_obituaries_list',
+          public:      'frontend_obituaries_show',
+          redirect:    'backend_obituaries_show',
+          saveItem:    'api_v1_backend_obituary_save_item',
+          savePreview: 'api_v1_backend_obituary_save_preview',
+          updateItem:  'api_v1_backend_obituary_update_item'
+        };
+
+        /**
+         * @inheritdoc
+         */
+        $scope.buildScope = function() {
+          $scope.localize($scope.data.item, 'item', true, [ 'related_contents' ]);
+
+          // Check if item is new (created) or existing for use default value or not
+          if (!$scope.data.item.pk_content) {
+            $scope.item.with_comment = $scope.data.extra.comments_enabled ? 1 : 0;
+          }
+
+          if ($scope.draftKey !== null && $scope.data.item.pk_content) {
+            $scope.draftKey = 'article-' + $scope.data.item.pk_content + '-draft';
+          }
+
+          $scope.checkDraft();
+          related.init($scope);
+          related.watch();
+          translator.init($scope);
+        };
+
+        /**
+         * @function getFrontendUrl
+         * @memberOf ObituaryCtrl
+         *
+         * @description
+         *   Generates the public URL basing on the item.
+         *
+         * @param {String} item  The item to generate route for.
+         *
+         * @return {String} The URL for the content.
+         */
+        $scope.getFrontendUrl = function(item) {
+          if (!$scope.selectedCategory) {
+            return '';
+          }
+
+          return $scope.getL10nUrl(
+            routing.generate($scope.routes.public, {
+              id: item.pk_content,
+              created: $window.moment(item.created).format('YYYYMMDDHHmmss'),
+              slug: item.slug
+            })
+          );
+        };
+
+        // Update title_int when title changes
+        $scope.$watch('item.title', function(nv, ov) {
+          if (!nv && !ov) {
+            return;
+          }
+
+          if (!$scope.item.title_int || ov === $scope.item.title_int) {
+            $scope.item.title_int = nv;
+          }
+
+          if (!$scope.item.pk_content) {
+            if ($scope.tm) {
+              $timeout.cancel($scope.tm);
+            }
+
+            if (!nv) {
+              $scope.item.slug = '';
+            }
+          }
+        }, true);
+
+        /**
+         * @inheritdoc
+         */
+        $scope.validate = function() {
+          if ($scope.form && $scope.form.$invalid) {
+            $('[name=form]')[0].reportValidity();
+            return false;
+          }
+
+          if (!$('[name=form]')[0].checkValidity()) {
+            $('[name=form]')[0].reportValidity();
+            return false;
+          }
+
+          return true;
+        };
+      }
+    ]);
+})();
---
+++ public/core/themes/admin/tpl/obituary/item.tpl
@@ -0,0 +1,115 @@
+{extends file="common/extension/item.tpl"}
+
+{block name="metaTitle"}
+  > {t}Obituaries{/t} >
+  {if empty($id)}
+    {t}Create{/t}
+  {else}
+    {t}Edit{/t} ({$id})
+  {/if}
+{/block}
+
+{block name="ngInit"}
+  ng-controller="ObituaryCtrl" ng-init="forcedLocale = '{$locale}'; getItem({$id});"
+{/block}
+
+{block name="icon"}
+  <i class="fa fa-address-card-o m-r-10"></i>
+{/block}
+
+{block name="title"}
+  <a class="no-padding" href="{url name=backend_obituaries_list}">
+    {t}Obituaries{/t}
+  </a>
+{/block}
+
+{block name="primaryActions"}
+  <li class="quicklinks hidden-xs ng-cloak" ng-if="draftSaved">
+    <h5>
+      <i class="p-r-15">
+        <i class="fa fa-check"></i>
+        {t}Draft saved at {/t}[% draftSaved %]
+      </i>
+    </h5>
+  </li>
+  <li class="quicklinks">
+    <button class="btn btn-white m-r-5" id="preview-button" ng-click="preview()" type="button" id="preview_button">
+      <i class="fa fa-desktop" ng-class="{ 'fa-circle-o-notch fa-spin': flags.http.generating_preview }" ></i>
+      {t}Preview{/t}
+    </button>
+  </li>
+  <li class="quicklinks">
+    <button class="btn btn-loading btn-success text-uppercase" ng-click="submit()" ng-disabled="flags.http.loading || flags.http.saving" type="button">
+      <i class="fa fa-save m-r-5" ng-class="{ 'fa-circle-o-notch fa-spin': flags.http.saving }"></i>
+      {t}Save{/t}
+    </button>
+  </li>
+{/block}
+
+{block name="rightColumn"}
+  <div class="grid simple">
+    <div class="grid-body no-padding">
+      <div class="grid-collapse-title">
+        {acl isAllowed="OBITUARY_AVAILABLE"}
+          {include file="ui/component/content-editor/accordion/published.tpl"}
+        {/acl}
+        <div class="m-t-5">
+          {include file="ui/component/content-editor/accordion/allow_comments.tpl"}
+        </div>
+      </div>
+      {include file="ui/component/content-editor/accordion/author.tpl"}
+      {include file="ui/component/content-editor/accordion/tags.tpl"}
+      {include file="ui/component/content-editor/accordion/slug.tpl" iRoute="[% getFrontendUrl(item) %]"}
+      {include file="ui/component/content-editor/accordion/input-text.tpl" field="mortuary" icon="fa-facebook" title="{t}Mortuary{/t}" iRoute="item.mortuary"}
+      {include file="ui/component/content-editor/accordion/input-text.tpl" field="website" icon="fa-external-link" title="{t}Website{/t}" iRoute="item.website"}
+      {include file="ui/component/content-editor/accordion/input-text.tpl" field="maps" icon="fa-map-marker" title="{t}Google maps url{/t}" iRoute="item.maps"}
+      {include file="ui/component/content-editor/accordion/scheduling.tpl"}
+    </div>
+  </div>
+  <div class="grid simple">
+    <div class="grid-body no-padding">
+      <div class="grid-collapse-title">
+        <i class="fa fa-cog m-r-10"></i> {t}Parameters{/t}
+      </div>
+      {include file="common/component/related-contents/_featured-media.tpl" iName="featuredFrontpage" iTitle="{t}Featured in frontpage{/t}" types="photo,video,album"}
+      {include file="common/component/related-contents/_featured-media.tpl" iName="featuredInner" iTitle="{t}Featured in inner{/t}" types="photo,video,album"}
+    </div>
+  </div>
+{/block}
+
+{block name="leftColumn"}
+  <div class="grid simple">
+    <div class="grid-body">
+      {include file="ui/component/input/text.tpl" iCounter=true iField="title" iNgActions="ng-blur=\"generate()\"" iRequired=true iTitle="{t}Title{/t}" iValidation=true}
+      {include file="ui/component/input/text.tpl" iCounter=true iField="title_int" iRequired=true iTitle="{t}Inner title{/t}" iValidation=true}
+      <div class="row">
+        <div class="col-sm-6">
+          {include file="ui/component/input/text.tpl" iCounter=true iField="agency" iTitle="{t}Signature{/t}"}
+        </div>
+        {is_module_activated name="NEWSLETTER_MANAGER"}
+          <div class="col-sm-6">
+            {include file="ui/component/input/text.tpl" iCounter=true iField="newsletter_agency" iTitle="{t}Newsletter Signature{/t}" iHelp="{t}Alternative signature for newsletter{/t}"}
+          </div>
+        {/is_module_activated}
+      </div>
+      {include file="ui/component/input/text.tpl" iCounter=true iField="pretitle" iTitle="{t}Pretitle{/t}"}
+      {include file="ui/component/content-editor/textarea.tpl" title="{t}Summary{/t}" field="description" rows=5 imagepicker=true}
+      {include file="ui/component/content-editor/textarea.tpl" title="{t}Body{/t}" field="body" preset="standard" rows=15 imagepicker=true}
+    </div>
+  </div>
+{/block}
+
+{block name="modals"}
+  <script type="text/ng-template" id="modal-preview">
+    <div class="modal-body clearfix no-padding">
+      <iframe ng-src="[% template.src %]" frameborder="0"></iframe>
+    </div>
+  </script>
+  <script type="text/ng-template" id="modal-translate">
+    {include file="common/modals/_translate.tpl"}
+  </script>
+  <script type="text/ng-template" id="modal-draft">
+    {include file="common/modals/_draft.tpl"}
+  </script>
+{/block}
+
