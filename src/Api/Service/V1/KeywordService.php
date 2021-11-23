9d9c79ef8385788efc47b3dc08f5946bc93d2757
Miguel Perez <miguel.perez@openhost.es>
2021-11-23 10:19:37 +0100

    ONM-7308: small refactor on replaceTerms

--- src/Api/Service/V1/KeywordService.php
+++ src/Api/Service/V1/KeywordService.php
@@ -21,34 +21,28 @@ class KeywordService extends OrmService
      */
     public function replaceTerms($text, $keywords)
     {
-        // Spaces necessary to evaluate first and last pattern matching
         $text = mb_detect_encoding($text) !== "UTF-8" ?
             utf8_decode(' ' . $text . ' ') :
-             ' ' . $text . ' ';
+            ' ' . $text . ' ';
+
+        $types = [
+            'url'       => '',
+            'email'     => 'mailto:',
+            'intsearch' => '/tag/'
+        ];
 
         foreach ($keywords as $keyword) {
-            // Select keyword type
-            $replacement = '<a href="%s" target="_blank">%s</a>';
-            switch ($keyword->type) {
-                case 'url':
-                    $replacement = sprintf($replacement, $keyword->value, $keyword->keyword);
-                    break;
-                case 'email':
-                    $replacement = sprintf($replacement, 'mailto:' . $keyword->value, $keyword->keyword);
-                    break;
-                case 'intsearch':
-                    $replacement = sprintf($replacement, "/tag/" . $keyword->value, $keyword->keyword);
-                    break;
-                default:
-                    continue 2;
+            if (array_key_exists($keyword->type, $types)) {
+                $text = preg_replace(
+                    '@\b' . $keyword->keyword . '\b@',
+                    sprintf(
+                        '\1<a href="%s" target="_blank">%s</a>\4',
+                        $types[$keyword->type] . $keyword->value,
+                        $keyword->keyword
+                    ),
+                    $text
+                );
             }
-
-            // The \b matches a word boundary
-            $text = preg_replace(
-                '@\b' . $keyword->keyword . '\b@',
-                '\1' . $replacement . '\4',
-                $text
-            );
         }
 
         return trim($text);
