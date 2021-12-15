03e11d61b3a9185cf3eb34241d893353a577ad7c
Adan Oliva <adan@openhost.es>
2021-12-15 17:28:47 +0100

    ONM-7388: Always collapse the list of columns

--- public/core/themes/admin/js/controllers/RestListCtrl.js
+++ public/core/themes/admin/js/controllers/RestListCtrl.js
@@ -27,6 +27,15 @@
          * @memberOf RestListCtrl
          *
          * @description
+         *   Always collapse the list of columns
+         *
+         */
+        $scope.app.columns.collapsed = true;
+
+        /**
+         * @memberOf RestListCtrl
+         *
+         * @description
          *   The criteria to search.
          *
          * @type {Object}
