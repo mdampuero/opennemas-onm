b70fb410a521a9f624f6ee834aad5458c7c6fcdd
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-02-07 08:31:31 +0100

    ONM-7403: Reset the array of related when the class changes

--- public/core/themes/admin/js/controllers/WidgetCtrl.js
+++ public/core/themes/admin/js/controllers/WidgetCtrl.js
@@ -345,7 +345,8 @@
         // Gets the form for widget when widget class changes
         $scope.$watch('item.class', function(nv, ov) {
           if (ov) {
-            $scope.item.params = [];
+            $scope.item.params  = [];
+            $scope.data.related = [];
           }
 
           if (!nv) {
