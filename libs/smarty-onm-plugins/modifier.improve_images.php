880e95e98a75a62bab914788e3ec5af1aae33328
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-02-09 14:39:37 +0100

    ONM-7508: Improve regex replace

--- libs/smarty-onm-plugins/modifier.improve_images.php
+++ libs/smarty-onm-plugins/modifier.improve_images.php
@@ -8,7 +8,17 @@
  */
 function smarty_modifier_improve_images($html)
 {
-    $html = preg_replace('@<figure class="image"@s', '<figure class="image ckeditor-image"', $html);
+    // Add class to identify ckeditor and width to the figure in the html.
+    $html = preg_replace(
+        '@<figure class="image(.*)"(.*<img.*width="([0-9]+)".*<\/figure>)@sU',
+        '<figure class="image $1 ckeditor-image" style="width: $3px"$2',
+        $html
+    );
 
-    return preg_replace('@<img(.*)src=@', '<img$1 class="lazyload" data-src=', $html);
+    // Use data-src instead of src on images in order to apply lazyload.
+    $html = preg_replace('@<img(.*)src=@', '<img$1 data-src=', $html);
+    $html = preg_replace('@<img(.*)class="([^"]+)"?@', '<img$1 class="$2 lazyload"', $html);
+
+    // Add the lazy load to the leftovers.
+    return preg_replace('@<img(.*)(?!class)>@', '<img$1 class="lazyload">', $html);
 }
