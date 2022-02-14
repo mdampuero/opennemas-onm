f8c4c642b29b3d6bbfd220ae06c6034d71d2d08a
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-02-14 12:46:33 +0100

    ONM-7489: Reorder redis and varnish keys

--- src/Api/Helper/Cache/ArticleCacheHelper.php
+++ src/Api/Helper/Cache/ArticleCacheHelper.php
@@ -8,26 +8,26 @@ class ArticleCacheHelper extends ContentCacheHelper
      * {@inheritdoc}
      */
     protected $varnishKeys = [
+        'archive-content',
         'article-{{pk_content}}-inner',
+        'authors-frontpage',
         'category-{{category_id}}',
+        'content-author-{{fk_author}}',
         'frontpage-page',
-        'sitemap',
         'rss-article,{{category_id}}',
         'rss-frontpage$',
         'rss-author-{{fk_author}}',
-        'authors-frontpage',
-        'content-author-{{fk_author}}',
+        'sitemap',
         'tag-{{tag_id}}',
-        'archive-content',
     ];
 
     /**
      * {@inheritdoc}
      */
     protected $redisKeys = [
-        '*WidgetLastInSectionWithPhoto-*-{{category_id}}',
+        '*suggested_contents_{{content_type_name}}_{{category_id}}',
         '*WidgetInfiniteScroll-*-*-*-{{category_id}}',
+        '*WidgetLastInSectionWithPhoto-*-{{category_id}}',
         '*WidgetNextPrevious-*-article-*-{{category_id}}',
-        '*suggested_contents_{{content_type_name}}_{{category_id}}'
     ];
 }
