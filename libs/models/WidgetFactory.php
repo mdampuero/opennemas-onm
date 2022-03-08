399f0edf5a370ffe5c7facaa7d5fb3b99d220b89
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-03-08 11:48:32 +0100

    ONM-7489: Improve widget factory to add support for x-tags and x-cache-for

--- libs/models/WidgetFactory.php
+++ libs/models/WidgetFactory.php
@@ -24,11 +24,50 @@ class WidgetFactory
     public $ttl = 1800;
 
     /**
+     * Default ttl for varnish cache of the widget.
+     *
+     * @var string
+     */
+    protected $defaultTtl = '100d';
+
+    /**
      * The service container.
      *
      * @var ServiceContainer
      */
     protected $container;
+
+    /**
+     * Flag to indicate if the widget is cacheable.
+     *
+     * @var boolean
+     */
+    protected $isCacheable = true;
+
+    /**
+     * Flag to indicate if the widget is static.
+     *
+     * @var boolean
+     */
+    protected $isStatic = false;
+
+    /**
+     * The default content type of the widget.
+     *
+     * @var string
+     */
+    protected $defaultType = 'article';
+
+    /**
+     * The properties of the current widget that are suitable to be in the x-tags array.
+     *
+     * @var array
+     */
+    protected $propertiesMap = [
+        'category'  => 'category',
+        'tag_name'  => 'tag',
+        'fk_author' => 'author'
+    ];
 
     /**
      * Initializes the WidgetFactory object instance
@@ -112,6 +151,106 @@ class WidgetFactory
         if (is_array($params)) {
             $this->params = array_merge($this->params, $params);
         }
+    }
+
+    /**
+     * Loads the array of parameters with all the data required to render the template.
+     */
+    public function hydrateShow()
+    {
+    }
+
+    /**
+     * Returns an array with the x-tags for the specific widget.
+     *
+     * @return array The array with the x-tags for the specific widget.
+     */
+    public function getXTags()
+    {
+        $xtags = [ sprintf('widget-%s', $this->content->pk_content) ];
+
+        if ($this->isStatic()) {
+            return $xtags;
+        }
+
+        if (!array_key_exists('content_type', $this->params)) {
+            $xtags[] = sprintf('content_type_name-widget-%s', $this->defaultType);
+        } else {
+            $xtags[] = is_array($this->params['content_type'])
+                ? sprintf('content_type_name-widget-%s', $this->params['content_type']['slug'])
+                : sprintf('content_type_name-widget-%s', underscore($this->params['content_type']));
+        }
+
+        foreach ($this->propertiesMap as $key => $value) {
+            if (!array_key_exists($key, $this->params) || empty($this->params[$key])) {
+                $xtags[] = sprintf('%s-widget-all', $value);
+                continue;
+            }
+
+            if (!is_array($this->params[$key])) {
+                $xtags[] = sprintf('%s-widget-%s', $value, $this->params[$key]);
+                continue;
+            }
+
+            foreach ($this->params[$key] as $param) {
+                $xtags[] = sprintf('%s-widget-%d', $value, $param);
+            }
+        }
+
+        foreach ($this->params['contents'] as $content) {
+            $xtags[] = sprintf('%s-%d', $content->content_type_name, $content->pk_content);
+        }
+
+        return $xtags;
+    }
+
+    /**
+     * Returns the x-cache-for header for the specific widget.
+     *
+     * @return string The x-cache-for value for the widget.
+     */
+    public function getXCacheFor()
+    {
+        // Return 100d by default if there is no contents.
+        if ($this->isStatic() || empty($this->params['contents'])) {
+            return $this->defaultTtl;
+        }
+
+        $endtime = $this->params['contents'][0]->endtime;
+
+        for ($i = 1; $i < count($this->params['contents']); $i++) {
+            if (!empty($this->params['contents'][$i]->endtime)
+                && $this->params['contents'][$i]->endtime < $endtime
+            ) {
+                $endtime = $this->params['contents'][$i]->endtime;
+            }
+        }
+
+        if (empty($endtime)) {
+            return $this->defaultTtl;
+        }
+
+        return $endtime->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Returns true if the widget doesn't depend on contents and is static, false otherwise.
+     *
+     * @return boolean True if the widget is static, false otherwise.
+     */
+    public function isStatic()
+    {
+        return $this->isStatic;
+    }
+
+    /**
+     * Returns true if the widget is cacheable, false otherwise.
+     *
+     * @return boolean True if the widget is cacheable, false otherwise.
+     */
+    public function isCacheable()
+    {
+        return $this->isCacheable;
     }
 
     /**
