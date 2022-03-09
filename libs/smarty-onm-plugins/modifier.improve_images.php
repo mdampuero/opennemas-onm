d2f4d38dc443a841dce73e2b62f0695dacab5396
Miguel Perez <miguel.perez@openhost.es>
2022-03-09 12:15:47 +0100

    ONM-7467: Fixed error when image heigth > width, do nothing in that case

--- libs/smarty-onm-plugins/modifier.improve_images.php
+++ libs/smarty-onm-plugins/modifier.improve_images.php
@@ -23,7 +23,8 @@ function smarty_modifier_improve_images($html)
     $html = preg_replace('@<img(((?!class).)*)/?>@U', '<img$1 class="lazyload">', $html);
 
     preg_match_all(
-        '@<img[^>]*(?(?=width)width="([0-9]+)"|(?!.width="))[^>]*data-src="((?!.*\://)(?!.*zoomcrop)[^"]+)"[^>]+>@mU',
+        '@<img[^>]*(?(?=width)width="([0-9]+)"|(?!.width="))[^>]*(?(?=height)height="([0-9]+)"|(?!.height="))[^>]' .
+            '*data-src="((?!.*\://)(?!.*zoomcrop)[^"]+)"[^>]+>@mU',
         $html,
         $out,
         PREG_OFFSET_CAPTURE
@@ -31,20 +32,24 @@ function smarty_modifier_improve_images($html)
     /*
      * out[0] => array of complete img tag matchs,
      * out[1] => array of widths from img match,
-     * out[2] => array of src from img matchs,
+     * out[2] => array of heights from img match,
+     * out[3] => array of src from img matchs,
      *
-     * out[1][0] and out[2][0] correspond to width and src from first img tag match (out[0][0])
+     * out[1][0] and out[2][0] correspond to width and height from first img tag match (out[0][0])
      */
     if ($out[0] && !empty($out[0])) {
         $ph = getService('core.helper.photo');
         foreach ($out[0] as $matchKey => $matchValue) {
             $width = !empty($out[1][$matchKey][0]) ? (int) $out[1][$matchKey][0] : PHP_INT_MAX;
-
-            $result = $ph->getSrcSetAndSizesFromImagePath($out[2][$matchKey][0], $width);
+            $height = !empty($out[2][$matchKey][0]) ? (int) $out[2][$matchKey][0] : 0;
+            if ($height > $width) {
+                continue;
+            }
+            $result = $ph->getSrcSetAndSizesFromImagePath($out[3][$matchKey][0], $width);
 
             $html = preg_replace(
                 '@<img(((?!srcset).)*src="'
-                . $out[2][$matchKey][0] .
+                . $out[3][$matchKey][0] .
                 '".*)>@U',
                 '<img$1 data-srcset="'
                 . $result['srcset'] .
