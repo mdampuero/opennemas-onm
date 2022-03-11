d0442edbaa5bca5ce2f45f071be4b2b5e915c797
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-03-11 13:01:31 +0100

    ONM-7489: Implement test for content cache helper

--- src/Api/Helper/Cache/NewsstandCacheHelper.php
+++ src/Api/Helper/Cache/NewsstandCacheHelper.php
@@ -9,16 +9,6 @@ class NewsstandCacheHelper extends ContentCacheHelper
     /**
      * {@inheritdoc}
      */
-    protected $varnishKeys = [
-        'content_type_name-widget-{{content_type_name}}' .
-        '.*category-widget-(({{category_id}})|(all))' .
-        '.*tag-widget-(({{tag_id}})|(all))',
-        '{{content_type_name}}-frontpage-{{starttime}}'
-    ];
-
-    /**
-     * {@inheritdoc}
-     */
     protected function replaceStarttime(Content $item)
     {
         return $item->starttime->format('Y-m');
--- tests/Api/Helper/Cache/ContentCacheHelperTest.php
+++ tests/Api/Helper/Cache/ContentCacheHelperTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Api\Helper\Cache;
 
-use Api\Helper\Cache\AlbumCacheHelper;
+use Api\Helper\Cache\ArticleCacheHelper;
 use Api\Helper\Cache\ContentCacheHelper;
 use Api\Helper\Cache\NewsstandCacheHelper;
 use Common\Model\Entity\Content;
@@ -30,6 +30,8 @@ class ContentCacheHelperTest extends \PHPUnit\Framework\TestCase
         $this->cache = $this->getMockBuilder('Opennemas\Cache\Core\Cache')
             ->disableOriginalConstructor()
             ->getMock();
+
+        $this->helper = new ArticleCacheHelper($this->instance, $this->queue, $this->cache);
     }
 
     /**
@@ -56,5 +58,159 @@ class ContentCacheHelperTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals('opinion-1', $cacheHelper->getXTags($opinion));
         $this->assertEquals('article-2', $cacheHelper->getXTags($article));
+    }
+
+    /**
+     * Tests deleteItem.
+     */
+    public function testDeleteItem()
+    {
+        $now = new DateTime();
+
+        $item = new Content([
+            'pk_content'        => 1,
+            'content_type_name' => 'article',
+            'categories'        => [ 22 ],
+            'starttime'         => $now,
+            'tags'              => [ 12, 13, 14 ]
+        ]);
+
+        $this->queue->expects($this->at(0))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'rss-instant-articles'
+                )
+            ]));
+
+        $this->queue->expects($this->at(1))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    sprintf('archive-page-%s', $now->format('Y-m-d'))
+                )
+            ]));
+
+        $this->queue->expects($this->at(2))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'category-22'
+                )
+            ]));
+
+        $this->queue->expects($this->at(3))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'content-author-0-frontpage'
+                )
+            ]));
+
+        $this->queue->expects($this->at(4))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'article-frontpage$'
+                )
+            ]));
+
+        $this->queue->expects($this->at(5))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'article-1'
+                )
+            ]));
+
+        $this->queue->expects($this->at(6))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'content_type_name-widget-article' .
+                    '.*category-widget-((22)|(all))' .
+                    '.*tag-widget-(((12)|(13)|(14))|(all))' .
+                    '.*author-widget-((0)|(all))'
+                )
+            ]));
+
+        $this->queue->expects($this->at(7))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'rss-author-0'
+                )
+            ]));
+
+        $this->queue->expects($this->at(8))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'rss-article$'
+                )
+            ]));
+
+        $this->queue->expects($this->at(9))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'sitemap'
+                )
+            ]));
+
+        $this->queue->expects($this->at(10))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    'tag-(12)|(13)|(14)'
+                )
+            ]));
+
+        $this->helper->deleteItem($item);
+    }
+
+    /**
+     * Tests deleteItem when the content is a newsstand.
+     */
+    public function testDeleteItemWhenNewsstand()
+    {
+        $now    = new DateTime();
+        $helper = new NewsstandCacheHelper($this->instance, $this->queue, $this->cache);
+
+        $item = new Content(
+            [
+                'pk_content'        => 10,
+                'content_type_name' => 'kiosko',
+                'path'              => '/glorp/baz',
+                'starttime'         => $now
+            ]
+        );
+
+        $this->queue->expects($this->at(0))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf('req.url ~ %s', $item->path)
+            ]));
+
+        $this->queue->expects($this->at(1))->method('push')
+            ->with(new ServiceTask('core.varnish', 'ban', [
+                sprintf(
+                    'obj.http.x-tags ~ instance-%s.*%s',
+                    $this->instance->internal_name,
+                    sprintf('archive-page-%s', $now->format('Y-m'))
+                )
+            ]));
+
+        $helper->deleteItem($item);
     }
 }
