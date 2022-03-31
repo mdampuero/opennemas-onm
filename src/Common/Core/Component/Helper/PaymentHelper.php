55e6b2884c6def0884196b9a23638d12bcb3b3c0
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-03-31 10:31:14 +0200

    ONM-7606: Implement helper to generate the response messages basing on the code

---
+++ src/Common/Core/Component/Helper/PaymentHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Common\Core\Component\Helper;
+
+use Symfony\Component\DependencyInjection\Container;
+
+/**
+* Perform auxiliary actions for Payments Controller.
+*/
+class PaymentHelper
+{
+    /**
+     * Initializes the payment helper.
+     *
+     * @param Container The service dependency injector.
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Returns the referer url with the response message as a query string.
+     *
+     * @param string $url     The referer url.
+     * @param int    $code    The code of the response.
+     *
+     * @return string The referer url with the response message added as a query string.
+     */
+    public function getRefererUrlWithMessage(string $url, string $code)
+    {
+        $messages = [
+            '00'      => _('The transaction was successful'),
+            '101'     => _('The transaction was declined by the bank'),
+            '501'     => _('This transaction was already processed'),
+            '110'     => _('There are some errors in the form'),
+            'default' => _('There was an issue with the transaction')
+        ];
+
+        $message = $messages[$code] ?? $messages['default'];
+
+        $urlHelper = $this->container->get('core.helper.url');
+
+        $parts = $urlHelper->parse($url);
+
+        $parts['query'] = sprintf('message=%s&code=%d', $message, $code);
+
+        $url = $urlHelper->unparse($parts);
+
+        return $url;
+    }
+}
