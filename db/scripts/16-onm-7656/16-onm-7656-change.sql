706f4b7d09d4df89fe01f829873fb33ec918246c
Ismael Vázquez Fernández <ivfernandez@openhost.es>
2022-04-06 09:27:54 +0200

    ONM-7604: Add script to create the companies module

---
+++ db/scripts/16-onm-7656/16-onm-7656-change.sql
@@ -0,0 +1,3 @@
+SET names 'utf8';
+INSERT INTO `extension` VALUES (86,'es.openhost.module.companies','a:3:{s:2:\"en\";s:9:\"Companies\";s:2:\"es\";s:8:\"Empresas\";s:2:\"gl\";s:8:\"Empresas\";}','module','Openhost, S.L.',NULL,NULL,NULL,'2022-04-05 14:55:28','2022-04-05 14:55:28',1,'http://www.openhost.es');
+INSERT INTO `extension_meta` VALUES (86,'category','module'),(86,'price','a:1:{i:0;a:2:{s:5:\"value\";i:25;s:4:\"type\";s:7:\"monthly\";}}');
