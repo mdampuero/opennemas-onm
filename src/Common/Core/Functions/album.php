1887b4c63824a71b3a540100de0ae2f428d08712
Alex Rodrigues <alex@openhost.es>
2022-04-13 12:51:28 +0200

    ONM-7612: Add max parameter for getting album photos

--- src/Common/Core/Functions/album.php
+++ src/Common/Core/Functions/album.php
@@ -4,12 +4,13 @@
  * Returns the list of photos of the album.
  *
  * @param Content $item The album.
+ * @param int     $max  The max number of photos.
  *
  * @return array The list of photos.
  */
-function get_album_photos($item) : array
+function get_album_photos($item, $max = null) : array
 {
-    return getService('core.helper.album')->getAlbumPhotos($item);
+    return getService('core.helper.album')->getAlbumPhotos($item, $max);
 }
 
 /**
