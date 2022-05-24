a4a8e9bf2102af2b2f93a133634ba61d7a10c485
Miguel Perez <miguel.perez@openhost.es>
2022-05-24 14:38:25 +0200

    ONM-7613: Added new extension files

---
+++ db/scripts/16-ONM-7776/changes.sql
@@ -0,0 +1,3 @@
+SET names 'utf8';
+INSERT INTO `extension` VALUES (87,'es.openhost.module.live_blog_posting','a:3:{s:2:\"en\";s:17:\"Live Blog Posting\";s:2:\"es\";s:17:\"Live Blog Posting\";s:2:\"gl\";s:17:\"Live Blog Posting\";}','module','Openhost, S.L.',NULL,NULL,NULL,'2022-05-24 12:24:00','2022-05-24 12:24:00',0,'http://www.openhost.es');
+INSERT INTO `extension_meta` VALUES (87,'category','module'),(87,'price','a:1:{i:0;a:2:{s:5:\"value\";i:25;s:4:\"type\";s:7:\"monthly\";}}');
