a84872d497d26b09d01019ffa7238e1960e8e85e
Miguel Perez <miguel.perez@openhost.es>
2022-08-10 08:47:35 +0200

    ONM-7945: Added varnish ban at the end of an automatic news agency import

--- src/Common/NewsAgency/Command/SynchronizeCommand.php
+++ src/Common/NewsAgency/Command/SynchronizeCommand.php
@@ -10,6 +10,8 @@
 namespace Common\NewsAgency\Command;
 
 use Common\Core\Command\Command;
+use Opennemas\Task\Component\Queue\Queue;
+use Opennemas\Task\Component\Task\ServiceTask;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
