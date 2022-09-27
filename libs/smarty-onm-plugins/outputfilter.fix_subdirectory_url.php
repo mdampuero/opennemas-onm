448ceeca1ddb23e76a60c953c22c10066e106e3c
Miguel Perez <miguel.perez@openhost.es>
2022-09-27 12:59:15 +0200

    ONM-8008: Added output filter in order to fix some in body urls when instance is migrated to subdirectory

---
+++ libs/smarty-onm-plugins/outputfilter.fix_subdirectory_url.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Adds the noindex directive when needed
+ *
+ * @param string $output
+ * @param \Smarty $smarty
+ *
+ * @return string
+ */
+function smarty_outputfilter_fix_subdirectory_url($output, $smarty)
+{
+    $instance = $smarty->getContainer()->get('core.instance');
+
+    if (!$instance || !$instance->isSubdirectory()) {
+        return $output;
+    }
+
+    $pattern = sprintf('@(href=")(/[^/](?!%s).*?/)@', trim($instance->getSubdirectory(), '/'));
+
+    return preg_replace(
+        $pattern,
+        sprintf('${1}%s${2}', $instance->getSubdirectory()),
+        $output
+    );
+}
