a9f6a65b5e5fa099ba2c02a35a678e67ccf81480
Miguel Perez <miguel.perez@openhost.es>
2022-10-03 10:23:16 +0200

    ONM-8008: Changed outputfilter to fix sudirectory url con related contents in body

--- libs/smarty-onm-plugins/outputfilter.fix_subdirectory_url.php
+++ libs/smarty-onm-plugins/outputfilter.fix_subdirectory_url.php
@@ -15,11 +15,24 @@ function smarty_outputfilter_fix_subdirectory_url($output, $smarty)
         return $output;
     }
 
-    $pattern = sprintf('@(href=")(/[^/](?!%s).*?/)@', trim($instance->getSubdirectory(), '/'));
+    $pattern = sprintf('@(?:href=(?:"|\'))(?!/%s/)(/[^/][^"\']*)@', trim($instance->getSubdirectory(), '/'));
 
-    return preg_replace(
+    $content = $smarty->getValue('o_content');
+
+    preg_match_all(
         $pattern,
-        sprintf('${1}%s${2}', $instance->getSubdirectory()),
-        $output
+        $content->body,
+        $matches,
+        PREG_OFFSET_CAPTURE
     );
+
+    if (empty($matches[1])) {
+        return $output;
+    }
+
+    foreach ($matches[1] as $match) {
+        $output = str_replace('"' . $match[0] . '"', '"' . $instance->getSubdirectory() . $match[0] . '"', $output);
+    }
+
+    return $output;
 }
