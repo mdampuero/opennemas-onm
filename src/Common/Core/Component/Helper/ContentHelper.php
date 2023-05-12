34207f500f45b55140c5f95a7d7394c21c3facc6
Alex Rodrigues <alex@openhost.es>
2023-05-12 09:14:30 +0200

    ONM-8436: Add temp debug

--- src/Common/Core/Component/Helper/ContentHelper.php
+++ src/Common/Core/Component/Helper/ContentHelper.php
@@ -167,6 +167,8 @@ class ContentHelper
             $item = $this->service->getItemBy($oql);
             return $item->created;
         } catch (\Exception $e) {
+            var_dump($e->getMessage());
+            die();
             return null;
         }
     }
