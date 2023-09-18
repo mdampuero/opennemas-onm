92107b8073d37220a5d37f315ccf1fd8efa62070
Miguel Perez <miguel.perez@openhost.es>
2023-09-18 15:53:02 +0200

    ONM-8646: Added missing use

--- src/Framework/Command/WebpushSendCommand.php
+++ src/Framework/Command/WebpushSendCommand.php
@@ -5,6 +5,7 @@ namespace Framework\Command;
 use Common\Core\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
