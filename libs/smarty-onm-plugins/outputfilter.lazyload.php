446d653452f3fe660d9f43b293ce80835b523f34
Alex Rodrigues <alex@openhost.es>
2023-11-17 13:03:01 +0100

    ONM-8820: Use facade for embed videos

--- libs/smarty-onm-plugins/outputfilter.lazyload.php
+++ libs/smarty-onm-plugins/outputfilter.lazyload.php
@@ -29,12 +29,65 @@ function smarty_outputfilter_lazyload($output, $smarty)
         && !preg_match('/\/rss\/(?!listado$)/', $uri)
         && !preg_match('@\.amp\.html@', $uri)
     ) {
-         // Add 'loading=lazy' to iframes before returning output
+        // Check for content, parse and replace embed
+        $content = $smarty->getValue('o_content');
+        if (!empty($content)) {
+            preg_match_all(
+                '@<iframe.*?src="(https:.*?'
+                . '(vimeo.com/video/|youtube.com/embed/|dailymotion.com/embed/video/)'
+                . '([^#\&\?]*)).*?".*?>(?s).*?<\/iframe>@',
+                $content->body,
+                $matches
+            );
+
+            $urls = str_replace(
+                ['youtube.com/embed/', 'player.vimeo.com/video/', 'dailymotion.com/embed/'],
+                ['youtube.com/watch?v=', 'vimeo.com/', 'dailymotion.com/'],
+                $matches[1]
+            );
+
+            $codes = [];
+            $tpl   = '<div class="lazyframe"'
+                . ' data-vendor="%s"'
+                . ' data-title="%s"'
+                . ' data-thumbnail="%s"'
+                . ' data-src="%s"'
+                . ' data-ratio="16:9">'
+                . '</div>';
+
+            foreach ($urls as $key => $url) {
+                $video = unserialize(
+                    $smarty->getContainer()
+                        ->get('data.manager.filter')
+                        ->set($url)
+                        ->filter('panorama')
+                        ->get()
+                );
+
+                if (empty($video)) {
+                    unset($matches[0][$key]);
+                    continue;
+                }
+
+                $codes[] = sprintf(
+                    $tpl,
+                    $video['service'] === 'Dailymotion' ? '' : strtolower($video['service']),
+                    htmlspecialchars($video['title']),
+                    $video['thumbnail'],
+                    $video['embedUrl']
+                );
+            }
+
+            $output = str_replace($matches[0], $codes, $output);
+        }
+
+        // Add 'loading=lazy' to iframes before returning output
         $output = preg_replace(
             '/<iframe([^<>]*)>/',
             '<iframe loading="lazy" $1>',
             $output
         );
     }
+
     return $output;
 }
