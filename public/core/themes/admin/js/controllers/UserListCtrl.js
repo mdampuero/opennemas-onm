c283a6719ee7ac144cee0c00e17e3821fceb7bdf
Miguel Ángel Úbeda Tirado <miguel.ubeda@openhost.es>
2023-11-20 12:02:52 +0100

    ONM-8821: Add hide columns functionality

--- public/core/themes/admin/js/controllers/UserListCtrl.js
+++ public/core/themes/admin/js/controllers/UserListCtrl.js
@@ -10,13 +10,16 @@
      * @requires $controller
      * @requires $scope
      * @requires oqlEncoder
+     * @requires $uibModal
+     * @requires http
+     * @requires messenger
      *
      * @description
      *   Handles all actions in users list.
      */
     .controller('UserListCtrl', [
-      '$controller', '$scope', 'oqlEncoder',
-      function($controller, $scope, oqlEncoder) {
+      '$controller', '$scope', 'oqlEncoder', '$uibModal', 'http', 'messenger',
+      function($controller, $scope, oqlEncoder, $uibModal, http, messenger) {
         $.extend(this, $controller('RestListCtrl', { $scope: $scope }));
 
         /**
@@ -32,7 +35,8 @@
           deleteList: 'api_v1_backend_user_delete_list',
           getList:    'api_v1_backend_user_get_list',
           patchItem:  'api_v1_backend_user_patch_item',
-          patchList:  'api_v1_backend_user_patch_list'
+          patchList:  'api_v1_backend_user_patch_list',
+          moveItem:   'api_v1_backend_user_move_item',
         };
 
         /**
@@ -59,7 +63,7 @@
          */
         $scope.init = function() {
           $scope.backup.criteria = $scope.criteria;
-
+          $scope.app.columns.selected = [ 'picture', 'name', 'email', 'usergroups', 'social', 'enabled' ];
           oqlEncoder.configure({
             placeholder: {
               name: 'name ~ "[value]" or username ~ "[value]"',
@@ -76,6 +80,52 @@
         $scope.isSelectable = function(item) {
           return $scope.backup.master ||
             $scope.getItemId(item) !== $scope.backup.id;
+        };
+
+        /**
+         * @function move
+         * @memberOf RestListCtrl
+         *
+         * @description
+         *   Confirm move action.
+         *
+         * @param {Integer} id The tag id.
+         * @param {Object} tag The tag object.
+         */
+        $scope.move = function(id, item) {
+          var modal = $uibModal.open({
+            templateUrl: 'modal-move',
+            backdrop: 'static',
+            controller: 'ModalCtrl',
+            resolve: {
+              template: function() {
+                return {
+                  exclude: [ id ],
+                  source: item
+                };
+              },
+              success: function() {
+                return function(modal, template) {
+                  var route = {
+                    name: $scope.routes.moveItem,
+                    params: { id: id }
+                  };
+
+                  return http.put(route, { target: template.target[0] });
+                };
+              }
+            }
+          });
+
+          modal.result.then(function(response) {
+            messenger.post(response.data);
+
+            if (response.success) {
+              $scope.list().then(function() {
+                $scope.selected = { all: false, items: [] };
+              });
+            }
+          });
         };
       }
     ]);
