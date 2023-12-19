6b269b02b650521449470af371bae8880175d752
Alex Rodrigues <alex@openhost.es>
2023-12-19 08:34:41 +0100

    ONM-8820: Use facade video only for YouTube embed

--- libs/smarty-onm-plugins/outputfilter.lazyload.php
+++ libs/smarty-onm-plugins/outputfilter.lazyload.php
@@ -29,61 +29,34 @@ function smarty_outputfilter_lazyload($output, $smarty)
         && !preg_match('/\/rss\/(?!listado$)/', $uri)
         && !preg_match('@\.amp\.html@', $uri)
     ) {
-        // Check for content, parse and replace embed
+        // Check for content, parse and replace youtube embed
         $content = $smarty->getValue('o_content');
         if (!empty($content)) {
             preg_match_all(
-                '@<iframe.*?src="(https:.*?'
-                . '(vimeo.com/video/|youtube.com/embed/|dailymotion.com/embed/video/)'
-                . '([^#\&\?]*)).*?".*?>(?s).*?<\/iframe>@',
+                '@<iframe.*?src="https:.*?youtube.com/embed/([^#\&\?]*).*?".*?>(?s).*?<\/iframe>@',
                 $content->body,
                 $matches
             );
 
-            $urls = str_replace(
-                ['youtube.com/embed/', 'player.vimeo.com/video/', 'dailymotion.com/embed/'],
-                ['youtube.com/watch?v=', 'vimeo.com/', 'dailymotion.com/'],
-                $matches[1]
-            );
+            if (empty($matches[0])) {
+                return $output;
+            }
 
             $codes = [];
             $tpl   = '<div class="lazyframe"'
-                . ' data-vendor="%s"'
-                . ' data-title="%s"'
+                . ' data-vendor="youtube"'
+                . ' data-title=""'
                 . ' data-thumbnail="%s"'
                 . ' data-src="%s"'
                 . ' data-ratio="16:9">'
                 . '</div>';
 
-            $cache = $smarty->getContainer()
-                ->get('cache.connection.instance');
-
-            foreach ($urls as $key => $url) {
-                $video = $cache->get(current(explode('.', $matches[2][$key])) . '_' . $matches[3][$key]);
-
-                if (empty($video)) {
-                    $video = unserialize(
-                        $smarty->getContainer()
-                            ->get('data.manager.filter')
-                            ->set($url)
-                            ->filter('panorama')
-                            ->get()
-                    );
-
-                    $cache->set(current(explode('.', $matches[2][$key])) . '_' . $matches[3][$key], $video, 3600 * 24);
-                }
-
-                if (empty($video)) {
-                    unset($matches[0][$key]);
-                    continue;
-                }
-
+            $ids = $matches[1];
+            foreach ($ids as $id) {
                 $codes[] = sprintf(
                     $tpl,
-                    $video['service'] === 'Dailymotion' ? '' : strtolower($video['service']),
-                    htmlspecialchars($video['title']),
-                    $video['thumbnail'],
-                    $video['embedUrl']
+                    'https://i.ytimg.com/vi/' . $id . '/hqdefault.jpg',
+                    'https://www.youtube.com/watch?v=' . $id
                 );
             }
 
