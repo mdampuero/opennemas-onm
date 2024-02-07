40f6664d6e80007797129872983fb8d0a06a6a79
Miguel Perez <miguel.perez@openhost.es>
2024-02-07 10:33:33 +0100

    ONM-8895: Added aspect ratio and widget to clean css rules

--- libs/smarty-onm-plugins/outputfilter.remove_unused_css.php
+++ libs/smarty-onm-plugins/outputfilter.remove_unused_css.php
@@ -61,7 +61,7 @@ function smarty_outputfilter_remove_unused_css($output, $smarty)
     $removeUnusedCss->styleSheets($newCssFilePath)
         ->htmlFiles($newHtmlFilePath)
         ->setFilenameSuffix('.refactored.min')
-        ->alwaysInclude('onm-new')
+        ->alwaysInclude('onm-new', 'onm-aspect-ratio', 'widget')
         ->minify()
         ->refactor()
         ->saveFiles();
