7f87573416243b98629b621192fa0e42cb72e2f5
Francisco Catalan <francisco.catalan@openhost.es>
2024-05-15 12:25:52 +0200

    ONM-9077: Improve readability and organization of link rendering function

--- libs/smarty-onm-plugins/function.renderLink.php
+++ libs/smarty-onm-plugins/function.renderLink.php
@@ -7,41 +7,77 @@
  *
  * @return string
  */
-function smarty_function_renderLink($params, $smarty)
+function smarty_function_renderLink($params, &$smarty)
 {
-    $item       = $params['item'];
-    $nameMenu   = $params['name'];
-    $nameUrlMap = [
-        'video'    => 'video',
-        'album'    => 'album',
-        'special'  => 'especiales',
-        'encuesta' => 'encuesta'
-    ];
+    $item     = $params['item'];
+    $nameMenu = $params['name'];
 
-    $nameUrl = isset($nameUrlMap[$nameMenu]) ? $nameUrlMap[$nameMenu] : 'seccion';
+    $nameUrlMap = getNameUrlMap();
+    $nameUrl    = $nameUrlMap[$nameMenu] ?? 'seccion';
 
-    $typeToUrlMap = [
-        'category'         => "/$nameUrl/$item->link/",
-        'videoCategory'    => "/video/$item->link/",
-        'albumCategory'    => "/album/$item->link/",
-        'pollCategory'     => "/encuesta/$item->link/",
-        'static'           => "/" . STATIC_PAGE_PATH . "/$item->link.html",
-        'internal'         => ($item->link == '/') ? "" : "/$item->link/",
-        'external'         => "$item->link",
-        'syncBlogCategory' => "/ext$nameUrl/blog/$item->link/",
-        'blog-category'    => "/blog/section/$item->link/",
-        'tags'             => "/tags/$item->link/",
-    ];
+    $typeToUrlMap = getTypeToUrlMap($item, $nameUrl);
 
-    $link = isset($typeToUrlMap[$item->type]) ? $typeToUrlMap[$item->type] : "/$item->link/";
+    $link = $typeToUrlMap[$item->type] ?? "/$item->link/";
 
-    if (array_key_exists('noslash', $params) && !empty($params['noslash'])) {
-        $link = substr($link, 1);
+    if (!empty($params['noslash'])) {
+        $link = ltrim($link, '/');
     }
 
     if ($item->type !== 'external') {
-        $link = $smarty->getContainer()->get('core.decorator.url')->prefixUrl($link);
+        $link = prefixInternalUrl($link, $smarty);
     }
 
     return $link;
+}
+
+/**
+ * Get the name URL map.
+ *
+ * @return array
+ */
+function getNameUrlMap()
+{
+    return [
+        'video' => 'video',
+        'album' => 'album',
+        'special' => 'especiales',
+        'encuesta' => 'encuesta'
+    ];
+}
+
+/**
+ * Get the type to URL map.
+ *
+ * @param object $item
+ * @param string $nameUrl
+ *
+ * @return array
+ */
+function getTypeToUrlMap($item, $nameUrl)
+{
+    return [
+        'category' => "/$nameUrl/$item->link/",
+        'videoCategory' => "/video/$item->link/",
+        'albumCategory' => "/album/$item->link/",
+        'pollCategory' => "/encuesta/$item->link/",
+        'static' => "/" . STATIC_PAGE_PATH . "/$item->link.html",
+        'internal' => ($item->link == '/') ? "" : "/$item->link/",
+        'external' => "$item->link",
+        'syncBlogCategory' => "/ext$nameUrl/blog/$item->link/",
+        'blog-category' => "/blog/section/$item->link/",
+        'tags' => "/tags/$item->link/",
+    ];
+}
+
+/**
+ * Prefix the URL for internal links.
+ *
+ * @param string $link
+ * @param \Smarty $smarty
+ *
+ * @return string
+ */
+function prefixInternalUrl($link, $smarty)
+{
+    return $smarty->getContainer()->get('core.decorator.url')->prefixUrl($link);
 }
