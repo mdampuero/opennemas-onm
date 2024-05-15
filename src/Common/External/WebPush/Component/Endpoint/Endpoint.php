16265a3efe9658c2f59ee69469361835eac1bc68
Miguel Perez <miguel.perez@openhost.es>
2024-05-15 15:44:46 +0200

    ONM-9045: removed client from enpoint contructor parameters

--- src/Common/External/WebPush/Component/Endpoint/Endpoint.php
+++ src/Common/External/WebPush/Component/Endpoint/Endpoint.php
@@ -9,6 +9,8 @@
  */
 namespace Common\External\WebPush\Component\Endpoint;
 
+use GuzzleHttp\Client;
+
 class Endpoint
 {
     /**
@@ -46,9 +48,9 @@ class Endpoint
      * @param Client                $client         The HTTP client.
      * @param string                $url            The WebPush API URL.
      */
-    public function __construct($auth, $client, $url)
+    public function __construct($auth, $url)
     {
-        $this->client = $client;
+        $this->client = new Client();
         $this->auth   = $auth;
         $this->url    = $url;
     }
