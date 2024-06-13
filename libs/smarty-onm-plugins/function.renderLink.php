8088c8b0ef42bd84e30fe5f9640b89dd80278ba6
Francisco Catalan <francisco.catalan@openhost.es>
2024-06-13 12:17:26 +0200

    ONM-9077: Refactor renderLink

--- libs/smarty-onm-plugins/function.renderLink.php
+++ libs/smarty-onm-plugins/function.renderLink.php
@@ -1,8 +1,5 @@
 <?php
 
-use phpDocumentor\Reflection\DocBlock\Tags\Var_;
-use SebastianBergmann\Environment\Console;
-
 /**
  * Check type of menu element and prepare link
  *
@@ -15,87 +12,64 @@ function smarty_function_renderLink($params, &$smarty)
 {
     $item        = $params['item'];
     $type        = $item->type;
-    $referenceId = !empty($item->referenceId) ? $item->referenceId : null;
-    $alt_url     = $type === 'category' ? true : false;
+    $referenceId = $item->referenceId ?? null;
+    $alt_url     = $type === 'category';
 
     $container     = $smarty->getContainer();
     $coreInstance  = $container->get('core.instance');
     $multilanguage = $coreInstance->hasMultilanguage();
     $localeDefault = $container->get('core.locale')->getLocaleShort('frontend');
-    $locale        = $multilanguage
-                ? $container->get('core.locale')->getRequestLocaleShort()
-                : null;
+    $locale        = $multilanguage ? $container->get('core.locale')->getRequestLocaleShort() : null;
 
-    static $fetchServices = [
+    $serviceMap = [
         'tags' => 'api.service.tag',
         'blog-category' => 'api.service.category',
         'category' => 'api.service.category',
         'static' => 'api.service.content',
     ];
 
-    static $nameUrlMap = [
-        'video'    => 'video',
-        'album'    => 'album',
-        'special'  => 'especiales',
+    $urlSegmentMap = [
+        'video' => 'video',
+        'album' => 'album',
+        'special' => 'especiales',
         'encuesta' => 'encuesta'
     ];
 
-    $fetchServices = $fetchServices[$type] ?? null;
-
-
-    if (!empty($fetchServices)) {
-        if ($referenceId) {
-            $fetchElementByReference = $container->get($fetchServices)->getItem($referenceId);
-        } else {
-            $nameUrl                 = empty($nameUrlMap[$item->name]) ? null : $nameUrlMap[$item->name];
-            $mapUrl                  = getTypeToUrlMap($item, $nameUrl);
-            $fetchElementByReference = null;
-            $fetchServices           = false;
-        }
-    } else {
-        $fetchElementByReference = $item->link;
+    if ($referenceId && isset($serviceMap[$type])) {
+        $fetchedItem = $container->get($serviceMap[$type])->getItem($referenceId);
+        return generateUrlForFetchedItem($container, $fetchedItem, $locale, $alt_url);
     }
 
-    $urlGenerator = $container->get('core.helper.url_generator');
+    $url = generateUrlForMenuItem($item, $urlSegmentMap, $multilanguage, $locale, $localeDefault);
+    return $container->get('core.decorator.url')->prefixUrl($url);
+}
 
-    switch ($type) {
+function generateUrlForFetchedItem($container, $fetchElement, $locale, $alt_url)
+{
+    $urlGenerator = $container->get('core.helper.url_generator');
+    return $urlGenerator->generate($fetchElement, [
+        'locale' => $locale,
+        'alternative_url' => $alt_url,
+        'absolute' => true
+    ]);
+}
+
+function generateUrlForMenuItem($item, $urlSegmentMap, $multilanguage, $locale, $localeDefault)
+{
+    $nameUrl = $urlSegmentMap[$item->name] ?? null;
+    $mapUrl  = mapItemTypeToUrl($item, $nameUrl);
+
+    switch ($item->type) {
         case 'internal':
             $formatLink = ltrim($item->link, '/');
-
-            if ($multilanguage) {
-                if ($locale === $localeDefault) {
-                    $url = '/' . $formatLink;
-                } else {
-                    $url = $url = '/' . $locale . '/' . $formatLink;
-                }
-            } else {
-                $url = $url = '/' . $formatLink;
-            }
-            break;
+            return $multilanguage
+                ? ($locale === $localeDefault ? '/' . $formatLink : '/' . $locale . '/' . $formatLink)
+                : '/' . $formatLink;
         case 'external':
-            $url = $item->link;
-            break;
+            return $item->link;
         default:
-            if (!empty($fetchServices)) {
-                if ($fetchElementByReference) {
-                    $url = $urlGenerator->generate($fetchElementByReference, [
-                        'locale' => $locale,
-                        'alternative_url' => $alt_url,
-                        'absolute' => true
-                    ]);
-                } else {
-                    $url = $mapUrl[$item->type] ?? "/$item->link/";
-                }
-            } else {
-                $url = $mapUrl[$item->type] ?? "/$item->link/";
-            }
+            return $mapUrl[$item->type] ?? "/$item->link/";
     }
-
-    if ($item->type !== 'external') {
-        $url = $smarty->getContainer()->get('core.decorator.url')->prefixUrl($url);
-    }
-
-    return $url;
 }
 
 /**
@@ -106,16 +80,16 @@ function smarty_function_renderLink($params, &$smarty)
  *
  * @return array
  */
-function getTypeToUrlMap($item, $nameUrl)
+function mapItemTypeToUrl($item, $nameUrl)
 {
     return [
-        'category' => ($nameUrl) ? "/$nameUrl/" : "/$item->link/",
+        'category' => $nameUrl ? "/$nameUrl/" : "/$item->link/",
         'videoCategory' => "/video/$item->link/",
         'albumCategory' => "/album/$item->link/",
         'pollCategory' => "/encuesta/$item->link/",
         'static' => "/" . STATIC_PAGE_PATH . "/$item->link.html",
-        'internal' => ($item->link == '/') ? "" : "/$item->link/",
-        'external' => "$item->link",
+        'internal' => $item->link === '/' ? "" : "/$item->link/",
+        'external' => $item->link,
         'syncBlogCategory' => "/ext$nameUrl/blog/$item->link/",
         'blog-category' => "/blog/section/$item->link/",
         'tags' => "/tags/$item->link/",
