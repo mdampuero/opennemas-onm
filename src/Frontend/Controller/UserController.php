45f92f22f68018c29023ff676cdd4a0d73366f72
Alex Rodrigues <alex@openhost.es>
2024-07-04 09:01:24 +0200

    ONM-8951: Improve variable assign on UserController

--- src/Frontend/Controller/UserController.php
+++ src/Frontend/Controller/UserController.php
@@ -276,13 +276,13 @@ class UserController extends Controller
         $securityInput = $request->request->get('register_control');
         $userGroups    = $request->request->get('user_groups', []);
         $userGroups    = !is_array($userGroups) ? [ $userGroups => '' ] : $userGroups;
+        $email         = $request->request->filter('email', null, FILTER_SANITIZE_EMAIL);
+        $name          = $request->request->filter('name', null, FILTER_SANITIZE_SPECIAL_CHARS);
         $data          = array_merge(
             $request->request->all(),
             [
-                'name'        => empty($request->request->filter('name', null, FILTER_SANITIZE_SPECIAL_CHARS)) ?
-                                    $request->request->filter('email', null, FILTER_SANITIZE_EMAIL) :
-                                    $request->request->filter('name', null, FILTER_SANITIZE_SPECIAL_CHARS),
-                'email'       => $request->request->filter('email', null, FILTER_SANITIZE_EMAIL),
+                'name'        => empty($name) ? $email : $name,
+                'email'       => $email,
                 'user_groups' => $userGroups,
                 'password'    => $request->request->get('password'),
             ]
