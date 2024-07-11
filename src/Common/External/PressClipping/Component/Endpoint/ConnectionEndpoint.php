12406169850aced6ac32cec33b7363f04a4dedca
Francisco Catalan <francisco.catalan@openhost.es>
2024-07-11 11:21:11 +0200

    ONM-9234: Updated PHPDocs in ConnectionEndpoint

--- src/Common/External/PressClipping/Component/Endpoint/ConnectionEndpoint.php
+++ src/Common/External/PressClipping/Component/Endpoint/ConnectionEndpoint.php
@@ -14,34 +14,37 @@ use Common\External\PressClipping\Component\Exception\PressClippingException;
 class ConnectionEndpoint extends Endpoint
 {
     /**
-     * Test API Connection
+     * Tests the connection to a specific endpoint by constructing a URL with
+     * an authentication token and then making a GET request to that URL.
      *
-     * @return string
-     *
-     * @throws PressClippingException if the action fails
+     * @return array The response body decoded from JSON as an associative array.
+     * @throws PressClippingException If there is an issue with the connection or request.
      */
-    public function testConnection($params = [])
+    public function testConnection()
     {
         try {
+            // Get authentication token
             $token = $this->auth->getToken();
 
+            // Construct the URL with the token
             $url = $this->url . $this->replaceUriWildCards(
                 $this->config['actions']['test_connection']['path'],
-                $params
+                []
             ) . '/' . $token;
 
+            // Make a GET request to the URL
             $response = $this->client->get($url);
 
-            // Decodificar el cuerpo de la respuesta
+            // Decode the JSON response body to an associative array
             $body = json_decode($response->getBody(), true);
 
+            // Return the response body
             return $body;
         } catch (\Exception $e) {
+            // Throw a custom exception if there is a connection failure
             throw new PressClippingException(
                 'pressclipping.connection.get.failure: ' . $e->getMessage()
             );
         }
-
-        return $body;
     }
 }
