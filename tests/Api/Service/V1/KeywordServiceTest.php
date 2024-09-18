213cfa42ec7a5fb4edf16e457b82d245ca29e1f3
Mauricio Ampuero <mauricio.ampuero@openhost.es>
2024-09-18 11:34:40 +0200

    ONM-9326:

--- tests/Api/Service/V1/KeywordServiceTest.php
+++ tests/Api/Service/V1/KeywordServiceTest.php
@@ -55,4 +55,48 @@ class KeywordServiceTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals($expected, $this->service->replaceTerms($this->text, $keywords));
     }
+
+    public function testCompositeKeywordsReplacement()
+    {
+        $keywords = [
+            new Keyword(['id' => '1', 'keyword' => 'Lorem', 'type' => 'url', 'value' => 'glorp']),
+            new Keyword(['id' => '2', 'keyword' => 'Lorem ipsum', 'type' => 'email', 'value' => 'link_lorem_ipsum']),
+            new Keyword(['id' => '3', 'keyword' => 'dolor sit amet', 'type' => 'intsearch', 'value' => 'foo']),
+            new Keyword(['id' => '4', 'keyword' => 'sit amet', 'type'   => 'url', 'value' => 'link_sit_amet']),
+            new Keyword(['id' => '5', 'keyword' => 'sit', 'type'   => 'email', 'value' => 'link_sit'])
+        ];
+        $newText = $this->text . ' Lorem dolor sit, consectetur adipiscing sit';
+
+        $expected = '<a href="mailto:link_lorem_ipsum" target="_blank">Lorem ipsum</a> ' .
+            '<a href="/tag/foo" target="_blank">dolor sit amet</a>, consectetur adipiscing elit. ' .
+            '<a href="glorp" target="_blank">Lorem</a> dolor ' .
+            '<a href="mailto:link_sit" target="_blank">sit</a>, consectetur adipiscing ' .
+            '<a href="mailto:link_sit" target="_blank">sit</a>';
+
+        $actual = $this->service->replaceTerms($newText, $keywords);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testReplaceTermsRepeat()
+    {
+        $keywords = [
+            new Keyword(['id' => '1', 'keyword' => 'Lorem', 'type' => 'url', 'value'       => 'glorp']),
+            new Keyword(['id' => '2', 'keyword' => 'ipsum', 'type' => 'email', 'value'     => 'baz']),
+            new Keyword(['id' => '3', 'keyword' => 'dolor', 'type' => 'intsearch', 'value' => 'foo']),
+            new Keyword(['id' => '4', 'keyword' => 'sit', 'type'   => '', 'value'          => 'default'])
+        ];
+
+        $expected = '<a href="glorp" target="_blank">Lorem</a> ' .
+            '<a href="mailto:baz" target="_blank">ipsum</a> ' .
+            '<a href="/tag/foo" target="_blank">dolor</a> ' .
+            'sit amet, consectetur adipiscing elit.';
+        $textreplace_1 = $this->service->replaceTerms($this->text, $keywords);
+
+        $textreplace_2 = $this->service->replaceTerms($textreplace_1, $keywords);
+
+        $actual = $this->service->replaceTerms($textreplace_2, $keywords);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
