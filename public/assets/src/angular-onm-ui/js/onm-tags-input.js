0a7c2b7af0f3e849f54c5db45f39330380f63ff4
Mauricio Ampuero <mauricio.ampuero@openhost.es>
2024-10-14 13:50:51 +0200

    ONM-9077: Add local.selected from config parent

--- public/assets/src/angular-onm-ui/js/onm-tags-input.js
+++ public/assets/src/angular-onm-ui/js/onm-tags-input.js
@@ -209,7 +209,7 @@
             if ($scope.locale && $scope.locale.multilanguage) {
               $scope.tagsInLocale = $scope.tags.filter(function(e) {
                 return !$scope.locale || !e.locale ||
-                  e.locale === $scope.locale.selected;
+                  e.locale === $scope.$parent.config.locale.selected;
               });
             }
           }, function() {
@@ -277,7 +277,7 @@
 
             if (!$scope.ignoreLocale && $scope.locale &&
               $scope.locale.multilanguage) {
-              criteria.locale = $scope.locale.selected;
+              criteria.locale = $scope.$parent.config.locale.selected;
             }
 
             oqlEncoder.configure({
@@ -311,7 +311,7 @@
                   var item = { id: query, name: query };
 
                   if ($scope.locale && $scope.locale.multilanguage) {
-                    item.locale = $scope.locale.selected;
+                    item.locale = $scope.$parent.config.locale.selected;
                   }
 
                   items.push(item);
@@ -467,7 +467,7 @@
               if ($scope.locale && $scope.locale.multilanguage) {
                 $scope.tagsInLocale = $scope.tags.filter(function(e) {
                   return !$scope.locale || !e.locale ||
-                    e.locale === $scope.locale.selected;
+                    e.locale === $scope.$parent.config.locale.selected;
                 });
               }
             }
@@ -496,7 +496,7 @@
             ovIds = ov.filter(function(e) {
               // Only delete tags for any or current locale
               return !$scope.locale || !e.locale ||
-                e.locale === $scope.locale.selected;
+                e.locale === $scope.$parent.config.locale.selected;
             }).map(function(e) {
               return e.id;
             });
@@ -529,7 +529,7 @@
                 $scope.ngModel.indexOf(e.id) === -1;
             }));
 
-            $scope.$parent.data.extra.tags[$scope.locale.selected] = $scope.tagsInLocale;
+            $scope.$parent.data.extra.tags[$scope.$parent.config.locale.selected] = $scope.tagsInLocale;
           }
         }, true);
       }
