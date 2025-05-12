85f2ef72605e5cc31c532c441b7f8b8155d57106
Francisco Catalan <francisco.catalan@openhost.es>
2025-05-12 15:55:07 +0200

    ONM-9333: added setRequestLocale to controller.

--- src/Backend/Controller/AuthenticationController.php
+++ src/Backend/Controller/AuthenticationController.php
@@ -30,13 +30,6 @@ class AuthenticationController extends Controller
             return $this->redirect($this->generateUrl('admin_welcome'));
         }
 
-        if ($request->query->has('language')) {
-            $this->get('core.locale')
-                ->setContext('backend')
-                ->setLocale($request->query->get('language'))
-                ->apply();
-        }
-
         $target  = $this->generateUrl('admin_welcome');
         $session = $request->getSession();
         $token   = $request->get('token');
@@ -84,6 +77,14 @@ class AuthenticationController extends Controller
 
         if ($auth->hasError()) {
             $session->getFlashBag()->add('error', $auth->getErrorMessage());
+        }
+
+        if ($request->query->has('language')) {
+            $this->get('core.locale')
+                ->setContext('backend')
+                ->setLocale($request->query->get('language'))
+                ->setRequestLocale($request->query->get('language'))
+                ->apply();
         }
 
         return $this->render('login/login.tpl', [
