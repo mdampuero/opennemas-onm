c45e198d9f94376c0f0e7098684b2f69ee102533
Mauricio Ampuero <mdampuero@gmail.com>
2025-08-07 14:02:28 +0200

    Adjust video storage handling

--- src/Api/Service/V1/TrashService.php
+++ src/Api/Service/V1/TrashService.php
@@ -36,4 +36,82 @@ class TrashService extends OrmService
             throw new ApiException($e->getMessage(), $e->getCode());
         }
     }
+
+    /**
+     * Restores a single item from the trash.
+     */
+    public function patchItem($id, $data)
+    {
+        parent::patchItem($id, $data);
+
+        if (isset($data['in_litter']) && (int) $data['in_litter'] === 0) {
+            $item = $this->getItem($id);
+            if ($item->content_type_name === 'video') {
+                $instance = $this->container->get('core.instance');
+                $this->container->get('api.service.video')
+                    ->removeFromStorage($id, $instance, 'restore');
+            }
+        }
+    }
+
+    /**
+     * Restores a list of items from the trash.
+     */
+    public function patchList($ids, $data)
+    {
+        $updated = parent::patchList($ids, $data);
+
+        if (isset($data['in_litter']) && (int) $data['in_litter'] === 0) {
+            $items    = $this->getListByIds($ids)['items'];
+            $videoIds = [];
+            foreach ($items as $item) {
+                if ($item->content_type_name === 'video') {
+                    $videoIds[] = $item->pk_content;
+                }
+            }
+            if (!empty($videoIds)) {
+                $instance = $this->container->get('core.instance');
+                $this->container->get('api.service.video')
+                    ->removeFromStorage($videoIds, $instance, 'restore');
+            }
+        }
+
+        return $updated;
+    }
+
+    /**
+     * Permanently deletes a single item from the trash.
+     */
+    public function deleteItem($id)
+    {
+        $item = $this->getItem($id);
+        if ($item->content_type_name === 'video') {
+            $instance = $this->container->get('core.instance');
+            $this->container->get('api.service.video')
+                ->removeFromStorage($id, $instance, 'delete');
+        }
+
+        parent::deleteItem($id);
+    }
+
+    /**
+     * Permanently deletes a list of items from the trash.
+     */
+    public function deleteList($ids)
+    {
+        $items    = $this->getListByIds($ids)['items'];
+        $videoIds = [];
+        foreach ($items as $item) {
+            if ($item->content_type_name === 'video') {
+                $videoIds[] = $item->pk_content;
+            }
+        }
+        if (!empty($videoIds)) {
+            $instance = $this->container->get('core.instance');
+            $this->container->get('api.service.video')
+                ->removeFromStorage($videoIds, $instance, 'delete');
+        }
+
+        return parent::deleteList($ids);
+    }
 }
