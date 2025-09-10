9eab64a65f094b7d773d3cdcf3d90f9cd6102f36
Alex Rodrigues <alex@openhost.es>
2025-09-10 11:10:17 +0200

    ONM-9942: Remove blank lines

--- src/Api/Controller/V1/Backend/MenuController.php
+++ src/Api/Controller/V1/Backend/MenuController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Onm package.
  *
@@ -8,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Api\Controller\V1\Backend;
 
 use Api\Controller\V1\ApiController;
