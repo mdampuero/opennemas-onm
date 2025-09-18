a7b5c16cd727c2d98041a7745f20a396f32e3b0f
Francisco Catalan <francisco.catalan@openhost.es>
2025-09-18 15:24:38 +0200

    ONM-9998: Implemented External Link on Poll.

--- src/Common/Core/Component/Helper/FormFieldHelper.php
+++ src/Common/Core/Component/Helper/FormFieldHelper.php
@@ -111,6 +111,7 @@ class FormFieldHelper
                     'opinion',
                     'event',
                     'company',
+                    'poll',
                 ]
             ],
             [
