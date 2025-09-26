4fc94e0cb381e65eed58ad1f760325843ff8ad8d
Mauricio Ampuero <mauricio.ampuero@openhost.es>
2025-09-26 09:15:48 +0200

    ONM-10004: Indent

--- src/Frontend/Controller/EventController.php
+++ src/Frontend/Controller/EventController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Onm package.
  *
@@ -8,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Frontend\Controller;
 
 use Api\Exception\GetItemException;
@@ -51,25 +49,25 @@ class EventController extends FrontendController
      * {@inheritdoc}
      */
     protected $positions = [
-        'list'             => [1, 2, 5, 6, 7],
-        'taglist'          => [1, 2, 5, 6, 7],
-        'typelist'         => [1, 2, 5, 6, 7],
-        'categorylist'     => [1, 2, 5, 6, 7],
-        'categorytaglist'  => [1, 2, 5, 6, 7],
-        'categorytypelist' => [1, 2, 5, 6, 7],
-        'show'             => [1, 2, 5, 6, 7]
+        'list'             => [ 1, 2, 5, 6, 7 ],
+        'taglist'          => [ 1, 2, 5, 6, 7 ],
+        'typelist'         => [ 1, 2, 5, 6, 7 ],
+        'categorylist'     => [ 1, 2, 5, 6, 7 ],
+        'categorytaglist'  => [ 1, 2, 5, 6, 7 ],
+        'categorytypelist' => [ 1, 2, 5, 6, 7 ],
+        'show'             => [ 1, 2, 5, 6, 7 ]
     ];
 
     /**
      * {@inheritdoc}
      */
     protected $queries = [
-        'list'             => ['page'],
-        'taglist'          => ['page', 'type', 'tag'],
-        'typelist'         => ['page', 'type'],
-        'categorylist'     => ['page', 'category_slug'],
-        'categorytaglist'  => ['page', 'category_slug', 'type', 'tag'],
-        'categorytypelist' => ['page', 'category_slug', 'type']
+        'list'             => [ 'page' ],
+        'taglist'          => [ 'page', 'type', 'tag' ],
+        'typelist'         => [ 'page', 'type' ],
+        'categorylist'     => [ 'page', 'category_slug'],
+        'categorytaglist'  => [ 'page', 'category_slug', 'type', 'tag' ],
+        'categorytypelist' => [ 'page', 'category_slug', 'type' ]
     ];
 
     /**
@@ -270,19 +268,19 @@ class EventController extends FrontendController
 
         $baseSql = sprintf(
             'FROM contents '
-                . 'inner join contentmeta as start_date_meta on contents.pk_content = start_date_meta.fk_content '
-                . 'and start_date_meta.meta_name = "event_start_date" '
-                . 'left join contentmeta as start_hour_meta on contents.pk_content = start_hour_meta.fk_content '
-                . 'and start_hour_meta.meta_name = "event_start_hour" '
-                . 'left join contentmeta as end_date_meta on contents.pk_content = end_date_meta.fk_content '
-                . 'and end_date_meta.meta_name = "event_end_date" '
+            . 'inner join contentmeta as start_date_meta on contents.pk_content = start_date_meta.fk_content '
+            . 'and start_date_meta.meta_name = "event_start_date" '
+            . 'left join contentmeta as start_hour_meta on contents.pk_content = start_hour_meta.fk_content '
+            . 'and start_hour_meta.meta_name = "event_start_hour" '
+            . 'left join contentmeta as end_date_meta on contents.pk_content = end_date_meta.fk_content '
+            . 'and end_date_meta.meta_name = "event_end_date" '
         );
 
         // Check for category at the start of the path
         if (!empty($categorySlug) && $category = $this->matchCategory($categorySlug)) {
             $baseSql .= sprintf(
                 'join content_category on contents.pk_content = content_category.content_id '
-                    . 'and content_category.category_id = %d ',
+                . 'and content_category.category_id = %d ',
                 $category->id
             );
         }
@@ -292,7 +290,7 @@ class EventController extends FrontendController
             if ($eventType = $this->get('core.helper.event')->getEventTypeBySlug($type)) {
                 $baseSql .= sprintf(
                     'join contentmeta as event_type_meta on contents.pk_content = event_type_meta.fk_content '
-                        . 'AND event_type_meta.meta_name = "event_type" AND event_type_meta.meta_value = "%s" ',
+                    . 'AND event_type_meta.meta_name = "event_type" AND event_type_meta.meta_value = "%s" ',
                     $eventType['id']
                 );
             } elseif (empty($categorySlug) && $category = $this->matchCategory($type)) {
@@ -300,7 +298,7 @@ class EventController extends FrontendController
 
                 $baseSql .= sprintf(
                     'join content_category on contents.pk_content = content_category.content_id '
-                        . 'and content_category.category_id = %d ',
+                    . 'and content_category.category_id = %d ',
                     $category->id
                 );
             } elseif (empty($tag) && $tagItem = $this->matchTag($type)) {
@@ -317,10 +315,10 @@ class EventController extends FrontendController
 
         $baseSql .= sprintf(
             'where content_type_name="event" and content_status=1 and in_litter=0 '
-                . 'and (start_date_meta.meta_value >= "%s" '
-                . 'or (start_date_meta.meta_value < "%s" and end_date_meta.meta_value >= "%s")) '
-                . 'and (starttime is null or starttime < "%s") '
-                . 'and (endtime is null or endtime > "%s") ',
+            . 'and (start_date_meta.meta_value >= "%s" '
+            . 'or (start_date_meta.meta_value < "%s" and end_date_meta.meta_value >= "%s")) '
+            . 'and (starttime is null or starttime < "%s") '
+            . 'and (endtime is null or endtime > "%s") ',
             $eventDate,
             $eventDate,
             $eventDate,
@@ -345,9 +343,9 @@ class EventController extends FrontendController
 
         $pagination = sprintf(
             'order by start_date_meta.meta_value asc, '
-                . 'start_hour_meta.meta_value asc, '
-                . 'contents.pk_content asc '
-                . 'limit %d offset %d',
+            . 'start_hour_meta.meta_value asc, '
+            . 'contents.pk_content asc '
+            . 'limit %d offset %d',
             $params['epp'],
             $params['epp'] * ($params['page'] - 1)
         );
@@ -418,7 +416,7 @@ class EventController extends FrontendController
     {
         return sprintf(
             'join contents_tags ct on contents.pk_content = ct.content_id '
-                . 'and ct.tag_id in (%s) ',
+            . 'and ct.tag_id in (%s) ',
             $tagItem->id
         );
     }
